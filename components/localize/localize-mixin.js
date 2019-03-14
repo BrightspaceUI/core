25114e6e1503d565cdd19af715038d3eaf605f9b
Margaree Peacocke <margaree.p@gmail.com>
2019-03-14 10:33:23 -0400

    restructure localize-mixin based on app-localize-behavior

--- components/localize/localize-mixin.js
+++ components/localize/localize-mixin.js
@@ -1,19 +1,108 @@
-let _i18nextInitialized = false;
-/**
- * Localize mixin
- *
- * @polymer
- * @mixinFunction
- **/
-export const localize = i18next => baseElement => class extends baseElement {
-	connectedCallback() {
-		if (!_i18nextInitialized) {
-			i18next.on('initialized', () => {
-				_i18nextInitialized = true;
-				if (super.connectedCallback) {
-					super.connectedCallback();
+import IntlMessageFormat from 'intl-messageformat/src/main.js';
+window.IntlMessageFormat = IntlMessageFormat;
+
+// 	 Internal singleton cache. This is the private implementation of the
+// 	 behaviour; don't interact with it directly.
+var __localizationCache = {
+	requests: {}, /* One iron-request per unique resources path. */
+	messages: {}, /* Unique localized strings. Invalidated when the language,
+					formats or resources change. */
+	ajax: null		/* Global iron-ajax object used to request resource files. */
+};
+
+export const AppLocalizeBehavior = superclass => class extends superclass {
+
+	/**
+	Fired after the resources have been loaded.
+	@event app-localize-resources-loaded
+	*/
+
+	/**
+	Fired when the resources cannot be loaded due to an error.
+	@event app-localize-resources-error
+	*/
+
+	static get properties() {
+		return {
+			language: {type: String},
+			resources: {type: Object},
+			formats: {
+				type: Object,
+				value: function() {
+					return {
+					}
 				}
-			});
+			},
+			useKeyIfMissing: {type: Boolean, value: false},
+			bubbleEvent: {type: Boolean, value: false}
+		}
+	}
+
+	connectedCallback() {
+		if (super.connectedCallback) {
+			super.connectedCallback();
+		}
+	}
+
+	constructor() {
+		super();
+		this.language = 'en';
+	}
+
+	localize(key) {
+		return this.__computeLocalize(this.language, this.resources, key);
+	}
+
+	__computeLocalize(language, resources, key) {
+		var proto = this.constructor.prototype;
+
+		// Check if localCache exist just in case.
+		this.__checkLocalizationCache(proto);
+
+		// Everytime any of the parameters change, invalidate the strings cache.
+		if (!proto.__localizationCache) {
+			proto['__localizationCache'] = {requests: {}, messages: {}, ajax: null};
+		}
+		proto.__localizationCache.messages = {};
+
+		return function() {
+			if (!key || !resources || !language || !resources[language])
+				return;
+
+			// Cache the key/value pairs for the same language, so that we don't
+			// do extra work if we're just reusing strings across an application.
+			var translatedValue = resources[language][key];
+
+			if (!translatedValue) {
+				return this.useKeyIfMissing ? key : '';
+			}
+
+			var messageKey = key + translatedValue;
+			var translatedMessage = proto.__localizationCache.messages[messageKey];
+
+			if (!translatedMessage) {
+				translatedMessage =
+						new IntlMessageFormat(translatedValue, language, this.formats);
+				proto.__localizationCache.messages[messageKey] = translatedMessage;
+			}
+
+			var args = {};
+			for (var i = 1; i < arguments.length; i += 2) {
+				args[arguments[i]] = arguments[i + 1];
+			}
+
+			return translatedMessage.format(args);
+		}.bind(this)();
+	}
+
+	__checkLocalizationCache(proto) {
+		// do nothing if proto is undefined.
+		if (proto === undefined)
+			return;
+
+		// In the event proto not have __localizationCache object, create it.
+		if (proto['__localizationCache'] === undefined) {
+			proto['__localizationCache'] = {requests: {}, messages: {}, ajax: null};
 		}
 	}
 };
--- components/more-less/more-less.js
+++ components/more-less/more-less.js
@@ -1,12 +1,11 @@
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { LitElement, html, css } from 'lit-element/lit-element.js';
-import { i18next } from '../localize/i18next.js';
-import { localize } from '../localize/localize-mixin.js';
+import { AppLocalizeBehavior } from '../localize/localize-mixin.js';
 import '../button/button-subtle.js';
 import 'd2l-icons/d2l-icon.js';
 import 'd2l-icons/tier1-icons.js';
 
-export class D2LMoreLess extends localize(i18next)(LitElement)  {
+export class D2LMoreLess extends AppLocalizeBehavior(LitElement)  {
 
 	static get properties() {
 		return {
@@ -52,6 +51,56 @@ export class D2LMoreLess extends localize(i18next)(LitElement)  {
 		super();
 		this.text = this.__computeText();
 		this.icon = this.__computeIcon();
+		this.resources = {
+			'ar': {
+				more: 'المزيد',
+				less: 'أقل'
+			},
+			'en': {
+				more: 'more',
+				less: 'less'
+			},
+			'es': {
+				more: 'más',
+				less: 'menos'
+			},
+			'fr': {
+				more: 'plus',
+				less: 'moins'
+			},
+			'ja': {
+				more: 'より多い',
+				less: 'より少ない'
+			},
+			'ko': {
+				more: '더 보기',
+				less: '축소'
+			},
+			'nl': {
+				more: 'meer',
+				less: 'minder'
+			},
+			'pt': {
+				more: 'mais',
+				less: 'menos'
+			},
+			'sv': {
+				more: 'mer',
+				less: 'mindre'
+			},
+			'tr': {
+				more: 'diğer',
+				less: 'daha az'
+			},
+			'zh': {
+				more: '更多',
+				less: '更少'
+			},
+			'zh-tw': {
+				more: '較多',
+				less: '較少'
+			}
+		};
 	}
 
 	render() {
@@ -63,7 +112,7 @@ export class D2LMoreLess extends localize(i18next)(LitElement)  {
 				icon="${ifDefined(this.icon)}"
 				aria-hidden="true"
 				@click="${this.__toggleOnClick}"
-				text="${i18next.t(`more-less:${this.text}`)}"
+				text="${this.localize(this.text)}"
 				h-align="${ifDefined(this.hAlign)}">
 			</d2l-button-subtle>
 		`;
