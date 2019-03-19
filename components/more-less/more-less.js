138987bce8d8f8e52b353d9df9bd7c79ddbde8a8
Margaree Peacocke <margaree.p@gmail.com>
2019-03-19 13:54:51 -0400

    more-less example

--- components/more-less/more-less.js
+++ components/more-less/more-less.js
@@ -22,56 +22,13 @@ export class D2LMoreLess extends LocalizeMixin(LitElement)  {
 
 	constructor() {
 		super();
-		this.resources = {
-			'ar': {
-				more: 'المزيد',
-				less: 'أقل'
-			},
-			'en': {
-				more: 'more',
-				less: 'less'
-			},
-			'es': {
-				more: 'más',
-				less: 'menos'
-			},
-			'fr': {
-				more: 'plus',
-				less: 'moins'
-			},
-			'ja': {
-				more: 'より多い',
-				less: 'より少ない'
-			},
-			'ko': {
-				more: '더 보기',
-				less: '축소'
-			},
-			'nl': {
-				more: 'meer',
-				less: 'minder'
-			},
-			'pt': {
-				more: 'mais',
-				less: 'menos'
-			},
-			'sv': {
-				more: 'mer',
-				less: 'mindre'
-			},
-			'tr': {
-				more: 'diğer',
-				less: 'daha az'
-			},
-			'zh': {
-				more: '更多',
-				less: '更少'
-			},
-			'zh-tw': {
-				more: '較多',
-				less: '較少'
-			}
+
+		this.component = 'more-less';
+		this.getResources = function(language) {
+			var path = `./locales/${language}.js`;
+			return import(path);
 		};
+		this.locales = ['en', 'ar'];
 	}
 
 	render() {
