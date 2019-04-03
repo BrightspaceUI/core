c04326c9b771700cf6304a5076b0f3172450c66f
Dave Batiste <dave.batiste@d2l.com>
2019-04-03 14:37:09 -0400

    No need to do visual diff of button for both wide and narrow viewport.

--- test/button/button-subtle-perceptual.js
+++ test/button/button-subtle-perceptual.js
@@ -18,57 +18,36 @@ describe('d2l-button-subtle', function() {
 
 	after(() => browser.close());
 
-	const runTests = () => {
-
-		it('normal', async function() {
-			const rect = await visualDiff.puppeteer.getRect(page, '#normal');
-			await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-		});
-
-		it('mouse', async function() {
-			await page.hover('#normal');
-			const rect = await visualDiff.puppeteer.getRect(page, '#normal');
-			await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-		});
-
-		it('focus', async function() {
-			await page.click('#normal');
-			const rect = await visualDiff.puppeteer.getRect(page, '#normal');
-			await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-		});
-
-		it('disabled', async function() {
-			const rect = await visualDiff.puppeteer.getRect(page, '#disabled');
-			await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-		});
-
-		it('with-icon', async function() {
-			const rect = await visualDiff.puppeteer.getRect(page, '#with-icon');
-			await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-		});
-
-	};
-
-	describe('wide', function() {
-
-		beforeEach(async function() {
-			await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
-			await page.goto(`${visualDiff.baseUrl}/demo/button/button-subtle.html`, {waitUntil: ['networkidle2', 'load']});
-		});
-
-		runTests();
-
+	beforeEach(async function() {
+		await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
+		await page.goto(`${visualDiff.baseUrl}/demo/button/button-subtle.html`, {waitUntil: ['networkidle2', 'load']});
 	});
 
-	describe('narrow', function() {
+	it('normal', async function() {
+		const rect = await visualDiff.puppeteer.getRect(page, '#normal');
+		await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
 
-		beforeEach(async function() {
-			await page.setViewport({width: 600, height: 800, deviceScaleFactor: 2});
-			await page.goto(`${visualDiff.baseUrl}/demo/button/button-subtle.html`, {waitUntil: ['networkidle2', 'load']});
-		});
+	it('mouse', async function() {
+		await page.hover('#normal');
+		const rect = await visualDiff.puppeteer.getRect(page, '#normal');
+		await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
 
-		runTests();
+	it('focus', async function() {
+		await page.click('#normal');
+		const rect = await visualDiff.puppeteer.getRect(page, '#normal');
+		await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
 
+	it('disabled', async function() {
+		const rect = await visualDiff.puppeteer.getRect(page, '#disabled');
+		await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('with-icon', async function() {
+		const rect = await visualDiff.puppeteer.getRect(page, '#with-icon');
+		await visualDiff.puppeteer.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
 });
