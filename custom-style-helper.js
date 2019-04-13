11feadf03ad7b0689f175ad38f1201f0ab289645
Dave Batiste <dave.batiste@d2l.com>
2019-04-13 11:04:07 -0400

    Add custom style helper function and update icon demo to use it.

---
+++ custom-style-helper.js
@@ -0,0 +1,13 @@
+import '@webcomponents/shadycss/custom-style-interface.min.js';
+
+export const addCustomStyle = (style) => {
+	const elem = document.createElement('style');
+	elem.textContent = style;
+	const head = document.getElementsByTagName('head')[0];
+	head.appendChild(elem);
+	window.ShadyCSS.CustomStyleInterface.addCustomStyle({
+		getStyle() {
+			return elem;
+		}
+	});
+}
