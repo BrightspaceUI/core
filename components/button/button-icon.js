a13eaf6339d49c6f5e30dabc316fff14c99e2603
Dave Batiste <dave.batiste@d2l.com>
2019-04-16 17:15:33 -0400

    Move d2l-button-icon specific styles into component to avoid unnecessary request.

--- components/button/button-icon.js
+++ components/button/button-icon.js
@@ -1,8 +1,7 @@
 import 'd2l-icons/d2l-icon.js';
 import 'd2l-icons/tier1-icons.js';
-import { html, LitElement } from 'lit-element/lit-element.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { VisibleOnAncestorMixin, visibleOnAncestorStyles } from '../../mixins/visible-on-ancestor-mixin.js';
-import { buttonIconStyles } from './button-icon-styles.js';
 import { ButtonMixin } from './button-mixin.js';
 import { buttonSharedStyles } from './button-shared-styles.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
@@ -20,7 +19,78 @@ class ButtonIcon extends ButtonMixin(VisibleOnAncestorMixin(RtlMixin(LitElement)
 	}
 
 	static get styles() {
-		return [ buttonSharedStyles, buttonIconStyles, visibleOnAncestorStyles ];
+		return [ buttonSharedStyles, visibleOnAncestorStyles,
+			css`
+				:host {
+					display: inline-block;
+					--d2l-button-icon-border-radius: 0.3rem;
+					--d2l-button-icon-min-height: calc(2rem + 2px);
+					--d2l-button-icon-min-width: calc(2rem + 2px);
+					--d2l-button-icon-h-align: calc(((2rem + 2px - 0.9rem) / 2) * -1);
+				}
+				:host([hidden]) {
+					display: none;
+				}
+
+				button {
+					background-color: transparent;
+					border-color: transparent;
+					font-family: inherit;
+					border-radius: var(--d2l-button-icon-border-radius);
+					min-height: var(--d2l-button-icon-min-height);
+					min-width: var(--d2l-button-icon-min-width);
+					position: relative;
+				}
+
+				:host([h-align="text"]) button {
+					left: var(--d2l-button-icon-h-align);
+				}
+				:host([dir="rtl"][h-align="text"]) button {
+					left: 0;
+					right: var(--d2l-button-icon-h-align);
+				}
+
+				// Firefox includes a hidden border which messes up button dimensions
+				button::-moz-focus-inner {
+					border: 0;
+				}
+				button[disabled]:hover,
+				button[disabled]:focus,
+				:host([active]) button[disabled] {
+					background-color: transparent;
+				}
+				button:hover,
+				button:focus,
+				:host([active]) button {
+					background-color: var(--d2l-color-gypsum);
+				}
+
+				.d2l-button-icon {
+					height: 0.9rem;
+					width: 0.9rem;
+				}
+
+				:host([translucent]) button {
+					background-color: rgba(0,0,0,0.5);
+					transition: background-color 0.5s;
+				}
+				:host([translucent]) .d2l-button-icon {
+					color: white;
+				}
+				:host([active][translucent]) button,
+				:host([translucent]) button:hover,
+				:host([translucent]) button:focus {
+					border: none;
+					background-color: var(--d2l-color-celestine);
+					box-shadow: none;
+				}
+
+				button[disabled] {
+					cursor: default;
+					opacity: 0.5;
+				}
+			`
+		];
 	}
 
 	render() {
