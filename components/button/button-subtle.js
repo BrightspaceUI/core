99709bc248e34053e4e33569a2817df08db98af7
Dave Batiste <dave.batiste@d2l.com>
2019-04-16 21:44:40 -0400

    Move d2l-button-subtle styles to avoid unnecessary request.

--- components/button/button-subtle.js
+++ components/button/button-subtle.js
@@ -1,16 +1,11 @@
 import 'd2l-icons/d2l-icon.js';
 import 'd2l-icons/tier1-icons.js';
-import { html, LitElement } from 'lit-element/lit-element.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { ButtonMixin } from './button-mixin.js';
 import { buttonSharedStyles } from './button-shared-styles.js';
-import { buttonSubtleStyles } from './button-subtle-styles.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { labelStyles } from '../typography/styles.js';
 import { RtlMixin } from '../../mixins/rtl-mixin.js';
-
-/* TODO: convert icons to Lit and update these imports */
-/* TODO: move tier1-icons.js out of here and figure out correct path for it */
-/* TODO: check to make sure nothing was missed */
 
 class ButtonSubtle extends ButtonMixin(RtlMixin(LitElement)) {
 
@@ -24,7 +19,94 @@ class ButtonSubtle extends ButtonMixin(RtlMixin(LitElement)) {
 	}
 
 	static get styles() {
-		return [ labelStyles, buttonSharedStyles, buttonSubtleStyles ];
+		return [ labelStyles, buttonSharedStyles,
+			css`
+				:host {
+					display: inline-block;
+				}
+				:host([hidden]) {
+					display: none;
+				}
+
+				button {
+					background-color: transparent;
+					border-color: transparent;
+					font-family: inherit;
+					padding: 0.5rem 0.6rem;
+					position: relative;
+				}
+
+				:host([h-align="text"]) button {
+					left: -0.6rem;
+				}
+				:host([dir="rtl"][h-align="text"]) button {
+					left: 0;
+					right: -0.6rem;
+				}
+
+				/* Firefox includes a hidden border which messes up button dimensions */
+				button::-moz-focus-inner {
+					border: 0;
+				}
+				button[disabled]:hover,
+				button[disabled]:focus,
+				:host([active]) button[disabled] {
+					background-color: transparent;
+				}
+				button:hover,
+				button:focus,
+				:host([active]) button {
+					background-color: var(--d2l-color-gypsum);
+				}
+
+				.d2l-button-subtle-content {
+					color: var(--d2l-color-celestine);
+					vertical-align: middle;
+				}
+				:host([icon]) .d2l-button-subtle-content {
+					padding-left: 1.2rem;
+				}
+				:host([icon][icon-right]) .d2l-button-subtle-content {
+					padding-left: 0;
+					padding-right: 1.2rem;
+				}
+
+				:host([dir="rtl"][icon]) .d2l-button-subtle-content {
+					padding-left: 0;
+					padding-right: 1.2rem;
+				}
+
+				:host([dir="rtl"][icon][icon-right]) .d2l-button-subtle-content {
+					padding-left: 1.2rem;
+					padding-right: 0;
+				}
+
+				d2l-icon.d2l-button-subtle-icon {
+					color: var(--d2l-color-celestine);
+					display: none;
+					height: 0.9rem;
+					position: absolute;
+					top: 50%;
+					transform: translateY(-50%);
+					width: 0.9rem;
+				}
+				:host([icon]) d2l-icon.d2l-button-subtle-icon {
+					display: inline-block;
+				}
+				:host([icon][icon-right]) d2l-icon.d2l-button-subtle-icon {
+					right: 0.6rem;
+				}
+				:host([dir="rtl"][icon][icon-right]) d2l-icon.d2l-button-subtle-icon {
+					left: 0.6rem;
+					right: auto;
+				}
+
+				button[disabled] {
+					cursor: default;
+					opacity: 0.5;
+				}
+			`
+		];
 	}
 
 	render() {
