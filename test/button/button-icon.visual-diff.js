adfdf518172db12b6fae6eecb35c64ae52f501b0
Dave Batiste <dave.batiste@d2l.com>
2019-04-17 10:38:05 -0400

    Wait until 0 connections for 500ms when loading page for visual-diff.

--- test/button/button-icon.visual-diff.js
+++ test/button/button-icon.visual-diff.js
@@ -11,7 +11,7 @@ describe('d2l-button-icon', function() {
 		browser = await puppeteer.launch();
 		page = await browser.newPage();
 		await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
-		await page.goto(`${visualDiff.getBaseUrl()}/test/button/button-icon.visual-diff.html`, {waitUntil: ['networkidle2', 'load']});
+		await page.goto(`${visualDiff.getBaseUrl()}/test/button/button-icon.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
 		await page.bringToFront();
 	});
 
