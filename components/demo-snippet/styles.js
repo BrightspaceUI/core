1e1a3adc680a4c88ecd64635dc397bbb6ed25d0b
Dave Batiste <dave.batiste@d2l.com>
2019-04-18 13:51:09 -0400

    Use d2l-color variables.

--- components/demo-snippet/styles.js
+++ components/demo-snippet/styles.js
@@ -2,7 +2,7 @@ import { css } from 'lit-element/lit-element.js';
 
 export const styles = css`
 	:host {
-		border: 1px solid #72777a;
+		border: 1px solid var(--d2l-color-tungsten);
 		border-radius: 6px;
 		box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
 		display: block;
@@ -21,9 +21,9 @@ export const styles = css`
 		padding: 0;
 	}
 	:host .d2l-demo-snippet-actions {
-		background-color: #f2f3f5;
-		border-bottom: 1px solid #72777a;
-		border-left: 1px solid #72777a;
+		background-color: var(--d2l-color-sylvite);
+		border-bottom: 1px solid var(--d2l-color-tungsten);
+		border-left: 1px solid var(--d2l-color-tungsten);
 		border-bottom-left-radius: 6px;
 		border-top-right-radius: 5px;
 		display: inline-block;
