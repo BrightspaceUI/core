992f158bdbd2c91c4001e373cc010f8c77be4e64
Dave Batiste <dave.batiste@d2l.com>
2019-04-29 11:13:55 -0400

    Wait for d2l-demo-page component to be defined.

--- components/demo/demo-page.js
+++ components/demo/demo-page.js
@@ -2,12 +2,46 @@ import './demo-snippet.js';
 import './code-view.js';
 import '../colors/colors.js';
 import '../typography/typography.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
 
 document.body.classList.add('d2l-typography');
 
-export const showPage = async() => {
-	if (document.fonts) {
-		await document.fonts.ready;
-	}
+(async() => {
+	const fontsPromise = document.fonts ? document.fonts.ready : Promise.resolve();
+	await Promise.all([
+		customElements.whenDefined('d2l-demo-page'),
+		fontsPromise
+	]);
 	document.body.removeAttribute('unresolved');
-};
+})();
+
+class DemoPage extends LitElement {
+
+	static get properties() {
+		return {
+			title: { type: String, reflect: true }
+		};
+	}
+
+	static get styles() {
+		return css`
+			:host {
+				display: block;
+			}
+		`;
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
+		const title = document.createElement('title');
+		title.textContent = this.title;
+		document.head.insertBefore(title, document.head.firstChild);
+	}
+
+	render() {
+		return html`<slot></slot>`;
+	}
+
+}
+
+customElements.define('d2l-demo-page', DemoPage);
