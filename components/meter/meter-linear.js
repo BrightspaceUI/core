430c726a6b7120977be61d3c3972dbedb6c128e8
Maya Jones <Maya.Jones@D2L.com>
2019-05-08 11:30:51 -0400

    Removed container Removed styles

--- components/meter/meter-linear.js
+++ components/meter/meter-linear.js
@@ -4,11 +4,11 @@ import { RtlMixin } from '../../mixins/rtl-mixin.js';
 class MeterLinear extends RtlMixin(LitElement)  {
 	static get styles() {
 		return [ css`
-			.container {
+			:host {
 				position: relative;
 			}
 
-			.d2l-meter-full-bar, .d2l-meter-inner-bar {
+			.d2l-meter-linear-full-bar, .d2l-meter-linear-inner-bar {
 				border-radius: 32px;
 				height: 9px;
 				left: 0;
@@ -17,11 +17,11 @@ class MeterLinear extends RtlMixin(LitElement)  {
 				width: 100%;
 			}
 
-			.d2l-meter-full-bar {
+			.d2l-meter-linear-full-bar {
 				background-color: var(--d2l-color-gypsum);
 			}
 
-			.d2l-meter-inner-bar {
+			.d2l-meter-linear-inner-bar {
 				background-color: var(--d2l-color-celestine);
 			}
 		`];
@@ -30,10 +30,8 @@ class MeterLinear extends RtlMixin(LitElement)  {
 	render() {
 		const progressBarWidth = this.value / this.max * 100;
 		return html `
-			<div class="container">
-				<div class="d2l-meter-full-bar">
-					<div class="d2l-meter-inner-bar" style="width:${progressBarWidth}%;"></div>
-				</div>
+			<div class="d2l-meter-linear-full-bar">
+				<div class="d2l-meter-linear-inner-bar" style="width:${progressBarWidth}%;"></div>
 			</div>
 		`;
 	}
