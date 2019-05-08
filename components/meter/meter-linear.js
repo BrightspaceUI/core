6d69cbfa11e4292e7094d6224536c40616a2d118
Maya Jones <Maya.Jones@D2L.com>
2019-05-08 09:01:21 -0400

    units is now value outOf is now max progressBarWidth is now calculated in render barWidth is now calculated in render.

--- components/meter/meter-linear.js
+++ components/meter/meter-linear.js
@@ -8,45 +8,31 @@ class MeterLinear extends RtlMixin(LitElement)  {
 	}
 
 	render() {
+		const barWidth = this.width - 10;
+		const progressBarWidth = this.value / this.max * barWidth;
 		return html `
 			<svg viewBox="-5 -4.5 ${this.width} 9" width="${this.width}">
-				<path class="d2l-meter-full-bar" d="M0,0 h ${this._actualWidth}" />
-				<path class="d2l-meter-progress-bar" d="M0,0 h ${this._computeProgressBarWidth(this.units, this.outOf, this._actualWidth)}" visibility="${this.units ? 'visable' : 'hidden'}"/>
+				<path class="d2l-meter-full-bar" d="M0,0 h ${barWidth}" />
+				<path class="d2l-meter-progress-bar" d="M0,0 h ${progressBarWidth}" visibility="${this.value ? 'visable' : 'hidden'}"/>
 			</svg>
 		`;
 	}
 
 	static get properties() {
 		return {
-			units: { type: Number, reflect: true },
-			outOf: { type: Number, reflect: true, attribute: 'out-of' },
+			value: { type: Number, reflect: true },
+			max: { type: Number, reflect: true },
 			primary: { type: String, reflect: true },
 			secondary: { type: String, reflect: true },
-			width: { type: Number },
-			_actualWidth: { type: Number }
+			width: { type: Number }
 		};
 	}
 
 	constructor() {
 		super();
-		this._width;
-		this._actualWidth;
-		this.units = 0;
-		this.outOf = 100;
+		this.value = 0;
+		this.max = 100;
 		this.width = 150;
-	}
-
-	set width(value) {
-		const oldValue = this.width;
-		this._actualWidth = value - 10;
-		this._width = value;
-		this.requestUpdate('width', oldValue);
-	}
-
-	get width() { return this._width; }
-
-	_computeProgressBarWidth(units, outOf, actualWidth) {
-		return units / outOf * actualWidth;
 	}
 }
 
