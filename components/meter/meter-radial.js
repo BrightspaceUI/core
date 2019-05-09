81bb69d752f1593a783724f364459a1fbbfd8835
Maya Jones <Maya.Jones@D2L.com>
2019-05-09 10:32:16 -0400

    Remove the <g> tag

--- components/meter/meter-radial.js
+++ components/meter/meter-radial.js
@@ -39,17 +39,15 @@ class MeterRadial extends LitElement {
 		const progressFill = this.value / this.max * lengthOfLine;
 		return html `
 			<svg viewBox="0 0 86 52" >
-				<g transform="translate(5 47)">
-					<path class="d2l-meter-radial-full-bar" d="M5 47a38 35 0 0 1 76 0" />
-					<path
-						class="d2l-meter-radial-progress-bar"
-						d="M5 47a38 35 0 0 1 76 0"
-						stroke-dasharray="${progressFill} ${lengthOfLine}"
-						visibility="${this.value ? 'visible' : 'hidden'}" />
-					<text class="d2l-heading-4 d2l-meter-radial-text" x="38" y="2" text-anchor="middle"  transform="translate(5 47)">
-						${this.value}/${this.max}
-					</text>
-				</g>
+				<path class="d2l-meter-radial-full-bar" d="M5 47a38 35 0 0 1 76 0" />
+				<path
+					class="d2l-meter-radial-progress-bar"
+					d="M5 47a38 35 0 0 1 76 0"
+					stroke-dasharray="${progressFill} ${lengthOfLine}"
+					visibility="${this.value ? 'visible' : 'hidden'}" />
+				<text class="d2l-heading-4 d2l-meter-radial-text" x="38" y="2" text-anchor="middle"  transform="translate(5 47)">
+					${this.value}/${this.max}
+				</text>
 			</svg>
 		`;
 	}
