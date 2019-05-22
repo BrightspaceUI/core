7b3313e6479640e7945bd521c817e25a2233375b
Dave Batiste <dave.batiste@d2l.com>
2019-05-22 11:40:37 -0400

    Add visual-diff tests for auto expand/collapse, and refactor duplicate async logic.

--- components/more-less/test/more-less.visual-diff.js
+++ components/more-less/test/more-less.visual-diff.js
@@ -15,49 +15,83 @@ describe('d2l-more-less', function() {
 		await page.bringToFront();
 	});
 
-	beforeEach(async() => {
-		await visualDiff.resetFocus(page);
-	});
-
 	after(() => browser.close());
+
+	describe('normal', function() {
+
+	});
 
 	it('collapsed', async function() {
 		const rect = await visualDiff.getRect(page, '#collapsed');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
-	it('expanded by click', async function() {
+	it('expands on click', async function() {
 		await click(page, '#collapsed');
 		const rect = await visualDiff.getRect(page, '#collapsed');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
-	it('collapsed by click', async function() {
+	it('collapses on click', async function() {
 		await click(page, '#expanded');
 		const rect = await visualDiff.getRect(page, '#expanded');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
-	it('with-height', async function() {
+	it('grows with content when expanded', async function() {
+		const resize = contentResize(page, '#expanded');
+		await page.evaluate(() => {
+			const elem = document.querySelector('#expanded').querySelector('p');
+			elem.textContent += 'Some content appended after component first render.';
+		});
+		await resize;
+		const rect = await visualDiff.getRect(page, '#expanded');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('auto-expands on focus-in', async function() {
+		const resize = contentResize(page, '#auto-expand');
+		await page.evaluate(() => document.querySelector('#auto-expand').querySelector('a').focus());
+		await resize;
+		const rect = await visualDiff.getRect(page, '#auto-expand');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('auto-collapses on focus-out', async function() {
+		const resize = contentResize(page, '#auto-expand');
+		await visualDiff.resetFocus(page);
+		await resize;
+		const rect = await visualDiff.getRect(page, '#auto-expand');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('with custom height', async function() {
 		const rect = await visualDiff.getRect(page, '#with-height');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
-	it('with-custom-blur', async function() {
+	it('with custom blur', async function() {
 		const rect = await visualDiff.getRect(page, '#with-custom-blur');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
-	const click = (page, selector) => {
+	const contentResize = (page, selector) => {
 		return page.evaluate((selector) => {
 			return new Promise((resolve) => {
 				const elem = document.querySelector(selector);
 				elem.shadowRoot.querySelector('.more-less-content').addEventListener('transitionend', (e) => {
 					if (e.propertyName === 'height') resolve();
 				});
-				elem.shadowRoot.querySelector('d2l-button-subtle').click();
 			});
 		}, selector);
+	};
+
+	const click = async(page, selector) => {
+		const resize = contentResize(page, selector);
+		await page.evaluate((selector) => {
+			document.querySelector(selector).shadowRoot.querySelector('d2l-button-subtle').click();
+		}, selector);
+		return resize;
 	};
 
 });
