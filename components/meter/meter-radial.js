ac4ca8e500b67b0b66faac186a807d4a0edebb35
Justin Lai <Justin.Lai@D2L.com>
2019-06-27 10:29:23 -0400

    adding secondary text to meter-radial

--- components/meter/meter-radial.js
+++ components/meter/meter-radial.js
@@ -1,20 +1,25 @@
 import '../colors/colors.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
-import { heading4Styles } from '../typography/styles.js';
+import { bodySmallStyles, heading4Styles } from '../typography/styles.js';
 
 class MeterRadial extends LitElement {
 	static get properties() {
 		return {
 			value: { type: Number },
-			max: { type: Number }
+			max: { type: Number },
+			text: { type: String }
 		};
 	}
 
 	static get styles() {
-		return [ heading4Styles, css`
+		return [ heading4Styles, bodySmallStyles, css`
 		:host {
 			display: inline-block;
 			width: 4.3rem;
+		}
+		.d2l-meter-radial {
+			display: flex;
+			flex-direction: column;
 		}
 		.d2l-meter-radial-full-bar,
 		.d2l-meter-radial-progress-bar {
@@ -37,18 +42,22 @@ class MeterRadial extends LitElement {
 	render() {
 		const lengthOfLine = 115; // found by approximating half the perimeter of the ellipse with radii 38 and 35
 		const progressFill = this.value / this.max * lengthOfLine;
+		const secondaryTextElement = this.text ? html`<div class="d2l-body-small">${this.text}</div>` : html``;
 		return html `
-			<svg viewBox="0 0 86 52" >
-				<path class="d2l-meter-radial-full-bar" d="M5 47a38 35 0 0 1 76 0" />
-				<path
-					class="d2l-meter-radial-progress-bar"
-					d="M5 47a38 35 0 0 1 76 0"
-					stroke-dasharray="${progressFill} ${lengthOfLine}"
-					visibility="${this.value ? 'visible' : 'hidden'}" />
-				<text class="d2l-heading-4 d2l-meter-radial-text" x="38" y="2" text-anchor="middle"  transform="translate(5 47)">
-					${this.value}/${this.max}
-				</text>
-			</svg>
+			<div class="d2l-meter-radial">
+				<svg viewBox="0 0 86 52" >
+					<path class="d2l-meter-radial-full-bar" d="M5 47a38 35 0 0 1 76 0" />
+					<path
+						class="d2l-meter-radial-progress-bar"
+						d="M5 47a38 35 0 0 1 76 0"
+						stroke-dasharray="${progressFill} ${lengthOfLine}"
+						visibility="${this.value ? 'visible' : 'hidden'}" />
+					<text class="d2l-heading-4 d2l-meter-radial-text" x="38" y="2" text-anchor="middle"  transform="translate(5 47)">
+						${this.value}/${this.max}
+					</text>
+				</svg>
+				${secondaryTextElement}
+			</div>
 		`;
 	}
 }
