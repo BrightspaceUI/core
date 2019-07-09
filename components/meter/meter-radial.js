d222955affe16b72a4436642a93f48143dad39f5
Maya Jones <Maya.Jones@D2L.com>
2019-07-09 15:06:33 -0400

    Center Radial Text. Required for lots of text

--- components/meter/meter-radial.js
+++ components/meter/meter-radial.js
@@ -37,8 +37,9 @@ class MeterRadial extends MeterMixin(RtlMixin(LitElement)) {
 			stroke: var(--d2l-color-celestine);
 		}
 		.d2l-meter-radial-text {
-			fill: var(--d2l-color-ferrite);
+			text-align: center;
 			color: var(--d2l-color-ferrite);
+			fill: var(--d2l-color-ferrite);
 		}
 	` ];
 	}
