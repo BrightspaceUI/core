e5952a1a42e0785c66bfd64e57f6867031dd44ba
BrightspaceGitHubReader <brightspacegithubreader@d2l.com>
2019-07-30 17:26:18 -0400

    removing focus reset to see if this fixes tests

--- components/button/test/button-icon.visual-diff.js
+++ components/button/test/button-icon.visual-diff.js
@@ -15,10 +15,6 @@ describe('d2l-button-icon', function() {
 		await page.bringToFront();
 	});
 
-	beforeEach(async() => {
-		await visualDiff.resetFocus(page);
-	});
-
 	after(() => browser.close());
 
 	[
