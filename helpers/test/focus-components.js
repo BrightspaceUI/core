e040d08d60038440179279586e9b60fde86a3886
Dave Batiste <dave.batiste@d2l.com>
2019-08-12 15:55:36 -0400

    Move focus helpers and tests from d2l-polymer-behaviors-ui.

---
+++ helpers/test/focus-components.js
@@ -0,0 +1,29 @@
+import { html, LitElement } from 'lit-element/lit-element.js';
+
+class FocusTest extends LitElement {
+
+	getContent() {
+		return this.shadowRoot.querySelector('#content');
+	}
+
+	getShadow1() {
+		return this.shadowRoot.querySelector('#shadow1');
+	}
+
+	getShadow2() {
+		return this.shadowRoot.querySelector('#shadow2');
+	}
+
+	render() {
+		return html`
+			<div>
+				<div><a id="shadow1" href="javascript:void(0);"></a></div>
+				<div id="content"><slot></slot></div>
+				<div><a id="shadow2" href="javascript:void(0);"></a></div>
+			</div>
+		`;
+	}
+
+}
+
+customElements.define('d2l-focus', FocusTest);
