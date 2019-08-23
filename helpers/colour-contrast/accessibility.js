fdf430742d9ddaf74c1c556f9cd6018fcc0e294d
Chris Kraljevic <Chris.Kraljevic@D2L.com>
2019-08-23 17:18:02 -0500

    More PR feedback

--- helpers/colour-contrast/accessibility.js
+++ helpers/colour-contrast/accessibility.js
@@ -1,15 +1,17 @@
 import { contrastColors, contrastRGB } from './contrast.js';
 
-export const contrastNormalText = 4.5;
+const contrastMin = 4.5;
+
+export const contrastNormalText = contrastMin;
 export const contrastLargeTxt = 3;
 export const contrastNonText = 3;
 
 // takes 2 color strings
 export function isAccessible(color1, color2, myContrastMin) {
-	return contrastColors(color1, color2) >= (myContrastMin || contrastNormalText);
+	return contrastColors(color1, color2) >= (myContrastMin || contrastMin);
 }
 
 // takes 2 RGBColor objects
 export function isAccessibleRGB(rgb1, rgb2, myContrastMin) {
-	return contrastRGB(rgb1, rgb2) >= (myContrastMin || contrastNormalText);
+	return contrastRGB(rgb1, rgb2) >= (myContrastMin || contrastMin);
 }
---
+++ helpers/test/colour-contrast/colour-contrast-components.js
@@ -0,0 +1,33 @@
+import { html, LitElement } from 'lit-element/lit-element.js';
+
+class ColourContrastTest extends LitElement {
+
+	getBackgroundColour(rating) {
+		const background = this.shadowRoot.querySelector(`#background-${rating}`);
+		return getComputedStyle(background).getPropertyValue('background-color');
+	}
+
+	getForegroundColour(rating) {
+		const foreground =  this.shadowRoot.querySelector(`#foreground-${rating}`);
+		return getComputedStyle(foreground).getPropertyValue('color');
+	}
+
+	render() {
+		return html`
+			<div>
+				<div id="background-AAA" style="background-color: #0000FF">
+					<p id="foreground-AAA" style="color: #FFFFFF"></p>
+				</div>
+				<div id="background-AA" style="background-color: #FFFFFF">
+					<p id="foreground-AA" style="color: #707070"></p>
+				</div>
+				<div id="background-F" style="background-color: #BBBBBB">
+					<p id="foreground-F" style="color: #FFFFFF"></p>
+				</div>
+			</div>
+		`;
+	}
+
+}
+
+customElements.define('d2l-test-colour-contrast', ColourContrastTest);
