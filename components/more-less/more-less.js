1db8fffc79ee6205bccf76bbc96396c006673295
BrightspaceGitHubReader <brightspacegithubreader@d2l.com>
2019-09-13 16:32:13 -0400

    not centralizing event dispatch code for now

--- components/more-less/more-less.js
+++ components/more-less/more-less.js
@@ -2,7 +2,6 @@ import '../button/button-subtle.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { getComposedChildren, isComposedAncestor } from '../../helpers/dom.js';
 import { classMap} from 'lit-html/directives/class-map.js';
-import { dispatchRenderEvent } from '../../helpers/dispatchRenderEvent.js';
 import { getUniqueId } from '../../helpers/uniqueId.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { LocalizeMixin } from '../../mixins/localize-mixin.js';
@@ -184,7 +183,12 @@ class MoreLess extends LocalizeMixin(LitElement)  {
 	}
 
 	render() {
-		dispatchRenderEvent(this);
+		requestAnimationFrame(
+			() => this.dispatchEvent(new CustomEvent('d2l-more-less-render', {
+				bubbles: false,
+				composed: false
+			}))
+		);
 		const contentClasses = {
 			'more-less-content': true,
 			'more-less-transition': this.__transitionAdded
