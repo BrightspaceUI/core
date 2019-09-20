73eb213e9d369d1b63102ac28f1733a0cb3f8dc4
BrightspaceGitHubReader <brightspacegithubreader@d2l.com>
2019-09-20 17:47:49 -0400

    porting text inputs to Lit

---
+++ components/inputs/test/input-text.visual-diff.js
@@ -0,0 +1,53 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-input-text', function() {
+
+	const visualDiff = new VisualDiff('input-text', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+		await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
+		await page.goto(`${visualDiff.getBaseUrl()}/components/inputs/test/input-text.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.bringToFront();
+	});
+
+	after(() => browser.close());
+
+	[
+		'basic',
+		'email',
+		'number',
+		'password',
+		'search',
+		'tel',
+		'url',
+		'disabled',
+		'placeholder',
+		'placeholder-disabled',
+		'invalid',
+		'invalid-disabled',
+		'aria-invalid',
+		'aria-invalid-disabled',
+		'override-height',
+		'override-padding',
+		'override-placeholder'
+	].forEach((name) => {
+		it(name, async function() {
+			const rect = await visualDiff.getRect(page, `#${name}`);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+		it(`${name}-focus`, async function() {
+			await page.evaluate((name) => {
+				const elem = document.querySelector(`#${name}`);
+				elem.focus();
+			}, name);
+			const rect = await visualDiff.getRect(page, `#${name}`);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+	});
+
+});
