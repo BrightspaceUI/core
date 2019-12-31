64cb9b0f6f505836fd788cc23289ed5169adb8be
Vincent Wong <vincent.wong@d2l.com>
2019-12-31 14:11:21 -0800

    Remove unnecessary ID

--- templates/primary-secondary/test/primary-secondary.visual-diff.js
+++ templates/primary-secondary/test/primary-secondary.visual-diff.js
@@ -19,13 +19,13 @@ describe('d2l-template-primary-secondary', function() {
 		});
 
 		[
-			{ name: 'normal', selector: '#normal', fileName: 'primary-secondary-desktop.visual-diff.html' },
-			{ name: 'larger-than-viewport-height', selector: '#larger-than-viewport-height', fileName: 'primary-secondary-desktop-large.visual-diff.html' }
+			{ name: 'normal', fileName: 'primary-secondary-desktop.visual-diff.html' },
+			{ name: 'larger-than-viewport-height', fileName: 'primary-secondary-desktop-large.visual-diff.html' }
 		].forEach((entry) => {
 			it(entry.name, async function() {
 				await page.goto(`${visualDiff.getBaseUrl()}/templates/primary-secondary/test/${entry.fileName}`, {waitUntil: ['networkidle0', 'load']});
 				await page.bringToFront();
-				const rect = await visualDiff.getRect(page, entry.selector);
+				const rect = await visualDiff.getRect(page, 'd2l-template-primary-secondary');
 				await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 			});
 		});
