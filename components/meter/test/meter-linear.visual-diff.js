c88c26640969eb18312348a825e9dc0e5ee29704
Tom Michiels <49882324+TomMichielsD2L@users.noreply.github.com>
2020-01-02 13:46:58 -0500

    Meter linear max width fix (#317) [increment minor]

--- components/meter/test/meter-linear.visual-diff.js
+++ components/meter/test/meter-linear.visual-diff.js
@@ -23,12 +23,15 @@ describe('d2l-meter-linear', function() {
 		{ title: 'completed', fixture: '#completed'},
 		{ title: 'max-zero', fixture: '#max-zero'},
 		{ title: 'round-to-zero', fixture: '#round-to-zero'},
+		{ title: 'over-100', fixture: '#over-100'},
 		{ title: 'no-progress-rtl', fixture: '#no-progress-rtl'},
 		{ title: 'has-progress-rtl', fixture: '#has-progress-rtl'},
 		{ title: 'completed-rtl', fixture: '#completed-rtl'},
+		{ title: 'over-100-rtl', fixture: '#over-100-rtl'},
 		{ title: 'no-progress-light', fixture: '#no-progress-light'},
 		{ title: 'has-progress-light', fixture: '#has-progress-light'},
-		{ title: 'completed-light', fixture: '#completed-light'}
+		{ title: 'completed-light', fixture: '#completed-light'},
+		{ title: 'over-100-light', fixture: '#over-100-light'}
 	].forEach((testData) => {
 		it(testData.title, async function() {
 			const rect = await visualDiff.getRect(page, testData.fixture);
