b5cf134b70b11ea956725c431dd8e3f0f92d4353
BrightspaceGitHubReader <brightspacegithubreader@d2l.com>
2020-02-04 14:15:34 -0500

    using open-wc for checkbox input tests

---
+++ components/inputs/test/input-checkbox.test.js
@@ -0,0 +1,252 @@
+import '../input-checkbox.js';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
+
+const checkedFixture = html`<d2l-input-checkbox checked></d2l-input-checkbox>`;
+const disabledFixture = html`<d2l-input-checkbox disabled></d2l-input-checkbox>`;
+const uncheckedFixture = html`<d2l-input-checkbox aria-label="basic"></d2l-input-checkbox>`;
+const indeterminateCheckedFixture = html`<d2l-input-checkbox indeterminate checked></d2l-input-checkbox>`;
+const indeterminateUncheckedFixture = html`<d2l-input-checkbox indeterminate></d2l-input-checkbox>`;
+const notTabbableFixture = html`<d2l-input-checkbox not-tabbable label="not-tabbable"></d2l-input-checkbox>`;
+
+describe('d2l-input-checkbox', () => {
+
+	function getInput(elem) {
+		return elem.shadowRoot.querySelector('input.d2l-input-checkbox');
+	}
+
+	describe('accessibility', () => {
+
+		it('should pass all aXe tests (unchecked)', async() => {
+			const elem = await fixture(uncheckedFixture);
+			await expect(elem).to.be.accessible;
+		});
+
+		it('should pass all aXe tests (checked)', async() => {
+			const elem = await fixture(checkedFixture);
+			await expect(elem).to.be.accessible;
+		});
+
+		it('should pass all aXe tests (disabled)', async() => {
+			const elem = await fixture(disabledFixture);
+			await expect(elem).to.be.accessible;
+		});
+
+		it('should pass all aXe tests when focused', async() => {
+			const elem = await fixture(uncheckedFixture);
+			setTimeout(() => getInput(elem).focus());
+			await oneEvent(elem, 'focus');
+			await expect(elem).to.be.accessible();
+		});
+
+	});
+
+	describe('default property values', () => {
+
+		let elem;
+		beforeEach(async() => {
+			elem = await fixture(uncheckedFixture);
+		});
+
+		['checked', 'disabled', 'indeterminate', 'notTabbable'].forEach((name) => {
+			it(`should default "${name}" property to "false"`, () => {
+				expect(elem[name]).to.be.false;
+			});
+		});
+
+		it('should default "name" property to empty when unset', () => {
+			expect(elem.name).to.equal('');
+		});
+
+		it('should default "value" property to "on" when unset', () => {
+			expect(elem.value).to.equal('on');
+		});
+
+	});
+
+	describe('property binding', () => {
+
+		let elem;
+		beforeEach(async() => {
+			elem = await fixture(uncheckedFixture);
+		});
+
+		it('should bind "value" attribute to input property', async() => {
+			elem.setAttribute('value', 'hello');
+			await elem.updateComplete;
+			expect(getInput(elem).value).to.equal('hello');
+		});
+
+		it('should bind "indeterminate" attribute to input property', async() => {
+			elem.setAttribute('indeterminate', 'indeterminate');
+			await elem.updateComplete;
+			expect(getInput(elem).indeterminate).to.be.true;
+		});
+
+		it('should bind "checked" attribute to input property', async() => {
+			elem.setAttribute('checked', 'checked');
+			await elem.updateComplete;
+			expect(getInput(elem).checked).to.be.true;
+		});
+
+		it('should bind "checked" property to input property', async() => {
+			elem.checked = true;
+			await elem.updateComplete;
+			expect(getInput(elem).checked).to.be.true;
+		});
+
+		it('should continue to bind "checked" property after interaction', async() => {
+			const input = getInput(elem);
+			input.click();
+			expect(input.checked).to.be.true;
+			expect(elem.checked).to.be.true;
+			await elem.updateComplete;
+			elem.checked = false; // eslint-disable-line require-atomic-updates
+			await elem.updateComplete;
+			expect(input.checked).to.be.false;
+		});
+
+		[
+			{name: 'aria-label', propName: 'ariaLabel', value: 'hello'},
+			{name: 'disabled', value: true},
+			{name: 'name', value: 'jim'}
+		].forEach((attr) => {
+			attr.propName = attr.propName || attr.name;
+			it(`should bind "${attr.name}" to input`, async() => {
+				if (typeof attr.value === 'boolean') {
+					elem.setAttribute(attr.name, attr.name);
+				} else {
+					elem.setAttribute(attr.name, attr.value);
+				}
+				await elem.updateComplete;
+				const input = getInput(elem);
+				if (typeof attr.value === 'boolean') {
+					expect(input.hasAttribute(attr.name)).to.be.true;
+					expect(input[attr.propName]).to.equal(attr.value);
+				} else {
+					expect(input.getAttribute(attr.name)).to.equal(attr.value.toString());
+					if (attr.name.indexOf('aria-') === -1) {
+						expect(input[attr.propName]).to.equal(attr.value);
+					}
+				}
+			});
+		});
+
+	});
+
+	describe('change event', () => {
+
+		let elem;
+		beforeEach(async() => {
+			elem = await fixture(uncheckedFixture);
+		});
+
+		it('should fire "change" event when input element is clicked', async() => {
+			setTimeout(() => getInput(elem).click());
+			const { target } = await oneEvent(elem, 'change');
+			expect(target).to.equal(elem);
+		});
+
+		it('should reflect that a previously unchecked input is now checked', async() => {
+			setTimeout(() => getInput(elem).click());
+			const { target } = await oneEvent(elem, 'change');
+			expect(target.checked).to.equal(true);
+		});
+
+	});
+
+	describe('simulateClick', () => {
+
+		let elem;
+		beforeEach(async() => {
+			elem = await fixture(uncheckedFixture);
+		});
+
+		it('should set "checked" property', () => {
+			elem.simulateClick();
+			expect(elem.checked).to.be.true;
+		});
+
+		it('should trigger an event', async() => {
+			setTimeout(() => elem.simulateClick());
+			await oneEvent(elem, 'change');
+			expect(elem.checked).to.be.true;
+		});
+
+	});
+
+	describe('indeterminate', () => {
+
+		it('should set aria-checked to "mixed" when indeterminate and checked', async() => {
+			const elem = await fixture(indeterminateCheckedFixture);
+			expect(getInput(elem).getAttribute('aria-checked')).to.equal('mixed');
+		});
+
+		it('should set aria-checked to "mixed" when indeterminate and unchecked', async() => {
+			const elem = await fixture(indeterminateUncheckedFixture);
+			expect(getInput(elem).getAttribute('aria-checked')).to.equal('mixed');
+		});
+
+		it('should clear "indeterminate" when checked interderminate clicked', async() => {
+			const elem = await fixture(indeterminateCheckedFixture);
+			getInput(elem).click();
+			await elem.updateComplete;
+			expect(elem.checked).to.be.false;
+			expect(elem.indeterminate).to.be.false;
+			expect(getInput(elem).hasAttribute('aria-checked')).to.be.false;
+		});
+
+		it('should fire "change" event when input element is clicked', async() => {
+			const elem = await fixture(indeterminateCheckedFixture);
+			setTimeout(() => getInput(elem).click());
+			const { target } = await oneEvent(elem, 'change');
+			expect(target.checked).to.equal(false);
+			expect(target.indeterminate).to.equal(false);
+		});
+
+		it('should clear "indeterminate" when unchecked indeterminate clicked', async() => {
+			const elem = await fixture(indeterminateUncheckedFixture);
+			getInput(elem).click();
+			await elem.updateComplete;
+			expect(elem.checked).to.be.true;
+			expect(elem.indeterminate).to.be.false;
+			expect(getInput(elem).hasAttribute('aria-checked')).to.be.false;
+		});
+
+	});
+
+	describe('not tabbable', () => {
+
+		it('should not put a tabindex on the checkbox by default', async() => {
+			const elem = await fixture(uncheckedFixture);
+			expect(getInput(elem).hasAttribute('tabindex')).to.be.false;
+		});
+
+		it('should apply -1 tabindex when set', async() => {
+			const elem = await fixture(notTabbableFixture);
+			expect(getInput(elem).getAttribute('tabindex')).to.equal('-1');
+		});
+
+	});
+
+	describe('focus management', () => {
+
+		let elem;
+		beforeEach(async() => {
+			elem = await fixture(uncheckedFixture);
+		});
+
+		it('should fire "focus" event when input element is focussed', async() => {
+			setTimeout(() => getInput(elem).focus());
+			const { target } = await oneEvent(elem, 'focus');
+			expect(target).to.equal(elem);
+		});
+
+		it('should fire "focus" event when custom element is focussed', async() => {
+			setTimeout(() => elem.focus());
+			const { target } = await oneEvent(elem, 'focus');
+			expect(target).to.equal(elem);
+		});
+
+	});
+
+});
