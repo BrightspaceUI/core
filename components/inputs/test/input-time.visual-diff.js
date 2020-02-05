4ffa98a89eec49500de4887a7c842ac83cf141b4
Scy Akulick <Scy.Akulick@D2L.com>
2020-02-05 11:07:05 -0500

    Visual diff fix

--- components/inputs/test/input-time.visual-diff.js
+++ components/inputs/test/input-time.visual-diff.js
@@ -11,7 +11,7 @@ describe('d2l-input-time', () => {
 		browser = await puppeteer.launch();
 		page = await browser.newPage();
 		await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
-		await page.goto(`${visualDiff.getBaseUrl()}/components/inputs/test/input-test.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.goto(`${visualDiff.getBaseUrl()}/components/inputs/test/input-time.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
 		await page.bringToFront();
 	});
 
