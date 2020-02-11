287c255edf7f6a6f150a5cb3ce450340739fc160
Allan Kerr <allan.kerr@D2L.com>
2020-02-11 12:59:47 -0500

    Convert dropdown-button visual diff tests to dropdown-openers visual diff tests. Add visual diff tests for the dropdown-context-menu opener.

--- components/dropdown/test/dropdown-openers.visual-diff.js
+++ components/dropdown/test/dropdown-openers.visual-diff.js
@@ -2,9 +2,9 @@ const puppeteer = require('puppeteer');
 const VisualDiff = require('@brightspace-ui/visual-diff');
 const helper = require('./dropdown-helper.js');
 
-describe('d2l-dropdown-button', () => {
+describe('d2l-dropdown-openers', () => {
 
-	const visualDiff = new VisualDiff('dropdown-button', __dirname);
+	const visualDiff = new VisualDiff('dropdown-openers', __dirname);
 
 	let browser, page;
 
@@ -12,17 +12,21 @@ describe('d2l-dropdown-button', () => {
 		browser = await puppeteer.launch();
 		page = await browser.newPage();
 		await page.setViewport({ width: 300, height: 400, deviceScaleFactor: 2 });
-		await page.goto(`${visualDiff.getBaseUrl()}/components/dropdown/test/dropdown-button.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
+		await page.goto(`${visualDiff.getBaseUrl()}/components/dropdown/test/dropdown-openers.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
 	});
 
 	after(() => browser.close());
 
 	[
-		'default',
-		'primary',
-		'subtle',
-		'rtl'
+		'button',
+		'button-primary',
+		'button-subtle',
+		'button-rtl',
+		'context-menu',
+		'context-menu-translucent',
+		'context-menu-disabled',
+		'context-menu-translucent-disabled'
 	].forEach((testName) => {
 		it(testName, async function() {
 			const selector = `#${testName}`;
@@ -32,8 +36,9 @@ describe('d2l-dropdown-button', () => {
 	});
 
 	[
-		{ name: 'default-open', id: '#default'},
-		{ name: 'subtle-open', id: '#subtle' },
+		{ name: 'button-open', id: '#button'},
+		{ name: 'button-subtle-open', id: '#button-subtle'},
+		{ name: 'context-menu-open', id: '#context-menu'}
 	].forEach((testCase) => {
 		it(testCase.name, async function() {
 			await helper.open(page, testCase.id);
