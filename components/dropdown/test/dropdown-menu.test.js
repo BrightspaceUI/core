57e5ef1559b9e2e53e96665e32219499e3a9c701
Allan Kerr <allan.kerr@D2L.com>
2020-02-12 13:39:27 -0500

    Add dropdown-menu unit tests and basic visual diff tests

---
+++ components/dropdown/test/dropdown-menu.test.js
@@ -0,0 +1,58 @@
+import '../dropdown.js';
+import '../dropdown-menu.js';
+import '../../menu/menu.js';
+import '../../menu/menu-item.js';
+import '../../menu/menu-item-radio';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
+
+const itemFixture = html`
+	<div>
+		<d2l-dropdown>
+			<button class="d2l-dropdown-opener">Open it!</button>
+			<d2l-dropdown-menu id="dropdown">
+				<d2l-menu label="Astronomy">
+					<d2l-menu-item text="Introduction" id="first-item"></d2l-menu-item>
+					<d2l-menu-item text="Searching for the Heavens "></d2l-menu-item>
+					<d2l-menu-item text="The Universe" id="last-item"></d2l-menu-item>
+				</d2l-menu>
+			</d2l-dropdown-menu>
+		</d2l-dropdown>
+	</div>
+`;
+
+const radioFixture = html`
+	<div>
+		<d2l-dropdown>
+			<button class="d2l-dropdown-opener">Open it!</button>
+			<d2l-dropdown-menu id="dropdown">
+				<d2l-menu label="Astronomy">
+						<d2l-menu-item-radio text="Radio Option 1" value="1" id="first-item"></d2l-menu-item-radio>
+						<d2l-menu-item-radio text="Radio Option 2" value="2"></d2l-menu-item-radio>
+						<d2l-menu-item-radio text="Radio Option 3" value="3" id="last-item"></d2l-menu-item-radio>
+				</d2l-menu>
+			</d2l-dropdown-menu>
+		</d2l-dropdown>
+	</div>
+`;
+
+describe('d2l-dropdown-menu', () => {
+
+	describe('auto-close', () => {
+
+		[
+			{ name: 'should close when menu item is selected', fixture: itemFixture },
+			{ name: 'should close when menu radio item is changed', fixture: radioFixture },
+		].forEach((testCase) => {
+			it(testCase.name, async() => {
+				const dropdown = await fixture(testCase.fixture);
+				const content = dropdown.querySelector('[d2l-dropdown-content]');
+				content.setAttribute('opened', true);
+				await oneEvent(content, 'd2l-dropdown-open');
+				setTimeout(() => dropdown.querySelector('#first-item').click());
+				await oneEvent(content, 'd2l-dropdown-close');
+				expect(content.opened).to.be.false;
+			});
+		});
+	});
+
+});
