78b331599f9a1e55bfdd37aa854c8506a7044145
Margaree Peacocke <margaree.p@gmail.com>
2020-04-06 14:07:00 -0400

    more timeout increases

--- karma.sauce.conf.js
+++ karma.sauce.conf.js
@@ -57,8 +57,16 @@ module.exports = config => {
 			reporters: ['dots', 'saucelabs'],
 			singleRun: true,
 			browserNoActivityTimeout: 120000,
+			browserDisconnectTimeout : 10000, // default 2000
+			browserDisconnectTolerance : 1, // default 0
 			processKillTimeout: 120000,
-			pingTimeout: 120000
+			captureTimeout : 120000, //default 60000
+			pingTimeout: 120000,
+			client: {
+				mocha: {
+					timeout : 20000 // 20 seconds
+				}
+			}
 		}),
 	);
 	return config;
