1f33974f5e3011dc5f6983e3977d600079347a95
Dave Batiste <dave.batiste@d2l.com>
2020-04-09 13:02:00 -0400

    Update visual-diff tests to use createPage which turns on reduce motion media feature.

--- components/backdrop/test/backdrop.visual-diff.js
+++ components/backdrop/test/backdrop.visual-diff.js
@@ -9,8 +9,7 @@ describe('d2l-backdrop', () => {
 
 	before(async() => {
 		browser = await puppeteer.launch();
-		page = await browser.newPage();
-		await page.emulateMediaFeatures([{ name: 'prefers-reduced-motion', value: 'reduce' }]);
+		page = await visualDiff.createPage(browser);
 		await page.setViewport({width: 800, height: 600, deviceScaleFactor: 2});
 		await page.goto(`${visualDiff.getBaseUrl()}/components/backdrop/test/backdrop.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
 		await page.bringToFront();
