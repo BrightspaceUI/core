ef8bba86f48fa426a32e889af2c97be4027b7214
Allan Kerr <allan.kerr@D2L.com>
2020-05-04 13:20:44 -0400

    Add alert visual diff tests

---
+++ components/alert/test/alert.visual-diff.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-alert', () => {
+
+	const visualDiff = new VisualDiff('alert', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await visualDiff.createPage(browser, { viewport: { width: 800, height: 1200 } });
+		await page.goto(`${visualDiff.getBaseUrl()}/components/alert/test/alert.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.bringToFront();
+	});
+
+	after(async() => await browser.close());
+
+	[
+		'default',
+		'success',
+		'critical',
+		'warning',
+		'close',
+		'button',
+		'button-close',
+		'rtl'
+	].forEach((testName) => {
+		it(testName, async function() {
+			const selector = `#${testName}`;
+			const rect = await visualDiff.getRect(page, selector);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+	});
+
+});
