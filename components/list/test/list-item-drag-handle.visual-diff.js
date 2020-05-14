3ffe02e304b8863e9f9f199d32b6e42387cc0ed1
Maya Jones <Maya.Jones@D2L.com>
2020-05-13 20:07:01 -0400

    Make the width of the visual tests smaller

--- components/list/test/list-item-drag-handle.visual-diff.js
+++ components/list/test/list-item-drag-handle.visual-diff.js
@@ -9,7 +9,7 @@ describe('d2l-list-item-drag-handle', () => {
 
 	before(async() => {
 		browser = await puppeteer.launch();
-		page = await visualDiff.createPage(browser, {viewport: {width: 1000, height: 3700}});
+		page = await visualDiff.createPage(browser);
 		await page.goto(`${visualDiff.getBaseUrl()}/components/list/test/list-item-drag-handle.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
 		await page.bringToFront();
 	});
