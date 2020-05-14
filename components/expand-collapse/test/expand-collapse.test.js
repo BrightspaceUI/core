330b986918898a68bf2dbd1bf0c8f9539a7b144f
Allan Kerr <allan.kerr@D2L.com>
2020-05-14 12:56:06 -0400

    Add expand-collapse unit tests

---
+++ components/expand-collapse/test/expand-collapse.test.js
@@ -0,0 +1,14 @@
+import '../expand-collapse.js';
+import { runConstructor } from '../../../tools/constructor-test-helper.js';
+
+describe('d2l-expand-collapse', () => {
+
+	describe('constructor', () => {
+
+		it('should construct', () => {
+			runConstructor('d2l-expand-collapse');
+		});
+
+	});
+
+});
