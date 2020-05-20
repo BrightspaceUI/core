8ead1620d104588c195c2853429295a50bff8e12
Margaree Peacocke <margaree.p@gmail.com>
2020-05-20 09:15:36 -0400

    check if today is in shown month instead

--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -434,13 +434,12 @@ class Calendar extends LocalizeStaticMixin(RtlMixin(LitElement)) {
 	updated(changedProperties) {
 		super.updated(changedProperties);
 
-		changedProperties.forEach(async(oldVal, prop) => {
+		changedProperties.forEach((oldVal, prop) => {
 			if (prop === '_shownMonth' && this._keyboardTriggeredMonthChange) {
 				this._focusDateAddFocus();
 			} else if (prop === 'selectedValue') {
-				const date = await this._getDateElement(this._today);
 				if (this.selectedValue) this._focusDate = getDateFromISODate(this.selectedValue);
-				else if (date) this._focusDate = new Date(this._today);
+				else if (this._today.getMonth() === this._shownMonth) this._focusDate = new Date(this._today);
 				else this._focusDate = new Date(this._shownYear, this._shownMonth, 1);
 			}
 		});
