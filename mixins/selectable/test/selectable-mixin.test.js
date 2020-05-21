a379e1d8dbc7549ed8f1c89f20b79d9079bbd458
BrightspaceGitHubReader <brightspacegithubreader@d2l.com>
2020-05-21 14:22:36 -0400

    axe test added

--- mixins/selectable/test/selectable-mixin.test.js
+++ mixins/selectable/test/selectable-mixin.test.js
@@ -14,6 +14,11 @@ const tag = defineCE(
 );
 
 describe.only('SelectableMixin', () => {
+	it('passes aXe test', async() => {
+		const element = await fixture(`<${tag} key="1234" selectable></${tag}>`);
+		expect(element).to.be.accessible();
+	});
+
 	describe('Sets selected status to undefined when no key is given', () => {
 		const cases = [
 			'disabled selected',
