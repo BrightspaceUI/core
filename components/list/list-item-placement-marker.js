207854ec3de9f6c762417cb12aec8e709ffad37d
Alexander Kobets <alexander.kobets@D2L.com>
2020-06-04 15:36:07 -0400

    import colors

--- components/list/list-item-placement-marker.js
+++ components/list/list-item-placement-marker.js
@@ -1,5 +1,6 @@
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { RtlMixin } from '../../mixins/rtl-mixin.js';
+import '../colors/colors.js';
 
 class ListItemPlacementMarker extends RtlMixin(LitElement) {
 
