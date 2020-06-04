d220e9b260a1b2f1dceebb3b5b3ff3ca99747828
Alexander Kobets <alexander.kobets@D2L.com>
2020-06-04 15:37:18 -0400

    add axe and visual-diff tests

---
+++ components/list/test/list-item-placement-marker.axe.js
@@ -0,0 +1,13 @@
+import '../list-item-placement-marker.js';
+import { expect, fixture, html, oneEvent } from '@open-wc/testing';
+
+describe('d2l-list-item-placement-marker', () => {
+	it('visible', async() => {
+		const element = await fixture(html`<d2l-list-item-placement-marker></d2l-list-item-placement-marker>`);
+		await expect(element).to.be.accessible();
+	});
+	it('visible-rtl', async() => {
+		const element = await fixture(html`<d2l-list-item-placement-marker dir="rtl"></d2l-list-item-placement-marker>`);
+		await expect(element).to.be.accessible();
+	});
+});
---
+++ components/list/test/list-item-placement-marker.visual-diff.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-list-item-placement-marker', () => {
+
+	const visualDiff = new VisualDiff('list-item-placement-marker', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await visualDiff.createPage(browser);
+		await page.goto(`${visualDiff.getBaseUrl()}/components/list/test/list-item-placement-marker.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.bringToFront();
+	});
+
+	after(async() => await browser.close());
+
+	beforeEach(async() => await visualDiff.resetFocus(page));
+
+	describe('placement-marker', () => {
+		it('ltr', async function() {
+			const rect = await visualDiff.getRect(page, '#placement-marker');
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+		it('rtl', async function() {
+			const rect = await visualDiff.getRect(page, '#placement-marker-rtl');
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+	});
+});
