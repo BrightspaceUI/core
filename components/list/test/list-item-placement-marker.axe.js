cd32c03d091d96cadc42b927a199e1dc187f6ffc
Alexander Kobets <alexander.kobets@D2L.com>
2020-06-11 13:45:25 -0400

    workaround for axe test

--- components/list/test/list-item-placement-marker.axe.js
+++ components/list/test/list-item-placement-marker.axe.js
@@ -3,7 +3,7 @@ import { expect, fixture, html } from '@open-wc/testing';
 
 describe('d2l-list-item-placement-marker', () => {
 	it('visible', async() => {
-		const element = await fixture(html`<d2l-list-item-placement-marker></d2l-list-item-placement-marker>`);
+		const element = await fixture(html`<d2l-list-item-placement-marker aria-label='drag placement'></d2l-list-item-placement-marker>`);
 		await expect(element).to.be.accessible();
 	});
 });
