9bab3f3e6837552244315ebc4bc73ecbe8fa5242
Allan Kerr <allan.kerr@D2L.com>
2020-06-15 11:53:10 -0400

    Fix positioning defect
    In Chrome offsetParent includes positioned nodes in ancestors' shadow DOM. In Firefox offsetParent positions relative to these
    but reports the first offsetParent as the first positioned ancestor in the same DOM context. This causes offsetTop and offsetLeft
    to be incorrect in Firefox if there is a positioned node in an ancestor's shadow DOM. To fix this, offsetTop and offsetLeft need
    to be computed using the getOffsetParent DOM helper rather than using the built-in properties.

--- components/tooltip/tooltip.js
+++ components/tooltip/tooltip.js
@@ -405,9 +405,9 @@ class Tooltip extends RtlMixin(LitElement) {
 		if (!this._target) {
 			return;
 		}
-
+		const offsetParent = getOffsetParent(this);
 		const targetRect = this._target.getBoundingClientRect();
-		const spaceAround = this._computeSpaceAround(targetRect);
+		const spaceAround = this._computeSpaceAround(offsetParent, targetRect);
 
 		// Compute the size of the spaces above, below, left and right and find which space to fit the tooltip in
 		const content = this._getContent();
@@ -420,9 +420,18 @@ class Tooltip extends RtlMixin(LitElement) {
 		this._openDir = space.dir;
 
 		// Compute the x and y position of the tooltip relative to its target
-		const tooltipRect = this.getBoundingClientRect();
-		const top = targetRect.top - tooltipRect.top + this.offsetTop;
-		const left = targetRect.left - tooltipRect.left + this.offsetLeft;
+		let parentTop;
+		let parentLeft;
+		if (offsetParent) {
+			const parentRect = offsetParent.getBoundingClientRect();
+			parentTop = parentRect.top + offsetParent.clientTop;
+			parentLeft = parentRect.left + offsetParent.clientLeft;
+		} else {
+			parentTop = 0;
+			parentLeft = 0;
+		}
+		const top = targetRect.top - parentTop;
+		const left = targetRect.left - parentLeft;
 
 		let positionRect;
 		if (this._isAboveOrBelow()) {
@@ -509,14 +518,13 @@ class Tooltip extends RtlMixin(LitElement) {
 		return spaces;
 	}
 
-	_computeSpaceAround(targetRect) {
+	_computeSpaceAround(offsetParent, targetRect) {
 		const spaceAround = {
 			above: targetRect.top - this._viewportMargin,
 			below: window.innerHeight - (targetRect.top + targetRect.height) - this._viewportMargin,
 			left: targetRect.left - this._viewportMargin,
 			right: document.documentElement.clientWidth - (targetRect.left + targetRect.width) - this._viewportMargin
 		};
-		const offsetParent = getOffsetParent(this);
 		if (this.boundary && offsetParent) {
 			const parentRect = offsetParent.getBoundingClientRect();
 			if (!isNaN(this.boundary.left)) {
