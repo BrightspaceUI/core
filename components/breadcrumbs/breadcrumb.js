9d8f467e8b4508856ef5aefa7c704a843cf1d5bf
Miad Abrin <Miad.Abrin@d2l.com>
2020-06-18 14:00:21 -0400

    enhance implementation

--- components/breadcrumbs/breadcrumb.js
+++ components/breadcrumbs/breadcrumb.js
@@ -1,12 +1,14 @@
-import '../colors/colors.js';
+import '../icons/icon.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { RtlMixin } from '../../mixins/rtl-mixin.js';
 
-class BreadCrumb extends LitElement {
+class BreadCrumb extends RtlMixin(LitElement) {
 
 	static get properties() {
 		return {
 			compact: {
-				type: String,
+				type: Boolean,
 				reflect: true
 			},
 			href: {
@@ -26,11 +28,50 @@ class BreadCrumb extends LitElement {
 				type: String,
 				reflect: true
 			},
+			_breadCrumbIcon: {
+				attribute: false,
+				type: String
+			}
 		};
 	}
 
 	static get styles() {
 		return css`
+			:host {
+				align-items: center;
+				display: inline-flex;
+			}
+
+			div.d2l-breadcrumb-wrapper {
+				align-items: center;
+			}
+
+			div.d2l-breadcrumb-wrapper.compact {
+				display: flex;
+				flex-direction: row-reverse;
+			}
+
+			d2l-icon {
+				height: 8px;
+				padding-left: 10px;
+				padding-right: 5px;
+				width: 8px;
+			}
+
+			d2l-icon:dir(rtl) {
+				padding-left: 5px;
+				padding-right: 10px;
+			}
+
+			div.d2l-breadcrumb-wrapper.compact d2l-icon {
+				padding-right: 10px;
+				padding-left: 0;
+			}
+
+			div.d2l-breadcrumb-wrapper.compact d2l-icon:dir(rtl) {
+				padding-right: 0;
+				padding-left: 10px;
+			}
 		`;
 	}
 
@@ -38,11 +79,20 @@ class BreadCrumb extends LitElement {
 		super();
 		this.href = '#';
 	}
-
 	render() {
+		const breadCrumbClasses = {
+			'd2l-breadcrumbs-wrapper': true,
+			'compact': this.compact
+		};
 		return html`
-			${this.text}
+			<div class="${classMap(breadCrumbClasses)}" role="navigation" aria-label="${this.ariaLabel ? this.ariaLabel : this.text}">
+				<d2l-link href="${this.href}" target="${this.text}">${this.text}</d2l-link>
+				<d2l-icon icon="${this._breadCrumbIcon}"></d2l-icon>
+			</div>
 		`;
+	}
+	get _breadCrumbIcon() {
+		return this.compact ? 'd2l-tier1:chevron-left' : 'd2l-tier1:chevron-right';
 	}
 
 }
--- components/breadcrumbs/breadcrumbs.js
+++ components/breadcrumbs/breadcrumbs.js
@@ -1,8 +1,9 @@
-import '../colors/colors.js';
 import './breadcrumb.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { RtlMixin } from '../../mixins/rtl-mixin.js';
 
-class BreadCrumbs extends LitElement {
+class BreadCrumbs extends RtlMixin(LitElement) {
 	static get properties() {
 		return {
 			compact: {
@@ -14,21 +15,78 @@ class BreadCrumbs extends LitElement {
 
 	static get styles() {
 		return css`
-		`;
-	}
+			:host {
+				--d2l-breadcrumbs-background-color: rgb(251, 252, 252);
+				display: block;
+				position: relative;
+			}
 
-	constructor() {
-		super();
-		this.state = 'default';
-		this.bold = false;
+			:host::after {
+				background: -webkit-gradient(linear, left top, right top, from(rgba(255, 255, 255, 0)), to(var(--d2l-breadcrumbs-background-color)));
+				background: -webkit-linear-gradient(left, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				background: -o-linear-gradient(left, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				background: linear-gradient(to right, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				bottom: 0;
+				content: '';
+				pointer-events: none;
+				position: absolute;
+				right: 0;
+				top: 0;
+				width: 10px;
+			}
+
+			:host-context([dir="rtl"])::after {
+				background: -webkit-gradient(linear, right top, left top, from(rgba(255, 255, 255, 0)), to(var(--d2l-breadcrumbs-background-color)));
+				background: -webkit-linear-gradient(right, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				background: -o-linear-gradient(right, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				background: linear-gradient(to left, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				left: 0;
+				right: auto;
+			}
+
+			:host(:dir(rtl))::after {
+				background: -webkit-gradient(linear, right top, left top, from(rgba(255, 255, 255, 0)), to(var(--d2l-breadcrumbs-background-color)));
+				background: -webkit-linear-gradient(right, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				background: -o-linear-gradient(right, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				background: linear-gradient(to left, rgba(255, 255, 255, 0), var(--d2l-breadcrumbs-background-color));
+				left: 0;
+				right: auto;
+			}
+
+			.d2l-breadcrumbs-wrapper {
+				overflow: hidden;
+				position: relative;
+				white-space: nowrap;
+			}
+
+			div.compact ::slotted(d2l-breadcrumb:not(:last-of-type)) {
+				display: none;
+			}
+		`;
 	}
 
 	render() {
+		const breadCrumbClasses = {
+			'd2l-breadcrumbs-wrapper': true,
+			'd2l-input-checkbox-text-disabled': this.compact
+		};
 		return html`
-			<div>
-				<slot></slot>
+			<div class="${classMap(breadCrumbClasses)}">
+				<slot @slotchange=${this._onSlotChange}></slot>
 			</div>
 		`;
+	}
+	updated() {
+		if (this.compact) {
+			for (const child of Array.from(this.children)) {
+				child.compact = true;
+			}
+		}
+
+	}
+
+	_onSlotChange() {
+		this.requestUpdate();
 	}
 
 }
