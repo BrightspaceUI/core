f3af2de52a0edb18a2725f17899d761d5dc0d7db
Dave Batiste <dave.batiste@d2l.com>
2020-06-19 16:02:25 -0400

    feat: Initial d2l-switch and d2l-switch-visibility with on/off semantics.
    * Initial switch component.
    
    * Refactor d2l-switch to enable d2l-switch-visibility.
    
    * Update switch to handle RTL.
    
    * Reflect attributes for SwitchMixin.
    
    * Update SwitchMixin to move focus target internal.
    
    * Add a change event for SwitchMixin.
    
    * Add disabled state to SwitchMixin.
    
    * Only dispatch change event after initialization.
    
    * Update label size for switch.
    
    * Add focus method for SwitchMixin.
    
    * Warn if no label is provided for switch components.
    
    * Add readme for d2l-switch and d2l-switch-visibility.
    
    * Update accessibility comment for d2l-switch-visibility since a default label value is provided.
    
    * Add some aXe and unit tests for d2l-switch and d2l-switch-visibility.
    
    * Only make switch element focusable if enabled (same as checkbox behavior).
    
    * Add visual-diff tests for d2l-switch and d2l-switch-visibility.
    
    * Prevent internal wrapping of switch components that would cause visual layout to break.

---
+++ components/switch/test/switch-visibility.test.js
@@ -0,0 +1,10 @@
+import '../switch-visibility.js';
+import { runConstructor } from '../../../tools/constructor-test-helper.js';
+
+describe('d2l-switch-visibility', () => {
+
+	it('should construct', () => {
+		runConstructor('d2l-switch-visibility');
+	});
+
+});
