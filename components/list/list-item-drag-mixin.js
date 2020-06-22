cf55ec77700b8ec4d9edf657b4e3ac890036a6ca
BrightspaceGitHubReader <brightspacegithubreader@d2l.com>
2020-06-22 15:03:10 -0400

    Added NewPositionEventDetails

--- components/list/list-item-drag-mixin.js
+++ components/list/list-item-drag-mixin.js
@@ -1,3 +1,4 @@
+import { announce } from '@brightspace-ui/core/helpers/announce.js';
 import { getUniqueId } from '../../helpers/uniqueId.js';
 import { html } from 'lit-element/lit-element.js';
 import { nothing } from 'lit-html';
@@ -98,3 +99,63 @@ export const ListItemDragMixin = superclass => class extends superclass {
 		` : nothing;
 	}
 };
+
+export class NewPositionEventDetails {
+	constructor(object) {
+		this.node = object.node;
+		this.oldPosition = object.oldPosition;
+		this.position = object.position;
+		this.temporaryMovement = object.tempMovement;
+	}
+
+	/**
+	 * Reorders an array in place with the current event information
+	 * @param { Array } list The array to reorder
+	 * @param { function(any, Number): String } [announceCallback] Callback function that returns the announcement text
+	 *        Takes the item in the array and the index as arguments. Should return the text to announce.
+	 *        This optional callback will be passed to announceMove if given.
+	 */
+	reorder(list, announceCallback = null) {
+		if (this.position === undefined || this.position === this.oldPosition) return;
+
+		if (list[this.position] === undefined || list[this.oldPosition] === undefined ) {
+			throw new Error('Reorder failed. Position does not exist in list.');
+		}
+
+		// move the item in the list to a new position in place
+		const item = list[this.oldPosition];
+		// now that we have a reference to the item, shove everything between the
+		// new position to the oldPosition over one
+		if (this.oldPosition > this.position) {
+			// shove to the right ♪┗ ( ･o･) ┓♪
+			for (let i = this.oldPosition; i > this.position; i--) {
+				list[i] = list[i - 1];
+			}
+		} else {
+			// shove to the left ♪┏(・o･)┛♪
+			for (let i = this.oldPosition; i < this.position; i++) {
+				list[i] = list[i + 1];
+			}
+		}
+		// there is now a copy of the left or rightmost item where the new
+		// position is; stick the item in this spot.
+		list[this.position] = item;
+
+		if (announceCallback) {
+			this.announceMove(list, announceCallback);
+		}
+	}
+
+	/**
+	 * Announces a move to screen readers on an array
+	 * @param { Array } list The array to announce the move on.
+	 * @param { function(any, Number): String } announceCallback Callback function that returns the announcement text
+	 *        Takes the item in the array and the index as arguments. Should return the text to announce.
+	 */
+	announceMove(list, announceCallback) {
+		const item = list[this.position];
+		if (!item) throw new Error('Item to announce move not found in given array.');
+		const message = announceCallback(item, this.position);
+		if (message) announce(message);
+	}
+}
--- components/list/test/list-item-drag-mixin.test.js
+++ components/list/test/list-item-drag-mixin.test.js
@@ -1,6 +1,6 @@
 import { defineCE, expect, fixture } from '@open-wc/testing';
 import { html, LitElement } from 'lit-element/lit-element.js';
-import { ListItemDragMixin } from '../list-item-drag-mixin.js';
+import { ListItemDragMixin, NewPositionEventDetails } from '../list-item-drag-mixin.js';
 
 const tag = defineCE(
 	class extends ListItemDragMixin(LitElement) {
@@ -17,5 +17,95 @@ describe('ListItemDragMixin', () => {
 	it('Sets checked status to false when no key is given', async() => {
 		const element = await fixture(`<${tag} draggable="true"></${tag}>`);
 		expect(element.draggable).to.be.false;
+	});
+});
+
+describe('NewPositionEventDetails', () => {
+	describe('reorder', () => {
+		const tests = [
+			{
+				description: 'does nothing when new and old position are the same',
+				input: {
+					array: [1, 2, 3],
+					oldPosition: 1,
+					newPosition: 1
+				},
+				expected: [1, 2, 3]
+			},
+			{
+				description: 'moves item forward in the middle of an array',
+				input: {
+					array: [1, 2, 3, 4, 5, 6],
+					oldPosition: 2,
+					newPosition: 4
+				},
+				expected: [1, 2, 4, 5, 3, 6]
+			},
+			{
+				description: 'moves item backward in the middle of an array',
+				input: {
+					array: [1, 2, 3, 4, 5, 6],
+					oldPosition: 4,
+					newPosition: 2
+				},
+				expected: [1, 2, 5, 3, 4, 6]
+			},
+			{
+				description: 'moves item backward to beginning of an array',
+				input: {
+					array: [1, 2, 3, 4, 5, 6],
+					oldPosition: 4,
+					newPosition: 0
+				},
+				expected: [5, 1, 2, 3, 4, 6]
+			},
+			{
+				description: 'moves item forward to end of an array',
+				input: {
+					array: [1, 2, 3, 4, 5, 6],
+					oldPosition: 2,
+					newPosition: 5
+				},
+				expected: [1, 2, 4, 5, 6, 3]
+			},
+			{
+				description: 'moves last item to beginning of an array',
+				input: {
+					array: [1, 2, 3, 4, 5, 6],
+					oldPosition: 5,
+					newPosition: 0
+				},
+				expected: [6, 1, 2, 3, 4, 5]
+			},
+			{
+				description: 'moves first item to end of an array',
+				input: {
+					array: [1, 2, 3, 4, 5, 6],
+					oldPosition: 0,
+					newPosition: 5
+				},
+				expected: [2, 3, 4, 5, 6, 1]
+			}
+		];
+
+		for (const test of tests) {
+			it(`${test.description}`, () => {
+				const event = new NewPositionEventDetails({
+					oldPosition: test.input.oldPosition,
+					position: test.input.newPosition
+				});
+				event.reorder(test.input.array);
+				expect(test.input.array).to.deep.equal(test.expected);
+			});
+		}
+
+		it('throws an error when a position does not exist', () => {
+			const event = new NewPositionEventDetails({
+				oldPosition: 1,
+				position: 10
+			});
+			expect(() => event.reorder([1, 2, 3])).to.throw(Error);
+
+		});
 	});
 });
