9960b3e053956e5b046e4ad96bcabc1ac5b3e315
Dave Lockhart <dave.lockhart@d2l.com>
2020-06-23 13:47:20 -0400

    Fix casing of `{format}` replacement.

--- lang/ko.js
+++ lang/ko.js
@@ -7,7 +7,7 @@ export default {
 	"components.calendar.show": "{month} 표시",
 	"components.dialog.close": "이 대화 상자 닫기",
 	"components.input-date.clear": "지우기",
-	"components.input-date.openInstructions": "{Format} 날짜 형식을 사용하십시오. 미니 달력에 접근하려면 아래쪽 화살표를 누르거나 Enter 키를 누르십시오.",
+	"components.input-date.openInstructions": "{format} 날짜 형식을 사용하십시오. 미니 달력에 접근하려면 아래쪽 화살표를 누르거나 Enter 키를 누르십시오.",
 	"components.input-date.setToToday": "오늘로 설정",
 	"components.input-date-time.date": "날짜",
 	"components.input-date-time.time": "시간",
