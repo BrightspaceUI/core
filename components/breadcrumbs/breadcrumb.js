2d8026ccf62bee3400cef0a90ebf13c00bc92b62
Miad Abrin <Miad.Abrin@d2l.com>
2020-06-24 15:15:29 -0400

    reorder entries

--- components/breadcrumbs/breadcrumb.js
+++ components/breadcrumbs/breadcrumb.js
@@ -1,8 +1,8 @@
 import '../icons/icon.js';
 import '../link/link.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
-import { RtlMixin } from '../../mixins/rtl-mixin.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { RtlMixin } from '../../mixins/rtl-mixin.js';
 
 class BreadCrumb extends RtlMixin(LitElement) {
 
