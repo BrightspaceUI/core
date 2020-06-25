a85afd49b1de48fc98a7b303de55dca25f3a7d5e
Alexander Kobets <alexander.kobets@d2l.com>
2020-06-25 14:31:34 -0400

    feat: adding d2l-list-item-placement-marker
    * draft d2l-list-item-placement-marker
    
    * better calculation of svg elements dimensions
    
    * add styles getter, fix lint issues
    
    * display:none when hidden
    
    * move calculations to render method, adjust stroke-width and radius to be close to desigh picture
    
    * remove accidental change
    
    * draw with 3 svg components, use flexbox
    
    * move fill property to d2l-list-drag-marker-circle css class
    
    * add element to demo page
    
    * add missing width to d2l-list-drag-marker-line
    
    * import colors
    
    * add axe and visual-diff tests
    
    * Updating golden with changes made in branch akobets/US115500-d2l-list-item-placement-marker
    
    * adjust margins
    
    * fix lint errors
    
    * adjust circle radius, add bordered/non-bordered visual diff tests
    
    * Updating golden with changes made in branch akobets/US115500-d2l-list-item-placement-marker
    
    * add role
    
    * address review comments
    
    * workaround for axe test
    
    * fix lint errors
    
    * Updating golden with changes made in branch akobets/US115500-d2l-list-item-placement-marker
    
    * remove role from d2l-list-placement-marker
    
    Co-authored-by: BrightspaceGitHubReader <brightspacegithubreader@d2l.com>

---
+++ components/list/list-item-placement-marker.js
@@ -0,0 +1,80 @@
+import '../colors/colors.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { RtlMixin } from '../../mixins/rtl-mixin.js';
+
+class ListItemPlacementMarker extends RtlMixin(LitElement) {
+
+	static get styles() {
+		return css`
+			:host {
+				display: block;
+			}
+
+			:host([hidden]) {
+				display: none;
+			}
+
+			.d2l-list-drag-marker-line {
+				height: 12px;
+				width: 100%;
+				stroke: var(--d2l-color-celestine);
+				stroke-width: 3px;
+				margin-left: -1px;
+				margin-right: -1px;
+			}
+
+			.d2l-list-drag-marker-linecap {
+				height: 12px;
+				width: 4px;
+				stroke: none;
+				fill: var(--d2l-color-celestine);
+				margin-left: -1px;
+				margin-right: 0;
+			}
+
+			:host([dir="rtl"]) .d2l-list-drag-marker-linecap {
+				margin-left: 0;
+				margin-right: -1px;
+			}
+
+			.d2l-list-drag-marker-circle {
+				height: 12px;
+				width: 12px;
+				stroke-width: 3px;
+				stroke: var(--d2l-color-celestine);
+				fill: none;
+				margin-left: 0;
+				margin-right: -1px;
+			}
+
+			:host([dir="rtl"]) .d2l-list-drag-marker-circle {
+				margin-left: -1px;
+				margin-right: 0;
+			}
+
+			.d2l-list-drag-marker {
+				flex-wrap: nowrap;
+				display: flex;
+			}
+		`;
+	}
+
+	render() {
+		return html`
+			<div class="d2l-list-drag-marker">
+				<svg class="d2l-list-drag-marker-circle">
+					<circle cx="50%" cy="50%" r="3.8px"/>
+				</svg>
+				<svg class="d2l-list-drag-marker-line">
+					<line x1="0" y1="50%" x2="100%" y2="50%" />
+				</svg>
+				<svg class="d2l-list-drag-marker-linecap">
+					<circle cx="50%" cy="50%" r="1.5px"/>
+				</svg>
+			</div>
+		`;
+	}
+
+}
+
+customElements.define('d2l-list-item-placement-marker', ListItemPlacementMarker);
