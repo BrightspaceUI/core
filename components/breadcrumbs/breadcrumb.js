8ac7a3e253d07193566056bb1a1cd57c35d38edc
Dave Lockhart <dave.lockhart@d2l.com>
2020-06-25 13:34:16 -0400

    only setting aria-label if property is defined

--- components/breadcrumbs/breadcrumb.js
+++ components/breadcrumbs/breadcrumb.js
@@ -125,7 +125,7 @@ class Breadcrumb extends RtlMixin(LitElement) {
 	render() {
 		return html`
 			<li class="d2l-breadcrumb-wrapper" ?data-compact=${this._compact}>
-				<d2l-link aria-label="${this.ariaLabel ? this.ariaLabel : this.text}" href="${this.href}" target="${ifDefined(this.target)}">${this.text}</d2l-link>
+				<d2l-link aria-label="${ifDefined(this.ariaLabel)}" href="${this.href}" target="${ifDefined(this.target)}">${this.text}</d2l-link>
 				${this._compact ? html`<d2l-icon icon="d2l-tier1:chevron-left"></d2l-icon>` : html`<d2l-icon icon="d2l-tier1:chevron-right"></d2l-icon>`}
 			</li>
 		`;
