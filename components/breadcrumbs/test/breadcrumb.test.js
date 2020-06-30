5872e756d6c4a960ba4c8294fba4634066a09ee0
Dave Lockhart <dave.lockhart@d2l.com>
2020-06-30 18:05:23 -0400

    actually testing rtl

--- components/breadcrumbs/test/breadcrumb.test.js
+++ components/breadcrumbs/test/breadcrumb.test.js
@@ -1,42 +1,8 @@
 import '../breadcrumb.js';
 import '../breadcrumb-current-page.js';
-import { expect, fixture, html } from '@open-wc/testing';
 import { runConstructor } from '../../../tools/constructor-test-helper.js';
 
 describe('d2l-breadcrumb', () => {
-
-	let elem;
-	beforeEach(async() => {
-		elem = await fixture(html`
-			<d2l-breadcrumb></d2l-breadcrumb>
-		`);
-	});
-
-	describe('attribute reflection', () => {
-		it('should reflect "href" property to attribute', async() => {
-			elem.href = '/someurl';
-			await elem.updateComplete;
-			expect(elem.getAttribute('href')).to.equal('/someurl');
-		});
-
-		it('should reflect "text" property to attribute', async() => {
-			elem.text = 'Awesome Url';
-			await elem.updateComplete;
-			expect(elem.getAttribute('text')).to.equal('Awesome Url');
-		});
-
-		it('should reflect "target" property to attribute', async() => {
-			elem.target = 'Awesome Target';
-			await elem.updateComplete;
-			expect(elem.getAttribute('target')).to.equal('Awesome Target');
-		});
-
-		it('should reflect "ariaLabel" property to attribute', async() => {
-			elem.ariaLabel = 'Awesome Label';
-			await elem.updateComplete;
-			expect(elem.getAttribute('aria-label')).to.equal('Awesome Label');
-		});
-	});
 
 	describe('constructor', () => {
 
@@ -48,16 +14,6 @@ describe('d2l-breadcrumb', () => {
 			runConstructor('d2l-breadcrumb-current-page');
 		});
 
-	});
-
-	describe('default property values', () => {
-		it('should default "text" property to "" when unset', () => {
-			expect(elem.text).to.equal('');
-		});
-
-		it('should default "_compact" property to "false" when unset', () => {
-			expect(elem._compact).to.be.false;
-		});
 	});
 
 });
