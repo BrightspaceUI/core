d4552acbd4a8a6dd89d9aa0920e1e78c10fad686
Margaree Peacocke <margaree.p@gmail.com>
2020-07-07 14:57:15 -0400

    remove unnecessary id, override label getter

--- components/inputs/input-date.js
+++ components/inputs/input-date.js
@@ -9,7 +9,6 @@ import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { formatDate, parseDate } from '@brightspace-ui/intl/lib/dateTime.js';
 import { formatDateInISO, getDateFromISODate, getDateTimeDescriptorShared, getToday } from '../../helpers/dateTime.js';
 import { FormElementMixin } from '../form/form-element-mixin.js';
-import { getUniqueId } from '../../helpers/uniqueId.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { LocalizeCoreElement } from '../../lang/localize-core-element.js';
 import { styleMap } from 'lit-html/directives/style-map.js';
@@ -117,7 +116,6 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 		this._dropdownOpened = false;
 		this._formattedValue = '';
 		this._hiddenContentWidth = '8rem';
-		this._inputId =  getUniqueId();
 		this._namespace = 'components.input-date';
 		this._shownValue = '';
 
@@ -225,6 +223,10 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 
 	focus() {
 		if (this._textInput) this._textInput.focus();
+	}
+
+	get label() {
+		return this.label;
 	}
 
 	get validationMessageRangeOverflow() {
