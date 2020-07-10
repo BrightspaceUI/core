6b2f1bfd137cd7c74b720122be2c82b8f2dec499
Margaree Peacocke <margaree.p@gmail.com>
2020-07-10 11:13:44 -0400

    code review feedback: use aria-label property instead of tracking valid state

--- components/inputs/input-date.js
+++ components/inputs/input-date.js
@@ -26,6 +26,10 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 	static get properties() {
 		return {
 			/**
+			 * Indicates that the input value is invalid
+			 */
+			ariaInvalid: { type: String, attribute: 'aria-invalid', reflect: true },
+			/**
 			 * Disables the input
 			 */
 			disabled: { type: Boolean },
@@ -54,7 +58,6 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 			 */
 			value: { type: String },
 			_hiddenContentWidth: { type: String },
-			_invalidInput: { type: Boolean },
 			_dateTimeDescriptor: { type: Object },
 			_dropdownOpened: { type: Boolean },
 			_formattedValue: { type: String },
@@ -117,7 +120,6 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 		this._dropdownOpened = false;
 		this._formattedValue = '';
 		this._hiddenContentWidth = '8rem';
-		this._invalidInput = false;
 		this._namespace = 'components.input-date';
 		this._shownValue = '';
 
@@ -165,7 +167,7 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 			</div>
 			<d2l-dropdown ?disabled="${this.disabled}" no-auto-open>
 				<d2l-input-text
-					aria-invalid="${this._invalidInput ? 'true' : 'false'}"
+					aria-invalid="${this.ariaInvalid}"
 					atomic="true"
 					@change="${this._handleChange}"
 					class="d2l-dropdown-opener"
@@ -182,9 +184,9 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 					title="${this.localize(`${this._namespace}.openInstructions`, {format: shortDateFormat})}"
 					.value="${this._formattedValue}">
 					<d2l-icon
-						icon="${this._invalidInput ? 'tier1:alert' : 'tier1:calendar'}"
+						icon="${this.ariaInvalid === 'true' ? 'tier1:alert' : 'tier1:calendar'}"
 						slot="left"
-						style="${styleMap({color: this._invalidInput ? 'var(--d2l-color-cinnabar)' : ''})}"></d2l-icon>
+						style="${styleMap({color: this.ariaInvalid === 'true' ? 'var(--d2l-color-cinnabar)' : ''})}"></d2l-icon>
 				</d2l-input-text>
 				<d2l-dropdown-content
 					@d2l-dropdown-close="${this._handleDropdownClose}"
@@ -362,11 +364,7 @@ class InputDate extends FormElementMixin(LocalizeCoreElement(LitElement)) {
 			rangeOverflow: dateInISO && this.maxValue && getDateFromISODate(dateInISO).getTime() > getDateFromISODate(this.maxValue).getTime()
 		});
 		const errors = await this.validate();
-		if (errors.length > 0) {
-			this._invalidInput = true;
-			return;
-		}
-		this._invalidInput = false;
+		if (errors.length > 0) return;
 		this.value = dateInISO;
 		this.dispatchEvent(new CustomEvent(
 			'change',
