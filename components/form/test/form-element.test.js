55689143a29d2b5a6d95077cc78792325d9983f7
Allan Kerr <allan.kerr@D2L.com>
2020-07-15 15:26:02 -0400

    Add showErrors parameter to validate
    This allows validate to be called with the purpose of only hiding the error message on validate.
    If a new error occurs and showErrors is false then the component will not be put in an invalid state.
    This allows errors to be shown when requestValidate(true) is called on blur.

--- components/form/test/form-element.test.js
+++ components/form/test/form-element.test.js
@@ -67,16 +67,32 @@ describe('form-element', () => {
 	describe('message', () => {
 
 		it('should set validation message if validate has errors', async() => {
-			await formElement.validate();
+			await formElement.validate(true);
 			expect(formElement.validationError).to.equal('Test form element is required.');
 		});
 
-		it('should remove message if validate has no errors', async() => {
-			await formElement.validate();
+		it('should not set validation message if validate has errors and show errors is false', async() => {
+			await formElement.validate(false);
+			expect(formElement.validationError).to.be.null;
+		});
+
+		[true, false].forEach(showErrors => {
+			it('should remove message if validate has no errors', async() => {
+				await formElement.validate(true);
+				expect(formElement.validationError).to.equal('Test form element is required.');
+				formElement.value = 'Non-empty';
+				await formElement.validate(showErrors);
+				expect(formElement.validationError).to.null;
+			});
+		});
+
+		it('should update the validation message if validate has errors and show errors is false', async() => {
+			await formElement.validate(true);
 			expect(formElement.validationError).to.equal('Test form element is required.');
 			formElement.value = 'Non-empty';
-			await formElement.validate();
-			expect(formElement.validationError).to.null;
+			formElement.isValidationCustomValid = false;
+			await formElement.validate(false);
+			expect(formElement.validationError).to.equal('Internal custom validation failed');
 		});
 
 	});
@@ -85,13 +101,13 @@ describe('form-element', () => {
 
 		it('should validate internal validation-customs', async() => {
 			formElement.isValidationCustomValid = false;
-			const errors = await formElement.validate();
+			const errors = await formElement.validate(true);
 			expect(errors).to.include.members(['Internal custom validation failed']);
 		});
 
 		it('should validate external validation-customs', async() => {
 			form.isValidationCustomValid = false;
-			const errors = await formElement.validate();
+			const errors = await formElement.validate(true);
 			expect(errors).to.include.members(['External custom validation failed']);
 		});
 
@@ -103,28 +119,35 @@ describe('form-element', () => {
 		it('should validate with default validity state message', async() => {
 			formElement.value = 'Non-empty';
 			formElement.setValidity({ badInput: true });
-			const errors = await formElement.validate();
+			const errors = await formElement.validate(true);
 			expect(errors).to.include.members(['Test form element is invalid.']);
 		});
 
 		it('should validate with overridden validity state message', async() => {
 			formElement.value = 'Non-empty';
 			formElement.setValidity({ rangeOverflow: true });
-			const errors = await formElement.validate();
+			const errors = await formElement.validate(true);
 			expect(errors).to.include.members(['Test form element failed with an overridden validation message']);
 		});
 
 		it('should validate with custom validity state message', async() => {
 			formElement.value = 'Non-empty';
 			formElement.setCustomValidity('Validation failed for custom validity');
-			const errors = await formElement.validate();
+			const errors = await formElement.validate(true);
 			expect(errors).to.include.members(['Validation failed for custom validity']);
 		});
 
 		it('should pass validation when no errors', async() => {
 			formElement.value = 'Non-empty';
-			const errors = await formElement.validate();
+			const errors = await formElement.validate(true);
 			expect(errors).to.be.empty;
+		});
+
+		it('should not be marked as invalid when show errors is false', async() => {
+			const errors = await formElement.validate(false);
+			expect(errors).to.not.be.empty;
+			expect(formElement.invalid).to.be.false;
+			expect(formElement.validationError).to.be.null;
 		});
 
 	});
@@ -133,7 +156,7 @@ describe('form-element', () => {
 
 		it('should not validate if canceled', async() => {
 			formElement.addEventListener('d2l-form-element-should-validate', e => e.preventDefault());
-			await formElement.requestValidate();
+			await formElement.requestValidate(true);
 			expect(formElement.validationError).to.be.null;
 		});
 
