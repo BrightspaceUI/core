b9402dc3147de38a46eb57f70e89d38f758defc5
Margaree Peacocke <margaree.p@gmail.com>
2020-07-15 15:20:11 -0400

    work to fix sauce flakiness (#697)

--- karma.sauce.conf.js
+++ karma.sauce.conf.js
@@ -56,10 +56,10 @@ module.exports = config => {
 			browsers: Object.keys(customLaunchers),
 			reporters: ['dots', 'saucelabs'],
 			singleRun: true,
-			browserDisconnectTimeout : 10000, // default 2000
-			browserDisconnectTolerance : 1, // default 0
-			browserNoActivityTimeout: 120000, // default 10000
-			captureTimeout: 120000, // default 60000
+			browserDisconnectTimeout : 20000, // default 2000
+			browserDisconnectTolerance : 2, // default 0
+			browserNoActivityTimeout: 200000, // default 10000
+			captureTimeout: 200000, // default 60000
 			client: {
 				mocha: {
 					timeout : 10000
