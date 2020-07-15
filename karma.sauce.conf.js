57e94e3a126c160725ae91aaad1751b8222b52b3
Margaree Peacocke <margaree.p@gmail.com>
2020-07-15 09:28:40 -0400

    bump browserDisconnectTimeout

--- karma.sauce.conf.js
+++ karma.sauce.conf.js
@@ -56,7 +56,7 @@ module.exports = config => {
 			browsers: Object.keys(customLaunchers),
 			reporters: ['dots', 'saucelabs'],
 			singleRun: true,
-			browserDisconnectTimeout : 10000, // default 2000
+			browserDisconnectTimeout : 20000, // default 2000
 			browserDisconnectTolerance : 1, // default 0
 			browserNoActivityTimeout: 120000, // default 10000
 			captureTimeout: 120000, // default 60000
