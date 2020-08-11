21d03e8d2715a4c643d994f215d538bff2109429
Allan Kerr <allan.kerr@D2L.com>
2020-08-11 12:47:46 -0400

    Add d2l-form component
    This component supports nesting forms and delegates data submission to the user

---
+++ components/form/form.js
@@ -0,0 +1,142 @@
+import { findFormElements, flattenMap, getFormElementData, isCustomFormElement, isNativeFormElement } from './form-helper.js';
+import { html, LitElement } from 'lit-element/lit-element.js';
+import { FormMixin } from './form-mixin.js';
+import { ValidationType } from './form-element-mixin.js';
+
+class Form extends FormMixin(LitElement) {
+
+	static get properties() {
+		return {
+			noNesting: { type: Boolean, attribute: 'no-nesting', reflect: true },
+		};
+	}
+
+	constructor() {
+		super();
+		this._isSubForm = false;
+		this._nestedForms = new Map();
+
+		this.addEventListener('d2l-form-connect', this._onFormConnect);
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
+		this._isSubForm = !this.dispatchEvent(new CustomEvent('d2l-form-connect', { bubbles: true, composed: true, cancelable: true }));
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		this.dispatchEvent(new CustomEvent('d2l-form-disconnected'));
+		this._isSubForm = false;
+	}
+
+	render() {
+		let errorSummary = null;
+		if (this._isRootForm()) {
+			const errors = [...flattenMap(this._errors)]
+				.filter(([, eleErrors]) => eleErrors.length > 0)
+				.map(([ele, eleErrors]) => ({ href: `#${ele.id}`, message: eleErrors[0], onClick: () => ele.focus() }));
+			errorSummary = html`<d2l-form-error-summary .errors=${errors}></d2l-form-error-summary>`;
+		}
+		return html`
+			${errorSummary}
+			<slot></slot>
+		`;
+	}
+
+	async requestSubmit(submitter) {
+		const errors = await this.validate();
+		if (errors.size > 0) {
+			return;
+		}
+		this._submitData(submitter);
+	}
+
+	async submit() {
+		return this.requestSubmit(null);
+	}
+
+	async validate() {
+		const errorMap = new Map();
+		const formElements = this._findFormElements();
+		for (const ele of formElements) {
+			if (this._hasSubForm(ele)) {
+				const form = this._getSubForm(ele);
+				if (!form.noNesting) {
+					const formErrors = await form.validate();
+					errorMap.set(ele, formErrors);
+				}
+			} else {
+				const eleErrors = await this._validateFormElement(ele, ValidationType.SHOW_NEW_ERRORS);
+				if (eleErrors.length > 0) {
+					errorMap.set(ele, eleErrors);
+				}
+			}
+		}
+		const flattenedErrorMap = flattenMap(errorMap);
+		this._errors = errorMap;
+		if (errorMap.size > 0) {
+			const errorSummary = this.shadowRoot.querySelector('d2l-form-error-summary');
+			if (errorSummary) {
+				this.updateComplete.then(() => errorSummary.focus());
+			}
+			this.dispatchEvent(new CustomEvent('d2l-form-invalid', { detail: { errors: flattenedErrorMap } }));
+		}
+		return flattenedErrorMap;
+	}
+
+	_findFormElements() {
+		const isFormElementPredicate = ele => this._hasSubForm(ele);
+		const visitChildrenPredicate = ele => !this._hasSubForm(ele);
+		return findFormElements(this, isFormElementPredicate, visitChildrenPredicate);
+	}
+
+	_getSubForm(ele) {
+		return this._nestedForms.get(ele);
+	}
+
+	_hasSubForm(ele) {
+		return this._nestedForms.has(ele);
+	}
+
+	_isRootForm() {
+		return !this._isSubForm || this.noNesting;
+	}
+
+	_onFormConnect(e) {
+		if (e.target === this) {
+			return;
+		}
+		e.stopPropagation();
+		e.preventDefault();
+		const form = e.composedPath()[0];
+		this._nestedForms.set(e.target, form);
+
+		const onFormDisconnect = () => {
+			form.removeEventListener('d2l-form-disconnect', onFormDisconnect);
+			this._nestedForms.delete(e.target);
+		};
+		form.addEventListener('d2l-form-disconnect', onFormDisconnect);
+	}
+
+	async _submitData(submitter) {
+		this._dirty = false;
+
+		let formData = {};
+		const formElements = this._findFormElements(this);
+		for (const ele of formElements) {
+			const eleData = getFormElementData(ele, submitter);
+			if (isCustomFormElement(ele) || isNativeFormElement(ele)) {
+				formData = { ...formData, ...eleData };
+			} else if (this._hasSubForm(ele)) {
+				const form = this._getSubForm(ele);
+				if (!form.noNesting) {
+					form._submitData(submitter);
+				}
+			}
+		}
+		this.dispatchEvent(new CustomEvent('d2l-form-submit', { detail: { formData } }));
+	}
+
+}
+customElements.define('d2l-form', Form);
