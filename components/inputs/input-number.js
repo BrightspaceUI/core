7f1579a7fd36599dff1dcc80402312817d4df7c8
Krishan Patel <krishan.patel@d2l.com>
2020-08-13 14:23:28 -0400

    Localize and format number inputs

--- components/inputs/input-number.js
+++ components/inputs/input-number.js
@@ -1,9 +1,18 @@
 import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { formatNumber, parseNumber } from '@brightspace-ui/intl/lib/number.js';
 import { getUniqueId } from '../../helpers/uniqueId.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { inputLabelStyles } from './input-label-styles.js';
 import { inputStyles } from './input-styles.js';
 import { offscreenStyles } from '../offscreen/offscreen.js';
+
+function formatValue(value, minFractionDigits, maxFractionDigits) {
+	const options = {
+		maximumFractionDigits: maxFractionDigits,
+		minimumFractionDigits: minFractionDigits
+	};
+	return formatNumber(value, options);
+}
 
 class InputNumber extends LitElement {
 
@@ -15,12 +24,15 @@ class InputNumber extends LitElement {
 			label: { type: String },
 			labelHidden: { type: Boolean, attribute: 'label-hidden' },
 			max: { type: Number },
+			maxFractionDigits: { type: Number, attribute: 'max-fraction-digits' },
 			min: { type: Number },
+			minFractionDigits: { type: Number, attribute: 'min-fraction-digits' },
 			name: { type: String },
 			placeholder: { type: String },
 			required: { type: Boolean },
 			step: { type: String },
-			value: { type: Number }
+			value: { type: Number },
+			_formattedValue: { type: String }
 		};
 	}
 
@@ -45,7 +57,15 @@ class InputNumber extends LitElement {
 		this.labelHidden = false;
 		this.required = false;
 
+		this._formattedValue = '';
 		this._inputId = getUniqueId();
+	}
+
+	firstUpdated(changedProperties) {
+		super.firstUpdated(changedProperties);
+		if (this.value !== undefined && this.value !== null) {
+			this._formattedValue = formatValue(this.value, this.minFractionDigits, this.maxFractionDigits);
+		}
 	}
 
 	render() {
@@ -68,9 +88,21 @@ class InputNumber extends LitElement {
 				name="${ifDefined(this.name)}"
 				placeholder="${ifDefined(this.placeholder)}"
 				step="${ifDefined(this.step)}"
-				type="number"
-				.value="${this.value}">
+				type="text"
+				.value="${this._formattedValue}">
 		`;
+	}
+
+	_handleChange(e) {
+		const inputValue = e.target.value.trim();
+		this.value = inputValue !== '' ? parseNumber(inputValue) : undefined;
+
+		if (!isNaN(this.value) && this.value !== undefined) {
+			this._formattedValue = formatValue(this.value, this.minFractionDigits, this.maxFractionDigits);
+			e.target.value = this._formattedValue;
+		} else {
+			e.target.value = '';
+		}
 	}
 }
 customElements.define('d2l-input-number', InputNumber);
