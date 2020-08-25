fe0ac9d79ce6758130aba83b5fe771a814070dd9
Dave Batiste <dave.batiste@d2l.com>
2020-08-25 11:56:54 -0400

    Add visual-diff tests for dropdown and tooltip usages with card.

--- components/card/test/card.visual-diff.js
+++ components/card/test/card.visual-diff.js
@@ -1,5 +1,7 @@
+
 const puppeteer = require('puppeteer');
 const VisualDiff = require('@brightspace-ui/visual-diff');
+const dropdownHelper = require('../../dropdown/test/dropdown-helper.js');
 
 describe('d2l-card', () => {
 
@@ -27,18 +29,25 @@ describe('d2l-card', () => {
 		{ name: 'badge', selector: '#badge' },
 		{ name: 'actions', selector: '#actions' },
 		{ name: 'actions-focus', selector: '#actions', action: (selector) => page.$eval(`${selector} > d2l-button-icon`, (elem) => elem.focus()) },
-		{ name: 'actions-voa', selector: '#actions-voa' },
 		{ name: 'actions-rtl', selector: '#actions-rtl' },
 		{ name: 'no-link-focus', selector: '#header-content', action: (selector) => page.$eval(selector, (elem) => elem.focus()) },
 		{ name: 'subtle', selector: '#subtle' },
 		{ name: 'link', selector: '#link' },
 		{ name: 'link-focus', selector: '#link', action: (selector) => page.$eval(selector, (elem) => elem.focus()) },
 		{ name: 'link-actions-focus', selector: '#link', action: (selector) => page.$eval(`${selector} > d2l-button-icon`, (elem) => elem.focus()) },
-		{ name: 'link-footer-focus', selector: '#link', action: (selector) => page.$eval(`${selector} > d2l-button`, (elem) => elem.focus()) }
+		{ name: 'link-footer-focus', selector: '#link', action: (selector) => page.$eval(`${selector} > d2l-button`, (elem) => elem.focus()) },
+		{ name: 'with-dropdown', selector: '#with-dropdown', margin: 20 },
+		{ name: 'with-dropdown-open', selector: '#with-dropdown', margin: 20, action: (selector) => dropdownHelper.open(page, `${selector} d2l-dropdown-more`) },
+		{ name: 'with-dropdown-adjacent-hover', selector: '#with-dropdown-adjacent-hover', margin: 20, action: async (selector) =>  {
+			await dropdownHelper.open(page, `${selector} d2l-dropdown-more`);
+			return page.hover(`${selector} #hover-target`);
+		} },
+		{ name: 'with-tooltip', selector: '#with-tooltip', margin: 20 },
+		{ name: 'with-tooltip-focus', selector: '#with-tooltip', margin: 20, action: (selector) => page.$eval(`${selector} #shiny-button`, (elem) => elem.focus()) }
 	].forEach((info) => {
 
 		it(info.name, async function() {
-			const rect = await visualDiff.getRect(page, info.selector);
+			const rect = await visualDiff.getRect(page, info.selector, info.margin);
 			if (info.action) await info.action(info.selector);
 			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 		});
