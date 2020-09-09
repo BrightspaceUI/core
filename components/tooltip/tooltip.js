9f2769cadc88cf82c5961adb014754b7e3927eaf
Allan Kerr <allan.kerr@D2L.com>
2020-09-09 17:14:32 -0400

    Adjust pointer sizing to account for the 1px border

--- components/tooltip/tooltip.js
+++ components/tooltip/tooltip.js
@@ -179,8 +179,11 @@ class Tooltip extends RtlMixin(LitElement) {
 
 			.d2l-tooltip-pointer {
 				border: 1px solid transparent; /* fixes a webket clipping defect */
+				box-sizing: border-box;
 				display: inline-block;
+				height: ${pointerLength}px;
 				position: absolute;
+				width: ${pointerLength}px;
 				z-index: 1;
 			}
 
@@ -238,6 +241,9 @@ class Tooltip extends RtlMixin(LitElement) {
 				border-radius: 0.1rem;
 				box-sizing: border-box;
 				height: ${pointerLength}px;
+				left: -1px;
+				position: absolute;
+				top: -1px;
 				-webkit-transform: rotate(45deg);
 				transform: rotate(45deg);
 				width: ${pointerLength}px;
