961a920fa89138057952a389fd50d56d693395af
Margaree Peacocke <margaree.p@gmail.com>
2020-09-24 17:20:35 -0400

    bump height for input-time-range visual diffs

--- components/inputs/test/input-time-range.visual-diff.js
+++ components/inputs/test/input-time-range.visual-diff.js
@@ -10,7 +10,7 @@ describe('d2l-input-time-range', () => {
 
 	before(async() => {
 		browser = await puppeteer.launch();
-		page = await visualDiff.createPage(browser, { viewport: { width: 800, height: 1500 } });
+		page = await visualDiff.createPage(browser, { viewport: { width: 800, height: 1600 } });
 		await page.goto(`${visualDiff.getBaseUrl()}/components/inputs/test/input-time-range.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
 	});
