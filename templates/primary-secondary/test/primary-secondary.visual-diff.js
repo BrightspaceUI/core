d4012848aad9db4748d926c5458baa834a8af12c
Allan Kerr <allan.kerr@D2L.com>
2020-10-16 15:40:37 -0400

    Update primary-secondary template visual-diff tests

--- templates/primary-secondary/test/primary-secondary.visual-diff.js
+++ templates/primary-secondary/test/primary-secondary.visual-diff.js
@@ -3,32 +3,125 @@ const VisualDiff = require('@brightspace-ui/visual-diff');
 
 describe('d2l-template-primary-secondary', () => {
 	const visualDiff = new VisualDiff('primary-secondary', __dirname);
-	let browser;
-	let page;
 
-	before(async() => {
-		browser = await puppeteer.launch();
-		page = await visualDiff.createPage(browser);
+	let browser, page;
+
+	const directions = Object.freeze({
+		LEFT: 37,
+		UP: 38,
+		RIGHT: 39,
+		DOWN: 40
 	});
 
-	after(async() => await browser.close());
+	async function moveDivider(page, selector, dir, steps) {
+		return page.$eval(selector, (ele, dir, steps) => {
+			const handle = ele.shadowRoot.querySelector('.d2l-template-primary-secondary-divider-handle');
+			const e = new KeyboardEvent('keydown', { keyCode: dir });
+			for (let i = 0; i < steps; i += 1) {
+				handle.dispatchEvent(e);
+			}
+		}, dir, steps);
+	}
 
-	describe('desktop', () => {
+	async function getRect(page, selector) {
+		return page.$eval(selector, (elem) => {
+			const rect = elem.getBoundingClientRect();
+			return {
+				x: rect.x,
+				y: rect.y,
+				width: rect.width,
+				height: rect.height
+			};
+		});
+	}
+
+	async function focusHandle(page, selector) {
+		return page.$eval(selector, (elem) => {
+			const handle = elem.shadowRoot.querySelector('.d2l-template-primary-secondary-divider-handle');
+			handle.focus();
+		});
+	}
+
+	describe('mobile', () => {
+
 		before(async() => {
-			await page.setViewport({ width: 1400, height: 930, deviceScaleFactor: 2 });
+			browser = await puppeteer.launch();
+			page = await visualDiff.createPage(browser, { viewport: { width: 768, height: 2000 } });
+			await page.goto(`${visualDiff.getBaseUrl()}/templates/primary-secondary/test/primary-secondary-mobile.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
+			await page.bringToFront();
 		});
 
+		after(async() => await browser.close());
+
 		[
-			{ name: 'normal-width', fileName: 'primary-secondary-desktop-normal-width.visual-diff.html' },
-			{ name: 'larger-than-viewport-height', fileName: 'primary-secondary-desktop-large.visual-diff.html' },
-			{ name: 'footer-hidden', fileName: 'primary-secondary-desktop-footer-hidden.visual-diff.html' },
-			{ name: 'background-shading-primary', fileName: 'primary-secondary-desktop-background-shading-primary.visual-diff.html' },
-			{ name: 'background-shading-secondary', fileName: 'primary-secondary-desktop-background-shading-secondary.visual-diff.html' }
-		].forEach((entry) => {
-			it(entry.name, async function() {
-				await page.goto(`${visualDiff.getBaseUrl()}/templates/primary-secondary/test/${entry.fileName}`, { waitUntil: ['networkidle0', 'load'] });
+			{ testName: 'default', options: {} },
+			{ testName: 'focus', options: { focus: true } },
+			{ testName: 'rtl', options: {} },
+			{ testName: 'focus-rtl', options: { focus: true } },
+			{ testName: 'expanded', options: { position: { dir: directions.UP, steps: 5 } } },
+			{ testName: 'middle', options: { position: { dir: directions.UP, steps: 1 } } },
+			{ testName: 'collapsed', options: { position: { dir: directions.DOWN, steps: 5 } } },
+			{ testName: 'hidden-footer', options: {} },
+			{ testName: 'hidden-footer-expanded', options: { position: { dir: directions.UP, steps: 5 } } },
+			{ testName: 'hidden-footer-collapsed', options: { position: { dir: directions.DOWN, steps: 5 } } },
+		].forEach((test) => {
+			it(test.testName, async function() {
 				await page.bringToFront();
-				const rect = await visualDiff.getRect(page, 'd2l-template-primary-secondary', 0);
+				const sel = `#${test.testName}`;
+				if (test.options.position) {
+					const pos = test.options.position;
+					await moveDivider(page, sel, pos.dir, pos.n);
+				}
+				if (test.options.focus) {
+					await focusHandle(page, sel);
+				}
+				const rect = await getRect(page, sel);
+				await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+			});
+		});
+	});
+
+	describe('desktop', () => {
+
+		before(async() => {
+			browser = await puppeteer.launch();
+			page = await visualDiff.createPage(browser, { viewport: { width: 1500, height: 4000 } });
+			await page.goto(`${visualDiff.getBaseUrl()}/templates/primary-secondary/test/primary-secondary-desktop.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
+			await page.bringToFront();
+		});
+
+		after(async() => await browser.close());
+
+		[
+			{ testName: 'fixed', options: {} },
+			{ testName: 'resizable', options: {} },
+			{ testName: 'expanded', options: { position: { dir: directions.LEFT, steps: 10 } } },
+			{ testName: 'collapsed', options: { position: { dir: directions.RIGHT, steps: 5 } } },
+			{ testName: 'focus', options: { focus: true } },
+			{ testName: 'focus-expanded', options: { focus: true, position: { dir: directions.LEFT, steps: 10 } } },
+			{ testName: 'focus-collapsed', options: { focus: true, position: { dir: directions.RIGHT, steps: 5 } } },
+			{ testName: 'focus-collapsed-rtl', options: { focus: true, position: { dir: directions.LEFT, steps: 5 } } },
+			{ testName: 'focus-expanded-rtl', options: { focus: true, position: { dir: directions.RIGHT, steps: 10 } } },
+			{ testName: 'background-shading-primary', options: {} },
+			{ testName: 'background-shading-primary-rtl', options: {} },
+			{ testName: 'background-shading-secondary', options: {} },
+			{ testName: 'background-shading-secondary-rtl', options: {} },
+			{ testName: 'hidden-footer', options: {} },
+			{ testName: 'width-fullscreen', options: {} },
+			{ testName: 'width-normal', options: {} },
+			{ testName: 'width-normal-collapsed', options: { focus: true, position: { dir: directions.RIGHT, steps: 5 } } },
+		].forEach((test) => {
+			it(test.testName, async function() {
+				await page.bringToFront();
+				const sel = `#${test.testName}`;
+				if (test.options.position) {
+					const pos = test.options.position;
+					await moveDivider(page, sel, pos.dir, pos.steps);
+				}
+				if (test.options.focus) {
+					await focusHandle(page, sel);
+				}
+				const rect = await getRect(page, sel);
 				await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 			});
 		});
