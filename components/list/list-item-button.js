3102587999197b1911999166d5b6c9c622b084ec
Dave Batiste <dave.batiste@d2l.com>
2020-11-12 19:25:46 -0500

    Add ListItemButton mixin and d2l-list-item-button component.

---
+++ components/list/list-item-button.js
@@ -0,0 +1,21 @@
+import { ListItemButtonMixin } from './list-item-button-mixin.js';
+import { ListItemMixin } from './list-item-mixin.js';
+import { LitElement } from 'lit-element/lit-element.js';
+
+/**
+ * A component for a "listitem" child within a list. It provides semantics, basic layout, breakpoints for responsiveness, a link for navigation, and selection.
+ * @slot - Default content placed inside of the component
+ * @slot illustration - Image associated with the list item located at the left of the item
+ * @slot actions - Actions (e.g., button icons) associated with the listen item located at the right of the item
+ * @fires d2l-list-item-button-click - Dispatched when the component item is clicked
+ * @fires d2l-list-item-selected - Dispatched when the component item is selected
+ */
+class ListItemButton extends ListItemButtonMixin(ListItemMixin(LitElement)) {
+
+	render() {
+		return this._renderListItem();
+	}
+
+}
+
+customElements.define('d2l-list-item-button', ListItemButton);
