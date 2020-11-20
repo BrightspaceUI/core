5f45bea176fdf1df6d68c93e233cd01d2e4a48ec
Margaree Peacocke <margaree.p@gmail.com>
2020-11-20 11:42:21 -0500

    more tweaks

--- web-test-runner.bs.config.mjs
+++ web-test-runner.bs.config.mjs
@@ -4,9 +4,11 @@ import { browserstackLauncher } from '@web/test-runner-browserstack';
 const sharedCapabilities = {
 	'browserstack.user': process.env.BROWSERSTACK_USERNAME,
 	'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
+	'browserstack.acceptSslCerts': true,
 	project: 'Brightspace UI Core Unit Tests',
 	name: 'Unit tests',
 	build: process.env.TRAVIS_BUILD_NUMBER,
+	acceptSslCerts: true,
 };
 
 const customLaunchers = [
