a51899683dd84be210b3365748dc6f810c2d523d
Margaree Peacocke <margaree.p@gmail.com>
2021-02-02 14:20:47 -0500

    fix: input-time: resize input based on largest input (#1089)

--- components/inputs/test/input-time-range.visual-diff.js
+++ components/inputs/test/input-time-range.visual-diff.js
@@ -10,7 +10,7 @@ describe('d2l-input-time-range', () => {
 
 	before(async() => {
 		browser = await puppeteer.launch();
-		page = await visualDiff.createPage(browser, { viewport: { width: 800, height: 2100 } });
+		page = await visualDiff.createPage(browser, { viewport: { width: 800, height: 2200 } });
 		await page.goto(`${visualDiff.getBaseUrl()}/components/inputs/test/input-time-range.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
 	});
