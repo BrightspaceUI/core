6658c4090d824471c7827c4e82fccb3b4efe106f
Margaree Peacocke <margaree.p@gmail.com>
2021-02-10 11:09:33 -0500

    dialog: remove await for scrolling

--- components/dialog/test/dialog.visual-diff.js
+++ components/dialog/test/dialog.visual-diff.js
@@ -95,7 +95,7 @@ describe('d2l-dialog', () => {
 
 				it('scroll top shadow', async function() {
 					await helper.open(page, '#dialogLong');
-					await page.$eval('#dialogLong #bottom', (bottom) => {
+					page.$eval('#dialogLong #bottom', (bottom) => {
 						bottom.scrollIntoView();
 					});
 					await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
