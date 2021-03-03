cd16a69901bd72818db0b9848dc3796f6c1ff742
Andy Wang <Andy.Wang@D2L.com>
2021-03-03 18:05:31 -0500

    clearer demo purposes (?)

--- mixins/demo/visibility-test-helper.js
+++ mixins/demo/visibility-test-helper.js
@@ -3,13 +3,6 @@ import './visibility-test.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 
 export class VisibilityTestHelper extends LitElement {
-	static get properties() {
-		return {
-			mixinMargin: { type: String, attribute: 'mixin-margin' },
-			mixinOpacity: { type: String, attribute: 'mixin-opacity' }
-		};
-	}
-
 	static get styles() {
 		return css`
 			:host {
@@ -20,17 +13,17 @@ export class VisibilityTestHelper extends LitElement {
 
 	constructor() {
 		super();
-		this.el = document.createElement('d2l-visibility-test');
 	}
 
 	render() {
-		if (this.mixinMargin) {
-			this.el.style.display = 'grid';
-			this.el.style.margin = this.mixinMargin;
+		// bind these helper functions to 'd2l-visibility-test' component if one already exists
+		if (this.parentElement.querySelector('d2l-visibility-test')) {
+			this.el = this.parentElement.querySelector('d2l-visibility-test');
+			this.el.animate = 'show'; // el is already displayed on the page
+		} else {
+			this.el = document.createElement('d2l-visibility-test');
 		}
-		if (this.mixinOpacity) {
-			this.el.style.opacity = this.mixinOpacity;
-		}
+
 		return html`
 			<d2l-button @click='${this.showItem}'>Show element</d2l-button>
 			<d2l-button @click='${this.hideItem}'>Hide element</d2l-button>
