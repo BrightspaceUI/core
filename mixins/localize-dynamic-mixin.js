3d3a92c2c235a495e422065616a003b6ac1c344e
Danny Gleckler <daniel.gleckler@d2l.com>
2021-03-04 17:56:48 -0500

    fix: Remove missing lang file warning (#1188)

--- mixins/localize-dynamic-mixin.js
+++ mixins/localize-dynamic-mixin.js
@@ -3,31 +3,15 @@ import { LocalizeMixin } from './localize-mixin.js';
 
 const fallbackLang = 'en';
 
-function warnMissingFiles(failures, resolvedLang) {
-	const { path } = failures[0].err.message.match(/https?:\/\/.*?\/(?<path>.*)\/.*\./).groups;
-	const langs = failures.map(f => f.lang).join('","');
-	console.warn(`Failed to load translation resources for languages "${langs}" from ${path}. Falling back to "${resolvedLang}".`);
-}
-
 export const LocalizeDynamicMixin = superclass => class extends LocalizeMixin(superclass) {
 
 	static async getLocalizeResources(langs, { importFunc, osloCollection }) {
-		const missingFiles = [];
 
 		for (const lang of [...langs, fallbackLang]) {
 
-			const resources = await importFunc(lang).catch(err => {
-				// TypeErrors are unsupported languages that fail to be imported (404). Report them.
-				// Errors are unsupported languages that are never imported and fall through. Supress them.
-				// In either case fallback languages are used (e.g. gd-ie -> gd -> en)
-				if (err.constructor === TypeError) missingFiles.push({ lang, err });
-			});
+			const resources = await importFunc(lang).catch(() => {});
 
 			if (resources) {
-
-				if (missingFiles.length) {
-					warnMissingFiles(missingFiles, lang);
-				}
 
 				if (osloCollection) {
 					return await getLocalizeOverrideResources(
