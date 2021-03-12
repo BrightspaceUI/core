48fd743b9abef96a6ebe486484e6b71e0f66f915
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-03-12 16:13:01 -0500

    using lists directly in the demo

--- directives/animate/demo/animate-test.js
+++ directives/animate/demo/animate-test.js
@@ -1,6 +1,7 @@
 import '../../../components/button/button.js';
 import '../../../components/inputs/input-checkbox.js';
-import './animate-test-list.js';
+import '../../../components/list/list.js';
+import '../../../components/list/list-item.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { hide, show } from '../animate.js';
 
@@ -18,12 +19,22 @@ export class AnimateTest extends LitElement {
 			:host {
 				display: block;
 			}
+			d2l-list {
+				margin-bottom: 0.9rem;
+				max-width: 400px;
+			}
 		`;
 	}
 
 	constructor() {
 		super();
+		this._counter = 3;
 		this._doAnimate = false;
+		this._items = [
+			{ number: 1, state: 'existing' },
+			{ number: 2, state: 'existing' },
+			{ number: 3, state: 'existing' }
+		];
 		this._listVisibility = true;
 		this._renderCount = 0;
 	}
@@ -32,16 +43,45 @@ export class AnimateTest extends LitElement {
 		const animateAction = this._listVisibility ?
 			show({ skip: !this._doAnimate }) :
 			hide({ skip: !this._doAnimate });
+		const items = this._items.map((item) => {
+			let animateAction = undefined;
+			let showItem = false;
+			if (item.state === 'remove') {
+				item.state = 'removed';
+				animateAction = hide();
+				showItem = true;
+			} else if (item.state === 'new') {
+				item.state = 'existing';
+				animateAction = show();
+				showItem = true;
+			} else if (item.state === 'existing') {
+				showItem = true;
+			}
+			return showItem ? html`<d2l-list-item .animate="${animateAction}">
+					Item ${item.number}
+					<d2l-button-icon slot="actions" icon="tier1:delete" text="Remove" @click="${this._handleRemove}" data-number="${item.number}"></d2l-button-icon>
+				</d2l-list-item>` : null;
+		});
 		return html`
 			<d2l-input-checkbox .checked="${this._listVisibility}" @change="${this._handleToggleList}">Show items</d2l-input-checkbox>
-			<d2l-animate-test-list .animate="${animateAction}"></d2l-animate-test-list>
+			<d2l-list .animate="${animateAction}">${items}</d2l-list>
 			<d2l-button @click="${this._handleAddItem}" ?disabled="${!this._listVisibility}">Add Item</d2l-button>
 			<d2l-button @click="${this._handleReRender}">Re-render (${this._renderCount})</d2l-button>
 		`;
 	}
 
 	_handleAddItem() {
-		this.shadowRoot.querySelector('d2l-animate-test-list').addItem();
+		this._counter++;
+		this._items.push({ number: this._counter, state: 'new' });
+		this.requestUpdate();
+	}
+
+	_handleRemove(e) {
+		const removeItem = this._items.find(item => item.number === parseInt(e.target.getAttribute('data-number')));
+		if (removeItem) {
+			removeItem.state = 'remove';
+			this.requestUpdate();
+		}
 	}
 
 	_handleReRender() {
