6aa39b3e2e8a2fe7efb7620dabdb851757ef46ce
George Plukov <george.plukov@d2l.com>
2021-03-24 16:51:35 -0400

    trailing space

--- components/overflow-group/test/overflow-group.visual-diff.js
+++ components/overflow-group/test/overflow-group.visual-diff.js
@@ -11,7 +11,7 @@ describe('d2l-overflow-group', () => {
 	before(async() => {
 		browser = await puppeteer.launch();
 		page = await visualDiff.createPage(browser);
-		await page.goto(`${visualDiff.getBaseUrl()}/components/overflow-group/test/overflow-group.visual-diff.html`, { 
+		await page.goto(`${visualDiff.getBaseUrl()}/components/overflow-group/test/overflow-group.visual-diff.html`, {
 			waitUntil: ['networkidle0', 'load'],
 			timeout: 1000
 		});
