dbe8c13a84f9f90951e3ed851aa0fc5d3b286796
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-04-13 16:41:51 -0400

    waiting for shadow elements to be fully rendered before manipulating them

--- components/inputs/test/input-number.test.js
+++ components/inputs/test/input-number.test.js
@@ -1,5 +1,5 @@
 import '../input-number.js';
-import { aTimeout, expect, fixture, html, oneEvent } from '@open-wc/testing';
+import { aTimeout, expect, fixture, html, oneEvent, waitUntil } from '@open-wc/testing';
 import { getDocumentLocaleSettings } from '@brightspace-ui/intl/lib/common.js';
 import { runConstructor } from '../../../tools/constructor-test-helper.js';
 
@@ -31,6 +31,18 @@ function dispatchKeypressEvent(elem, key) {
 	event.key = key;
 	elem.shadowRoot.querySelector('d2l-input-text').dispatchEvent(event);
 	return event;
+}
+
+async function fixtureInit(f) {
+	const elem = await fixture(f);
+	await waitUntil(() => {
+		const textInput = elem.shadowRoot.querySelector('d2l-input-text');
+		if (!textInput) return false;
+		const input = textInput.shadowRoot.querySelector('.d2l-input');
+		if (!input) return false;
+		return true;
+	}, { interval: 10 });
+	return elem;
 }
 
 function getInnerInputValue(elem) {
@@ -354,8 +366,7 @@ describe('d2l-input-number', () => {
 
 		['a', ' ', '+', '(', ')', '?'].forEach((key) => {
 			it(`should suppress non-numeric character: "${key}"`, async() => {
-				const elem = await fixture(normalFixture);
-				await aTimeout(1); // underlying input isn't ready
+				const elem = await fixtureInit(normalFixture);
 				const event = dispatchKeypressEvent(elem, key);
 				expect(event.defaultPrevented).to.be.true;
 			});
@@ -363,16 +374,14 @@ describe('d2l-input-number', () => {
 
 		['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach((key) => {
 			it(`should not suppress numeric character: "${key}"`, async() => {
-				const elem = await fixture(normalFixture);
-				await aTimeout(1); // underlying input isn't ready
+				const elem = await fixtureInit(normalFixture);
 				const event = dispatchKeypressEvent(elem, key);
 				expect(event.defaultPrevented).to.be.false;
 			});
 		});
 
 		it('should not suppress negative symbol at the beginning for "en"', async() => {
-			const elem = await fixture(normalFixture);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(normalFixture);
 			setCursorPosition(elem, 0);
 			const event = dispatchKeypressEvent(elem, '-');
 			expect(event.defaultPrevented).to.be.false;
@@ -380,48 +389,42 @@ describe('d2l-input-number', () => {
 
 		it('should not suppress negative symbol at the end for "ar-SA"', async() => {
 			documentLocaleSettings.language = 'ar-SA';
-			const elem = await fixture(defaultValueFixture);
-			await aTimeout(1); // underlying input isn't ready
-			setCursorPosition(elem, 3);
+			const elem = await fixtureInit(defaultValueFixture);
+			await setCursorPosition(elem, 3);
 			const event = dispatchKeypressEvent(elem, '-');
 			expect(event.defaultPrevented).to.be.false;
 		});
 
 		it('should suppress negative symbol when cursor is not at beginning', async() => {
-			const elem = await fixture(defaultValueFixture);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(defaultValueFixture);
 			setCursorPosition(elem, 1);
 			const event = dispatchKeypressEvent(elem, '-');
 			expect(event.defaultPrevented).to.be.true;
 		});
 
 		it('should suppress decimal symbol if one already exists', async() => {
-			const elem = await fixture(html`<d2l-input-number label="label" value="1.2"></d2l-input-number>`);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(html`<d2l-input-number label="label" value="1.2"></d2l-input-number>`);
 			const event = dispatchKeypressEvent(elem, '.');
 			expect(event.defaultPrevented).to.be.true;
 			expect(elem._hintType).to.equal(1);
 		});
 
 		it('should suppress decimal symbol only integers are allowed', async() => {
-			const elem = await fixture(html`<d2l-input-number label="label" value="1" max-fraction-digits="0"></d2l-input-number>`);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(html`<d2l-input-number label="label" value="1" max-fraction-digits="0"></d2l-input-number>`);
 			const event = dispatchKeypressEvent(elem, '.');
 			expect(event.defaultPrevented).to.be.true;
 			expect(elem._hintType).to.equal(4);
 		});
 
 		it('should suppress incorrect decimal symbol ","', async() => {
-			const elem = await fixture(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
 			const event = dispatchKeypressEvent(elem, ',');
 			expect(event.defaultPrevented).to.be.true;
 			expect(elem._hintType).to.equal(3);
 		});
 
 		it('should not suppress correct decimal symbol "."', async() => {
-			const elem = await fixture(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
 			const event = dispatchKeypressEvent(elem, '.');
 			expect(event.defaultPrevented).to.be.false;
 			expect(elem._hintType).to.equal(0);
@@ -429,8 +432,7 @@ describe('d2l-input-number', () => {
 
 		it('should suppress incorrect decimal symbol "."', async() => {
 			documentLocaleSettings.language = 'fr-CA';
-			const elem = await fixture(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
 			const event = dispatchKeypressEvent(elem, '.');
 			expect(event.defaultPrevented).to.be.true;
 			expect(elem._hintType).to.equal(2);
@@ -438,16 +440,14 @@ describe('d2l-input-number', () => {
 
 		it('should not suppress correct decimal symbol ","', async() => {
 			documentLocaleSettings.language = 'fr-CA';
-			const elem = await fixture(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
 			const event = dispatchKeypressEvent(elem, ',');
 			expect(event.defaultPrevented).to.be.false;
 			expect(elem._hintType).to.equal(0);
 		});
 
 		it('should reset hint after next keypress', async() => {
-			const elem = await fixture(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
-			await aTimeout(1); // underlying input isn't ready
+			const elem = await fixtureInit(html`<d2l-input-number label="label" value="1"></d2l-input-number>`);
 			dispatchKeypressEvent(elem, ',');
 			dispatchKeypressEvent(elem, '5');
 			expect(elem._hintType).to.equal(0);
