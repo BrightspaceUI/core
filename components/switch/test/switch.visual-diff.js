4bdb6f612194f17111365cf7db90855c83722602
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2021-05-17 16:41:57 -0400

    Strip down visual diff test

--- components/switch/test/switch.visual-diff.js
+++ components/switch/test/switch.visual-diff.js
@@ -1,116 +1,45 @@
-/*global forceFocusVisible */
 import puppeteer from 'puppeteer';
 import VisualDiff from '@brightspace-ui/visual-diff';
 
 describe('d2l-switch', () => {
 
 	const visualDiff = new VisualDiff('switch', __dirname);
-	const resetFocus = async(page) => {
-		await page.evaluate(() => {
-			let elem = document.querySelector('#vd-focus');
-			if (!elem) {
-				elem = document.createElement('button');
-				elem.id = 'vd-focus';
-				elem.innerHTML = 'reset focus';
-				elem.style.opacity = 0;
-				document.body.insertBefore(elem, document.body.firstChild);
-			}
-			return new Promise(resolve => {
-				elem.focus();
-				requestAnimationFrame(resolve);
-			});
-		});
-	};
 
 	let browser, page;
 
 	before(async() => {
-		browser = await puppeteer.launch();
-		page = await visualDiff.createPage(browser);
+		browser = await puppeteer.launch({ headless:false });
+		page = await browser.newPage();
+		await page.emulateMediaFeatures([{
+			name: 'prefers-reduced-motion', value: 'reduce'
+		}]);
+		await page.setViewport({ width: 1200, height: 1200, deviceScaleFactor: 2 });
 		page.on('console', msg => console.log(msg.text()));
 	});
 
-	beforeEach(async() => {
-		await resetFocus(page);
-	});
-
-	after(async() => await browser.close());
+	//after(async() => await browser.close());
 
 	describe('ltr', () => {
-
-		async function getShadowElem(id, selector) {
-			return await page.evaluateHandle(
-				`document.querySelector('${id}').shadowRoot.querySelector('${selector}')`
-			);
-		}
-
-		async function getSwitch(id) {
-			return getShadowElem(id, '.d2l-switch-inner');
-		}
-
 		before(async() => {
 			await page.goto(`${visualDiff.getBaseUrl()}/components/switch/test/switch.visual-diff.html?dir=ltr`, { waitUntil: ['networkidle0', 'load'] });
 			await page.bringToFront();
 		});
 
 		[
-			{ name: 'off', selector: '#off' },
-			{ name: 'off-focus', selector: '#off', action: (selector) => page.$eval(selector, (elem) => forceFocusVisible(elem)) },
-			{ name: 'off-disabled', selector: '#off-disabled' },
-			{ name: 'off-hover', selector: '#off',
-				action: async(selector) => {
-					const d2lSwitch = await getSwitch(selector);
-					await d2lSwitch.hover();
-				}
-			},
-			{ name: 'on', selector: '#on' },
-			{ name: 'on-focus', selector: '#on', action: (selector) => page.$eval(selector, (elem) => forceFocusVisible(elem)) },
-			{ name: 'on-hover', selector: '#on',
-				action: async(selector) => {
-					const d2lSwitch = await getSwitch(selector);
-					await d2lSwitch.hover();
-				}
-			},
-			{ name: 'on-disabled', selector: '#on-disabled' },
-			{ name: 'tooltip-focus', selector: '#tooltip', action: () => page.$eval('#tooltip > d2l-switch', (elem) => forceFocusVisible(elem)) },
-			{ name: 'text-hidden', selector: '#text-hidden' },
-			{ name: 'tooltip-focus2', selector: '#tooltip', action: () => page.$eval('#tooltip > d2l-switch', (elem) => forceFocusVisible(elem)) },
-			{ name: 'text-start', selector: '#text-start' },
-			{ name: 'text-end', selector: '#text-end' },
-			{ name: 'tooltip-focus3', selector: '#tooltip', action: () => page.$eval('#tooltip > d2l-switch', (elem) => forceFocusVisible(elem)) },
-			{ name: 'toggle on', selector: '#off', action: (selector) => page.$eval(selector, (elem) => elem.on = true) },
-			{ name: 'toggle off', selector: '#on', action: (selector) => page.$eval(selector, (elem) => elem.on = false) },
-			{ name: 'tooltip', selector: '#tooltip' },
-			{ name: 'tooltip-focus4', selector: '#tooltip', action: () => page.$eval('#tooltip > d2l-switch', (elem) => forceFocusVisible(elem)) },
-			{ name: 'tooltip-focus5', selector: '#tooltip', action: () => page.$eval('#tooltip > d2l-switch', (elem) => forceFocusVisible(elem)) }
+			{ name: 'test', selector: '#test' },
+			{ name: 'test2', selector: '#test' },
+			{ name: 'test3', selector: '#test' },
+			{ name: 'test4', selector: '#test' },
+			{ name: 'test5', selector: '#test' },
+			{ name: 'test6', selector: '#test' },
+			{ name: 'test7', selector: '#test' },
+			{ name: 'test8', selector: '#test' },
+			{ name: 'test9', selector: '#test' },
+			{ name: 'test10', selector: '#test' }
 		].forEach((info) => {
-
 			it.only(info.name, async function() {
-				const rect = await visualDiff.getRect(page, info.selector);
-				if (info.action) await info.action(info.selector);
-				await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+				await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
 			});
-		});
-
-	});
-
-	describe('rtl', () => {
-
-		before(async() => {
-			await page.goto(`${visualDiff.getBaseUrl()}/components/switch/test/switch.visual-diff.html?dir=rtl`, { waitUntil: ['networkidle0', 'load'] });
-			await page.bringToFront();
-		});
-
-		[
-			{ name: 'off', selector: '#off' },
-			{ name: 'on', selector: '#on' }
-		].forEach(info => {
-
-			it(info.name, async function() {
-				const rect = await visualDiff.getRect(page, info.selector);
-				await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-			});
-
 		});
 
 	});
