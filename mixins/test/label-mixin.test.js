d69353f697efb003846395d59c06bf6d445d085b
Dave Batiste <dave.batiste@d2l.com>
2021-06-09 22:11:53 -0400

    Add documentation and tests for LabelledMixin.

---
+++ mixins/test/label-mixin.test.js
@@ -0,0 +1,162 @@
+
+import { defineCE, expect, fixture, html, nextFrame } from '@open-wc/testing';
+import { LabelledMixin, LabelMixin } from '../labelled-mixin.js';
+import { LitElement } from 'lit-element/lit-element.js';
+
+const labelledTag = defineCE(
+	class extends LabelledMixin(LitElement) {
+		render() {
+			return html`
+				<input type="text" aria-label="${this.label}">
+			`;
+		}
+	}
+);
+
+const labelTag = defineCE(
+	class extends LabelMixin(LitElement) {
+		static get properties() {
+			return {
+				text: { type: String }
+			};
+		}
+		render() {
+			return html`
+				<span>${this.text}</span>
+			`;
+		}
+		updated(changedProperties) {
+			super.updated(changedProperties);
+			this.updateLabel(this.text);
+		}
+	}
+);
+
+describe.only('LabelMixin', () => {
+
+	let elem;
+
+	beforeEach(async() => {
+		elem = await fixture(`
+			<${labelTag} text="the label value"></${labelTag}>
+		`);
+	});
+
+	it('reflects label value', async() => {
+		expect(elem.getAttribute('_label')).to.equal('the label value');
+	});
+
+	it('reflects label value change', async() => {
+		elem.text = 'new label value';
+		await nextFrame();
+		expect(elem.getAttribute('_label')).to.equal('new label value');
+	});
+
+});
+
+describe.only('LabelledMixin', () => {
+
+	let elem;
+
+	beforeEach(async() => {
+		elem = await fixture(`
+			<div>
+				<table>
+					<tr>
+						<td><${labelledTag} labelled-by="label1"></${labelledTag}></td>
+						<td><span id="label1">native element</span></td>
+					</tr>
+					<tr>
+						<td><${labelledTag} label="explicit label"></${labelledTag}></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><${labelledTag} labelled-by="label2"></${labelledTag}></td>
+						<td><${labelTag} id="label2" text="custom elemnt"></${labelTag}></td>
+					</tr>
+				</table>
+				<span id="label3">other element</span>
+			</div>
+		`);
+	});
+
+	describe('labelling with element', () => {
+
+		it('initially applies label', async() => {
+			const labelledElem = elem.querySelector('[labelled-by="label1"]');
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('native element');
+		});
+
+		it('updates label when labelling element text changes', async() => {
+			const labelledElem = elem.querySelector('[labelled-by="label1"]');
+			const labelElem = elem.querySelector('#label1');
+			labelElem.textContent = 'new label value';
+			await nextFrame();
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('new label value');
+		});
+
+		it('updates label when labelling element is replaced', async() => {
+			const labelledElem = elem.querySelector('[labelled-by="label1"]');
+			const labelElem = elem.querySelector('#label1');
+			const newLabelElem = document.createElement('span');
+			newLabelElem.id = 'label1';
+			newLabelElem.textContent = 'new label value';
+			labelElem.parentNode.replaceChild(newLabelElem, labelElem);
+			await nextFrame();
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('new label value');
+		});
+
+		it('updates label when labelledBy changes', async() => {
+			const labelledElem = elem.querySelector('[labelled-by="label1"]');
+			labelledElem.labelledBy = 'label3';
+			await nextFrame();
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('other element');
+		});
+
+	});
+
+	describe('labelling with custom element', () => {
+
+		it('initially applies label', async() => {
+			const labelledElem = elem.querySelector('[labelled-by="label2"]');
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('custom elemnt');
+		});
+
+		it('updates label when labelling element text changes', async() => {
+			const labelledElem = elem.querySelector('[labelled-by="label2"]');
+			const labelElem = elem.querySelector('#label2');
+			labelElem.text = 'new label value';
+			await nextFrame();
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('new label value');
+		});
+
+		it('updates label when labelling element is replaced', async() => {
+			const labelledElem = elem.querySelector('[labelled-by="label2"]');
+			const labelElem = elem.querySelector('#label2');
+			const newLabelElem = document.createElement(labelTag);
+			newLabelElem.id = 'label2';
+			newLabelElem.text = 'new label value';
+			labelElem.parentNode.replaceChild(newLabelElem, labelElem);
+			await nextFrame();
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('new label value');
+		});
+
+	});
+
+	describe('explicit label', () => {
+
+		it('initially applies label', async() => {
+			const labelledElem = elem.querySelector('[label]');
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('explicit label');
+		});
+
+		it('updates label when explicit label changes', async() => {
+			const labelledElem = elem.querySelector('[label]');
+			labelledElem.label = 'new label value';
+			await nextFrame();
+			expect(labelledElem.shadowRoot.querySelector('input').getAttribute('aria-label')).to.equal('new label value');
+		});
+
+	});
+
+});
