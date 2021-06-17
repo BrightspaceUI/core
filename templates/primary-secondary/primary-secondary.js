4ef356283ed6d8e70a5b03461a0a4e11efa239c5
Andrew Wong <awong@desire2learn.com>
2021-06-17 15:50:10 -0700

    fix: FACE Assignment > RHP Accordions cannot open on Android devices
    Add `z-index` to d2l-template-primary-secondary-divider-shadow

--- templates/primary-secondary/primary-secondary.js
+++ templates/primary-secondary/primary-secondary.js
@@ -852,6 +852,7 @@ class TemplatePrimarySecondary extends FocusVisiblePolyfillMixin(RtlMixin(Locali
 					height: 100%;
 					position: absolute;
 					width: 100%;
+					z-index: -1;
 				}
 			}
 		`;
