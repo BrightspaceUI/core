3fe008633f113a995f6f54d2d7a8d84bae470f8d
Andrew Wong <awong@desire2learn.com>
2021-06-17 15:50:10 -0700

    fix: DE44014-RHP-Accordions-cannot-open-on-Android-devices
    Add `z-index` to d2l-template-primary-secondary-divider-shadow

--- templates/primary-secondary/primary-secondary.js
+++ templates/primary-secondary/primary-secondary.js
@@ -851,6 +851,7 @@ class TemplatePrimarySecondary extends FocusVisiblePolyfillMixin(RtlMixin(Locali
 					height: 100%;
 					position: absolute;
 					width: 100%;
+					z-index: -1;
 				}
 			}
 		`;
