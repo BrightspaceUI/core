349058112e26ab7ded7bf70c9def1a13740cc041
George Plukov <george.plukov@d2l.com>
2021-07-15 11:47:13 -0400

    fix: Focus visible for tabs (#1483)
    * initial switch to new styling
    
    * Change focus visible styling for tabs
    
    * Changes some margin to padding, removes old focus-visible styles
    
    * Tests for focus
    
    * Updating Visual Diff Goldens (#1484)
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- components/tabs/test/tabs.visual-diff.js
+++ components/tabs/test/tabs.visual-diff.js
@@ -1,3 +1,4 @@
+/*global forceFocusVisible */
 import puppeteer from 'puppeteer';
 import VisualDiff from '@brightspace-ui/visual-diff';
 
@@ -116,6 +117,23 @@ describe('d2l-tabs', () => {
 					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 				});
 
+				it('focus next', async function() {
+					await page.$eval('#focus-next', (elem) => {
+						const buttonElement = elem.shadowRoot.querySelector('.d2l-tabs-scroll-next-container > button');
+						forceFocusVisible(buttonElement);
+					});
+					const rect = await visualDiff.getRect(page, '#focus-next');
+					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+				});
+
+				it('focus previous', async function() {
+					const rect = await visualDiff.getRect(page, '#focus-previous');
+					await page.$eval('#focus-previous', (elem) => {
+						const buttonElement = elem.shadowRoot.querySelector('.d2l-tabs-scroll-previous-container > button');
+						forceFocusVisible(buttonElement);
+					});
+					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+				});
 			});
 
 		});
