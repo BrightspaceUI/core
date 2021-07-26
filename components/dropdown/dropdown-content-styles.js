443948b7e5b9b6c430339bf3e933898d914ae61f
Annabel Segall <Annabel.Segall@D2L.com>
2021-07-26 11:07:20 -0400

    add square corners to slide out trays depedning on direction

--- components/dropdown/dropdown-content-styles.js
+++ components/dropdown/dropdown-content-styles.js
@@ -221,50 +221,62 @@ export const dropdownContentStyles = css`
 		100% { opacity: 0; transform: translateY(100%); }
 	}
 
-	:host([data-mobile][mobile-tray="left"]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width,
-	:host([data-mobile][mobile-tray="right"]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width {
+	:host([data-mobile][mobile-tray="left"]) .d2l-dropdown-content-width {
+		border-bottom-left-radius: 0;
+		border-top-left-radius: 0;
 		height: 100vh;
 		position: fixed;
 		top: 0;
 		z-index: 1000;
 	}
 
-	:host([data-mobile][mobile-tray="bottom"]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width {
+	:host([data-mobile][mobile-tray="right"]) .d2l-dropdown-content-width {
+		border-bottom-right-radius: 0;
+		border-top-right-radius: 0;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		z-index: 1000;
+	}
+
+	:host([data-mobile][mobile-tray="bottom"]) .d2l-dropdown-content-width {
+		border-bottom-left-radius: 0;
+		border-bottom-right-radius: 0;
 		left: 0;
 		position: fixed;
 		z-index: 1000;
 	}
 
-	:host([data-mobile][mobile-tray="right"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width {
+	:host([data-mobile][mobile-tray="right"][opened]) .d2l-dropdown-content-width {
 		animation: d2l-dropdown-mobile-tray-right 300ms ease-out;
 		right: 0;
 	}
 
-	:host([data-mobile][mobile-tray="left"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width {
+	:host([data-mobile][mobile-tray="left"][opened]) .d2l-dropdown-content-width {
 		animation: d2l-dropdown-mobile-tray-left 300ms ease-out;
 		left: 0;
 	}
 
-	:host([data-mobile][mobile-tray="bottom"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width {
+	:host([data-mobile][mobile-tray="bottom"][opened]) .d2l-dropdown-content-width {
 		animation: d2l-dropdown-mobile-tray-bottom 300ms ease-out;
 		bottom: 0;
 	}
 	
-	:host([data-mobile][mobile-tray="right"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width[data-closing] {
+	:host([data-mobile][mobile-tray="right"][opened]) .d2l-dropdown-content-width[data-closing] {
 		animation: d2l-dropdown-mobile-tray-right-close 300ms ease-out;
 	}
 
-	:host([data-mobile][mobile-tray="left"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width[data-closing] {
+	:host([data-mobile][mobile-tray="left"][opened]) .d2l-dropdown-content-width[data-closing] {
 		animation: d2l-dropdown-mobile-tray-left-close 300ms ease-out;
 	}
 
-	:host([data-mobile][mobile-tray="bottom"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width[data-closing] {
+	:host([data-mobile][mobile-tray="bottom"][opened]) .d2l-dropdown-content-width[data-closing] {
 		animation: d2l-dropdown-mobile-tray-bottom-close 300ms ease-out;
 	}
 
 
-	:host([data-mobile][mobile-tray="left"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width > .d2l-dropdown-content-container,
-	:host([data-mobile][mobile-tray="right"][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width > .d2l-dropdown-content-container {
+	:host([data-mobile][mobile-tray="left"][opened]) .d2l-dropdown-content-container,
+	:host([data-mobile][mobile-tray="right"][opened]) .d2l-dropdown-content-container {
 		height: 100vh;
 	}
 
@@ -277,17 +289,17 @@ export const dropdownContentStyles = css`
 		animation: none;
 	}
 
-	:host([data-mobile][mobile-tray]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width > .d2l-dropdown-content-bottom,
-	:host([data-mobile][mobile-tray]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width > .d2l-dropdown-content-top {
+	:host([data-mobile][mobile-tray]) .d2l-dropdown-content-bottom,
+	:host([data-mobile][mobile-tray]) .d2l-dropdown-content-top {
 		min-height: auto;
 	}
 
 	@media (prefers-reduced-motion: reduce) {
-		:host([data-mobile][mobile-tray][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width {
+		:host([data-mobile][mobile-tray][opened]) .d2l-dropdown-content-width {
 			animation: none;
 		}
 
-		:host([data-mobile][mobile-tray][opened]) > .d2l-dropdown-content-position > .d2l-dropdown-content-width[data-closing] {
+		:host([data-mobile][mobile-tray][opened]) .d2l-dropdown-content-width[data-closing] {
 			animation: none;
 		}
 	}
