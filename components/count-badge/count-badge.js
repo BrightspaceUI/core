a1783d20154e43b6c8f5e7a31f63c20a195730ab
Annabel Segall <Annabel.Segall@D2L.com>
2021-08-31 14:05:28 -0400

    add basic styling

--- components/count-badge/count-badge.js
+++ components/count-badge/count-badge.js
@@ -1,3 +1,4 @@
+import '../colors/colors.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { RtlMixin } from '../../mixins/rtl-mixin.js';
 
@@ -15,6 +16,22 @@ class CountBadge extends RtlMixin(LitElement) {
 
 	static get styles() {
 		return [ css`
+		.d2l-count-badge-number {
+			color: white;
+			font-size: 0.6rem;
+			font-weight: bold;
+			line-height: 0.9rem;
+			padding-left: 0.3rem;
+			padding-right: 0.3rem;
+		}
+
+		.d2l-count-badge-background {
+			background: var(--d2l-color-carnelian-minus-1);
+			border: 2px white;
+			border-radius: 0.45rem;
+			display: inline-block;
+			min-width: 0.9rem;
+		}
 		`];
 	}
 
@@ -25,7 +42,11 @@ class CountBadge extends RtlMixin(LitElement) {
 
 	render() {
 		return html`
-        <div>${this.number}</div>`;
+		<div class="d2l-count-badge-background">
+        	<div class="d2l-count-badge-number">
+				${this.number}
+			</div>
+		<div>`;
 	}
 }
 
