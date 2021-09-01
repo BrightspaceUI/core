48464e355dded30e959a4d0c0ee1e7db8c59d697
Annabel Segall <Annabel.Segall@D2L.com>
2021-09-01 09:44:44 -0400

    delete axe test- no accessibility features yet

--- components/count-badge/test/count-badge.axe.js
+++
@@ -1,9 +0,0 @@
-import '../count-badge.js';
-import { expect, fixture, html } from '@open-wc/testing';
-
-describe('d2l-count-badge', () => {
-	it('default', async() => {
-		const elem = await fixture(html`<d2l-count-badge>5</d2l-count-badge>`);
-		await expect(elem).to.be.accessible();
-	});
-});
