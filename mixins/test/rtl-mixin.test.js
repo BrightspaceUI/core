e37dadc5f227b9b61abecf40e51fa456690aa144
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-09-02 17:24:17 -0400

    fix: only apply "dir" attribute if it's not "ltr" to avoid performance cost of sprouting the attribute (#1629)

---
+++ mixins/test/rtl-mixin.test.js
@@ -0,0 +1,37 @@
+
+import { defineCE, expect, fixture, html } from '@open-wc/testing';
+import { LitElement } from 'lit-element/lit-element.js';
+import { RtlMixin } from '../rtl-mixin.js';
+
+const rtlMixinTag = defineCE(
+	class extends RtlMixin(LitElement) {
+		render() {
+			return html`<p>hello</p>`;
+		}
+	}
+);
+
+describe('RtlMixin', () => {
+
+	afterEach(() => {
+		document.documentElement.removeAttribute('dir');
+	});
+
+	it('does not get a "dir" attribute if none is set on the document', async() => {
+		const elem = await fixture(`<${rtlMixinTag}></${rtlMixinTag}>`);
+		expect(elem.hasAttribute('dir')).to.be.false;
+	});
+
+	it('does not get a "dir" attribute if document direction is "ltr"', async() => {
+		document.documentElement.setAttribute('dir', 'ltr');
+		const elem = await fixture(`<${rtlMixinTag}></${rtlMixinTag}>`);
+		expect(elem.hasAttribute('dir')).to.be.false;
+	});
+
+	it('does get dir="rtl" attribute if document direction is "rtl"', async() => {
+		document.documentElement.setAttribute('dir', 'rtl');
+		const elem = await fixture(`<${rtlMixinTag}></${rtlMixinTag}>`);
+		expect(elem.getAttribute('dir')).to.equal('rtl');
+	});
+
+});
