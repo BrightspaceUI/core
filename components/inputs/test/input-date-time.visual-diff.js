1cefbffb9e6f8f7f0d9cc7b96daf3f556fed1c9b
Margaree Peacocke <margaree.p@gmail.com>
2021-09-13 15:27:11 -0400

    input-date and time visual-diffs: await updateComplete

--- components/inputs/test/input-date-time.visual-diff.js
+++ components/inputs/test/input-date-time.visual-diff.js
@@ -114,7 +114,10 @@ describe('d2l-input-date-time', () => {
 
 	describe('opened behavior', () => {
 
-		before(async() => await page.reload());
+		before(async() => {
+			await page.reload();
+			await page.$eval('#opened', async(elem) => await elem.updateComplete);
+		});
 
 		after(async() => {
 			await page.$eval('#opened-skeleton', (elem) => elem.removeAttribute('opened'));
