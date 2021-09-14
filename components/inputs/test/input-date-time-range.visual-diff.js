e9fa6c105916fe89245ebcd54863f9a8515fc645
Margaree Peacocke <margaree.p@gmail.com>
2021-09-14 14:17:46 -0400

    await updateComplete for end opened test

--- components/inputs/test/input-date-time-range.visual-diff.js
+++ components/inputs/test/input-date-time-range.visual-diff.js
@@ -96,9 +96,10 @@ describe('d2l-input-date-time-range', () => {
 		});
 
 		it('end opened', async function() {
-			await page.$eval('#opened', (elem) => {
+			await page.$eval('#opened', async(elem) => {
 				elem.removeAttribute('start-opened');
 				elem.endOpened = true;
+				await elem.updateComplete;
 			});
 			const rect = await getRect(page, '#opened', 1);
 			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
