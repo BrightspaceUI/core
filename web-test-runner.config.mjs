596b5bd67cc17d53b33cefaf06ebde1150488ed5
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-09-21 15:18:57 -0400

    fixing onerror

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -25,20 +25,15 @@ export default {
 		`<html>
 			<body>
 				<script>
-					(function() {
-						const e = window.onerror;
-						window.onerror = function(err) {
-							if (err === 'ResizeObserver loop limit exceeded') {
-								console.warn('Ignored: ResizeObserver loop limit exceeded');
-								return false;
-							} else if (err === 'ResizeObserver loop completed with undelivered notifications.') {
-								console.warn('Ignored: ResizeObserver loop completed with undelivered notifications');
-								return false;
-							} else {
-								return e(...arguments);
-							}
-						};
-					})();
+					window.onerror = function(err) {
+						if (err === 'ResizeObserver loop limit exceeded') {
+							console.warn('Ignored: ResizeObserver loop limit exceeded');
+							return true;
+						} else if (err === 'ResizeObserver loop completed with undelivered notifications.') {
+							console.warn('Ignored: ResizeObserver loop completed with undelivered notifications');
+							return true;
+						}
+					};
 				</script>
 				<script type="module" src="${testFramework}"></script>
 				<script src="./tools/perf-test-helper.js" type="module"></script>
