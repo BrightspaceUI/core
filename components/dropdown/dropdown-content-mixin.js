528c6f645ea6d70e026ca8b330399fbdd52a1554
Annabel Segall <Annabel.Segall@D2L.com>
2021-09-21 10:55:37 -0400

    fix the console errors

--- components/dropdown/dropdown-content-mixin.js
+++ components/dropdown/dropdown-content-mixin.js
@@ -1037,6 +1037,7 @@ export const DropdownContentMixin = superclass => class extends LocalizeCoreElem
 	async _handleMobileResize() {
 		this._useMobileStyling =  this.mediaQueryList.matches;
 		if (this.opened) this._showBackdrop = this._useMobileStyling && this.mobileTray;
+		console.log(this._showBackdrop);
 		if (this.opened) await this.__position();
 	}
 
