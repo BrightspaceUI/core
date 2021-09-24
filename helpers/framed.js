6d6ba4c51c98457d895832547c3b9f11b38cf749
Grant Cleary <Grant.Cleary@D2L.com>
2021-09-24 12:23:13 -0400

    Shift evt handler into promise to make sure we can remove the event listener; add safety net try-catch around window.parent lookup

--- helpers/framed.js
+++ helpers/framed.js
@@ -3,21 +3,31 @@ let framed;
 export async function isFramed() {
 	if (framed !== undefined) return framed;
 
-	if ((window.D2L && window.D2L.IsNotAnIframedApp) || window === window.parent) {
+	if (window.D2L && window.D2L.IsNotAnIframedApp) {
 		framed = false;
 		return framed;
 	}
 
-	const handleIsFramedResponse = (evt, resolve) => {
-		if (!evt || !evt.data || !evt.data.isFramed) return;
-		window.removeEventListener('message', evt => handleIsFramedResponse(evt, resolve), false);
-		framed = evt.data.isFramed;
-		resolve(framed);
-	};
+	try{
+		if (window === window.parent) {
+			framed = false;
+			return framed;
+		}
+	} catch {
+		framed = false;
+		return framed;
+	}
 
 	framed = await Promise.race([
 		new Promise(resolve => {
-			window.addEventListener('message', evt => handleIsFramedResponse(evt, resolve), false);
+			const handleIsFramedResponse = evt => {
+				if (!evt || !evt.data || !evt.data.isFramed === undefined) return;
+				window.removeEventListener('message', handleIsFramedResponse, false);
+				framed = evt.data.isFramed;
+				resolve(framed);
+			};
+			
+			window.addEventListener('message', handleIsFramedResponse, false);
 			window.parent.postMessage('isFramedRequest', '*');
 		}),
 		new Promise(resolve => {
