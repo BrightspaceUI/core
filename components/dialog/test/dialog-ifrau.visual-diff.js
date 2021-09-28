403b2786ba23258013bc8e727ce5e370010b24de
Grant Cleary <Grant.Cleary@D2L.com>
2021-09-28 10:37:05 -0400

    Update dialog component to use new ifrau SlimClient when necessary

--- components/dialog/test/dialog-ifrau.visual-diff.js
+++ components/dialog/test/dialog-ifrau.visual-diff.js
@@ -1,4 +1,4 @@
-import { open, reset } from './dialog-helper.js';
+import oneEvent from '@brightspace-ui/visual-diff/helpers/oneEvent.js';
 import puppeteer from 'puppeteer';
 import VisualDiff from '@brightspace-ui/visual-diff';
 
@@ -15,19 +15,59 @@ describe('d2l-dialog-ifrau', () => {
 
 	after(async() => await browser.close());
 
+	const getDialog = dialogContainer => {
+		const iframe = dialogContainer.querySelector('iframe');
+		return iframe.contentWindow.document.body.querySelector('#ifrau-dialog');
+	};
+
+	const open = async(page, selector) => {
+		const openEvent = oneEvent(page, selector, 'ifrau-dialog-open');
+		await page.$eval(selector, dialogContainer => {
+			const dialog = getDialog(dialogContainer);
+			dialog.opened = true;
+		});
+		return openEvent;
+	};
+
+	const reset = async(page, selector) => {
+		const closeEvent = oneEvent(page, selector, 'ifrau-dialog-close');
+		await Promise.race([
+			page.$eval(selector, dialogContainer => {
+				return new Promise(resolve => {
+					const dialog = getDialog(dialogContainer);
+					
+					dialog._fullscreenWithin = 0;
+					dialog.shadowRoot.querySelector('.d2l-dialog-content').scrollTo(0, 0);
+					if (dialog._state) {
+						dialog.opened = false;
+					} else {
+						resolve();
+					}
+				});
+			}),
+			closeEvent
+		]);
+
+		return page.$eval(selector, dialogContainer => {
+			const iframe = dialogContainer.querySelector('iframe');
+			iframe.contentWindow.document.body.querySelector('#open').click();
+		});
+	};
+
 	['native', 'custom'].forEach((name) => {
 
 		describe(name, () => {
 
 			before(async() => {
 				const preferNative = (name === 'native' ? '' : '?preferNative=false');
-				await page.goto(`${visualDiff.getBaseUrl()}/components/dialog/test/dialog-ifrau.visual-diff.html${preferNative}`, { waitUntil: ['networkidle0', 'load'] });
+				const src = `${visualDiff.getBaseUrl()}/components/dialog/test/dialog-ifrau-contents.visual-diff.html${preferNative}`;
+				await page.goto(`${visualDiff.getBaseUrl()}/components/dialog/test/dialog-ifrau.visual-diff.html?src=${encodeURIComponent(src)}`, { waitUntil: ['networkidle0', 'load'] });
 				await page.setViewport({ width: 650, height: 450, deviceScaleFactor: 2 });
 				await page.bringToFront();
 			});
 
 			beforeEach(async() => {
-				await reset(page, '#ifrau-dialog');
+				await reset(page, '#ifrau-dialog-container');
 			});
 
 			[
@@ -41,7 +81,7 @@ describe('d2l-dialog-ifrau', () => {
 						window.ifrauAvailableHeight = info.ifrau.availableHeight;
 						window.ifrauTop = info.ifrau.top;
 					}, info);
-					await open(page, '#ifrau-dialog');
+					await open(page, '#ifrau-dialog-container');
 					await visualDiff.screenshotAndCompare(page, this.test.fullTitle());
 				});
 			});
--- helpers/ifrauBackdropService.js
+++ helpers/ifrauBackdropService.js
@@ -1,10 +1,13 @@
+import { isFramed } from './framed';
+import { SlimClient } from 'ifrau/client/slim.js';
+
 let ifrauBackdropService;
 
 export async function tryGetIfrauBackdropService() {
-	if (!window.ifrauclient) return null;
+	if (!await isFramed()) return null;
 	if (ifrauBackdropService) return ifrauBackdropService;
 
-	const ifrauClient = await window.ifrauclient().connect();
+	const ifrauClient = await new SlimClient().connect();
 	ifrauBackdropService = await ifrauClient.getService('dialogWC', '0.1');
 
 	return ifrauBackdropService;
