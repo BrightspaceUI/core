fcb72a16f1ba448c9ac84317bcc0b02308038483
Annabel Segall <Annabel.Segall@D2L.com>
2021-10-14 14:40:39 -0400

    remove unchanged files

--- helpers/ifrauBackdropService.js
+++ helpers/ifrauBackdropService.js
@@ -6,5 +6,9 @@ export async function tryGetIfrauBackdropService() {
 	if (!await isFramed()) return null;
 	if (ifrauBackdropService) return ifrauBackdropService;
 
+	const ifrau = await import('ifrau/client/slim.js');
+	const ifrauClient = await new ifrau.SlimClient().connect();
+	ifrauBackdropService = await ifrauClient.getService('dialogWC', '0.1');
+
 	return ifrauBackdropService;
 }
