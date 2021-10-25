97883ac53eea20c7c9a224ecdf9ca3131e3802b1
Annabel Segall <Annabel.Segall@D2L.com>
2021-10-25 10:51:44 -0400

    remove unneeded import

--- components/dropdown/dropdown-button.js
+++ components/dropdown/dropdown-button.js
@@ -3,11 +3,10 @@ import '../icons/icon.js';
 import { DropdownButtonMixin } from './dropdown-button-mixin.js';
 import { DropdownOpenerMixin } from './dropdown-opener-mixin.js';
 import { LitElement } from 'lit-element/lit-element.js';
-import { RtlMixin } from '../../mixins/rtl-mixin.js';
 
 /**
  * A "d2l-button" opener for dropdown content.
  * @slot - Dropdown content (e.g., "d2l-dropdown-content", "d2l-dropdown-menu" or "d2l-dropdown-tabs")
  */
-class DropdownButton extends DropdownButtonMixin(DropdownOpenerMixin(RtlMixin(LitElement))) {}
+class DropdownButton extends DropdownButtonMixin(DropdownOpenerMixin(LitElement)) {}
 customElements.define('d2l-dropdown-button', DropdownButton);
--- components/dropdown/dropdown-hover-button.js
+++ components/dropdown/dropdown-hover-button.js
@@ -3,11 +3,10 @@ import '../icons/icon.js';
 import { DropdownButtonMixin } from './dropdown-button-mixin.js';
 import { DropdownHoverOpenerMixin } from './dropdown-hover-opener-mixin.js';
 import { LitElement } from 'lit-element/lit-element.js';
-import { RtlMixin } from '../../mixins/rtl-mixin.js';
 
 /**
  * A "d2l-button" opener for dropdown content.
  * @slot - Dropdown content (e.g., "d2l-dropdown-content", "d2l-dropdown-menu" or "d2l-dropdown-tabs")
  */
-class DropdownHoverButton extends DropdownButtonMixin(DropdownHoverOpenerMixin(RtlMixin(LitElement))) {}
+class DropdownHoverButton extends DropdownButtonMixin(DropdownHoverOpenerMixin(LitElement)) {}
 customElements.define('d2l-dropdown-hover-button', DropdownHoverButton);
