e0476fd21c4f1c8f39981656c4a1bdb8b4f14f5f
Annabel Segall <Annabel.Segall@D2L.com>
2021-12-16 12:43:39 -0500

    removing extra changes to another PR

--- components/demo/code-view.js
+++ components/demo/code-view.js
@@ -36,9 +36,7 @@ class CodeView extends LitElement {
 			const path = `/node_modules/prismjs/components/prism-${language}.min.js`;
 			this._dependenciesPromise = import(path);
 		}
-		if (this.shadowRoot) {
-			this._updateCode(this.shadowRoot.querySelector('slot'));
-		}
+		this._updateCode(this.shadowRoot.querySelector('slot'));
 		super.attributeChangedCallback(name, oldval, newval);
 	}
 
