af1b820e5c75fc996ab67d60e74272601491d9b5
Annabel Segall <Annabel.Segall@D2L.com>
2022-01-12 14:49:16 -0500

    remove _dir property

--- mixins/rtl-mixin.js
+++ mixins/rtl-mixin.js
@@ -2,12 +2,6 @@ import { dedupeMixin } from '@open-wc/dedupe-mixin';
 
 export const RtlMixin = dedupeMixin(superclass => class extends superclass {
 
-	static get properties() {
-		return {
-			_dir: { type: String, reflect: true, attribute: 'dir' }
-		};
-	}
-
 	constructor() {
 		super();
 		const dir = document.documentElement.getAttribute('dir');
