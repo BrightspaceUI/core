4eed2cf1773abfb1e46c9784bcd9cfcf5523da02
Annabel Segall <Annabel.Segall@D2L.com>
2022-01-13 13:40:45 -0500

    add dir as a public property to rtl mixin

--- mixins/rtl-mixin.js
+++ mixins/rtl-mixin.js
@@ -2,12 +2,18 @@ import { dedupeMixin } from '@open-wc/dedupe-mixin';
 
 export const RtlMixin = dedupeMixin(superclass => class extends superclass {
 
+	static get properties() {
+		return {
+			dir: { type: String, reflect: true }
+		};
+	}
+
 	constructor() {
 		super();
 		const dir = document.documentElement.getAttribute('dir');
 		// avoid reflecting "ltr" for better performance
 		if (dir && dir !== 'ltr') {
-			this.setAttribute('dir', 'rtl');
+			this.dir = dir;
 		}
 	}
 
