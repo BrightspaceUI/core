af5a69aa434d0b4a47a0c11d4c52fb6b5c62f8b1
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2022-03-18 10:58:46 -0400

    Add d2l-tag-list, d2l-tag-list-item, and TagListItemMixin

---
+++ components/tag-list/tag-list.js
@@ -0,0 +1,97 @@
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { ArrowKeysMixin } from '../../mixins/arrow-keys-mixin.js';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { offscreenStyles } from '../offscreen/offscreen.js';
+
+const ITEM_SPACING = 6;
+
+class TagList extends ArrowKeysMixin(LitElement) {
+
+	static get properties() {
+		return {
+			/**
+			 * REQUIRED: A description of the tag list for additional accessibility context
+			 * @type {string}
+			 */
+			description: { type: String },
+			/**
+			 * @ignore
+			 */
+			role: { type: String, reflect: true },
+		};
+	}
+
+	static get styles() {
+		return [ offscreenStyles, css`
+			:host {
+				display: block;
+			}
+			:host([hidden]) {
+				display: none;
+			}
+			.tag-list-container {
+				display: flex;
+				flex-wrap: wrap;
+				margin: -${ITEM_SPACING}px -${ITEM_SPACING}px 0 0;
+				padding: 0;
+				position: relative;
+			}
+			::slotted(*) {
+				margin: ${ITEM_SPACING}px ${ITEM_SPACING}px 0 0;
+			}
+		`];
+	}
+
+	constructor() {
+		super();
+		/** @ignore */
+		this.role = 'application';
+		/** @ignore */
+		this.arrowKeysDirection = 'leftrightupdown';
+		this._items = [];
+	}
+
+	firstUpdated(changedProperties) {
+		super.firstUpdated(changedProperties);
+
+		requestAnimationFrame(() => {
+			this._items = this._getTagListItems();
+			if (this._items.length > 0) this._items[0].setAttribute('tabindex', 0);
+		});
+	}
+
+	render() {
+		const description = this.description ? html`<div id="d2l-tag-list-description" class="d2l-offscreen">${this.description}</div>` : null;
+		const list = html`
+			<div role="list" class="tag-list-container" aria-describedby="${ifDefined(this.description && 'd2l-tag-list-description')}">
+				<slot></slot>
+			</div>
+		`;
+		return html`
+			${this.arrowKeysContainer(list)}
+			${description}
+		`;
+	}
+
+	async arrowKeysFocusablesProvider() {
+		return this._items;
+	}
+
+	focus() {
+		if (this._items[0]) this._items[0].focus();
+	}
+
+	_getTagListItems() {
+		const slot = this.shadowRoot && this.shadowRoot.querySelector('slot');
+		if (!slot) return;
+		const items = slot.assignedNodes({ flatten: true }).filter((node) => node.nodeType === Node.ELEMENT_NODE);
+
+		return items.filter((item) => {
+			const role = item.getAttribute('role');
+			return (role === 'listitem');
+		});
+	}
+
+}
+
+customElements.define('d2l-tag-list', TagList);
---
+++ components/tag-list/test/tag-list-item-mixin-consumer.js
@@ -0,0 +1,73 @@
+import '../../dropdown/dropdown.js';
+import '../../dropdown/dropdown-content.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { TagListItemMixin } from '../tag-list-item-mixin.js';
+
+class TagListItemMixinConsumer extends TagListItemMixin(LitElement) {
+
+	static get properties() {
+		return {
+			name: { type: String }
+		};
+	}
+
+	static get styles() {
+		return [super.styles, css`
+			.color-block {
+				background-color: purple;
+				border-radius: 6px;
+				display: inline-block;
+				height: 24px;
+				left: 3px;
+				position: absolute;
+				top: 50%;
+				transform: translate(0, -50%);
+				width: 24px;
+			}
+			:host([dir="rtl"]) .color-block {
+				left: unset;
+				margin-left: 6px;
+				right: 3px;
+			}
+			.text {
+				padding-left: calc(34px - 0.6rem);
+				vertical-align: middle;
+			}
+			:host([dir="rtl"]) .text {
+				padding-left: 0;
+				padding-right: calc(34px - 0.6rem);
+			}
+			d2l-dropdown {
+				min-width: 0;
+			}
+		`];
+	}
+
+	firstUpdated(changedProperties) {
+		super.firstUpdated(changedProperties);
+
+		this.addEventListener('focus', this._toggleDropdown);
+		this.addEventListener('blur', this._toggleDropdown);
+	}
+
+	render() {
+		const tagContent = html`
+			<div class="color-block"></div>
+			<span class="text">${this.name}</span>
+		`;
+
+		return html`
+			<d2l-dropdown open-on-hover>
+				<div role="button" class="d2l-dropdown-opener">${this._renderTag(tagContent)}</div>
+				<d2l-dropdown-content no-auto-focus>Custom</d2l-dropdown-content>
+			</d2l-dropdown>
+		`;
+	}
+
+	_toggleDropdown() {
+		const dropdown = this.shadowRoot.querySelector('d2l-dropdown');
+		if (dropdown) dropdown.toggleOpen();
+	}
+}
+
+customElements.define('d2l-tag-list-item-mixin-consumer', TagListItemMixinConsumer);
