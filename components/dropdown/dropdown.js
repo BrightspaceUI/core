16903d6462e8d00f1128c335ea3de6b78c195bc0
Annabel Segall <Annabel.Segall@D2L.com>
2022-03-31 15:46:46 -0400

    add the flatten back in!

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -23,10 +23,7 @@ class Dropdown extends DropdownOpenerMixin(LitElement) {
 	getOpenerElement() {
 		if (!this.shadowRoot) return undefined;
 		return this.shadowRoot.querySelector('slot')
-			.assignedNodes()
-			.filter(node => node.classList && node.classList.contains('d2l-dropdown-opener'))[0]
-		|| this.querySelector('slot[name="opener"]')
-			?.assignedNodes({ flatten: true })
+			.assignedNodes({ flatten: true })
 			.filter(node => node.classList && node.classList.contains('d2l-dropdown-opener'))[0];
 	}
 
