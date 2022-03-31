4a84815d9bfffa718edf574944764944e8d840d6
Dave Batiste <dave.batiste@d2l.com>
2022-03-31 10:29:27 -0400

    Update components/inputs/input-text.js
    Co-authored-by: Dave Lockhart <dlockhart@users.noreply.github.com>

--- components/inputs/input-text.js
+++ components/inputs/input-text.js
@@ -470,8 +470,11 @@ class InputText extends FocusMixin(LabelledMixin(FormElementMixin(SkeletonMixin(
 
 	_getAriaLabel() {
 		let label;
-		if (this.label && (this.labelHidden || this.labelledBy)) label = this.label;
-		if (this.hasAttribute('aria-label')) label = this.getAttribute('aria-label');
+		if (this.label && (this.labelHidden || this.labelledBy)) {
+			label = this.label;
+		} else if (this.hasAttribute('aria-label')) {
+			label = this.getAttribute('aria-label');
+		}
 		if (label) {
 			const unitLabel = this.unit ? ` ${this.unit}` : '';
 			return `${label}${unitLabel}`;
