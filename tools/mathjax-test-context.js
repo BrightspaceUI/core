6b0456f51f80a57e471e4e7806f982875e0f7536
Devon Powell <Devon.Powell@D2L.com>
2022-04-06 14:48:35 -0400

    fix: add mathjax test context warning message

--- tools/mathjax-test-context.js
+++ tools/mathjax-test-context.js
@@ -1,3 +1,5 @@
+console.warn('Using mathjax test context, this is intended for demo pages and tests only');
+
 const disabled = window.location.search.indexOf('latex=false') !== -1;
 
 document.getElementsByTagName('html')[0].dataset.mathjaxContext = JSON.stringify({
