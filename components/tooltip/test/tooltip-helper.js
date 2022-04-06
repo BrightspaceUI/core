b507be5d2cf33a61eca1d368ba07e8fa1b9a0a7f
Stacey Van Herk <Stacey.VanHerk@d2l.com>
2022-04-06 15:02:29 -0400

    feat: US136319 - Add ability for tooltip to only show when an element is truncating (#2268)

--- components/tooltip/test/tooltip-helper.js
+++ components/tooltip/test/tooltip-helper.js
@@ -11,3 +11,30 @@ export async function show(page, selector) {
 	page.$eval(`${selector} d2l-tooltip`, tooltip => tooltip.show());
 	return openEvent;
 }
+
+export function getRect(page, selector) {
+	return page.$eval(selector, elem => {
+		let x, y, width, height;
+		const openerRect = elem.getBoundingClientRect();
+		const content = elem.querySelector('d2l-tooltip');
+		if (!content.showing) {
+			x = openerRect.x;
+			y = openerRect.y;
+			width = openerRect.right - x;
+			height = openerRect.bottom - y;
+		} else {
+			const contentWidth = content.shadowRoot.querySelector('.d2l-tooltip-content');
+			const contentRect = contentWidth.getBoundingClientRect();
+			x = Math.min(openerRect.x, contentRect.x);
+			y = Math.min(openerRect.y, contentRect.y);
+			width = Math.max(openerRect.right, contentRect.right) - x;
+			height = Math.max(openerRect.bottom, contentRect.bottom) - y;
+		}
+		return {
+			x: x - 10,
+			y: y - 10,
+			width: width + 20,
+			height: height + 20
+		};
+	});
+}
