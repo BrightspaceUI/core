26f475fa3e0f7ae399cfb501f5b224047615a339
Devon Powell <Devon.Powell@D2L.com>
2022-04-06 11:25:16 -0400

    Use mathjax context in demos (#2289)

--- tools/mathjax-test-context.js
+++ tools/mathjax-test-context.js
@@ -1,4 +1,6 @@
-document.documentElement.dataset.mathjaxContext = JSON.stringify({
+const disabled = window.location.search.indexOf('latex=false') !== -1;
+
+document.getElementsByTagName('html')[0].dataset.mathjaxContext = JSON.stringify({
 	outputScale: 1.1,
-	renderLatex: true
+	renderLatex: !disabled
 });
