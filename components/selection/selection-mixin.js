356cfa9cd10af16f601ce8679bea8f0577dd45db
Oscar Elhanafey <oelhanafey@desire2learn.com>
2022-06-15 11:28:32 -0400

    fixed bug for arrow key single-selection navigation

--- components/selection/selection-mixin.js
+++ components/selection/selection-mixin.js
@@ -147,7 +147,8 @@ export const SelectionMixin = superclass => class extends RtlMixin(superclass) {
 		if (!e.composedPath()[0].classList.contains('d2l-selection-input-radio')) return;
 		if (e.keyCode < keyCodes.LEFT || e.keyCode > keyCodes.DOWN) return;
 
-		const selectables = Array.from(this._selectionSelectables.values());
+		const selectables = Array.from(this._selectionSelectables.values())
+			.filter(item => !item.disabled);
 		let currentIndex = selectables.findIndex(selectable => selectable.selected);
 		if (currentIndex === -1) currentIndex = 0;
 		let newIndex = currentIndex;
@@ -155,17 +156,13 @@ export const SelectionMixin = superclass => class extends RtlMixin(superclass) {
 		if ((this.dir !== 'rtl' && e.keyCode === keyCodes.RIGHT)
 			|| (this.dir === 'rtl' && e.keyCode === keyCodes.LEFT)
 			|| e.keyCode === keyCodes.DOWN) {
-				do {
-					if (newIndex === selectables.length - 1) newIndex = 0;
-					else newIndex = newIndex + 1;
-				} while (!!selectables[newIndex].disabled);
+			if (newIndex === selectables.length - 1) newIndex = 0;
+			else newIndex = newIndex + 1;
 		} else if ((this.dir !== 'rtl' && e.keyCode === keyCodes.LEFT)
 			|| (this.dir === 'rtl' && e.keyCode === keyCodes.RIGHT)
 			|| e.keyCode === keyCodes.UP) {
-				do {
-					if (newIndex === 0) newIndex = selectables.length - 1;
-					else newIndex = newIndex - 1;
-				} while (!!selectables[newIndex].disabled);
+			if (newIndex === 0) newIndex = selectables.length - 1;
+			else newIndex = newIndex - 1;
 		}
 		selectables[newIndex].selected = true;
 		selectables[newIndex].focus();
