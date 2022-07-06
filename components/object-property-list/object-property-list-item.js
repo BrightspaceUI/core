d284d60903658d5b60b5cef3453096255d9976e6
Erik Koopmans <erik.koopmans@d2l.com>
2022-07-06 14:42:15 -0400

    Use getSeparator directly from intl library

--- components/object-property-list/object-property-list-item.js
+++ components/object-property-list/object-property-list-item.js
@@ -1,20 +1,9 @@
 import '../colors/colors.js';
 import '../icons/icon.js';
 import { css, html, LitElement } from 'lit';
-import { getLanguage } from '@brightspace-ui/intl/lib/common.js';
-// import { getSeparator } from '@brightspace-ui/intl/lib/list.js';
+import { getSeparator } from '@brightspace-ui/intl/lib/list.js';
 import { offscreenStyles } from '../offscreen/offscreen.js';
 import { RtlMixin } from '../../mixins/rtl-mixin.js';
-
-export function getSeparator(nonBreaking) {
-	const langTag = getLanguage();
-	const nbsp = '\xa0';
-	const space = nonBreaking ? nbsp : ' ';
-
-	if (langTag === 'ar') return `${space}،${space}`;
-	if (['ja', 'zh-cn', 'zh-tw'].includes(langTag)) return '、';
-	return `,${space}`;
-}
 
 /**
  * A placeholder.
@@ -74,7 +63,7 @@ export class ObjectPropertyListItem extends RtlMixin(LitElement) {
 	_renderSeparator() {
 		return html`
 			<span class="separator">
-				<span class="d2l-offscreen">${getSeparator(true)}</span>
+				<span class="d2l-offscreen">${getSeparator({ nonBreaking: true })}</span>
 				<d2l-icon icon="tier1:bullet" aria-hidden="true"></d2l-icon>
 			</span>
 		`;
