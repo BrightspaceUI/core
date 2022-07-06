83e1d66d7a9d93849ab6307669d1aba627680920
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2022-07-06 13:15:22 -0400

    Clean up search logic.

--- components/dialog/dialog-mixin.js
+++ components/dialog/dialog-mixin.js
@@ -3,7 +3,7 @@ import '../../helpers/viewport-size.js';
 import { allowBodyScroll, preventBodyScroll } from '../backdrop/backdrop.js';
 import { clearDismissible, setDismissible } from '../../helpers/dismissible.js';
 import { findComposedAncestor, isComposedAncestor } from '../../helpers/dom.js';
-import { forceFocusVisible, getComposedActiveElement, getNextFocusable, tryApplyFocus } from '../../helpers/focus.js';
+import { forceFocusVisible, getComposedActiveElement, getNextFocusable, isFocusable, tryApplyFocus } from '../../helpers/focus.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { getUniqueId } from '../../helpers/uniqueId.js';
 import { html } from 'lit';
@@ -190,9 +190,15 @@ export const DialogMixin = superclass => class extends RtlMixin(superclass) {
 		}
 
 		const content = slot.assignedElements({ flatten: true });
-		const el = content.find(el => el.hasAttribute('autofocus'))
-			?? content.find(el => el.querySelector('[autofocus]'))?.querySelector('[autofocus]');
-		return el;
+
+		const searchOneElement = (el) => {
+			const foundElementOrNull = el.hasAttribute('autofocus') ? el : el.querySelector('[autofocus]');
+			return isFocusable(foundElementOrNull, false, false) ? foundElementOrNull : null;
+		};
+
+		let autofocusElement = null;
+		content.some(x => (autofocusElement = searchOneElement(x)));
+		return autofocusElement;
 	}
 
 	_focusFirst() {
