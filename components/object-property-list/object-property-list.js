0b346ba940f30967cf4e70bd31b2a5f382e89353
Erik Koopmans <erik.koopmans@d2l.com>
2022-07-08 17:10:47 -0400

    feat: Add object-property-list component (#2628)

---
+++ components/object-property-list/object-property-list.js
@@ -0,0 +1,31 @@
+import { css, html, LitElement } from 'lit';
+import { bodySmallStyles } from '../typography/styles.js';
+
+/**
+ * A dot-separated list of object properties.
+ * @slot - Items of the type d2l-object-property-list-item* to be added to the container
+ */
+class ObjectPropertyList extends LitElement {
+	static get styles() {
+		return [bodySmallStyles, css`
+			:host {
+				display: contents;
+			}
+			:host([hidden]) {
+				display: none;
+			}
+			slot {
+				display: block;
+			}
+			::slotted(:last-child) {
+				--d2l-object-property-list-item-separator-display: none;
+			}
+		`];
+	}
+
+	render() {
+		return html`<slot class="d2l-body-small"></slot>`;
+	}
+}
+
+customElements.define('d2l-object-property-list', ObjectPropertyList);
