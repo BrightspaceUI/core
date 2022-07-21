57d20bcde2c60a9feaa8f41a53db1410717b46e8
dcostiuc <daniel.costiuc@D2L.com>
2022-07-21 16:33:07 -0400

    apply PR feedback: remove redundant test

--- components/switch/test/switch-visibility.test.js
+++ components/switch/test/switch-visibility.test.js
@@ -1,16 +1,10 @@
 import '../switch-visibility.js';
-import { expect, fixture, html } from '@open-wc/testing';
 import { runConstructor } from '../../../tools/constructor-test-helper.js';
 
 describe('d2l-switch-visibility', () => {
 
 	it('should construct', () => {
 		runConstructor('d2l-switch-visibility');
-	});
-
-	it('should have conditions if conditions are passed in', async() => {
-		const switchFixture = await fixture(html`<d2l-switch-visibility on>Conditions</d2l-switch-visibility>`);
-		expect(switchFixture._hasConditions).to.be.true;
 	});
 
 });
