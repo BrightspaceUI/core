d59970f2e58127017fa9b17c472d2ddb341b283a
Margaree Peacocke <margaree.p@gmail.com>
2022-08-04 14:36:04 -0400

    improve visual-diffs, add filter-tags to demo page

--- components/filter/test/filter-overflow-group.visual-diff.js
+++ components/filter/test/filter-overflow-group.visual-diff.js
@@ -14,64 +14,51 @@ describe('d2l-filter-overflow-group', () => {
 		await page.bringToFront();
 	});
 
-	// async function getShadowElem(id, selector) {
-	// 	return await page.evaluateHandle(
-	// 		`document.querySelector('${id}').shadowRoot.querySelector('${selector}')`
-	// 	);
-	// }
-
 	after(async() => await browser.close());
 
-	const minMaxTests = [
-		{
-			name: 'more-than-max-to-show'
-		},
-		{
-			name: 'less-than-min-to-show'
-		},
-		{
-			name: 'between-min-max-to-show'
-		},
-		{
-			name: 'exactly-max-to-show'
-		}
-	];
-
-	const hiddenButtonTests = [
-		{
-			name: 'ignores-hidden-filter',
-		}
-	];
-	// const autoShow = [
-	// 	{
-	// 		name: 'auto-show-small',
-	// 		action: async(selector) => {
-	// 			const overflowMenu = await getShadowElem(selector, '.d2l-overflow-dropdown');
-	// 			await overflowMenu.click();
-	// 		}
-	// 	},
-	// 	{
-	// 		name: 'auto-show',
-	// 		action: async(selector) => {
-	// 			const overflowMenu = await getShadowElem(selector, '.d2l-overflow-dropdown');
-	// 			await overflowMenu.click();
-	// 		}
-	// 	},
-	// ];
 	[
-		...hiddenButtonTests,
-		...minMaxTests,
-		// ...autoShow,
+		'more-than-max-to-show',
+		'one-more-than-max-to-show',
+		'less-than-min-to-show',
+		'between-min-max-to-show',
+		'exactly-max-to-show',
+		'ignores-hidden-filter',
+		'small-width'
 	].forEach((test) => {
-		it(test.name, async function() {
-			const selector = `#${test.name}`;
-			const containerSelector = `#${test.name}-container`;
+		it(test, async function() {
+			const containerSelector = `#${test}-container`;
 			await page.$eval(containerSelector || selector, (elem) => elem.scrollIntoView());
-			const rect = await visualDiff.getRect(page, containerSelector || selector);
-			if (test.action) {
-				await test.action(selector);
-			}
+			const rect = await visualDiff.getRect(page, containerSelector);
 			await visualDiff.screenshotAndCompare(page, `${this.test.fullTitle()}`, { captureBeyondViewport: false, clip: rect });
 		});
+	});
+
+	[700, 400, 320, 500].forEach((width) => {
+		describe(`basic at width ${width}`, () => {
+			const selector = '#basic';
+
+			before(async() => {
+				await page.$eval(selector, async(elem, width) => {
+					elem.parentNode.style.width = `${width}px`;
+					await elem.updateComplete;
+				}, width);
+				await page.waitForTimeout(200);
+			});
+
+			it('is correct', async function() {
+				const rect = await visualDiff.getRect(page, selector);
+				await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { captureBeyondViewport: false, clip: rect });
+			});
+		});
+	});
+
+	it.skip('click dropdown opener', async function() {
+		const overflowMenu = await page.evaluateHandle(
+			'document.querySelector("#more-than-max-to-show").shadowRoot.querySelector(".d2l-overflow-dropdown")'
+		);
+		await overflowMenu.click();
+		const rect = await visualDiff.getRect(page, '#more-than-max-to-show-container');
+		rect.height += 200;
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { captureBeyondViewport: false, clip: rect });
 	});
 });
