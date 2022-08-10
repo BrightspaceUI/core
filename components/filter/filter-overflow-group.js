f6c7e51e70ac6753d220309a2c3dfe2946e57199
Margaree Peacocke <margaree.p@gmail.com>
2022-08-10 08:30:22 -0400

    feat: Filter overflow group functionality

--- components/filter/filter-overflow-group.js
+++ components/filter/filter-overflow-group.js
@@ -1,26 +1,63 @@
-import { css, LitElement } from 'lit';
-import { OverflowGroupMixin } from '../overflow-group/overflow-group-mixin.js';
+import './filter.js';
+import { css, html, LitElement } from 'lit';
+import { OVERFLOW_CLASS, OverflowGroupMixin } from '../overflow-group/overflow-group-mixin.js';
+import { RtlMixin } from '../../mixins/rtl-mixin.js';
+
+function createFilterItem(node) {
+	const dimensionSets = node.querySelectorAll('d2l-filter-dimension-set');
+	const clones = Array.from(dimensionSets).map((set) => set.cloneNode(true));
+	return clones;
+}
 
 /**
  * A component that can be used to display a group of filters that will be put into an overflow filter when they no longer fit on the first line of their container
  * @slot - d2l-filters to be added to the container
 */
-class FilterOverflowGroup extends OverflowGroupMixin(LitElement) {
+class FilterOverflowGroup extends OverflowGroupMixin(RtlMixin(LitElement)) {
 
 	static get styles() {
 		return [super.styles, css`
 			::slotted(d2l-filter) {
 				margin-right: 0.3rem;
 			}
+			:host([dir="rtl"]) ::slotted(d2l-filter) {
+				margin-left: 0.3rem;
+				margin-right: 0;
+			}
 		`];
+	}
+
+	firstUpdated(changedProperties) {
+		super.firstUpdated(changedProperties);
+
+		this.addEventListener('d2l-filter-change', this._handleFilterChange);
 	}
 
 	convertToOverflowItem(node) {
 		const tagName = node.tagName.toLowerCase();
 		if (tagName !== 'd2l-filter') console.warn(`d2l-filter-overflow-group: ${tagName} is invalid in this group. This group should only contain d2l-filter direct child elements.`);
+		else return createFilterItem(node);
 	}
 
-	getOverflowContainer() {
+	getOverflowContainer(overflowItems) {
+		return html`
+			<d2l-filter class="${OVERFLOW_CLASS}" @d2l-filter-change="${this._handleFilterChange}">
+				${overflowItems}
+			</d2l-filter>
+		`;
+	}
+
+	_handleFilterChange(e) {
+		const target = (e.target.classList && e.target.classList.contains(OVERFLOW_CLASS)) ? this : e.target;
+		e.detail.dimensions.forEach((dimension) => {
+			const filterSet = target.querySelector(`d2l-filter-dimension-set[key=${dimension.dimensionKey}`);
+			if (!filterSet) return;
+			dimension.changes.forEach((change) => {
+				const filterSetValue = filterSet.querySelector(`d2l-filter-dimension-set-value[key=${change.valueKey}]`);
+				if (!filterSetValue) return;
+				filterSetValue.selected = change.selected;
+			});
+		});
 	}
 
 }
--- components/overflow-group/overflow-group-mixin.js
+++ components/overflow-group/overflow-group-mixin.js
@@ -2,6 +2,7 @@ import { css, html, nothing } from 'lit';
 import { LocalizeCoreElement } from '../../helpers/localize-core-element.js';
 import { offscreenStyles } from '../offscreen/offscreen.js';
 import ResizeObserver from 'resize-observer-polyfill/dist/ResizeObserver.es.js';
+import { styleMap } from 'lit/directives/style-map.js';
 
 export const OVERFLOW_CLASS = 'd2l-overflow-container';
 export const OVERFLOW_MINI_CLASS = 'd2l-overflow-container-mini';
@@ -28,8 +29,7 @@ export const OverflowGroupMixin = superclass => class extends LocalizeCoreElemen
 	static get properties() {
 		return {
 			/**
-			 * Use predefined classes on slot elements to set min and max slotted items to show
-			 * @type {boolean}
+			 * @ignore
 			 */
 			autoShow: {
 				type: Boolean,
@@ -96,6 +96,7 @@ export const OverflowGroupMixin = superclass => class extends LocalizeCoreElemen
 		this._resizeObserver = new ResizeObserver((entries) => requestAnimationFrame(() => this._handleResize(entries)));
 
 		this._isObserving = false;
+		this._itemHeight = 0;
 		this._mini = this.openerType === OPENER_TYPE.ICON;
 		this._overflowContainerHidden = false;
 		this._slotItems = [];
@@ -129,8 +130,13 @@ export const OverflowGroupMixin = superclass => class extends LocalizeCoreElemen
 			}
 		});
 
+		const containerStyles = {
+			minHeight: `${this._itemHeight}px`,
+			maxHeight: `${this._itemHeight}px`
+		};
+
 		return html`
-			<div class="d2l-overflow-group-container">
+			<div class="d2l-overflow-group-container" style="${styleMap(containerStyles)}">
 				<slot @slotchange="${this._handleSlotChange}"></slot>
 				${overflowContainer}
 			</div>
@@ -277,12 +283,15 @@ export const OverflowGroupMixin = superclass => class extends LocalizeCoreElemen
 
 	_getItemLayouts(filteredNodes) {
 		const items = filteredNodes.map((node) => {
+			node.removeAttribute('data-is-chomped');
 			const computedStyles = window.getComputedStyle(node);
+			const itemHidden = computedStyles.display === 'none';
+			this._itemHeight = !itemHidden ? Math.max(this._itemHeight, Math.ceil(parseFloat(computedStyles.height))) : this._itemHeight;
 
 			return {
 				type: node.tagName.toLowerCase(),
 				isChomped: false,
-				isHidden: computedStyles.display === 'none',
+				isHidden: itemHidden,
 				width: Math.ceil(parseFloat(computedStyles.width) || 0)
 					+ parseInt(computedStyles.marginRight) || 0
 					+ parseInt(computedStyles.marginLeft) || 0,
@@ -318,7 +327,9 @@ export const OverflowGroupMixin = superclass => class extends LocalizeCoreElemen
 
 		this._updateOverflowItemsRequested = true;
 		setTimeout(() => {
-			this._overflowItems = this._slotItems.map(node => this.convertToOverflowItem(node));
+			this._itemLayouts = this._getItemLayouts(this._slotItems);
+			this._overflowItems = this._slotItems.map((node) => this.convertToOverflowItem(node));
+			this._chomp();
 			this._updateOverflowItemsRequested = false;
 			this.requestUpdate();
 		}, 0);
