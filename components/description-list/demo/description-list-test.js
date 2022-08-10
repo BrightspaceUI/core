7742437efa66e607afdfe415fe5e33dde6e3dd75
James Klassen <James.Klassen@D2L.com>
2022-08-10 08:21:27 -0500

    clean up `d2l-test-dl` render logic

--- components/description-list/demo/description-list-test.js
+++ components/description-list/demo/description-list-test.js
@@ -41,29 +41,34 @@ class TestDescriptionList extends LitElement {
 	}
 
 	render() {
+		return html`
+			<d2l-dl-wrapper breakpoint="${this.breakpoint}">
+				${this._renderExample()}
+			</d2l-dl-wrapper>
+		`;
+	}
+
+	_renderExample() {
 		if (this.type === 'default') {
 			return html`
-				<d2l-dl-wrapper>
-					<dl>
-						<dt>Course Name</dt>
-						<dd>Brightspace 101B</dd>
+				<dl>
+					<dt>Course Name</dt>
+					<dd>Brightspace 101B</dd>
 
-						<dt>Course Code</dt>
-						<dd>BSPC 101B</dd>
+					<dt>Course Code</dt>
+					<dd>BSPC 101B</dd>
 
-						<dt>Start Date</dt>
-						<dd>June 14 2022</dd>
+					<dt>Start Date</dt>
+					<dd>June 14 2022</dd>
 
-						<dt>Semester</dt>
-						<dd>2022 Summer</dd>
-					</dl>
-				</d2l-dl-wrapper>
+					<dt>Semester</dt>
+					<dd>2022 Summer</dd>
+				</dl>
 			`;
 		}
 
 		if (this.type === 'long') {
 			return html`
-				<d2l-dl-wrapper breakpoint="300">
 					<dl>
 						<dt>Course Code That Represents The Course as a Short String</dt>
 						<dd>A short string that represents the course, often with important information such as section or room number.</dd>
@@ -71,58 +76,51 @@ class TestDescriptionList extends LitElement {
 						<dt>Availability Dates</dt>
 						<dd>The start and end date for the course. Learners can't access courses outside these dates.</dd>
 					</dl>
-				</d2l-dl-wrapper>
 			`;
 		}
 
 		if (this.type === 'activity-display') {
 			return html`
-				<d2l-dl-wrapper>
-					<dl>
-						<dt>Submission type</dt>
-						<dd>File submission</dd>
+				<dl>
+					<dt>Submission type</dt>
+					<dd>File submission</dd>
 
-						<dt>Files allowed per submission</dt>
-						<dd>Unlimited</dd>
+					<dt>Files allowed per submission</dt>
+					<dd>Unlimited</dd>
 
-						<dt>File extension</dt>
-						<dd>PDF</dd>
+					<dt>File extension</dt>
+					<dd>PDF</dd>
 
-						<dt>Allowed Submissions</dt>
-						<dd>Only one submission allowed</dd>
-					</dl>
-				</d2l-dl-wrapper>
+					<dt>Allowed Submissions</dt>
+					<dd>Only one submission allowed</dd>
+				</dl>
 			`;
 		}
 
 		if (this.type === 'bulk-course-import') {
 			return html`
-				<d2l-dl-wrapper>
-					<dl>
-						<dt>Course code</dt>
-						<dd>fd6b9fd8-29bd-44ef-8322-d6b379b71411</dd>
+				<dl>
+					<dt>Course code</dt>
+					<dd>fd6b9fd8-29bd-44ef-8322-d6b379b71411</dd>
 
-						<dt>Course name</dt>
-						<dd>Telkom MOMP Course 1</dd>
+					<dt>Course name</dt>
+					<dd>Telkom MOMP Course 1</dd>
 
-						<dt>Course ID</dt>
-						<dd>250003</dd>
-					</dl>
-				</d2l-dl-wrapper>
+					<dt>Course ID</dt>
+					<dd>250003</dd>
+				</dl>
 			`;
 		}
 
 		if (this.type === 'slotted') {
 			return html`
-				<d2l-dl-wrapper>
-					<dl style="align-items: center">
-						<dt>User</dt>
-						<dd><div class="user"><div class="avatar">JS</div>John Smith</div></dd>
+				<dl style="align-items: center">
+					<dt>User</dt>
+					<dd><div class="user"><div class="avatar">JS</div>John Smith</div></dd>
 
-						<dt>Submitted</dt>
-						<dd>Dec 30, 2021 5:34 PM</dd>
-					</dl>
-				</d2l-dl-wrapper>
+					<dt>Submitted</dt>
+					<dd>Dec 30, 2021 5:34 PM</dd>
+				</dl>
 			`;
 		}
 	}
