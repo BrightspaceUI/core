7b9491ed5a17a5a40286d05821158dfd26b6a8eb
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2022-09-21 13:07:23 -0400

    try to reproduce

--- components/list/test/list.visual-diff.js
+++ components/list/test/list.visual-diff.js
@@ -73,6 +73,7 @@ describe('d2l-list', () => {
 		page = await visualDiff.createPage(browser, { viewport: { width: 1000, height: 6500 } });
 		await page.goto(`${visualDiff.getBaseUrl()}/components/list/test/list.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
+		page.on('console', msg => console.log(msg.text()));
 	});
 
 	beforeEach(async() => {
@@ -82,94 +83,31 @@ describe('d2l-list', () => {
 	after(async() => await browser.close());
 
 	[
-		{ category: 'general', tests: [
-			{ name: 'simple', selector: '#simple' },
-			{ name: 'no padding', selector: '#noPadding' }
-		] },
-		{ category: 'illustration', tests: [
-			{ name: 'default', selector: '#illustration' }
-		] },
-		{ category: 'separators', tests: [
-			{ name: 'default', selector: '#simple' },
-			{ name: 'none', selector: '#separatorsNone' },
-			{ name: 'all', selector: '#separatorsAll' },
-			{ name: 'between', selector: '#separatorsBetween' },
-			{ name: 'extended', selector: '#separatorsExtended' }
-		] },
-		{ category: 'actions', tests: [
-			{ name: 'default', selector: '#actions' },
-			{ name: 'extended separators', selector: '#actionsSeparatorsExtended' },
-			{ name: 'rtl', selector: '#actionsRtl' }
-		] },
-		{ category: 'item-content', tests: [
-			{ name: 'all', selector: '#itemContent' },
-			{ name: 'no padding', selector: '#itemContentNoPadding' }
-		] },
-		{ category: 'href', tests: [
-			{ name: 'default', selector: '#href' },
-			{ name: 'focus', selector: '#href', action: () => focusLink('#href d2l-list-item') },
-			{ name: 'hover', selector: '#href', action: () => hover('#href d2l-list-item') }
-		] },
-		{ category: 'button', tests: [
-			{ name: 'default', selector: '#button' },
-			{ name: 'focus', selector: '#button', action: () => focusButton('#button d2l-list-item-button') },
-			{ name: 'hover', selector: '#button', action: () => hover('#button d2l-list-item-button') }
-		] },
-		{ category: 'selectable', tests: [
-			{ name: 'not selected', selector: '#selectable' },
-			{ name: 'not selected focus', selector: '#selectable', action: () => focusInput('#selectable [selectable]') },
-			{ name: 'not selected hover', selector: '#selectable', action: () => hover('#selectable [selectable]') },
-			{ name: 'selected', selector: '#selectableSelected' },
-			{ name: 'selected focus', selector: '#selectableSelected', action: () => focusInput('#selectableSelected [selectable]') },
-			{ name: 'selected hover', selector: '#selectableSelected', action: () => hover('#selectableSelected [selectable]') },
-			{ name: 'item-content', selector: '#selectableItemContent' },
-			{ name: 'skeleton', selector: '#selectableSkeleton' },
-			{ name: 'extended separators', selector: '#selectableSeparatorsExtended' }
-		] },
-		{ category: 'header', tests: [
-			{ name: 'not selectable', selector: '#noSelectableHeader' },
-			{ name: 'none selected', selector: '#selectableHeader' },
-			{ name: 'some selected', selector: '#selectableSomeSelectedHeader' },
-			{ name: 'all selected', selector: '#selectableAllSelectedHeader' },
-			{ name: 'all selected pages', selector: '#selectableAllSelectedHeaderPages' },
-			{ name: 'sticky top', selector: '#stickyHeader', action: () => scrollTo('#stickyHeader > div', 0) },
-			{ name: 'sticky scrolled', selector: '#stickyHeader', action: () => scrollTo('#stickyHeader > div', 45) }
-		] },
-		{ category: 'draggable', tests: [
-			{ name: 'default', selector: '#draggable' },
-			{ name: 'focus', selector: '#draggable', action: () => focusMethod('#draggable [key="1"]') },
-			{ name: 'hover', selector: '#draggable', action: () => hover('#draggable [key="1"]') },
-			{ name: 'selectable', selector: '#draggableSelectable' },
-			{ name: 'selectable focus', selector: '#draggableSelectable', action: () => focusInput('#draggableSelectable [key="1"]') },
-			{ name: 'selectable hover', selector: '#draggableSelectable', action: () => hover('#draggableSelectable [key="1"]') },
-			{ name: 'extended separators', selector: '#draggableSeparatorsExtended' }
-		] },
-		{ category: 'focus method', tests: [
-			{ name: 'href', selector: '#href', action: () => focusMethod('#href d2l-list-item') },
-			{ name: 'button', selector: '#button', action: () => focusMethod('#button d2l-list-item-button') },
-			{ name: 'selectable', selector: '#selectable', action: () => focusMethod('#selectable [selectable]') },
-			{ name: 'actions', selector: '#actions', action: () => focusMethod('#actions d2l-list-item') }
-		] },
-		{ category: 'breakpoints', tests: [
-			{ name: '842', selector: '#breakpoint-842' },
-			{ name: '636', selector: '#breakpoint-636' },
-			{ name: '580', selector: '#breakpoint-580' },
-			{ name: '0', selector: '#breakpoint-0' }
-		] },
-		{ category: 'dropdown', tests: [
-			{ name: 'open down', selector: '#dropdown-tooltips', action: () => openDropdown('#open-down'), after: () => closeDropdown('#open-down') }
-		] },
-		{ category: 'tooltip', tests: [
-			{ name: 'open down', selector: '#dropdown-tooltips', action: () => showTooltip('#open-down'), after: () => hideTooltip('#open-down') }
-		] },
 		{ category: 'nested', tests: [
-			{ name: 'none-selected', selector: '#nested-none-selected' },
-			{ name: 'some-selected', selector: '#nested-some-selected' },
-			{ name: 'all-selected', selector: '#nested-all-selected', action: () => wait(100) }
+			{ name: 'all-selected-1', selector: '#nested-all-selected', action: () => page.waitForFunction(() => {
+				console.log(document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length);
+				return document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length === 5;
+			}) },
+			{ name: 'all-selected-2', selector: '#nested-all-selected', action: () => page.waitForFunction(() => {
+				console.log(document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length);
+				return document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length === 5;
+			}) },
+			{ name: 'all-selected-3', selector: '#nested-all-selected', action: () => page.waitForFunction(() => {
+				console.log(document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length);
+				return document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length === 5;
+			}) },
+			{ name: 'all-selected-4', selector: '#nested-all-selected', action: () => page.waitForFunction(() => {
+				console.log(document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length);
+				return document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length === 5;
+			}) },
+			{ name: 'all-selected-5', selector: '#nested-all-selected', action: () => page.waitForFunction(() => {
+				console.log(document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length);
+				return document.querySelector('#nested-all-selected d2l-list').getSelectionInfo(true).keys.length === 5;
+			}) }
 		] }
 	].forEach((info) => {
 
-		describe(info.category, () => {
+		describe.only(info.category, () => {
 
 			info.tests.forEach((info) => {
 				afterEach(async function() {
@@ -185,9 +123,9 @@ describe('d2l-list', () => {
 					if (info.action) {
 						await info.action();
 					}
-					await page.evaluate(() => {
+					/*await page.evaluate(() => {
 						return new Promise(resolve => setTimeout(resolve, 0));
-					});
+					});*/
 					const rect = await (info.rect ? info.rect() : visualDiff.getRect(page, info.selector, 24));
 					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 				});
