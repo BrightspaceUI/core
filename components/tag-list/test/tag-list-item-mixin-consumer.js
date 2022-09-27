840218888a4f676286d29b01b7dbe2fdd7dd23a7
Dave Batiste <dave.batiste@d2l.com>
2022-09-27 09:12:32 -0400

    Remove unnecessary div from test component.

--- components/tag-list/test/tag-list-item-mixin-consumer.js
+++ components/tag-list/test/tag-list-item-mixin-consumer.js
@@ -57,7 +57,7 @@ class TagListItemMixinConsumer extends TagListItemMixin(LitElement) {
 
 		return html`
 			<d2l-dropdown open-on-hover>
-				<div>${this._renderTag(tagContent, { focusableClass: 'd2l-dropdown-opener' })}</div>
+				${this._renderTag(tagContent, { focusableClass: 'd2l-dropdown-opener' })}
 				<d2l-dropdown-content no-auto-focus>Custom</d2l-dropdown-content>
 			</d2l-dropdown>
 		`;
