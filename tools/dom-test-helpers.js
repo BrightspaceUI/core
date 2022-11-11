af0aed6a63f7578a827e50ae859431bac58ffded
Dave Batiste <dave.batiste@d2l.com>
2022-11-11 13:56:08 -0500

    feat: Add helper to enable waiting for updateComplete on all elements in a sub-tree. (#2995)

--- tools/dom-test-helpers.js
+++ tools/dom-test-helpers.js
@@ -1,3 +1,4 @@
+import { getComposedChildren } from '../helpers/dom.js';
 
 export function keyDown(element, keycode) {
 	const event = new CustomEvent('keydown', {
@@ -9,4 +10,19 @@ export function keyDown(element, keycode) {
 	event.keyCode = keycode;
 	event.code = keycode;
 	element.dispatchEvent(event);
+}
+
+export async function getUpdateCompleteAll(node) {
+
+	if (!node || (node.nodeType !== Node.ELEMENT_NODE && node.nodeType !== Node.DOCUMENT_NODE && node.nodeType !== Node.DOCUMENT_FRAGMENT_NODE)) {
+		throw new TypeError('Invalid node. Must be nodeType document, element or document fragment');
+	}
+
+	const getUpdateComplete = async elem => {
+		if (elem.updateComplete) await elem.updateComplete;
+		const childElements = getComposedChildren(elem);
+		await Promise.all(childElements.map(childElement => getUpdateComplete(childElement)));
+	};
+
+	return getUpdateComplete(node);
 }
