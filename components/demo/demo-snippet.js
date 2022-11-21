2f9ec24cec4fb9de3ad50036127db62d97f7318b
Dave Batiste <dave.batiste@d2l.com>
2022-11-21 13:18:05 -0500

    Use elevated background-color for snippet.

--- components/demo/demo-snippet.js
+++ components/demo/demo-snippet.js
@@ -22,7 +22,7 @@ class DemoSnippet extends LitElement {
 	static get styles() {
 		return css`
 			:host {
-				background-color: var(--d2l-color-background-elevated);
+				background-color: var(--d2l-color-background-default);
 				border: 1px solid var(--d2l-color-container-border);
 				border-radius: 6px;
 				box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2); /* todo: dark-mode */
