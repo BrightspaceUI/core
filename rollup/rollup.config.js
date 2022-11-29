4aa6afa504c83820ff9a8637c3802b2a1eeaebe9
Erik Koopmans <erik.koopmans@d2l.com>
2022-11-29 16:26:25 -0500

    feat: Add build version and date to demo index (#3022)

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -2,10 +2,16 @@ import copy from 'rollup-plugin-copy';
 import del from 'rollup-plugin-delete';
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 import glob from 'glob-all';
+import { readFileSync } from 'fs';
+import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
+
+const { version } = JSON.parse(readFileSync('./package.json'));
+const buildDate = Intl.DateTimeFormat('en-CA', { timeZone: 'America/Toronto' }).format(new Date());
 
 const jsGlob = [
 	'@(components|controllers|directives|helpers|mixins|templates)/**/*.js',
+	'./index.js',
 	'!**/*@(test|axe|visual-diff).js',
 ];
 const nonJsGlob = [
@@ -27,6 +33,14 @@ export default {
 				dest: 'build',
 				rename: (_name, _extension, fullpath) => fullpath,
 			}],
+		}),
+		replace({
+			include: './index.js',
+			preventAssignment: false,
+			values: {
+				'window.__buildDate__': JSON.stringify(buildDate),
+				'window.__buildVersion__': JSON.stringify(version),
+			},
 		}),
 		resolve(),
 		dynamicImportVars(),
