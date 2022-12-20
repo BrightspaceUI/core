2d15d8c96cf760d32f18191d27c372bb1690b262
James Klassen <James.Klassen@D2L.com>
2022-12-20 13:09:28 -0600

    cr: put `"` around string properties in css

--- components/collapsible-panel/collapsible-panel.js
+++ components/collapsible-panel/collapsible-panel.js
@@ -77,12 +77,12 @@ class CollapsiblePanel extends RtlMixin(LitElement) {
 				--d2l-collapsible-panel-arrow-time: 0.5s;
 				display: block;
 			}
+			:host([padding="large"][type="inline"]) {
+				--d2l-collapsible-panel-spacing-inline: 2rem;
+			}
 			.d2l-collapsible-panel {
 				border: 1px solid var(--d2l-color-mica);
 				border-radius: 0.3rem;
-			}
-			:host([padding=large][type=inline]) .d2l-collapsible-panel {
-				--d2l-collapsible-panel-spacing-inline: 2rem;
 			}
 			:host(:not([expanded])) .d2l-collapsible-panel {
 				cursor: pointer;
