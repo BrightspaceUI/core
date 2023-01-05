8267cc7dd819c63a9ebe618ab1e835e881759e38
James Klassen <James.Klassen@D2L.com>
2023-01-05 16:13:49 -0600

    fix: more explicit vdiff tests

--- components/collapsible-panel/test/collapsible-panel.visual-diff.js
+++ components/collapsible-panel/test/collapsible-panel.visual-diff.js
@@ -11,11 +11,6 @@ describe('d2l-collapsible-panel', () => {
 	before(async() => {
 		browser = await puppeteer.launch();
 		page = await visualDiff.createPage(browser);
-
-		// TODO: remove this before merge
-		page.on('console', (msg) => {
-			console.log(msg.text());
-		});
 	});
 
 	beforeEach(async() => await visualDiff.resetFocus(page));
@@ -36,6 +31,38 @@ describe('d2l-collapsible-panel', () => {
 		[
 			{ name: 'default', selector: '#default' },
 			{ name: 'default-focus', selector: '#default', action: focusElement },
+			{ name: 'default-expanded', selector: '#default-expanded' },
+			{ name: 'default-expanded-focus', selector: '#default-expanded', action: focusElement },
+			{ name: 'default-summary', selector: '#default-summary' },
+			{ name: 'default-summary-expanded', selector: '#default-summary-expanded' },
+			{ name: 'default-summary-focus', selector: '#default-summary', action: focusElement },
+			{ name: 'default-large-padding', selector: '#default-large-padding' },
+			{ name: 'default-large-padding', selector: '#default-large-padding-summary' },
+			{ name: 'default-large-padding-expanded', selector: '#default-large-padding-expanded' },
+			{ name: 'subtle', selector: '#subtle' },
+			{ name: 'subtle-focus', selector: '#subtle', action: focusElement },
+			{ name: 'subtle-expanded', selector: '#subtle-expanded' },
+			{ name: 'subtle-expanded-focus', selector: '#subtle-expanded', action: focusElement },
+			{ name: 'subtle-summary', selector: '#subtle-summary' },
+			{ name: 'subtle-summary-expanded', selector: '#subtle-summary-expanded' },
+			{ name: 'subtle-summary-focus', selector: '#subtle-summary', action: focusElement },
+			{ name: 'subtle-large-padding', selector: '#subtle-large-padding' },
+			{ name: 'subtle-large-padding', selector: '#subtle-large-padding-summary' },
+			{ name: 'subtle-large-padding-expanded', selector: '#subtle-large-padding-expanded' },
+			{ name: 'inline', selector: '#inline' },
+			{ name: 'inline-focus', selector: '#inline', action: focusElement },
+			{ name: 'inline-expanded', selector: '#inline-expanded' },
+			{ name: 'inline-expanded-focus', selector: '#inline-expanded', action: focusElement },
+			{ name: 'inline-summary', selector: '#inline-summary' },
+			{ name: 'inline-summary-expanded', selector: '#inline-summary-expanded' },
+			{ name: 'inline-summary-focus', selector: '#inline-summary', action: focusElement },
+			{ name: 'inline-large-padding', selector: '#inline-large-padding' },
+			{ name: 'inline-large-padding', selector: '#inline-large-padding-summary' },
+			{ name: 'inline-large-padding-expanded', selector: '#inline-large-padding-expanded' },
+			{ name: 'custom', selector: '#custom' },
+			{ name: 'custom-expanded', selector: '#custom-expanded' },
+			{ name: 'custom-summary', selector: '#custom-summary' },
+			{ name: 'custom-summary-expanded', selector: '#custom-summary-expanded' },
 		].forEach((info) => {
 
 			it(info.name, async function() {
@@ -56,6 +83,19 @@ describe('d2l-collapsible-panel', () => {
 		[
 			{ name: 'default', selector: '#default' },
 			{ name: 'default-focus', selector: '#default', action: focusElement },
+			{ name: 'default-expanded', selector: '#default-expanded' },
+			{ name: 'default-summary', selector: '#default-summary' },
+			{ name: 'subtle', selector: '#subtle' },
+			{ name: 'subtle-focus', selector: '#subtle', action: focusElement },
+			{ name: 'subtle-expanded', selector: '#subtle-expanded' },
+			{ name: 'subtle-summary', selector: '#subtle-summary' },
+			{ name: 'inline', selector: '#inline' },
+			{ name: 'inline-focus', selector: '#inline', action: focusElement },
+			{ name: 'inline-expanded', selector: '#inline-expanded' },
+			{ name: 'inline-summary', selector: '#inline-summary' },
+			{ name: 'custom', selector: '#custom' },
+			{ name: 'custom-expanded', selector: '#custom-expanded' },
+			{ name: 'custom-summary', selector: '#custom-summary' },
 		].forEach((info) => {
 
 			it(info.name, async function() {
