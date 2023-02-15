39fb9d31fa6dc9750d38917b41327d32b461d572
Mitchel Paulin <Mitchel.Paulin@D2L.com>
2023-02-15 14:34:14 -0500

    Update the role when the parent lists grid attribute changes

--- components/list/list-item-role-mixin.js
+++ components/list/list-item-role-mixin.js
@@ -24,12 +24,26 @@ export const ListItemRoleMixin = superclass => class extends superclass {
 		const parent = findComposedAncestor(this.parentNode, node => node && node.tagName === 'D2L-LIST');
 		if (!parent) return;
 
+		this.observer = new MutationObserver(() => { this.role = parent.hasAttribute('grid') ? 'rowgroup' : 'listitem'; });
+		this.observer.observe(parent, {
+			attributes: true, /* required for legacy-Edge, otherwise attributeFilter throws a syntax error */
+			attributeFilter: ['grid'],
+			childList: false,
+			subtree: false,
+			characterData: false
+		});
+
 		const separators = parent.getAttribute('separators');
 
 		this.role = parent.hasAttribute('grid') ? 'rowgroup' : 'listitem';
 		this._nested = (parent.slot === 'nested');
 		this._separators = separators || undefined;
 		this._extendSeparators = parent.hasAttribute('extend-separators');
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		this.observer.disconnect();
 	}
 
 };
