ba37ae3dcd1802db6a8c69fb29ebb6c5151809fd
Danny Gleckler <daniel.gleckler@d2l.com>
2023-02-17 18:33:46 -0500

    Use href in tabs test

--- components/tabs/test/tabs.axe.js
+++ components/tabs/test/tabs.axe.js
@@ -9,7 +9,7 @@ describe('d2l-tabs', () => {
 			<d2l-tabs>
 				<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
 				<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
-				<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
+				<d2l-tab-panel text="Chemistry" href="chemistry">Tab content for Chemistry</d2l-tab-panel>
 			</d2l-tabs>`);
 		await expect(el).to.be.accessible();
 	});
