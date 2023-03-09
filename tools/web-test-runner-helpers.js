79cc4c6ce2dac5a94d6deeebd1c0d710938040f6
Erik Koopmans <erik.koopmans@d2l.com>
2023-03-09 09:45:35 -0500

    Clean up line breaks

--- tools/web-test-runner-helpers.js
+++ tools/web-test-runner-helpers.js
@@ -4,8 +4,8 @@ export const focusWithKeyboard = async(element) => {
 	await sendKeys({ press: 'Tab' });
 	element.focus();
 };
+
 export const focusWithMouse = async(element) => {
 	const { x, y } = element.getBoundingClientRect();
 	await sendMouse({ type: 'click', position: [x, y] });
 };
-
