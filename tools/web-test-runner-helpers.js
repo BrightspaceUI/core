2f9a7af2d2e10841a513a81b2dc8c57fe2fb35b8
Erik Koopmans <erik.koopmans@d2l.com>
2023-03-09 10:44:52 -0500

    Fix firefox breaking test

--- tools/web-test-runner-helpers.js
+++ tools/web-test-runner-helpers.js
@@ -1,11 +1,12 @@
-import { sendKeys, sendMouse } from '@web/test-runner-commands';
+import { resetMouse, sendKeys, sendMouse } from '@web/test-runner-commands';
 
 export const focusWithKeyboard = async(element) => {
 	await sendKeys({ press: 'Tab' });
-	element.focus();
+	element.focus({ focusVisible: true });
 };
 
 export const focusWithMouse = async(element) => {
 	const { x, y } = element.getBoundingClientRect();
-	await sendMouse({ type: 'click', position: [x, y] });
+	await sendMouse({ type: 'click', position: [Math.ceil(x), Math.ceil(y)] });
+	await resetMouse();
 };
