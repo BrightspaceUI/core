a1930f9972962eda514a4a28fe9898ae3764c2a6
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-03-15 16:48:13 -0400

    slightly less magic

--- components/inputs/test/input-color.visual-diff.js
+++ components/inputs/test/input-color.visual-diff.js
@@ -36,7 +36,7 @@ describe('d2l-input-color', () => {
 	].forEach((name) => {
 		it(name, async function() {
 			const rect = await visualDiff.getRect(page, `#${name} > d2l-input-color`);
-			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect, captureBeyondViewport: false });
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 		});
 		it(`${name}-focus`, async function() {
 			const e = oneEvent(page, `#${name} > d2l-input-color`, 'd2l-tooltip-show');
