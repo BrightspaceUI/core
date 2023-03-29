a56a0214e567f6b0962c91b23e1b02ead1e62e20
Erik Koopmans <erik.koopmans@d2l.com>
2023-03-29 17:56:57 -0400

    feat: Support subscribers on pageable-mixin

--- components/paging/pageable-mixin.js
+++ components/paging/pageable-mixin.js
@@ -1,35 +1,59 @@
 import { html } from 'lit';
+import { SubscriberRegistryController } from '../../controllers/subscriber/subscriberControllers.js';
 
 export const PageableMixin = superclass => class extends superclass {
 
+	static get properties() {
+		return {
+			/**
+			 * Total number of items. If not specified, neither it nor the count of items showing will be displayed.
+			 * @type {number}
+			 */
+			itemCount: { type: Number, attribute: 'item-count', reflect: true },
+			_itemShowingCount: { state: true },
+		};
+	}
+
 	constructor() {
 		super();
-		this._pageable = true;
+
+		this.itemCount = -1;
+		this._itemShowingCount = 0;
+		this._pageableSubscriberRegistry = new SubscriberRegistryController(this, 'pageable', {
+			onSubscribe: this._updatePageableSubscriber.bind(this),
+			updateSubscribers: this._updatePageableSubscribers.bind(this)
+		});
+	}
+
+	updated(changedProperties) {
+		super.updated(changedProperties);
+
+		if (changedProperties.has('itemCount') || changedProperties.has('_itemShowingCount')) {
+			this._pageableSubscriberRegistry.updateSubscribers();
+		}
 	}
 
 	/* must be implemented by consumer */
 	_getItemByIndex(index) { } // eslint-disable-line no-unused-vars
 
-	/* must be implemented by consumer */
-	async _getItemsShowingCount() { }
-
-	/* must be implemented by consumer */
-	_getLastItemIndex() { }
-
-	async _handlePagerSlotChange(e) {
-		this._updatePagerCount(await this._getItemsShowingCount(), e.target);
+	_getLastItemIndex() {
+		return this._itemShowingCount - 1;
 	}
 
 	_renderPagerContainer() {
-		return html`<slot name="pager" @slotchange="${this._handlePagerSlotChange}"></slot>`;
+		return html`<slot name="pager"></slot>`;
 	}
 
-	_updatePagerCount(count, slot) {
-		if (!slot) slot = this.shadowRoot.querySelector('slot[name="pager"]');
-		const elements = slot.assignedElements({ flatten: true });
-		if (elements.length > 0) {
-			elements[0].itemShowingCount = count;
-		}
+	_setItemShowingCount(count) {
+		this._itemShowingCount = count;
+	}
+
+	_updatePageableSubscriber(subscriber) {
+		subscriber._pageableInfo = { itemShowingCount: this._itemShowingCount, itemCount: this.itemCount };
+	}
+
+	_updatePageableSubscribers(subscribers) {
+		subscribers.forEach(subscriber => this._updatePageableSubscriber(subscriber));
 	}
 
 };
---
+++ components/paging/pageable-subscriber-mixin.js
@@ -0,0 +1,28 @@
+import { EventSubscriberController, IdSubscriberController } from '../../controllers/subscriber/subscriberControllers.js';
+
+export const PageableSubscriberMixin = superclass => class extends superclass {
+
+	static get properties() {
+		return {
+			/**
+			 * Id of the `PageableMixin` component this component wants to observe (if not located within that component)
+			 * @type {string}
+			 */
+			pageableFor: { type: String, reflect: true, attribute: 'pageable-for' },
+			_pageableInfo: { state: true }
+		};
+	}
+
+	constructor() {
+		super();
+
+		this._pageableInfo = { itemCount: -1, itemShowingCount: 0 };
+		this._pageableEventSubscriber = new EventSubscriberController(this, 'pageable');
+		this._pageableIdSubscriber = new IdSubscriberController(this, 'pageable', { idPropertyName: 'pageableFor' });
+	}
+
+	_getPageableRegistries() {
+		return this.pageableFor ? this._pageableIdSubscriber.registries : [ this._pageableEventSubscriber.registry ];
+	}
+
+};
