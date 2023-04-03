de9dd722fae091be2f94b9a595518bbe92bd2927
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2023-04-03 10:14:00 -0400

    US150791 - Fix flaky list visual-diff

--- components/list/test/list.visual-diff.js
+++ components/list/test/list.visual-diff.js
@@ -147,8 +147,8 @@ describe('d2l-list', () => {
 			{ name: 'some selected', selector: '#selectableSomeSelectedControls' },
 			{ name: 'all selected', selector: '#selectableAllSelectedControls' },
 			{ name: 'all selected pages', selector: '#selectableAllSelectedControlsPages' },
-			{ name: 'sticky top', selector: '#stickyControls', action: () => scrollTo('#stickyControls > div', 0) },
-			{ name: 'sticky scrolled', selector: '#stickyControls', action: () => scrollTo('#stickyControls > div', 45) }
+			{ name: 'sticky top', selector: '#stickyControls', action: () => scrollTo('#stickyControls > div', 0), screenshotOptions: { captureBeyondViewport: false } },
+			{ name: 'sticky scrolled', selector: '#stickyControls', action: () => scrollTo('#stickyControls > div', 45), screenshotOptions: { captureBeyondViewport: false } }
 		] },
 		{ category: 'draggable', tests: [
 			{ name: 'default', selector: '#draggable' },
@@ -211,8 +211,11 @@ describe('d2l-list', () => {
 					await page.evaluate(() => {
 						return new Promise(resolve => setTimeout(resolve, 0));
 					});
+
+					const options = info.screenshotOptions || {};
 					const rect = await (info.rect ? info.rect() : visualDiff.getRect(page, info.selector, 24));
-					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+					options.clip = rect;
+					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), options);
 				});
 			});
 
