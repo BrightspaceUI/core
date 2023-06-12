df4b9dd7efc3019ac239d784d0e21f4b84bb4cf5
Danny Gleckler <daniel.gleckler@d2l.com>
2023-06-12 10:12:05 -0400

    Bump @brightspace-ui/testing

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,4 +1,4 @@
-import { createConfig, getBrowsers } from '@brightspace-ui/testing';
+import { createConfig, getBrowsers } from '@brightspace-ui/testing/wtr-config.js';
 
 const pattern = type => `+(components|controllers|directives|helpers|mixins|templates)/**/*.${type}.js`;
 
