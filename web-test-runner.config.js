5c072e0951aeecc7891798bbc98c40adc0fffe23
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2023-07-05 14:40:05 -0400

    Try running all three at once

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -3,6 +3,7 @@ import { createConfig, getBrowsers } from '@brightspace-ui/testing/wtr-config.js
 const pattern = type => `+(components|controllers|directives|helpers|mixins|templates)/**/*.${type}.js`;
 
 export default createConfig({
+	concurrentBrowsers: 3,
 	pattern,
 	groups: [{
 		name: 'aXe',
