512d7fa3e7e21335b0d8d7c8646f8b0bfea6accc
Danny Gleckler <daniel.gleckler@d2l.com>
2023-07-10 09:36:27 -0400

    Simplify with do...while; Always return previousElement

--- components/list/list-item-generic-layout.js
+++ components/list/list-item-generic-layout.js
@@ -406,21 +406,18 @@ class ListItemGenericLayout extends RtlMixin(LitElement) {
 		while (previousElement) {
 			if (previousElement.role === 'rowgroup') {
 
-				// this check needs to account for standard list-items as well as custom
-				let nestedList = previousElement.querySelector('[slot="nested"]') || previousElement.shadowRoot.querySelector('d2l-list');
-				if (nestedList) {
-					let nestedListItems = [...nestedList.children].filter(node => node.role === 'rowgroup');
-
-					let lastItem;
-					while (nestedListItems?.length) {
-						lastItem = nestedListItems[nestedListItems.length - 1];
-						nestedList = lastItem.querySelector('[slot="nested"]') || lastItem.shadowRoot.querySelector('d2l-list');
-						nestedListItems = nestedList && [...nestedList.children].filter(node => node.role === 'rowgroup');
+				let nestedList;
+				do {
+					// this check needs to account for standard list-items as well as custom
+					nestedList = previousElement.querySelector('[slot="nested"]') || previousElement.shadowRoot.querySelector('d2l-list');
+					if (nestedList) {
+						const nestedListItems = [...nestedList.children].filter(node => node.role === 'rowgroup');
+						if (nestedListItems.length) {
+							previousElement = nestedListItems[nestedListItems.length - 1];
+						}
 					}
-					return lastItem;
-				}
+				}	while (nestedList);
 				return previousElement;
-
 			}
 			previousElement = previousElement.previousElementSibling;
 		}
