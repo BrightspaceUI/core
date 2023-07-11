e0bb9dac34ae1bc8a2bd83ce25ffd996b6915d01
James Klassen <James.Klassen@D2L.com>
2023-07-11 16:25:11 -0500

    add some vdiff tests

---
+++ components/skeleton/demo/skeleton-group-wrapper.js
@@ -0,0 +1,14 @@
+import '../../collapsible-panel/collapsible-panel.js';
+import '../../collapsible-panel/collapsible-panel-group.js';
+import { html, LitElement } from 'lit';
+import { SkeletonGroupMixin } from '../skeleton-group-mixin.js';
+
+class SkeletonGroupTestWrapper extends SkeletonGroupMixin(LitElement) {
+	render() {
+		return html`
+			<slot></slot>
+		`;
+	}
+}
+customElements.define('d2l-skeleton-group-test-wrapper', SkeletonGroupTestWrapper);
+
