b750d2477bfbe527597c9e7c8a9647673013e0ce
James Klassen <James.Klassen@D2L.com>
2023-07-12 08:05:46 -0500

    Remove rtl vdiffs and await `$eval` calls

--- components/skeleton/test/skeleton-group.visual-diff.js
+++ components/skeleton/test/skeleton-group.visual-diff.js
@@ -14,73 +14,65 @@ describe('d2l-skeleton-group', () => {
 
 	after(async() => await browser.close());
 
-	['ltr', 'rtl'].forEach(dir => {
+	before(async() => {
+		await page.goto(
+			`${visualDiff.getBaseUrl()}/components/skeleton/test/skeleton-group.visual-diff.html`,
+			{ waitUntil: ['networkidle0', 'load'] }
+		);
+		await page.bringToFront();
+	});
 
-		describe(dir, () => {
-
-			before(async() => {
-				await page.goto(
-					`${visualDiff.getBaseUrl()}/components/skeleton/test/skeleton-group.visual-diff.html?dir=${dir}`,
-					{ waitUntil: ['networkidle0', 'load'] }
-				);
-				await page.bringToFront();
-			});
-
-			[
-				'all-skeleton',
-				'one-skeleton',
-				'no-skeleton',
-			].forEach((name) => {
-				it(name, async function() {
-					const rect = await visualDiff.getRect(page, `#${name}`);
-					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-				});
-			});
-
-			[
-				{ name: 'make-one-skeleton', skeleton: true },
-				{ name: 'make-one-not-skeleton', skeleton: false },
-			].forEach((info) => {
-				it(info.name, async function() {
-					page.$eval(`#${info.name}`, async(element, skeleton) => {
-						const panelGroup = element.querySelector('d2l-skeleton-group-test-wrapper').querySelector('d2l-collapsible-panel-group');
-						const el = panelGroup.querySelector('d2l-collapsible-panel');
-						el.skeleton = skeleton;
-					}, info.skeleton);
-					const rect = await visualDiff.getRect(page, `#${info.name}`);
-					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-				});
-			});
-
-			[
-				'add-element',
-			].forEach((name) => {
-				it(name, async function() {
-					page.$eval(`#${name}`, async(element) => {
-						const panelGroup = element.querySelector('d2l-skeleton-group-test-wrapper').querySelector('d2l-collapsible-panel-group');
-						panelGroup.innerHTML += '<d2l-collapsible-panel skeleton panel-title="blah"></d2l-collapsible-panel>';
-					});
-					const rect = await visualDiff.getRect(page, `#${name}`);
-					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-				});
-			});
-
-			[
-				'remove-element',
-			].forEach((name) => {
-				it(name, async function() {
-					page.$eval(`#${name}`, async(element) => {
-						const panelGroup = element.querySelector('d2l-skeleton-group-test-wrapper').querySelector('d2l-collapsible-panel-group');
-						const el = panelGroup.querySelector('d2l-collapsible-panel');
-						el.remove();
-					});
-					const rect = await visualDiff.getRect(page, `#${name}`);
-					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-				});
-			});
-
+	[
+		'all-skeleton',
+		'one-skeleton',
+		'no-skeleton',
+	].forEach((name) => {
+		it(name, async function() {
+			const rect = await visualDiff.getRect(page, `#${name}`);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 		});
+	});
 
+	[
+		{ name: 'make-one-skeleton', skeleton: true },
+		{ name: 'make-one-not-skeleton', skeleton: false },
+	].forEach((info) => {
+		it(info.name, async function() {
+			await page.$eval(`#${info.name}`, async(element, skeleton) => {
+				const panelGroup = element.querySelector('d2l-skeleton-group-test-wrapper').querySelector('d2l-collapsible-panel-group');
+				const panel = panelGroup.querySelector('d2l-collapsible-panel');
+				panel.skeleton = skeleton;
+			}, info.skeleton);
+			const rect = await visualDiff.getRect(page, `#${info.name}`);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+	});
+
+	[
+		'add-element',
+	].forEach((name) => {
+		it(name, async function() {
+			await page.$eval(`#${name}`, async(element) => {
+				const panelGroup = element.querySelector('d2l-skeleton-group-test-wrapper').querySelector('d2l-collapsible-panel-group');
+				panelGroup.innerHTML += '<d2l-collapsible-panel skeleton panel-title="blah"></d2l-collapsible-panel>';
+			});
+			const rect = await visualDiff.getRect(page, `#${name}`);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
+	});
+
+	[
+		'remove-element',
+	].forEach((name) => {
+		it(name, async function() {
+			await page.$eval(`#${name}`, async(element) => {
+				const panelGroup = element.querySelector('d2l-skeleton-group-test-wrapper').querySelector('d2l-collapsible-panel-group');
+				const panel = panelGroup.querySelector('d2l-collapsible-panel');
+				panel.remove();
+			});
+			const rect = await visualDiff.getRect(page, `#${name}`);
+			await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+		});
 	});
 
 });
