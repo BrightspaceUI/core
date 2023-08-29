d862605a7a08aee3d6a7f21e14da2bb5cb30c070
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2023-08-29 15:42:06 -0400

    test: US152268 - Migrate meter vdiff tests (#3946)
    * US152268 - Migrate meter vdiff tests
    
    * Migrate existing goldens to new location
    
    * Remove hardcoded old styles
    
    * Updating vdiff goldens (#3947)
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ components/meter/test/meter-circle.vdiff.js
@@ -0,0 +1,44 @@
+import '../../colors/colors.js';
+import '../meter-circle.js';
+import { expect, fixture, html } from '@brightspace-ui/testing';
+
+describe('meter-circle', () => {
+	[true, false].forEach(rtl => {
+		[
+			{ name: 'progress', template: html`<d2l-meter-circle value="16" max="47"></d2l-meter-circle>` },
+			{ name: 'percent', template: html`<d2l-meter-circle value="16" max="47" percent></d2l-meter-circle>` }
+		].forEach(({ name, template }) => {
+			it(`${name}${ rtl ? '-rtl' : ''}`, async() => {
+				const elem = await fixture(template, { rtl });
+				await expect(elem).to.be.golden();
+			});
+		});
+	});
+
+	[
+		{ name: 'no-progress', template: html`<d2l-meter-circle value="0" max="10"></d2l-meter-circle>` },
+		{ name: 'complete', template: html`<d2l-meter-circle value="5" max="5"></d2l-meter-circle>` },
+		{ name: 'round-to-zero', template: html`<d2l-meter-circle value="0.004" max="100"></d2l-meter-circle>` },
+		{ name: 'max-zero-with-value', template: html`<d2l-meter-circle value="10" max="0"></d2l-meter-circle>` },
+		{ name: 'foreground-light', wrapped: true, template: html`
+			<div style="background-color: var(--d2l-color-celestine); padding: 1rem;">
+				<d2l-meter-circle value="16" max="47" foreground-light></d2l-meter-circle>
+			</div>
+		` },
+		{ name: 'scaled-larger', wrapped: true, template: html`
+			<div style="width: 90px;">
+				<d2l-meter-circle value="16" max="47" style="width: 300%;"></d2l-meter-circle>
+			</div>
+		` },
+		{ name: 'scaled-smaller', wrapped: true, template: html`
+			<div style="width: 90px;">
+				<d2l-meter-circle value="16" max="47" style="width: 30%;"></d2l-meter-circle>
+			</div>
+		` }
+	].forEach(({ name, template, wrapped }) => {
+		it(name, async() => {
+			const elem = await fixture(template);
+			await expect(!wrapped ? elem : elem.querySelector('d2l-meter-circle')).to.be.golden();
+		});
+	});
+});
