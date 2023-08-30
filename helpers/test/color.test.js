1554e8d8c356daf70ec84c756f8405381b26c6a7
Margaree Peacocke <margaree.p@gmail.com>
2023-08-30 14:12:59 -0400

    code review feedback: improve color regex to handle 3, 4, 6, 8 and char possibilities

--- helpers/test/color.test.js
+++ helpers/test/color.test.js
@@ -1,42 +1,44 @@
 import { expect } from '@brightspace-ui/testing';
 import { getValidHexColor } from '../color.js';
 
+const hexVals = {
+	3: '#abc',
+	4: '#00ff',
+	6: '#eeff00',
+	7: '#ef02300',
+	8: '#abcdef12',
+	10: '#abcdef12ab',
+	'notHex': 'notHex'
+};
+
 describe('color', () => {
 
-	it('should return color as expected when valid 6 character hex color and canHaveAlpha is false', async() => {
-		expect(getValidHexColor('#fF0a9b')).to.equal('#FF0A9B');
-	});
+	[
+		{ numChars: 3, canHaveAlpha: false, shouldThrow: false },
+		{ numChars: 3, canHaveAlpha: true, shouldThrow: false },
+		{ numChars: 4, canHaveAlpha: false, shouldThrow: true, errorMsg: '3 or 6' },
+		{ numChars: 4, canHaveAlpha: true, shouldThrow: false },
+		{ numChars: 6, canHaveAlpha: false, shouldThrow: false },
+		{ numChars: 6, canHaveAlpha: true, shouldThrow: false },
+		{ numChars: 7, canHaveAlpha: false, shouldThrow: true, errorMsg: '3 or 6' },
+		{ numChars: 7, canHaveAlpha: true, shouldThrow: true, errorMsg: '3, 4, 6, or 8' },
+		{ numChars: 8, canHaveAlpha: false, shouldThrow: true, errorMsg: '3 or 6' },
+		{ numChars: 8, canHaveAlpha: true, shouldThrow: false },
+		{ numChars: 10, canHaveAlpha: false, shouldThrow: true, errorMsg: '3 or 6' },
+		{ numChars: 10, canHaveAlpha: true, shouldThrow: true, errorMsg: '3, 4, 6, or 8' },
+		{ numChars: 'notHex', canHaveAlpha: false, shouldThrow: true, errorMsg: '3 or 6' },
+		{ numChars: 'notHex', canHaveAlpha: true, shouldThrow: true, errorMsg: '3, 4, 6, or 8' },
+	].forEach((testCase) => {
 
-	it('should return color as expected when valid 6 character hex color and canHaveAlpha is true', async() => {
-		expect(getValidHexColor('#fF0a9b', true)).to.equal('#FF0A9B');
-	});
+		it.only(`should behave as expected with ${testCase.numChars} character hex code and canHaveAlpha is ${testCase.canHaveAlpha}`, async() => {
+			const testVal = hexVals[testCase.numChars];
+			if (testCase.shouldThrow) {
+				expect(() => getValidHexColor(testVal, testCase.canHaveAlpha)).to.throw(`Invalid HEX color value "${testVal}". Expecting a ${testCase.errorMsg} character HEX color.`);
+			} else {
+				expect(getValidHexColor(testVal, testCase.canHaveAlpha)).to.equal(testVal.toUpperCase());
+			}
+		});
 
-	it('should throw when 7 character hex color and canHaveAlpha is false', async() => {
-		expect(() => getValidHexColor('fF0a9ba')).to.throw('Invalid HEX color value "fF0a9ba". Expecting a 6 character HEX color.');
-	});
-
-	it('should throw when 7 character hex color when canHaveAlpha is true', async() => {
-		expect(() => getValidHexColor('fF0a9ba', true)).to.throw('Invalid HEX color value "fF0a9ba". Expecting a 6 or 8 character HEX color.');
-	});
-
-	it('should throw when 8 character hex color and canHaveAlpha is false', async() => {
-		expect(() => getValidHexColor('fF0a9bab')).to.throw('Invalid HEX color value "fF0a9bab". Expecting a 6 character HEX color.');
-	});
-
-	it('should return color as expected when valid 8 character hex color when canHaveAlpha is true', async() => {
-		expect(getValidHexColor('#fF0a9bab', true)).to.equal('#FF0A9BAB');
-	});
-
-	it('should throw if invalid HEX value is set and canHaveAlpha is false', async() => {
-		expect(() => getValidHexColor('notHex')).to.throw('Invalid HEX color value "notHex". Expecting a 6 character HEX color.');
-	});
-
-	it('should throw if invalid HEX value is set and canHaveAlpha is true', async() => {
-		expect(() => getValidHexColor('notHex', true)).to.throw('Invalid HEX color value "notHex". Expecting a 6 or 8 character HEX color.');
-	});
-
-	it('should return undefined if value is undefined', async() => {
-		expect(getValidHexColor(undefined, true)).to.equal(undefined);
 	});
 
 });
