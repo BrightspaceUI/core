8254d144db50a9a68cf7e97c220232b1a1430d2a
Margaree Peacocke <margaree.p@gmail.com>
2023-09-11 13:40:57 -0400

    Add test reporting

--- d2l-test-runner.config.js
+++ d2l-test-runner.config.js
@@ -1,6 +1,21 @@
+import { reporter as testReporter } from '@d2l/test-reporting/reporters/web-test-runner.js';
+import { defaultReporter } from '@web/test-runner';
+
 const pattern = type => `+(components|controllers|directives|helpers|mixins|templates)/**/*.${type}.js`;
 
 export default {
+	reporters: [
+		defaultReporter(),
+		testReporter({
+			experience: 'Test',
+			tool: 'Test',
+			type: {
+				default: 'Unit',
+				aXe: 'Accessibility'
+			},
+			debug: true
+		})
+	],
 	pattern,
 	groups: [{
 		name: 'aXe',
