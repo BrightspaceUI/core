26e8566cf0bccc75e6c8dee66018d18ea455df65
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-09-18 16:08:21 -0400

    migrate skeleton visual-diff tests

--- components/skeleton/demo/skeleton-group-test-wrapper.js
+++ components/skeleton/demo/skeleton-group-test-wrapper.js
@@ -1,7 +1,16 @@
-import { html, LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { SkeletonGroupMixin } from '../skeleton-group-mixin.js';
 
 class SkeletonGroupTestWrapper extends SkeletonGroupMixin(LitElement) {
+	static get styles() {
+		return css`
+			:host {
+				display: flex;
+				flex-direction: column;
+				gap: 0.3rem;
+			}
+		`;
+	}
 	render() {
 		return html`<slot></slot>`;
 	}
