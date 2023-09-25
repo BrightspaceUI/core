2409fca5ad60fa999ed28844e1ec8c6cfa52e8ba
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-09-25 09:51:12 -0400

    Migration input search and number vdiffs (#4092)

--- components/inputs/input-text.js
+++ components/inputs/input-text.js
@@ -469,9 +469,9 @@ class InputText extends FocusMixin(LabelledMixin(FormElementMixin(SkeletonMixin(
 		if (!this.skeleton) {
 			if (this.validationError && !this.noValidate) {
 				// this tooltip is using "announced" since we don't want aria-describedby wire-up which would bury the message in VoiceOver's More Content Available menu
-				tooltip = html`<d2l-tooltip state="error" announced align="start">${this.validationError} <span class="d2l-offscreen">${this.description}</span></d2l-tooltip>`;
+				tooltip = html`<d2l-tooltip state="error" announced align="start" class="vdiff-target">${this.validationError} <span class="d2l-offscreen">${this.description}</span></d2l-tooltip>`;
 			} else if (this.instructions) {
-				tooltip = html`<d2l-tooltip align="start" for="${this._inputId}" delay="1000">${this.instructions}</d2l-tooltip>`;
+				tooltip = html`<d2l-tooltip align="start" for="${this._inputId}" delay="1000" class="vdiff-target">${this.instructions}</d2l-tooltip>`;
 			}
 		}
 
---
+++ components/inputs/test/input-search.vdiff.js
@@ -0,0 +1,57 @@
+import '../input-search.js';
+import { expect, fixture, focusElem, hoverElem, html } from '@brightspace-ui/testing';
+
+const noValueFixture = html`<d2l-input-search label="search"></d2l-input-search>`;
+const hasValueFixture = html`<d2l-input-search label="search" value="Apples"></d2l-input-search>`;
+
+const viewport = { width: 376 };
+
+describe('d2l-input-search', () => {
+
+	[
+		{ name: 'no-value', template: noValueFixture },
+		{ name: 'has-value', template: hasValueFixture },
+		{ name: 'no-clear', template: html`<d2l-input-search label="search" value="Apples" no-clear></d2l-input-search>` },
+		{ name: 'disabled', template: html`<d2l-input-search label="search" value="Apples" disabled></d2l-input-search>` },
+		{ name: 'placeholder', template: html`<d2l-input-search label="search" placeholder="Search for something..."></d2l-input-search>` },
+		{ name: 'placeholder-disabled', template: html`<d2l-input-search label="search" placeholder="Search for something..." disabled></d2l-input-search>` },
+		{ name: 'padding', template: html`<d2l-input-search label="search" style="padding: 10px;"></d2l-input-search>` },
+		{ name: 'flexbox', template: html`<div style="display: flex;"><d2l-input-search label="search"></d2l-input-search><p>stuff</p></div>` }
+	].forEach(({ name, template }) => {
+		it(name, async() => {
+			const elem = await fixture(template, { viewport });
+			await expect(elem).to.be.golden();
+		});
+	});
+
+	it('focus-input', async() => {
+		const elem = await fixture(noValueFixture, { viewport });
+		await focusElem(elem);
+		await expect(elem).to.be.golden();
+	});
+
+	it('focus-search-button', async() => {
+		const elem = await fixture(noValueFixture, { viewport });
+		await focusElem(elem.shadowRoot.querySelector('d2l-button-icon[icon="tier1:search"]'));
+		await expect(elem).to.be.golden();
+	});
+
+	it('focus-clear-button', async() => {
+		const elem = await fixture(hasValueFixture, { viewport });
+		await focusElem(elem.shadowRoot.querySelector('d2l-button-icon[icon="tier1:close-default"]'));
+		await expect(elem).to.be.golden();
+	});
+
+	it('hover-search-button', async() => {
+		const elem = await fixture(noValueFixture, { viewport });
+		await hoverElem(elem.shadowRoot.querySelector('d2l-button-icon[icon="tier1:search"]'));
+		await expect(elem).to.be.golden();
+	});
+
+	it('hover-clear-button', async() => {
+		const elem = await fixture(hasValueFixture, { viewport });
+		await hoverElem(elem.shadowRoot.querySelector('d2l-button-icon[icon="tier1:close-default"]'));
+		await expect(elem).to.be.golden();
+	});
+
+});
