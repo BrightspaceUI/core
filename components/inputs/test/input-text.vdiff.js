c468e42be35e58a2d66a100701cb3e47d00ee926
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-09-25 17:10:56 -0400

    migrate text input vdiffs

---
+++ components/inputs/test/input-text.vdiff.js
@@ -0,0 +1,173 @@
+import '../../button/button-icon.js';
+import '../../icons/icon.js';
+import '../input-text.js';
+import { expect, fixture, focusElem, html } from '@brightspace-ui/testing';
+import { loadSass, unloadSass } from '../../../test/load-sass.js';
+
+const createIcon = (icon, slot) => html`<d2l-icon icon="tier1:${icon}" slot="${slot}" style="margin-left: 0.55rem; margin-right: 0.55rem"></d2l-icon>`;
+
+const labelHiddenFixture = html`<d2l-input-text label="Name" label-hidden value="text"></d2l-input-text>`;
+const labelledFixture = html`<d2l-input-text label="Name" value="text"></d2l-input-text>`;
+const unitFixture = html`<d2l-input-text label="unit" label-hidden value="value" unit="%"></d2l-input-text>`;
+const unitInvalidFixture = html`<d2l-input-text label="unit" label-hidden value="value" unit="%" aria-invalid="true"></d2l-input-text>`;
+const iconLeftFixture = html`
+	<d2l-input-text label="Name" label-hidden value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+		${createIcon('calendar', 'left')}
+	</d2l-input-text>
+`;
+const iconRightFixture = html`
+	<d2l-input-text label="Name" label-hidden value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+		${createIcon('calendar', 'right')}
+	</d2l-input-text>
+`;
+const iconRightInvalidFixture = html`
+	<d2l-input-text label="Name" label-hidden aria-invalid="true" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+		${createIcon('calendar', 'right')}
+	</d2l-input-text>
+`;
+
+const viewport = { width: 376 };
+
+describe('d2l-input-text', () => {
+
+	[
+		{ name: 'basic', template: html`<d2l-input-text label="Name" label-hidden value="text"></d2l-input-text>` },
+		{ name: 'email', template: html`<d2l-input-text label="Name" label-hidden type="email" value="bill@nye.com"></d2l-input-text>` },
+		{ name: 'number', template: html`<d2l-input-text label="Name" label-hidden type="number" value="500"></d2l-input-text>` },
+		{ name: 'password', template: html`<d2l-input-text label="Name" label-hidden type="password" value="123456"></d2l-input-text>` },
+		{ name: 'search', template: html`<d2l-input-text label="Name" label-hidden type="search" value="search"></d2l-input-text>` },
+		{ name: 'tel', template: html`<d2l-input-text label="Name" label-hidden type="tel" value="123-456-7890"></d2l-input-text>` },
+		{ name: 'url', template: html`<d2l-input-text label="Name" label-hidden type="url" value="https://www.d2l.com"></d2l-input-text>` },
+		{ name: 'disabled', template: html`<d2l-input-text label="Name" label-hidden disabled value="text disabled"></d2l-input-text>` },
+		{ name: 'placeholder', template: html`<d2l-input-text label="Name" label-hidden placeholder="placeholder"></d2l-input-text>` },
+		{ name: 'placeholder-disabled', template: html`<d2l-input-text label="Name" label-hidden disabled placeholder="placeholder disabled"></d2l-input-text>` },
+		{ name: 'invalid', template: html`<d2l-input-text label="Name" label-hidden type="email" value="invalid@"></d2l-input-text>` },
+		{ name: 'invalid-disabled', template: html`<d2l-input-text label="Name" label-hidden disabled type="email" value="invalid-disabled@"></d2l-input-text>` },
+		{ name: 'aria-invalid', template: html`<d2l-input-text label="Name" label-hidden value="aria-invalid" aria-invalid="true"></d2l-input-text>` },
+		{ name: 'aria-invalid-disabled', template: html`<d2l-input-text label="Name" label-hidden disabled value="aria-invalid-disabled" aria-invalid="true"></d2l-input-text>` },
+		{ name: 'labelled', template: labelledFixture },
+		{ name: 'labelled-focus', template: labelledFixture, focus: true },
+		{ name: 'labelled-skeleton', template: html`<d2l-input-text label="Name" value="text" skeleton></d2l-input-text>` },
+		{ name: 'label-hidden', template: labelHiddenFixture },
+		{ name: 'label-hidden-skeleton', template: html`<d2l-input-text label="Name" label-hidden value="text" skeleton></d2l-input-text>` },
+		{ name: 'required', template: html`<d2l-input-text label="Name" required value="text"></d2l-input-text>` },
+		{ name: 'required-skeleton', template: html`<d2l-input-text label="Name" required value="text" skeleton></d2l-input-text>` },
+		{ name: 'custom-width-skeleton', template: html`<d2l-input-text label="Custom Width Longer Than Input" input-width="100px" skeleton></d2l-input-text>` },
+		{ name: 'overflowing', template: html`<d2l-input-text label="Name" label-hidden value="overflowing value that renders ellipsis"></d2l-input-text>` },
+		{ name: 'unit', template: unitFixture },
+		{ name: 'unit-rtl', template: unitFixture, rtl: true },
+		{ name: 'unit-disabled', template: html`<d2l-input-text label="unit" label-hidden value="value" unit="%" disabled></d2l-input-text>` },
+		{ name: 'unit-invalid', template: unitInvalidFixture },
+		{ name: 'unit-invalid-focus', template: unitInvalidFixture, focus: true },
+		{ name: 'unit-invalid-rtl', template: unitInvalidFixture, rtl: true },
+		{ name: 'unit-invalid-rtl-focus', template: unitInvalidFixture, focus: true, rtl: true },
+		{
+			name: 'unit-init-hidden',
+			template: html`<d2l-input-text label="unit" label-hidden value="overflowing value should not overlap unit" unit="%" style="display: none;"></d2l-input-text>`,
+			action: async(elem) => elem.style.display = 'inline-block'
+		},
+		{
+			name: 'unit-change',
+			template: html`<d2l-input-text label="unit" label-hidden value="3" value-align="end" unit="/5" unit-label="out of 5"></d2l-input-text>`,
+			action: async(elem) => { elem.unit = '/5000'; await elem.updateComplete; }
+		},
+		{ name: 'override-height', template: html`<d2l-input-text label="Name" label-hidden value="text" style="--d2l-input-height: 100px;"></d2l-input-text>` },
+		{ name: 'override-padding', template: html`<d2l-input-text label="Name" label-hidden value="text" style="--d2l-input-padding: 2rem 3rem;--d2l-input-padding-focus: calc(2rem - 1px) calc(3rem - 1px);"></d2l-input-text>` },
+		{ name: 'override-text-align', template: html`<d2l-input-text label="Name" label-hidden value="text" style="--d2l-input-text-align: end;"></d2l-input-text>` },
+		{ name: 'icon-left', template: iconLeftFixture },
+		{ name: 'icon-left-rtl', template: iconLeftFixture, rtl: true },
+		{ name: 'icon-right', template: iconRightFixture },
+		{ name: 'icon-right-rtl', template: iconRightFixture, rtl: true },
+		{
+			name: 'icon-left-label',
+			template: html`
+				<d2l-input-text label="Name" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+					${createIcon('calendar', 'left')}
+				</d2l-input-text>
+			`
+		},
+		{
+			name: 'icon-right-label',
+			template: html`
+				<d2l-input-text label="Name" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+					${createIcon('calendar', 'right')}
+				</d2l-input-text>
+			`
+		},
+		{
+			name: 'icon-left-right',
+			template: html`
+				<d2l-input-text label="Name" label-hidden value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+					${createIcon('search', 'left')}
+					${createIcon('search', 'right')}
+				</d2l-input-text>
+			`
+		},
+		{
+			name: 'button-icon-left',
+			template: html`
+				<d2l-input-text label="Name" label-hidden value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+					${createIcon('search', 'left')}
+				</d2l-input-text>
+			`
+		},
+		{
+			name: 'button-icon-right',
+			template: html`
+				<d2l-input-text label="Name" label-hidden value="Lorem ipsum dolor sit amet, consectetur adipiscing elit">
+					${createIcon('search', 'right')}
+				</d2l-input-text>
+			`
+		},
+		{ name: 'icon-right-invalid', template: iconRightInvalidFixture },
+		{ name: 'icon-right-invalid-focus', template: iconRightInvalidFixture, focus: true },
+		{ name: 'icon-right-invalid-rtl', template: iconRightInvalidFixture, rtl: true },
+		{ name: 'icon-right-invalid-rtl-focus', template: iconRightInvalidFixture, focus: true, rtl: true }
+	].forEach(({ name, template, action, focus, rtl }) => {
+
+		it(name, async() => {
+			const elem = await fixture(template, { rtl, viewport });
+			if (action) await action(elem);
+			if (focus) await focusElem(elem);
+			await expect(elem).to.be.golden();
+		});
+
+	});
+
+	describe('sass', () => {
+
+		before(loadSass);
+		after(unloadSass);
+
+		[
+			{ name: 'basic', template: html`<input class="d2l-test-input-text" type="text" value="text">` },
+			{ name: 'email', template: html`<input class="d2l-test-input-text" type="email" value="bill@nye.com">` },
+			{ name: 'number', template: html`<input class="d2l-test-input-text" type="number" value="500">` },
+			{ name: 'password', template: html`<input class="d2l-test-input-text" type="password" value="123456">` },
+			{ name: 'search', template: html`<input class="d2l-test-input-text" type="search" value="search">` },
+			{ name: 'tel', template: html`<input class="d2l-test-input-text" type="tel" value="123-456-7890">` },
+			{ name: 'url', template: html`<input class="d2l-test-input-text" type="url" value="https://www.d2l.com">` },
+			{ name: 'disabled', template: html`<input class="d2l-test-input-text" disabled value="text disabled">` },
+			{ name: 'placeholder', template: html`<input class="d2l-test-input-text" placeholder="placeholder">` },
+			{ name: 'placeholder-disabled', template: html`<input class="d2l-test-input-text" disabled placeholder="placeholder disabled">` },
+			{ name: 'invalid', template: html`<input class="d2l-test-input-text" type="email" value="invalid@">` },
+			{ name: 'invalid-disabled', template: html`<input class="d2l-test-input-text" disabled type="email" value="invalid-disabled@">` },
+			{ name: 'aria-invalid', template: html`<input class="d2l-test-input-text" value="aria-invalid" aria-invalid="true">` },
+			{ name: 'aria-invalid-disabled', template: html`<input class="d2l-test-input-text" disabled value="aria-invalid-disabled" aria-invalid="true">` }
+		].forEach(({ name, template }) => {
+			it(name, async() => {
+				const elem = await fixture(template, { viewport });
+				await expect(elem).to.be.golden();
+			});
+			if (name.indexOf('disabled') === -1) {
+				it(`${name}-focus`, async() => {
+					const elem = await fixture(template, { viewport });
+					await focusElem(elem);
+					await expect(elem).to.be.golden();
+				});
+			}
+		});
+
+	});
+
+});
