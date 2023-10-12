379d2e1c319cba97b4a31032caeb110613a3d032
Danny Gleckler <daniel.gleckler@d2l.com>
2023-10-12 17:31:00 -0400

    Use css variable to set direction

--- components/offscreen/offscreen.js
+++ components/offscreen/offscreen.js
@@ -1,9 +1,11 @@
-import { css, html, LitElement } from 'lit';
+import { css, html, LitElement, unsafeCSS } from 'lit';
 import { RtlMixin } from '../../mixins/rtl/rtl-mixin.js';
+import { getDocumentLocaleSettings } from '@brightspace-ui/intl/lib/common.js';
 
-const getOffscreenStyles = () => css`
-	.d2l-offscreen {
-		direction: ${document.dir || css`unset`};
+const documentLocaleSettings = getDocumentLocaleSettings();
+
+const offscreenStyleDeclarations = css`
+		direction: var(--d2l-document-direction);
 		height: 1px;
 		inset-inline-start: -10000px;
 		left: -10000px;
@@ -11,6 +13,11 @@ const getOffscreenStyles = () => css`
 		position: absolute !important;
 		white-space: nowrap;
 		width: 1px;
+`;
+
+export const offscreenStyles = css`
+	.d2l-offscreen {
+		${offscreenStyleDeclarations}
 	}
 	:host([dir="rtl"]) .d2l-offscreen {
 		left: 0;
@@ -18,15 +25,21 @@ const getOffscreenStyles = () => css`
 	}
 `;
 
-export const offscreenStyles = getOffscreenStyles();
-
 /**
  * A component for positioning content offscreen to only be visible to screen readers.
  * @slot - Default content placed inside of the component
  */
 class Offscreen extends RtlMixin(LitElement) {
 	static get styles() {
-		return getOffscreenStyles();
+		return css`
+			:host {
+				${offscreenStyleDeclarations}
+			}
+			:host([dir="rtl"]) {
+				left: 0;
+				right: -10000px;
+			}
+		`;
 	}
 	render() {
 		return html`<slot></slot>`;
