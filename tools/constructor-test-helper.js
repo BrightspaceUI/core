a2065d9defab6454d02fba26e0fc386246ed5f9e
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-10-12 16:30:30 -0400

    restore original

--- tools/constructor-test-helper.js
+++ tools/constructor-test-helper.js
@@ -1,1 +1,7 @@
-export { runConstructor } from '@brightspace-ui/testing';
+import { expect } from '@open-wc/testing';
+
+export function runConstructor(nodeName) {
+	const ctor = customElements.get(nodeName);
+	expect(ctor).to.not.be.undefined;
+	document.createElement(nodeName);
+}
