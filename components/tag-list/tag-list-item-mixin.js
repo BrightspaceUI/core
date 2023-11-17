ce3d2c5c45e8c4ca9b24b232fcce051ad447873b
Dave Lockhart <dlockhart@users.noreply.github.com>
2023-11-17 10:38:14 -0500

    more resilient check

--- components/tag-list/tag-list-item-mixin.js
+++ components/tag-list/tag-list-item-mixin.js
@@ -282,7 +282,7 @@ export const TagListItemMixin = superclass => class extends LocalizeCoreElement(
 		// don't error immediately in case it doesn't get set immediately
 		this._validatingPlainTextTimeout = setTimeout(() => {
 			this._validatingPlainTextTimeout = null;
-			const hasPlainText = (typeof this._plainText === 'string') && this._plainText.length > 0;
+			const hasPlainText = (this._plainText?.constructor !== String) && this._plainText?.length > 0;
 			if (!hasPlainText) {
 				throw new Error(`TagListItemMixin: "${this.tagName.toLowerCase()}" called "_render()" with empty "plainText" option`);
 			}
