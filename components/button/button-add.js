1e1e89df8a0d54090fedcddd9b53c05928d1b8a9
Margaree Peacocke <margaree.p@gmail.com>
2023-11-24 16:07:06 -0500

    button-add: Add hint type and timing

--- components/button/button-add.js
+++ components/button/button-add.js
@@ -1,13 +1,22 @@
 import '../colors/colors.js';
 import '../icons/icon.js';
 import '../tooltip/tooltip.js';
-import { css, html, LitElement, unsafeCSS } from 'lit';
+import { css, html, LitElement, nothing, unsafeCSS } from 'lit';
+import { VisibleOnAncestorMixin, visibleOnAncestorStyles } from '../../mixins/visible-on-ancestor/visible-on-ancestor-mixin.js';
 import { FocusMixin } from '../../mixins/focus/focus-mixin.js';
 import { getFocusPseudoClass } from '../../helpers/focus.js';
 import { getUniqueId } from '../../helpers/uniqueId.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { LocalizeCoreElement } from '../../helpers/localize-core-element.js';
 import { PropertyRequiredMixin } from '../../mixins/property-required/property-required-mixin.js';
+import { styleMap } from 'lit/directives/style-map.js';
+
+const HINT_TIMING = {
+	ALWAYS: 'always',
+	NEARBY: 'nearby',
+	NEVER: 'never'
+};
+const DEFAULT_HINT_DISTANCE = 37;
 
 /**
  * A component for quickly adding items to a specific locaiton.
@@ -15,6 +24,18 @@ import { PropertyRequiredMixin } from '../../mixins/property-required/property-r
 class ButtonAdd extends PropertyRequiredMixin(FocusMixin(LocalizeCoreElement(LitElement))) {
 	static get properties() {
 		return {
+			/**
+			 * ONLY when visible-text is FALSE and hint-timing is nearby
+			 * Distance in px where when the user is hovering within that distance the icon will appears. Default is 37.
+			 * @type {number}
+			 */
+			hintNearbyDistance: { type: Number, reflect: true, attribute: 'hint-nearby-distance' },
+			/**
+			 * ONLY when visible-text is FALSE
+			 * When to show the icon/icon text. Always - always visible, Nearby - when mouse within certain distance, Never - do not show until hover/focus.
+			 * @type {'always'|'nearby'|'never'}
+			 */
+			hintTiming: { type: String, reflect: true, attribute: 'hint-timing' },
 			/**
 			 * When text-visible is true, the text to show in the button. When text-visible is false, the text to show in the tooltip.
 			 * @type {string}
@@ -24,7 +45,8 @@ class ButtonAdd extends PropertyRequiredMixin(FocusMixin(LocalizeCoreElement(Lit
 			 * When true, show the button with icon and visible text. When false, only show icon.
 			 * @type {boolean}
 			 */
-			textVisible: { type: Boolean, reflect: true, attribute: 'text-visible' }
+			textVisible: { type: Boolean, reflect: true, attribute: 'text-visible' },
+			_hasFocus: { state: true }
 		};
 	}
 
@@ -59,30 +81,126 @@ class ButtonAdd extends PropertyRequiredMixin(FocusMixin(LocalizeCoreElement(Lit
 			button:${unsafeCSS(getFocusPseudoClass())} .line {
 				border-top-color: var(--d2l-color-celestine);
 			}
+		`;
+	}
 
-			.content {
+	constructor() {
+		super();
+
+		this.hintTiming = HINT_TIMING.ALWAYS;
+		this.textVisible = false;
+
+		this._buttonId = getUniqueId();
+		this._hasFocus = false;
+		this._visibleDistance = 0;
+
+		this.addEventListener('focus', this._onFocusHover);
+		this.addEventListener('mouseover', this._onFocusHover);
+		this.addEventListener('blur', this._onBlur);
+		this.addEventListener('mouseout', this._onBlur);
+	}
+
+	static get focusElementSelector() {
+		return 'button';
+	}
+
+	render() {
+		const buttonSpacing = this._getButtonSpacing();
+		const text = this.text || this.localize('components.button-add.addItem');
+		const content = this.textVisible ? this._renderWithTextVisible(text) : this._renderWithTextHidden(text);
+		const id = !this.textVisible ? this._buttonId : undefined;
+
+		return html`
+			<button class="d2l-label-text d2l-visible-on-ancestor-target" id="${ifDefined(id)}" style="${styleMap(buttonSpacing)}">
+				<div class="line"></div>
+				${content}
+			</button>
+		`;
+	}
+
+	_getButtonSpacing() {
+		if (this.textVisible || this.hintTiming !== HINT_TIMING.NEARBY) return {};
+
+		this._visibleDistance = this.hintNearbyDistance || DEFAULT_HINT_DISTANCE;
+		return {
+			marginBottom: `-${this._visibleDistance}px`,
+			marginTop: `-${this._visibleDistance}px`,
+			paddingBottom: `${this._visibleDistance}px`,
+			paddingTop: `${this._visibleDistance}px`
+		};
+	}
+
+	_onBlur() {
+		this._hasFocus = false;
+	}
+	_onFocusHover() {
+		this._hasFocus = true;
+	}
+
+	_renderWithTextHidden(text) {
+		if (this.textVisible) return this._renderWithTextVisible();
+
+		const visibleOnAncestor = this.hintTiming === HINT_TIMING.NEARBY || this.hintTiming === HINT_TIMING.NEVER;
+		const offset = -(this._visibleDistance - 18);
+
+		return html`
+			<d2l-button-add-icon-text
+				?show-focus="${this._hasFocus}"
+				?visible-on-ancestor="${visibleOnAncestor}"
+			></d2l-button-add-icon-text>
+			<d2l-tooltip class="vdiff-target" offset="${offset}" for="${this._buttonId}" for-type="label">${text}</d2l-tooltip>
+		`;
+	}
+
+	_renderWithTextVisible(text) {
+		if (!this.textVisible) return this._renderWithTextHidden();
+
+		return html`
+			<d2l-button-add-icon-text
+				text="${text}"
+				?show-focus="${this._hasFocus}"
+			></d2l-button-add-icon-text>
+		`;
+	}
+
+}
+customElements.define('d2l-button-add', ButtonAdd);
+
+/**
+ * @ignore
+ */
+class ButtonAddIconText extends VisibleOnAncestorMixin(LitElement) {
+	static get properties() {
+		return {
+			text: { type: String },
+			showFocus: { type: Boolean, reflect: true, attribute: 'show-focus' }
+		};
+	}
+
+	static get styles() {
+		return [visibleOnAncestorStyles, css`
+			:host {
 				align-items: center;
 				background-color: white;
 				display: flex;
 				position: absolute;
 			}
-			:host([text-visible]) .content {
+			:host([text]) {
 				color: var(--d2l-color-celestine);
 				height: 1.5rem;
 				padding: 0 0.3rem;
 			}
 
-			:host([text-visible]) d2l-icon,
-			:host(:not([text-visible])) button:hover d2l-icon,
-			:host(:not([text-visible])) button:${unsafeCSS(getFocusPseudoClass())} d2l-icon {
+			:host([text]) d2l-icon,
+			:host([show-focus]:not([text])) d2l-icon {
 				color: var(--d2l-color-celestine);
 			}
-			:host(:not([text-visible])) d2l-icon {
+			:host(:not([text])) d2l-icon {
 				color: var(--d2l-color-galena);
 				margin: -3px; /** hover/click target */
 				padding: 3px; /** hover/click target */
 			}
-			:host([text-visible]) d2l-icon {
+			:host([text]) d2l-icon {
 				padding-inline-end: 0.2rem;
 			}
 
@@ -91,37 +209,15 @@ class ButtonAdd extends PropertyRequiredMixin(FocusMixin(LocalizeCoreElement(Lit
 				font-weight: 700;
 				letter-spacing: 0.2px;
 				line-height: 1rem;
-			}
-		`;
-	}
-
-	constructor() {
-		super();
-
-		this.textVisible = false;
-		this._buttonId = getUniqueId();
-	}
-
-	static get focusElementSelector() {
-		return 'button';
+			}`
+		];
 	}
 
 	render() {
-		const text = this.text || this.localize('components.button-add.addItem');
-		const id = !this.textVisible ? this._buttonId : undefined;
-
 		return html`
-			<button class="d2l-label-text" id="${ifDefined(id)}">
-				<div class="line"></div>
-				<div class="content">
-					<d2l-icon icon="tier1:plus-default"></d2l-icon>
-					${this.textVisible
-		? html`<span>${text}</span>`
-		: html`<d2l-tooltip class="vdiff-target" offset="18" for="${this._buttonId}" for-type="label">${text}</d2l-tooltip>`}
-				</div>
-			</button>
+			<d2l-icon icon="tier1:plus-default"></d2l-icon>
+			${this.text ? html`<span class="d2l-label-text">${this.text}</span>` : nothing}
 		`;
 	}
 }
-customElements.define('d2l-button-add', ButtonAdd);
-
+customElements.define('d2l-button-add-icon-text', ButtonAddIconText);
--- components/button/test/button-add.vdiff.js
+++ components/button/test/button-add.vdiff.js
@@ -2,26 +2,30 @@ import '../button-add.js';
 import { clickElem, expect, fixture, focusElem, hoverElem, html, oneEvent } from '@brightspace-ui/testing';
 
 describe('button-add', () => {
-	[ true, false ].forEach((textVisible) => {
-		describe(`text-visible ${textVisible}`, () => {
-			[
-				{ category: 'basic', template: html`<d2l-button-add ?text-visible="${textVisible}"></d2l-button-add>` },
-				{ category: 'text', template: html`<d2l-button-add text="Custom Text" ?text-visible="${textVisible}"></d2l-button-add>` },
-				{ category: 'dashed line', template: html`<d2l-button-add style="--d2l-button-add-line-style: dashed;" ?text-visible="${textVisible}"></d2l-button-add>` }
-			].forEach(({ category, template }) => {
-
-				describe(category, () => {
+	[ 'always', 'nearby', 'never', undefined ].forEach((hintTiming) => {
+		describe(`hint-timing ${hintTiming}`, () => {
+			[ true, false ].forEach((textVisible) => {
+				describe(`text-visible ${textVisible}`, () => {
 					[
-						{ name: 'normal' },
-						{ name: 'hover', action: hoverElem },
-						{ name: 'focus', action: focusElem },
-						{ name: 'click', action: clickElem }
-					].forEach(({ action, name }) => {
-						it(name, async() => {
-							const elem = await fixture(template);
-							if (action) await action(elem);
-							if ((name === 'hover' || name === 'focus') && !elem.textVisible) await oneEvent(elem, 'd2l-tooltip-show');
-							await expect(elem).to.be.golden();
+						{ category: 'basic', template: html`<d2l-button-add ?text-visible="${textVisible}" hint-timing="${hintTiming}"></d2l-button-add>` },
+						{ category: 'text', template: html`<d2l-button-add text="Custom Text" ?text-visible="${textVisible}" hint-timing="${hintTiming}"></d2l-button-add>` },
+						{ category: 'dashed line', template: html`<d2l-button-add style="--d2l-button-add-line-style: dashed;" ?text-visible="${textVisible}" hint-timing="${hintTiming}"></d2l-button-add>` }
+					].forEach(({ category, template }) => {
+
+						describe(category, () => {
+							[
+								{ name: 'normal' },
+								{ name: 'hover', action: hoverElem },
+								{ name: 'focus', action: focusElem },
+								{ name: 'click', action: clickElem }
+							].forEach(({ action, name }) => {
+								it(name, async() => {
+									const elem = await fixture(template);
+									if (action) await action(elem);
+									if ((name === 'hover' || name === 'focus') && !elem.textVisible) await oneEvent(elem, 'd2l-tooltip-show');
+									await expect(elem).to.be.golden();
+								});
+							});
 						});
 					});
 				});
