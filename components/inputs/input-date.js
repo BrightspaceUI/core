ab7b246c5e3028f2db710c80e638da0720b45b9d
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-01-16 17:51:57 -0500

    fix: remove "2" from term names as it's not necessary when OSLO isn't in play (#4353)

--- components/inputs/input-date.js
+++ components/inputs/input-date.js
@@ -180,9 +180,9 @@ class InputDate extends FocusMixin(LabelledMixin(SkeletonMixin(FormElementMixin(
 			if (minDate && maxDate) {
 				return this.localize(`${this._namespace}.errorOutsideRange`, { minDate, maxDate });
 			} else if (maxDate) {
-				return this.localize(`${this._namespace}.errorMaxDateOnly2`, { maxDate });
+				return this.localize(`${this._namespace}.errorMaxDateOnly`, { maxDate });
 			} else if (this.minValue) {
-				return this.localize(`${this._namespace}.errorMinDateOnly2`, { minDate });
+				return this.localize(`${this._namespace}.errorMinDateOnly`, { minDate });
 			}
 		}
 		return super.validationMessage;
