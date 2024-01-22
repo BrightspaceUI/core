5141a7c7e554fe671a86a3d08ab948de74ceda90
Saif Ali <saifaldindeveloper@gmail.com>
2024-01-22 09:50:00 -0500

    fix: small fix

--- components/inputs/test/input-number.vdiff.js
+++ components/inputs/test/input-number.vdiff.js
@@ -15,8 +15,13 @@ const inlineHelpComponents = {
 	multiline: html`
 		<d2l-input-number label="Age" value="18">
 			<div slot="inline-help">
-				Lorem ipsum dolor sit amet, elit,
-				sed do eiusmod.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+				nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+				reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+				pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+				qui officia deserunt mollit anim id est laborum.
 			</div>
 		</d2l-input-number>
 	`
