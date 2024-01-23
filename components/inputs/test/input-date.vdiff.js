b26f1db29b0c7031d8aae900823b1842bf4b4b1c
Saif Ali <saifaldindeveloper@gmail.com>
2024-01-23 16:27:50 -0500

    fix: lint errors

--- components/inputs/test/input-date.vdiff.js
+++ components/inputs/test/input-date.vdiff.js
@@ -2,7 +2,7 @@ import '../input-date.js';
 import { clickElem, expect, fixture, focusElem, html, nextFrame, oneEvent, sendKeys, sendKeysElem } from '@brightspace-ui/testing';
 import { reset, useFakeTimers } from 'sinon';
 import { ifDefined } from 'lit/directives/if-defined.js';
-import { inlineHelpFixtures, inlineHelpSlots } from './input-shared-content.js';
+import { inlineHelpFixtures } from './input-shared-content.js';
 
 const create = (opts = {}) => {
 	const { disabled, emptyText, label, labelHidden, maxValue, minValue, opened, required, skeleton, value } = {
