4120e0b57a41083d73dc669fa50d247f082f1670
Saif Ali <saifaldindeveloper@gmail.com>
2024-01-31 09:44:26 -0500

    fix: lint error

--- components/inputs/test/input-checkbox.vdiff.js
+++ components/inputs/test/input-checkbox.vdiff.js
@@ -1,8 +1,8 @@
 import '../input-checkbox.js';
 import '../input-checkbox-spacer.js';
 import { expect, fixture, focusElem, html } from '@brightspace-ui/testing';
-import { inlineHelpFixtures } from './input-shared-content.js';
 import { loadSass, unloadSass } from '../../../test/load-sass.js';
+import { inlineHelpFixtures } from './input-shared-content.js';
 
 describe('d2l-input-checkbox', () => {
 
