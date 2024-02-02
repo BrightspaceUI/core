2d96467e7bc6b91d5176c41330d7658b0c55c44d
Danny Gleckler <daniel.gleckler@d2l.com>
2024-02-02 17:06:35 -0500

    Wait for the right tooltip

--- components/tag-list/test/tag-list.vdiff.js
+++ components/tag-list/test/tag-list.vdiff.js
@@ -116,8 +116,9 @@ describe('tag-list', () => {
 		});
 
 		it('delete first item', async() => {
-			await sendKeys('press', 'Tab');
+			elem.focus();
 			sendKeys('press', 'Delete');
+			await oneEvent(elem, 'd2l-tag-list-item-clear');
 			await oneEvent(elem, 'd2l-tooltip-show');
 			await expect(elem).to.be.golden();
 		});
