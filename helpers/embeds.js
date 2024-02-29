b8f79c8045b53e1a6a66aca3565eb3c137755000
Grant Cleary <gcleary@c02d43d3md6q.desire2learn.d2l>
2024-02-29 16:05:57 -0500

    Switch to nesting using templates

--- helpers/embeds.js
+++ helpers/embeds.js
@@ -1,82 +1,65 @@
-import { html, render } from 'lit';
-import { findComposedAncestor } from '../helpers/dom.js';
+import { html, nothing, render } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { map } from 'lit/directives/map.js';
 import { tryGetPluginByKey } from '@brightspace-ui/core/helpers/plugins.js';
 
 const embedTypeAttributeName = 'data-d2l-embed-type';
-
-class RenderNode {
-	#childNodes;
-	#node;
-
-	constructor(node) {
-		this.#node = node;
-		this.#childNodes = [];
-	}
-
-	addChildNode(childNode) {
-		this.#childNodes.push(childNode);
-	}
-
-	async renderNode(options) {
-		await Promise.all(this.#childNodes.map(childNode => childNode.renderNode(options)));
-
-		const embedRendererPlugin = tryGetPluginByKey('d2l-html-embed-renderer', this.#node.dataset.d2lEmbedType);
-		if (!embedRendererPlugin) return;
-
-		const fragment = new DocumentFragment();
-		this.#node.childNodes.forEach(node => {
-			const temp = node.cloneNode(true);
-			fragment.appendChild(temp);
-		});
-
-		let props = this.#node.dataset.d2lEmbedData;
-		try {
-			props = JSON.parse(this.#node.dataset.d2lEmbedData);
-		} catch (e) { /* empty */ }
-
-		const embed = await embedRendererPlugin.renderView(fragment, props || {}, options || {});
-		if (embed) {
-			render(embed, this.#node.parentNode, { renderBefore: this.#node });
-			this.#node.remove();
-		}
-	}
-
-}
 
 export async function createEmbedPlaceholder(embedType, options) {
 	const embedRendererPlugin = tryGetPluginByKey('d2l-html-embed-renderer', embedType);
 	if (!embedRendererPlugin) return;
 
 	const placeholderData = await embedRendererPlugin.getPlaceholder(options);
-	/* NOT IMPLEMENTED */
+	const contents = placeholderData.contents
+		? map(Object.entries(placeholderData.contents), ([id, content]) => html`<template data-d2l-embed-template-id=${id}>${content}</template>`)
+		: nothing;
+
+	const attributes = placeholderData.attributes ? JSON.stringify(placeholderData.attributes) : undefined;
+
+	return placeholderData.inline
+		? html`
+			<span data-d2l-embed-type="${embedType}" data-d2l-embed-props="${ifDefined(attributes)}">
+				${contents}
+			</span>
+		` : html`
+			<div data-d2l-embed-type="${embedType}" data-d2l-embed-props="${ifDefined(attributes)}">
+				${contents}
+			</div>
+		`;
 }
 
 export async function renderEmbeds(node, options) {
-	const elems = node.querySelectorAll(`div[${embedTypeAttributeName}], span[${embedTypeAttributeName}]`);
-	if (elems.length === 0) return;
+	const placeholders = [...node.querySelectorAll(`div[${embedTypeAttributeName}], span[${embedTypeAttributeName}]`)];
+	if (placeholders.length === 0) return;
 
-	const renderNodes = new Map();
-	const rootNodes = [];
-	const constructRenderNode = elem => {
-		if (renderNodes.has(elem)) return;
+	const processPlaceholder = async placeholder => {
+		const embedRendererPlugin = tryGetPluginByKey('d2l-html-embed-renderer', placeholder.dataset.d2lEmbedType);
+		if (!embedRendererPlugin) return;
 
-		const renderNode = new RenderNode(elem);
-		renderNodes.set(elem, renderNode);
+		const templates = [...placeholder.querySelectorAll('template[data-d2l-embed-template-id]')];
+		const processedTemplates = await Promise.all(templates.map(async template => {
+			const templateNode = template.content.cloneNode(true);
+			await renderEmbeds(templateNode, options);
+			return html`${templateNode}`;
+		}));
 
-		const isEmbedAncestor = ancestor => {
-			if (ancestor === elem || ancestor.nodeType !== Node.ELEMENT_NODE) return;
-			return ancestor.hasAttribute(embedTypeAttributeName);
-		};
+		const processedTemplateContents = templates.reduce((acc, template, index) => {
+			acc[template.dataset.d2lEmbedTemplateId] = processedTemplates[index];
+			return acc;
+		}, {});
 
-		const nearestEmbedAncestor = findComposedAncestor(elem, isEmbedAncestor);
-		if (!nearestEmbedAncestor) {
-			rootNodes.push(renderNode);
-			return;
-		}
+		let props = placeholder.dataset.d2lEmbedProps;
+		try {
+			props = JSON.parse(placeholder.dataset.d2lEmbedProps);
+		} catch (e) { /* empty */ }
 
-		renderNodes.get(nearestEmbedAncestor).addChildNode(renderNode);
+		return embedRendererPlugin.renderView(processedTemplateContents, props, options || {});
 	};
 
-	elems.forEach(constructRenderNode);
-	await Promise.all(rootNodes.map(rootNode => rootNode.renderNode(options)));
+	const embeds = await Promise.all(placeholders.map(processPlaceholder));
+	placeholders.forEach((placeholder, index) => {
+		if (!embeds[index]) return;
+		render(embeds[index], placeholder.parentNode, { renderBefore: placeholder });
+		placeholder.remove();
+	});
 }
