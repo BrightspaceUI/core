9a2cba47572aec8d6f69645260ebd11d6e057d0f
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-03-28 16:31:14 -0400

    Validate output of web-component-analyzer (#4552)

---
+++ cli/validate-wca.js
@@ -0,0 +1,73 @@
+import chalk from 'chalk';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { readFile } from 'node:fs/promises';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const customElementsPath = path.join(__dirname, '../custom-elements.json');
+
+function validateTag(tag) {
+
+	// assume an element with no description is to be ignored
+	if (!tag.description) {
+		return 0;
+	}
+
+	let errors = 0;
+
+	tag.attributes?.forEach(a => {
+		if (!a.description) {
+			console.log(`Attribute "${a.name}" on tag "${tag.name}" is missing a description.`);
+			errors++;
+		}
+	});
+
+	tag.events?.forEach(e => {
+		if (!e.description) {
+			console.log(`Event "${e.name}" on tag "${tag.name}" is missing a description.`);
+			errors++;
+		}
+	});
+
+	tag.slots?.forEach(s => {
+		if (!s.description) {
+			console.log(`Slot "${s.name}" on tag "${tag.name}" is missing a description.`);
+			errors++;
+		}
+	});
+
+	return errors;
+
+}
+
+async function exec() {
+
+	const customElementsContent = await readFile(customElementsPath, { encoding: 'utf8' });
+	const customElements = JSON.parse(customElementsContent);
+
+	console.log(chalk.blue('Validating custom-elements.json...'));
+	console.group();
+
+	let errors = 0;
+	customElements.tags.forEach(tag => {
+		errors += validateTag(tag);
+	});
+
+	console.groupEnd();
+
+	if (errors > 0) {
+		console.log(chalk.red('Validation failed.'));
+		return 1;
+	}
+
+	console.log(chalk.green('Success!'));
+	return 0;
+
+}
+
+exec()
+	.then((success) => process.exitCode = success)
+	.catch((err) => {
+		console.error(err);
+		process.exitCode = 1;
+	});
