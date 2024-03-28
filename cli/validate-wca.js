2cccbd9f4f9402328abb8eece5b3e3b76b7ec8b3
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-03-28 15:30:06 -0400

    Update cli/validate-wca.js
    Co-authored-by: Danny Gleckler <daniel.gleckler@d2l.com>

--- cli/validate-wca.js
+++ cli/validate-wca.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { readFile } from 'node:fs/promises';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const customElementsPath = path.join(__dirname, '../custom-elements.json');
