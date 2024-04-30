269507835cfa6660dc405b1f71925081dc70c981
Dave Batiste <dave.batiste@d2l.com>
2024-04-30 15:52:27 -0400

    Update TagListItemMixin to use focus-visible when supported.

--- components/tag-list/tag-list-item-mixin.js
+++ components/tag-list/tag-list-item-mixin.js
@@ -107,9 +107,15 @@ export const TagListItemMixin = superclass => class extends LocalizeCoreElement(
 				padding: 0.25rem 0.6rem;
 				text-overflow: ellipsis;
 			}
-			:host(:focus-within) .tag-list-item-container,
-			:host(:focus-within:hover) .tag-list-item-container {
+			:host(:hover) .tag-list-item-container:has(:focus-visible),
+			.tag-list-item-container:has(:focus-visible) {
 				box-shadow: inset 0 0 0 2px var(--d2l-color-celestine), 0 2px 4px rgba(0, 0, 0, 0.03);
+			}
+			@supports not selector(:has(a, b)) {
+				:host(:focus-within) .tag-list-item-container,
+				:host(:focus-within:hover) .tag-list-item-container {
+					box-shadow: inset 0 0 0 2px var(--d2l-color-celestine), 0 2px 4px rgba(0, 0, 0, 0.03);
+				}
 			}
 			:host(:hover) .tag-list-item-container,
 			:host(:focus-within) .tag-list-item-container {
