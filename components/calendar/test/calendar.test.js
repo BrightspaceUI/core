73e5e043020071e1c88d39d46154320762887a75
Dave Batiste <dave.batiste@d2l.com>
2024-09-25 13:30:40 -0400

    Update components/calendar/test/calendar.test.js
    Co-authored-by: Dave Lockhart <dlockhart@users.noreply.github.com>

--- components/calendar/test/calendar.test.js
+++ components/calendar/test/calendar.test.js
@@ -97,7 +97,7 @@ describe('d2l-calendar', () => {
 		it('dispatches d2l-calendar-view-change event when user changes to previous month', async() => {
 			const calendar = await fixture(normalFixture);
 			const el = calendar.shadowRoot.querySelectorAll('d2l-button-icon')[0];
-			setTimeout(() => el.click());
+			clickElem(el);
 			const { detail } = await oneEvent(calendar, 'd2l-calendar-view-change');
 			await aTimeout(1);
 			expect(detail.minValue).to.deep.equal(new Date(2015, 6, 26));
