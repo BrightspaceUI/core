da877b54740fee7a5bb1af021481b9f0089ed3c2
Margaree Peacocke <margaree.p@gmail.com>
2024-10-16 08:23:29 -0400

    add tests for aria-labels to axe tests to prevent regressions

--- components/meter/test/meter-linear.axe.js
+++ components/meter/test/meter-linear.axe.js
@@ -25,6 +25,13 @@ describe('d2l-meter-linear', () => {
 
 	it('text hidden', async() => {
 		const elem = await fixture(html`<d2l-meter-linear value="10" max="10" text="Completed" text-hidden></d2l-meter-linear>`);
+		expect(elem.shadowRoot.querySelector('div').getAttribute('aria-label')).to.equal('Completed, 10 out of 10, Progress Indicator');
+		await expect(elem).to.be.accessible();
+	});
+
+	it('text hidden with replacements', async() => {
+		const elem = await fixture(html`<d2l-meter-linear value="3" max="6" text="Visited: {x/y}" percent text-hidden></d2l-meter-linear>`);
+		expect(elem.shadowRoot.querySelector('div').getAttribute('aria-label')).to.equal('Visited: 3 out of 6, 50 %, Progress Indicator');
 		await expect(elem).to.be.accessible();
 	});
 
