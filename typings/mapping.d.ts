8fef89fdd3bf910021a70376023292c409578915
Anthony Wikkerink <awikkerink@desire2learn.com>
2024-11-10 09:54:28 -0500

    Add mapping.d.ts

---
+++ typings/mapping.d.ts
@@ -0,0 +1,8 @@
+/** Generated from {@link ../tools/create-mapping.sh} */
+import { LitElement } from 'lit';
+declare global {
+	interface HTMLElementTagNameMap {
+		// Components with export class...extends LitElement
+		'd2l-build-info': import('../index.js').BuildInfo;
+	}
+}
