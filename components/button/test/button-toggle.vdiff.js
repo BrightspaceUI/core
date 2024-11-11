f3e27491d1afc0731578a1bc0f9c8d5db5820eba
Margaree Peacocke <margaree.p@gmail.com>
2024-11-11 11:27:42 -0500

    add vdiffs

--- components/button/test/button-toggle.vdiff.js
+++ components/button/test/button-toggle.vdiff.js
@@ -5,18 +5,19 @@ import { clickElem, expect, fixture, focusElem, hoverElem, html, sendKeysElem } 
 
 describe('button-toggle', () => {
 
+	const buttonIconTemplate = html`<d2l-button-toggle><d2l-button-icon slot="not-pressed" icon="tier1:pin-hollow" text="Unpinned, click to pin."></d2l-button-icon><d2l-button-icon slot="pressed" icon="tier1:pin-filled" text="Pinned, click to unpin."></d2l-button-icon></d2l-button-toggle>`;
+	const getActiveButton = elem => {
+		if (elem.pressed) return elem.querySelector('[slot="pressed"]');
+		else return elem.querySelector('[slot="not-pressed"]');
+	};
+
 	[
-		{ category: 'button-icon', template: html`<d2l-button-toggle><d2l-button-icon slot="not-pressed" icon="tier1:pin-hollow" text="Unpinned, click to pin."></d2l-button-icon><d2l-button-icon slot="pressed" icon="tier1:pin-filled" text="Pinned, click to unpin."></d2l-button-icon></d2l-button-toggle>` },
+		{ category: 'button-icon', template: buttonIconTemplate },
 		{ category: 'button-icon-pressed', template: html`<d2l-button-toggle pressed><d2l-button-icon slot="not-pressed" icon="tier1:pin-hollow" text="Unpinned, click to pin."></d2l-button-icon><d2l-button-icon slot="pressed" icon="tier1:pin-filled" text="Pinned, click to unpin."></d2l-button-icon></d2l-button-toggle>` },
 		{ category: 'button-subtle', template: html`<d2l-button-toggle><d2l-button-subtle slot="not-pressed" icon="tier1:lock-unlock" text="Unlocked" description="Click to lock."></d2l-button-subtle><d2l-button-subtle slot="pressed" icon="tier1:lock-locked" text="Locked" description="Click to unlock."></d2l-button-subtle></d2l-button-toggle>` },
 		{ category: 'button-subtle-pressed', template: html`<d2l-button-toggle pressed><d2l-button-subtle slot="not-pressed" icon="tier1:lock-unlock" text="Unlocked" description="Click to lock."></d2l-button-subtle><d2l-button-subtle slot="pressed" icon="tier1:lock-locked" text="Locked" description="Click to unlock."></d2l-button-subtle></d2l-button-toggle>` },
 		{ category: 'button-subtle-disabled', template: html`<d2l-button-toggle><d2l-button-subtle slot="not-pressed" disabled icon="tier1:lock-unlock" text="Unlocked" description="Click to lock."></d2l-button-subtle><d2l-button-subtle slot="pressed" disabled icon="tier1:lock-locked" text="Locked" description="Click to unlock."></d2l-button-subtle></d2l-button-toggle>` }
 	].forEach(({ category, template }) => {
-
-		const getActiveButton = elem => {
-			if (elem.pressed) return elem.querySelector('[slot="pressed"]');
-			else return elem.querySelector('[slot="not-pressed"]');
-		};
 
 		describe(category, () => {
 			[
@@ -34,6 +35,67 @@ describe('button-toggle', () => {
 			});
 		});
 
+	});
+
+	describe('consumer manages state', () => {
+
+		it('normal', async() => {
+			const elem = await fixture(buttonIconTemplate);
+			const buttonIcons = elem.querySelectorAll('d2l-button-icon');
+			buttonIcons[0].addEventListener('click', (e) => {
+				e.preventDefault();
+			});
+			buttonIcons[1].addEventListener('click', (e) => {
+				e.preventDefault();
+			});
+			await expect(elem).to.be.golden();
+		});
+
+		it('click with no state management', async() => {
+			const elem = await fixture(buttonIconTemplate);
+			const buttonIcons = elem.querySelectorAll('d2l-button-icon');
+			buttonIcons[0].addEventListener('click', (e) => {
+				e.preventDefault();
+			});
+			buttonIcons[1].addEventListener('click', (e) => {
+				e.preventDefault();
+			});
+			clickElem(getActiveButton(elem));
+			await expect(elem).to.be.golden();
+		});
+
+		it('click once with state management', async() => {
+			const elem = await fixture(buttonIconTemplate);
+			const buttonIcons = elem.querySelectorAll('d2l-button-icon');
+			buttonIcons[0].addEventListener('click', (e) => {
+				e.preventDefault();
+				elem.pressed = true;
+			});
+			buttonIcons[1].addEventListener('click', (e) => {
+				e.preventDefault();
+				elem.pressed = false;
+			});
+
+			clickElem(getActiveButton(elem));
+			await expect(elem).to.be.golden();
+		});
+
+		it('click twice with state management', async() => {
+			const elem = await fixture(buttonIconTemplate);
+			const buttonIcons = elem.querySelectorAll('d2l-button-icon');
+			buttonIcons[0].addEventListener('click', (e) => {
+				e.preventDefault();
+				elem.pressed = true;
+			});
+			buttonIcons[1].addEventListener('click', (e) => {
+				e.preventDefault();
+				elem.pressed = false;
+			});
+
+			clickElem(getActiveButton(elem));
+			clickElem(getActiveButton(elem));
+			await expect(elem).to.be.golden();
+		});
 	});
 
 });
