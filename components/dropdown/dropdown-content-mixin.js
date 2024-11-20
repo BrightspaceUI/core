2b37b37442b23c1439ef8eca3da9dd7f83e86619
Dave Lockhart <dlockhart@users.noreply.github.com>
2024-11-20 10:05:42 -0500

    move wait code into open

--- components/dropdown/dropdown-content-mixin.js
+++ components/dropdown/dropdown-content-mixin.js
@@ -626,10 +626,6 @@ export const DropdownContentMixin = superclass => class extends LocalizeCoreElem
 
 	async __openedChanged(newValue) {
 
-		// DE44538: wait for dropdown content to fully render,
-		// otherwise this.getContentContainer() can return null.
-		await this.__waitForContentContainer();
-
 		this.__previousFocusableAncestor =
 			newValue === true
 				? getPreviousFocusableAncestor(this, false, false)
@@ -637,6 +633,9 @@ export const DropdownContentMixin = superclass => class extends LocalizeCoreElem
 
 		const doOpen = async() => {
 
+			// DE44538: wait for dropdown content to fully render,
+			// otherwise this.getContentContainer() can return null.
+			await this.__waitForContentContainer();
 			const content = this.getContentContainer();
 
 			if (!this.noAutoFit) {
