8002ca3735593d3e4de27313b2bc4096fcd34592
Dave Batiste <dave.batiste@d2l.com>
2024-12-04 16:22:20 -0500

    feat: Add max-selection-count option to SelectionActionMixin. (#5196)

--- components/selection/selection-action.js
+++ components/selection/selection-action.js
@@ -3,7 +3,6 @@ import { css, html, LitElement } from 'lit';
 import { ButtonMixin } from '../button/button-mixin.js';
 import { FocusMixin } from '../../mixins/focus/focus-mixin.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
-import { LocalizeCoreElement } from '../../helpers/localize-core-element.js';
 import { SelectionActionMixin } from './selection-action-mixin.js';
 import { SelectionInfo } from './selection-mixin.js';
 
@@ -12,7 +11,7 @@ import { SelectionInfo } from './selection-mixin.js';
  * @fires d2l-selection-action-click - Dispatched when the user clicks the action button. The `SelectionInfo` is provided as the event `detail`. If `requires-selection` was specified then the event will only be dispatched if items are selected.
  * @fires d2l-selection-observer-subscribe - Internal event
  */
-class Action extends FocusMixin(LocalizeCoreElement(SelectionActionMixin(ButtonMixin(LitElement)))) {
+class Action extends FocusMixin(SelectionActionMixin(ButtonMixin(LitElement))) {
 
 	static get properties() {
 		return {
@@ -55,12 +54,14 @@ class Action extends FocusMixin(LocalizeCoreElement(SelectionActionMixin(ButtonM
 	}
 
 	render() {
+		const disabledTooltip = this._disabledTooltip || (this.disabled && this.disabledTooltip ? this.disabledTooltip : undefined);
+
 		return html`
 			<d2l-button-subtle
 				class="vdiff-target"
 				@click="${this._handleActionClick}"
 				?disabled="${this.disabled}"
-				disabled-tooltip="${ifDefined(this.disabled ? this.localize('components.selection.action-hint') : undefined)}"
+				disabled-tooltip="${ifDefined(disabledTooltip)}"
 				icon="${ifDefined(this.icon)}"
 				text="${this.text}">
 			</d2l-button-subtle>
