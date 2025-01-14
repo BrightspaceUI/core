a19e22e93daa78bf750d82945052ace31039e6e5
Dave Lockhart <dlockhart@users.noreply.github.com>
2025-01-14 17:42:44 -0500

    wait for loading-complete from dialog mixin

--- helpers/internal/waitForElem.js
+++ helpers/internal/waitForElem.js
@@ -4,44 +4,22 @@ export async function waitForElem(elem, predicate = () => true) {
 
 	if (!elem) return;
 
-	const doWait = async() => {
-
-		const update = elem.updateComplete;
-		if (typeof update === 'object' && Promise.resolve(update) === update) {
-			await update;
-			await new Promise(resolve => {
-				requestAnimationFrame(() => resolve());
-			});
-		}
-
-		if (typeof elem.getLoadingComplete === 'function') {
-			await elem.getLoadingComplete();
-			await new Promise(resolve => {
-				requestAnimationFrame(() => resolve());
-			});
-		}
-
-		const children = getComposedChildren(elem, predicate);
-		await Promise.all(children.map(e => waitForElem(e, predicate)));
-
-	};
-
-	await new Promise((resolve) => {
-		const observer = new MutationObserver((records) => {
-			for (const record of records) {
-				for (const removedNode of record.removedNodes) {
-					if (removedNode === elem) {
-						observer.disconnect();
-						resolve();
-						return;
-					}
-				}
-			}
+	const update = elem.updateComplete;
+	if (typeof update === 'object' && Promise.resolve(update) === update) {
+		await update;
+		await new Promise(resolve => {
+			requestAnimationFrame(() => resolve());
 		});
-		observer.observe(elem.parentNode, { childList: true });
-		doWait()
-			.then(() => observer.disconnect())
-			.then(resolve);
-	});
+	}
+
+	if (typeof elem.getLoadingComplete === 'function') {
+		await elem.getLoadingComplete();
+		await new Promise(resolve => {
+			requestAnimationFrame(() => resolve());
+		});
+	}
+
+	const children = getComposedChildren(elem, predicate);
+	await Promise.all(children.map(e => waitForElem(e, predicate)));
 
 }
