55506ce48c0ef575521d6bb6c7b5a2e5afe228b0
Trevor Kearse <kearse.trevor@gmail.com>
2025-01-16 16:00:49 -0500

    fix: add updateSize to within flag block (#5322)
    * add updateSize to within flag block
    * update ff name

--- components/dialog/dialog-mixin.js
+++ components/dialog/dialog-mixin.js
@@ -453,11 +453,11 @@ export const DialogMixin = superclass => class extends RtlMixin(superclass) {
 			if (reduceMotion) await new Promise(resolve => requestAnimationFrame(resolve));
 			else await animPromise;
 
-			const flag = window.D2L?.LP?.Web?.UI?.Flags.Flag('GAUD-7397-dialog-resize-updateComplete', true) ?? true;
+			const flag = window.D2L?.LP?.Web?.UI?.Flags.Flag('GAUD-7397-dialog-resize-update-complete', true) ?? true;
 			if (flag) {
 				await this.#waitForUpdateComplete();
+				await this._updateSize();
 			}
-			await this._updateSize();
 			/** Dispatched when the dialog is opened */
 			this.dispatchEvent(new CustomEvent(
 				'd2l-dialog-open', { bubbles: true, composed: true }
