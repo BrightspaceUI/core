d8070edf1688f17372db73ba76a36279194f4f1c
KearseTrevor <tkearse@desire2learn.com>
2025-02-05 09:45:55 -0500

    conditional to updatePos

--- components/tooltip/tooltip.js
+++ components/tooltip/tooltip.js
@@ -988,7 +988,7 @@ class Tooltip extends RtlMixin(LitElement) {
 				);
 				logAccessibilityWarning = false;
 			}
-			if (this.showing) {
+			if (!this._initiallyFocused && this.showing) {
 				this.updatePosition();
 			} else if (isComposedAncestor(this._target, getComposedActiveElement())) {
 				this._onTargetFocus();
