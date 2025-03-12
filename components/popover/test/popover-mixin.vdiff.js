fe208c3447ad6760e253e7fac335b89b923d4b2c
Dave Batiste <dave.batiste@d2l.com>
2025-03-12 10:32:07 -0400

    feat: Update dropdown content components to use PopoverMixin (flag off) (#5422)

--- components/popover/test/popover-mixin.vdiff.js
+++ components/popover/test/popover-mixin.vdiff.js
@@ -4,11 +4,11 @@ import { expect, fixture, html } from '@brightspace-ui/testing';
 describe('popover-mixin', () => {
 
 	[
-		{ name: 'default', template: html`<d2l-test-popover style="max-width: 400px;">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover>` }
+		{ name: 'default', template: html`<div><button>Open</button><d2l-test-popover style="max-width: 400px;">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover></div>` }
 	].forEach(({ name, template }) => {
 		it(name, async() => {
 			const el = await fixture(template, { viewport: { width: 700, height: 400 } });
-			el.opened = true;
+			el.querySelector('d2l-test-popover').open(el.querySelector('button'));
 			await expect(document).to.be.golden();
 		});
 	});
