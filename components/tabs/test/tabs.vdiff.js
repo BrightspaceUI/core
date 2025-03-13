98bf5914ef3355684b315ffb759bc0f0e856bb3a
Margaree Peacocke <margaree.p@gmail.com>
2025-03-13 10:22:04 -0400

    return to old vdiffs

--- components/tabs/test/tabs.vdiff.js
+++ components/tabs/test/tabs.vdiff.js
@@ -1,27 +1,20 @@
 import '../../button/button.js';
-import '../tab.js';
 import '../tabs.js';
 import '../tab-panel.js';
 import { clickElem, expect, fixture, focusElem, html, sendKeysElem } from '@brightspace-ui/testing';
 
 const noPanelSelectedFixture = html`
 	<d2l-tabs>
-		<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-		<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-		<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-		<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-		<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-		<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+		<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
+		<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
+		<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 	</d2l-tabs>
 `;
 const panelSelectedFixture = html`
 	<d2l-tabs>
-		<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-		<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-		<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
-		<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-		<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-		<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+		<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
+		<d2l-tab-panel text="Biology" selected>Tab content for Biology</d2l-tab-panel>
+		<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 	</d2l-tabs>
 `;
 
@@ -44,8 +37,7 @@ describe('d2l-tabs', () => {
 		it('one tab', async() => {
 			const elem = await fixture(html`
 				<d2l-tabs>
-					<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
+					<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
 				</d2l-tabs>
 			`, { viewport });
 			await expect(elem).to.be.golden();
@@ -54,29 +46,22 @@ describe('d2l-tabs', () => {
 		it('skeleton', async() => {
 			const elem = await fixture(html`
 				<d2l-tabs skeleton>
-					<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-					<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-					<d2l-tab id="physics" text="Physics" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="physics" slot="panels">Tab content for Physics</d2l-tab-panel>
-					<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+					<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
+					<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
+					<d2l-tab-panel text="Physics">Tab content for Physics</d2l-tab-panel>
+					<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 				</d2l-tabs>
 			`, { viewport });
 			await expect(elem).to.be.golden();
 		});
 
-		it.skip('skeleton no text', async() => {
+		it('skeleton no text', async() => {
 			const elem = await fixture(html`
 				<d2l-tabs skeleton>
-					<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
+					<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
 					<d2l-tab-panel>Tab content for Biology</d2l-tab-panel>
-					<d2l-tab id="physics" text="Physics" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="physics" slot="panels">Tab content for Physics</d2l-tab-panel>
-					<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+					<d2l-tab-panel text="Physics">Tab content for Physics</d2l-tab-panel>
+					<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 				</d2l-tabs>
 			`, { viewport });
 			await expect(elem).to.be.golden();
@@ -85,12 +70,9 @@ describe('d2l-tabs', () => {
 		it('ellipsis', async() => {
 			const elem = await fixture(html`
 				<d2l-tabs>
-					<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-					<d2l-tab id="earth" text="Earth &amp; Planetary Sciences" slot="tabs" selected></d2l-tab>
-					<d2l-tab-panel labelled-by="earth" slot="panels">Tab content for Earth &amp; Planetary Sciences</d2l-tab-panel>
-					<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+					<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
+					<d2l-tab-panel text="Earth &amp; Planetary Sciences" selected>Tab content for Earth &amp; Planetary Sciences</d2l-tab-panel>
+					<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 				</d2l-tabs>
 			`, { viewport });
 			await expect(elem).to.be.golden();
@@ -111,10 +93,8 @@ describe('d2l-tabs', () => {
 		it('action slot', async() => {
 			const elem = await fixture(html`
 				<d2l-tabs>
-					<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-					<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
+					<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
+					<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
 					<d2l-button slot="ext">Search</d2l-button>
 				</d2l-tabs>
 			`, { viewport });
@@ -124,10 +104,8 @@ describe('d2l-tabs', () => {
 		it('no padding', async() => {
 			const elem = await fixture(html`
 				<d2l-tabs>
-					<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="all" slot="panels" no-padding style="background-color: orange;">Tab content for All</d2l-tab-panel>
-					<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
+					<d2l-tab-panel text="All" no-padding style="background-color: orange;">Tab content for All</d2l-tab-panel>
+					<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
 				</d2l-tabs>
 			`, { viewport });
 			await expect(elem).to.be.golden();
@@ -139,26 +117,18 @@ describe('d2l-tabs', () => {
 
 		const nextFixture = html`
 			<d2l-tabs>
-				<d2l-tab id="all" text="All Courses" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-				<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
-				<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-				<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
-				<d2l-tab id="geology" text="Geology" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="geology" slot="panels">Tab content for Geology</d2l-tab-panel>
+				<d2l-tab-panel text="All Courses">Tab content for All</d2l-tab-panel>
+				<d2l-tab-panel text="Biology" selected>Tab content for Biology</d2l-tab-panel>
+				<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
+				<d2l-tab-panel text="Geology">Tab content for Geology</d2l-tab-panel>
 			</d2l-tabs>
 		`;
 		const previousFixture = html`
 			<d2l-tabs>
-				<d2l-tab id="all" text="All Courses" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-				<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-				<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
-				<d2l-tab id="geology" text="Geology" slot="tabs" selected></d2l-tab>
-				<d2l-tab-panel labelled-by="geology" slot="panels">Tab content for Geology</d2l-tab-panel>
+				<d2l-tab-panel text="All Courses">Tab content for All</d2l-tab-panel>
+				<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
+				<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
+				<d2l-tab-panel text="Geology" selected>Tab content for Geology</d2l-tab-panel>
 			</d2l-tabs>
 		`;
 
@@ -193,12 +163,9 @@ describe('d2l-tabs', () => {
 				it('action slot', async() => {
 					const elem = await fixture(html`
 						<d2l-tabs>
-							<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-							<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-							<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-							<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-							<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-							<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+							<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
+							<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
+							<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 							<d2l-button slot="ext">Search</d2l-button>
 						</d2l-tabs>
 					`, { viewport, rtl });
@@ -228,12 +195,9 @@ describe('d2l-tabs', () => {
 		beforeEach(async() => {
 			elem = await fixture(html`
 				<d2l-tabs max-to-show="2">
-					<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-					<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
-					<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-					<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+					<d2l-tab-panel text="All">Tab content for All</d2l-tab-panel>
+					<d2l-tab-panel text="Biology" selected>Tab content for Biology</d2l-tab-panel>
+					<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 				</d2l-tabs>
 			`, { viewport });
 		});
@@ -258,12 +222,9 @@ describe('d2l-tabs', () => {
 
 		const keyboardFixture = html`
 			<d2l-tabs>
-				<d2l-tab id="all" text="All Courses" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-				<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
-				<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
-				<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
-				<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
+				<d2l-tab-panel text="All Courses">Tab content for All</d2l-tab-panel>
+				<d2l-tab-panel text="Biology" selected>Tab content for Biology</d2l-tab-panel>
+				<d2l-tab-panel text="Chemistry">Tab content for Chemistry</d2l-tab-panel>
 			</d2l-tabs>
 		`;
 
@@ -295,10 +256,8 @@ describe('d2l-tabs', () => {
 			it(`selects on ${key}`, async() => {
 				const elem = await fixture(html`
 					<d2l-tabs>
-						<d2l-tab id="all" text="All Courses" slot="tabs"></d2l-tab>
-						<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-						<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-						<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
+						<d2l-tab-panel text="All Courses">Tab content for All</d2l-tab-panel>
+						<d2l-tab-panel text="Biology">Tab content for Biology</d2l-tab-panel>
 					</d2l-tabs>
 				`, { viewport });
 				await sendKeysElem(elem, 'press', `ArrowRight+${key}`);
