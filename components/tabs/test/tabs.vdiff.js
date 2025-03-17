fff1f8b370c0161192824e7839aad0398261f084
Margaree Peacocke <margaree.p@gmail.com>
2025-03-17 10:54:52 -0400

    fix where "selected" is set in paired test cases

--- components/tabs/test/tabs.vdiff.js
+++ components/tabs/test/tabs.vdiff.js
@@ -36,8 +36,8 @@ const panelSelectedFixture = {
 		<d2l-tabs>
 			<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
 			<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-			<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-			<d2l-tab-panel labelled-by="biology" slot="panels" selected>Tab content for Biology</d2l-tab-panel>
+			<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
+			<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
 			<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
 			<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
 		</d2l-tabs>
@@ -116,8 +116,8 @@ const ellipsisFixture = {
 		<d2l-tabs>
 			<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
 			<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-			<d2l-tab id="earth" text="Earth &amp; Planetary Sciences" slot="tabs"></d2l-tab>
-			<d2l-tab-panel labelled-by="earth" slot="panels" selected>Tab content for Earth &amp; Planetary Sciences</d2l-tab-panel>
+			<d2l-tab id="earth" text="Earth &amp; Planetary Sciences" slot="tabs" selected></d2l-tab>
+			<d2l-tab-panel labelled-by="earth" slot="panels">Tab content for Earth &amp; Planetary Sciences</d2l-tab-panel>
 			<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
 			<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
 		</d2l-tabs>
@@ -194,8 +194,8 @@ const maxToShowFixture = {
 		<d2l-tabs max-to-show="2">
 			<d2l-tab id="all" text="All" slot="tabs"></d2l-tab>
 			<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-			<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-			<d2l-tab-panel labelled-by="biology" slot="panels" selected>Tab content for Biology</d2l-tab-panel>
+			<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
+			<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
 			<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
 			<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
 		</d2l-tabs>
@@ -278,8 +278,8 @@ describe('d2l-tabs', () => {
 				<d2l-tabs>
 					<d2l-tab id="all" text="All Courses" slot="tabs"></d2l-tab>
 					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-					<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="biology" slot="panels" selected>Tab content for Biology</d2l-tab-panel>
+					<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
+					<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
 					<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
 					<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
 					<d2l-tab id="geology" text="Geology" slot="tabs"></d2l-tab>
@@ -304,8 +304,8 @@ describe('d2l-tabs', () => {
 					<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
 					<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
 					<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
-					<d2l-tab id="geology" text="Geology" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="geology" slot="panels" selected>Tab content for Geology</d2l-tab-panel>
+					<d2l-tab id="geology" text="Geology" slot="tabs" selected></d2l-tab>
+					<d2l-tab-panel labelled-by="geology" slot="panels">Tab content for Geology</d2l-tab-panel>
 				</d2l-tabs>
 			`
 		};
@@ -397,8 +397,8 @@ describe('d2l-tabs', () => {
 				<d2l-tabs>
 					<d2l-tab id="all" text="All Courses" slot="tabs"></d2l-tab>
 					<d2l-tab-panel labelled-by="all" slot="panels">Tab content for All</d2l-tab-panel>
-					<d2l-tab id="biology" text="Biology" slot="tabs"></d2l-tab>
-					<d2l-tab-panel labelled-by="biology" slot="panels" selected>Tab content for Biology</d2l-tab-panel>
+					<d2l-tab id="biology" text="Biology" slot="tabs" selected></d2l-tab>
+					<d2l-tab-panel labelled-by="biology" slot="panels">Tab content for Biology</d2l-tab-panel>
 					<d2l-tab id="chemistry" text="Chemistry" slot="tabs"></d2l-tab>
 					<d2l-tab-panel labelled-by="chemistry" slot="panels">Tab content for Chemistry</d2l-tab-panel>
 				</d2l-tabs>
