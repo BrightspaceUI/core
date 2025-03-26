9cdc85e1d6cc5c878c3aaa1d39f10e80061d9f25
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-03-26 13:22:50 -0400

    Make empty state focusable

--- components/empty-state/empty-state-simple.js
+++ components/empty-state/empty-state-simple.js
@@ -4,12 +4,14 @@ import { html, LitElement } from 'lit';
 import { bodyCompactStyles } from '../typography/styles.js';
 import { PropertyRequiredMixin } from '../../mixins/property-required/property-required-mixin.js';
 import { RtlMixin } from '../../mixins/rtl/rtl-mixin.js';
+import { FocusMixin } from '../../mixins/focus/focus-mixin.js';
+import { getFirstFocusableDescendant } from '../../helpers/focus.js';
 
 /**
  * The `d2l-empty-state-simple` component is an empty state component that displays a description. An empty state action component can be placed inside of the default slot to add an optional action.
  * @slot - Slot for empty state actions
  */
-class EmptyStateSimple extends PropertyRequiredMixin(RtlMixin(LitElement)) {
+class EmptyStateSimple extends FocusMixin(PropertyRequiredMixin(RtlMixin(LitElement))) {
 
 	static get properties() {
 		return {
@@ -25,13 +27,23 @@ class EmptyStateSimple extends PropertyRequiredMixin(RtlMixin(LitElement)) {
 		return [bodyCompactStyles, emptyStateStyles, emptyStateSimpleStyles];
 	}
 
+	static get focusElementSelector() {
+		return 'p.d2l-empty-state-description';
+	}
+
 	render() {
 		return html`
 			<div class="empty-state-container">
-				<p class="d2l-body-compact d2l-empty-state-description">${this.description}</p>
+				<p class="d2l-body-compact d2l-empty-state-description" tabindex="-1">${this.description}</p>
 				<slot class="action-slot"></slot>
 			</div>
 		`;
+	}
+
+	_getFocusTarget() {
+		const slotElement = this.shadowRoot.querySelector('slot.action-slot');
+		const focusTarget = getFirstFocusableDescendant(slotElement);
+		return focusTarget ?? super._getFocusTarget();
 	}
 
 }
