0baade7413ce204240efd8e9ddfe2fb83497069f
Dave Batiste <dave.batiste@d2l.com>
2025-04-16 15:02:41 -0400

    Move toast's alert role to d2l-alert for improved announcement ux.

--- components/alert/alert-toast.js
+++ components/alert/alert-toast.js
@@ -232,13 +232,14 @@ class AlertToast extends LitElement {
 			'd2l-alert-toast-container-lowest': !this._totalSiblingHeightBelow,
 			'vdiff-target': true
 		};
+
 		return html`
 			<div
 				class="${classMap(containerClasses)}"
 				data-state="${this._state}"
 				style="${styleMap(containerStyles)}"
 				@transitionend=${this._onTransitionEnd}>
-				<d2l-alert
+				<d2l-alert role="${ this._state !== states.CLOSED ? 'alert' : '' }"
 					@blur=${this._onBlur}
 					button-text="${ifDefined(this.buttonText)}"
 					@d2l-alert-button-press=${this._handleButtonPress}
@@ -392,7 +393,6 @@ class AlertToast extends LitElement {
 		this._totalSiblingHeightBelow = 0;
 		this._numAlertsBelow = 0;
 		this._closeClicked = false;
-		this.removeAttribute('role');
 	}
 
 	_onTransitionEnd() {
@@ -420,13 +420,11 @@ class AlertToast extends LitElement {
 					this._state = states.OPEN;
 				}
 			}
-			this.setAttribute('role', 'alert');
 		} else {
 			if (!this._innerContainer) return;
 
 			if (activeReduceMotion || this._state === states.PREOPENING) {
 				cancelAnimationFrame(this._preopenFrame);
-				this.removeAttribute('role');
 			} else if (this._state === states.OPENING || this._state === states.OPEN || this._state === states.SLIDING) {
 				this._state = states.CLOSING;
 			}
