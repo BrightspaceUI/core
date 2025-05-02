a610fedfb8766b308fbb8875492a1fe13d806445
Dave Lockhart <dlockhart@users.noreply.github.com>
2025-05-02 14:56:13 -0400

    add support for inline-help and supporting content

--- components/inputs/input-radio.js
+++ components/inputs/input-radio.js
@@ -3,10 +3,11 @@ import { classMap } from 'lit/directives/class-map.js';
 import { FocusMixin } from '../../mixins/focus/focus-mixin.js';
 import { getUniqueId } from '../../helpers/uniqueId.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
+import { InputInlineHelpMixin } from './input-inline-help.js';
 import { PropertyRequiredMixin } from '../../mixins/property-required/property-required-mixin.js';
 import { radioStyles } from './input-radio-styles.js';
 
-class InputRadio extends FocusMixin(PropertyRequiredMixin(LitElement)) {
+class InputRadio extends InputInlineHelpMixin(FocusMixin(PropertyRequiredMixin(LitElement))) {
 
 	static get properties() {
 		return {
@@ -26,19 +27,25 @@ class InputRadio extends FocusMixin(PropertyRequiredMixin(LitElement)) {
 			 */
 			label: { required: true, type: String },
 			/**
+			 * Hides the supporting slot when unchecked
+			 * @type {boolean}
+			 */
+			supportingHiddenWhenUnchecked: { type: Boolean, attribute: 'supporting-hidden-when-unchecked', reflect: true },
+			/**
 			 * Value of the input
 			 * @type {string}
 			 */
 			value: { type: String },
 			_checked: { state: true },
 			_firstFocusable: { state: true },
-			_hovering: { state: true },
+			_hasSupporting: { state: true },
+			_isHovered: { state: true },
 			_invalid: { state: true }
 		};
 	}
 
 	static get styles() {
-		return [radioStyles, css`
+		return [super.styles, radioStyles, css`
 			:host {
 				display: block;
 			}
@@ -47,6 +54,18 @@ class InputRadio extends FocusMixin(PropertyRequiredMixin(LitElement)) {
 			}
 			.d2l-input-radio-label {
 				cursor: default;
+				margin-block-end: 0;
+			}
+			.d2l-input-inline-help,
+			.d2l-input-radio-supporting {
+				margin-inline-start: 1.7rem;
+			}
+			.d2l-input-radio-supporting {
+				display: none;
+				margin-block-start: 0.6rem;
+			}
+			.d2l-input-radio-supporting-visible {
+				display: block;
 			}
 		`];
 	}
@@ -54,10 +73,12 @@ class InputRadio extends FocusMixin(PropertyRequiredMixin(LitElement)) {
 	constructor() {
 		super();
 		this.disabled = false;
+		this.supportingHiddenWhenUnchecked = false;
 		this.value = 'on';
 		this._checked = false;
 		this._firstFocusable = false;
-		this._hovering = false;
+		this._hasSupporting = false;
+		this._isHovered = false;
 		this._invalid = false;
 	}
 
@@ -84,14 +105,18 @@ class InputRadio extends FocusMixin(PropertyRequiredMixin(LitElement)) {
 		const radioClasses = {
 			'd2l-input-radio': true,
 			'd2l-disabled': this.disabled,
-			'd2l-hovering': this._hovering && !this.disabled
+			'd2l-hovering': this._isHovered && !this.disabled
+		};
+		const supportingClasses = {
+			'd2l-input-radio-supporting': true,
+			'd2l-input-radio-supporting-visible': this._hasSupporting && (!this.supportingHiddenWhenUnchecked || this._checked),
 		};
 		const tabindex = (!this.disabled && (this._checked || this._firstFocusable)) ? '0' : undefined;
-		// TODO: handle secondary content slot
 		return html`
 			<div class="${classMap(labelClasses)}" @mouseover="${this.#handleMouseOver}" @mouseout="${this.#handleMouseOut}">
 				<div
 					aria-checked="${this._checked}"
+					aria-describedby="${ifDefined(this._hasInlineHelp ? this.#inlineHelpId : undefined)}"
 					aria-disabled="${ifDefined(this.disabled ? 'true' : undefined)}"
 					aria-invalid="${ifDefined(this._invalid ? 'true' : undefined)}"
 					aria-labelledby="${this.#labelId}"
@@ -100,17 +125,29 @@ class InputRadio extends FocusMixin(PropertyRequiredMixin(LitElement)) {
 					tabindex="${ifDefined(tabindex)}"></div>
 				<div id="${this.#labelId}">${this.label}</div>
 			</div>
+			${this._renderInlineHelp(this.#inlineHelpId)}
+			<div class="${classMap(supportingClasses)}" @change="${this.#handleSupportingChange}"><slot name="supporting" @slotchange="${this.#handleSupportingSlotChange}"></slot></div>
 		`;
 	}
 
+	#inlineHelpId = getUniqueId();
 	#labelId = getUniqueId();
 
 	#handleMouseOut() {
-		this._hovering = false;
+		this._isHovered = false;
 	}
 
 	#handleMouseOver() {
-		this._hovering = true;
+		this._isHovered = true;
+	}
+
+	#handleSupportingChange(e) {
+		e.stopPropagation();
+	}
+
+	#handleSupportingSlotChange(e) {
+		const content = e.target.assignedNodes({ flatten: true });
+		this._hasSupporting = content?.length > 0;
 	}
 
 }
