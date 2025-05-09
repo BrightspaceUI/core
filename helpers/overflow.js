076628eef3276f0c6c282551469f856b57569476
Danny Gleckler <daniel.gleckler@d2l.com>
2025-05-09 11:50:09 -0400

    Add overflow helper

---
+++ helpers/overflow.js
@@ -0,0 +1,24 @@
+import { unsafeCSS } from 'lit';
+
+const defaultOverflowDeclarations = getOverflowDeclarations({});
+
+export function getOverflowDeclarations({ textOverflow = 'ellipsis', lines = 0, lit = true } = {}) {
+	if (!arguments.length) return defaultOverflowDeclarations;
+
+	const declarations = `
+		overflow: visible;
+		overflow-x: clip;
+		${lines
+		? `
+	  	display: -webkit-box;
+		overflow-clip-margin: 0.2em;
+		overflow-y: clip;
+		-webkit-box-orient: vertical;
+  		-webkit-line-clamp: ${lines};`
+    	: `
+     	overflow-clip-margin: 1em;
+    	text-overflow: ${textOverflow};
+     	white-space: nowrap;`}
+	`;
+	return lit ? unsafeCSS(declarations) : declarations;
+}
