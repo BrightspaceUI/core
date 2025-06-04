7f2541e61619221d65658245f6c7c3921091c69e
Margaree Peacocke <margaree.p@gmail.com>
2025-06-04 09:41:01 +0200

    feat: Add button: Change plus icon background color to white from transparent

--- components/button/button-add.js
+++ components/button/button-add.js
@@ -3,6 +3,7 @@ import '../tooltip/tooltip.js';
 import { css, html, LitElement, nothing, unsafeCSS } from 'lit';
 import { VisibleOnAncestorMixin, visibleOnAncestorStyles } from '../../mixins/visible-on-ancestor/visible-on-ancestor-mixin.js';
 import { FocusMixin } from '../../mixins/focus/focus-mixin.js';
+import { getFlag } from '../../helpers/flags.js';
 import { getFocusPseudoClass } from '../../helpers/focus.js';
 import { getUniqueId } from '../../helpers/uniqueId.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
@@ -15,6 +16,7 @@ const MODE = {
 	icon_and_text: 'icon-and-text',
 	icon_when_interacted: 'icon-when-interacted'
 };
+const whiteAddButtonBackgroundFlag = getFlag('GAUD-7495-add-button-white-background', true);
 
 /**
  * A component for quickly adding items to a specific locaiton.
@@ -31,7 +33,8 @@ class ButtonAdd extends RtlMixin(PropertyRequiredMixin(FocusMixin(LocalizeCoreEl
 			 * ACCESSIBILITY: The text associated with the button. When mode is `icon-and-text` this text is displayed next to the icon, otherwise this text is in a tooltip.
 			 * @type {string}
 			 */
-			text: { type: String, required: true }
+			text: { type: String, required: true },
+			_whiteBackgroundAddButton: { type: Boolean, attribute: '_white-background-add-button', reflect: true }
 		};
 	}
 
@@ -105,6 +108,9 @@ class ButtonAdd extends RtlMixin(PropertyRequiredMixin(FocusMixin(LocalizeCoreEl
 				border-radius: 0.3rem;
 				box-shadow: 0 0 0 2px var(--d2l-button-add-hover-focus-color);
 			}
+			:host([_white-background-add-button]) button:${unsafeCSS(getFocusPseudoClass())} d2l-button-add-icon-text {
+				background-color: white;
+			}
 			:host([mode="icon-when-interacted"]) button:${unsafeCSS(getFocusPseudoClass())} d2l-button-add-icon-text,
 			:host([mode="icon"]) button:${unsafeCSS(getFocusPseudoClass())} d2l-button-add-icon-text {
 				border-radius: 0.2rem;
@@ -159,6 +165,7 @@ class ButtonAdd extends RtlMixin(PropertyRequiredMixin(FocusMixin(LocalizeCoreEl
 		this.mode = MODE.icon;
 
 		this._buttonId = getUniqueId();
+		this._whiteBackgroundAddButton = whiteAddButtonBackgroundFlag;
 	}
 
 	static get focusElementSelector() {
@@ -237,13 +244,25 @@ class ButtonAddIconText extends VisibleOnAncestorMixin(LitElement) {
 	}
 
 	render() {
-		return html`
+		const whiteBackgroundAddButton = html`
+			<svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+				<g>
+					<circle cx="9" cy="9" r="9" fill="#fff"/>
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M9 2a7 7 0 1 1-7 7 7.008 7.008 0 0 1 7-7Zm0-2a9 9 0 1 0 0 18A9 9 0 0 0 9 0Z"/>
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M10 5a1 1 0 0 0-2 0v3H5a1 1 0 0 0 0 2h3v3a1 1 0 1 0 2 0v-3h3a1 1 0 1 0 0-2h-3V5Z"/>
+				</g>
+			</svg>
+		`;
+		const translucentBackgroundAddButton = html`
 			<svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
 				<g>
 					<path fill-rule="evenodd" clip-rule="evenodd" d="M9 2C10.3845 2 11.7378 2.41054 12.889 3.17971C14.0401 3.94888 14.9373 5.04213 15.4672 6.32122C15.997 7.6003 16.1356 9.00777 15.8655 10.3656C15.5954 11.7235 14.9287 12.9708 13.9497 13.9497C12.9708 14.9287 11.7235 15.5954 10.3656 15.8655C9.00777 16.1356 7.6003 15.997 6.32122 15.4672C5.04213 14.9373 3.94888 14.0401 3.17971 12.889C2.41054 11.7378 2 10.3845 2 9C2.00212 7.14413 2.7403 5.36489 4.05259 4.05259C5.36489 2.7403 7.14413 2.00212 9 2V2ZM9 0C6.61305 0 4.32387 0.948212 2.63604 2.63604C0.948212 4.32387 0 6.61305 0 9C0 11.3869 0.948212 13.6761 2.63604 15.364C4.32387 17.0518 6.61305 18 9 18C11.3869 18 13.6761 17.0518 15.364 15.364C17.0518 13.6761 18 11.3869 18 9C18 6.61305 17.0518 4.32387 15.364 2.63604C13.6761 0.948212 11.3869 0 9 0V0Z" />
 					<path fill-rule="evenodd" clip-rule="evenodd" d="M10 5C10 4.44772 9.55228 4 9 4C8.44772 4 8 4.44772 8 5V8H5C4.44772 8 4 8.44772 4 9C4 9.55228 4.44772 10 5 10H8V13C8 13.5523 8.44772 14 9 14C9.55228 14 10 13.5523 10 13V10H13C13.5523 10 14 9.55228 14 9C14 8.44772 13.5523 8 13 8H10V5Z" />
 				</g>
 			</svg>
+		`;
+		return html`
+			${whiteAddButtonBackgroundFlag ? whiteBackgroundAddButton : translucentBackgroundAddButton}
 			${this.text ? html`<span class="d2l-label-text">${this.text}</span>` : nothing}
 		`;
 	}
