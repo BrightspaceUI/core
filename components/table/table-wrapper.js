78aac11fe25c11b69a389ca75728b2ff2b516eed
Danny Gleckler <daniel.gleckler@d2l.com>
2025-06-20 10:25:03 -0400

    Initialize as false

--- components/table/table-wrapper.js
+++ components/table/table-wrapper.js
@@ -394,7 +394,7 @@ export class TableWrapper extends RtlMixin(PageableMixin(SelectionMixin(LitEleme
 		this._controlsMutationObserver = null;
 		this._controlsScrolled = false;
 		this._controlsScrolledMutationObserver = null;
-		this._noScrollWidth = true;
+		this._noScrollWidth = false;
 		this._table = null;
 		this._tableIntersectionObserver = null;
 		this._tableMutationObserver = null;
