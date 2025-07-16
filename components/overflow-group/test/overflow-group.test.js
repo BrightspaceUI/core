bb9b87258b31a7154c239c8aa7b9b466b87712ae
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2025-07-16 17:25:37 -0400

    Add test

--- components/overflow-group/test/overflow-group.test.js
+++ components/overflow-group/test/overflow-group.test.js
@@ -1,6 +1,28 @@
 import '../overflow-group.js';
 import '../../button/button.js';
-import { fixture, html, oneEvent, runConstructor } from '@brightspace-ui/testing';
+import '../../button/button-icon.js';
+import '../../button/button-subtle.js';
+import '../../selection/selection-action.js';
+import { defineCE, expect, fixture, html, oneEvent, runConstructor } from '@brightspace-ui/testing';
+import { LitElement } from 'lit';
+import { LocalizeCoreElement } from '../../../helpers/localize-core-element.js';
+
+const tagName = defineCE(
+	class extends LocalizeCoreElement(LitElement) {
+		render() {
+			return html`
+				<d2l-overflow-group min-to-show="0" max-to-show="0">
+					<d2l-button>${this.localize('components.overflow-group.moreActions')}</d2l-button>
+					<d2l-button> ${this.localize('components.overflow-group.moreActions')}</d2l-button>
+					<button>${this.localize('components.overflow-group.moreActions')}</button>
+					<d2l-button-icon icon="tier1:gear" text="${this.localize('components.overflow-group.moreActions')}"></d2l-button-icon>
+					<d2l-button-subtle text="${this.localize('components.overflow-group.moreActions')}"></d2l-button-subtle>
+					<d2l-selection-action icon="tier1:bookmark-hollow" text="${this.localize('components.overflow-group.moreActions')}"></d2l-selection-action>
+				</d2l-overflow-group>
+			`;
+		}
+	}
+);
 
 describe('d2l-overflow-group', () => {
 	describe('constructor', () => {
@@ -34,6 +56,17 @@ describe('d2l-overflow-group', () => {
 			await oneEvent(container, 'd2l-overflow-group-updated');
 		});
 
+	});
+
+	it('use correct node to grab text for menu item', async() => {
+		const container = await fixture(`<${tagName}></${tagName}>`);
+		const overflowGroup = container.shadowRoot.querySelector('d2l-overflow-group');
+		await overflowGroup.updateComplete;
+
+		const menuItems = overflowGroup.shadowRoot.querySelectorAll('d2l-menu-item');
+		Array.from(menuItems).forEach(item => {
+			expect(item.text).to.equal('More Actions');
+		});
 	});
 
 });
