38bdba516e77cfd72944bf3f6f48d02aaefb99d5
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2025-07-17 13:34:53 -0400

    fix: GAUD-8414 - Fix d2l-overflow-group using localize with d2l-button (#5955)
    * Ignore comment nodes and text nodes with just spaces
    * Add test and enhance demo

--- components/overflow-group/demo/demo-overflow-group.js
+++ components/overflow-group/demo/demo-overflow-group.js
@@ -1,5 +1,8 @@
 
 import '../../button/button.js';
+import '../../button/button-icon.js';
+import '../../button/button-subtle.js';
+import '../../selection/selection-action.js';
 import '../overflow-group.js';
 import { html, LitElement } from 'lit';
 import { LocalizeCoreElement } from '../../../helpers/localize-core-element.js';
@@ -15,7 +18,10 @@ class DemoOverflowGroup extends LocalizeCoreElement(LitElement) {
 				<d2l-button>${this.localize('components.more-less.more')}</d2l-button>
 				<d2l-button>${this.localize('components.more-less.less')}</d2l-button>
 				<d2l-button> ${this.localize('components.more-less.more')}</d2l-button>
-				<d2l-button> ${this.localize('components.more-less.less')}</d2l-button>
+				<button>${this.localize('components.more-less.less')}</button>
+				<d2l-button-icon icon="tier1:gear" text="${this.localize('components.more-less.more')}"></d2l-button-icon>
+				<d2l-button-subtle text="${this.localize('components.more-less.less')}"></d2l-button-subtle>
+				<d2l-selection-action icon="tier1:bookmark-hollow" text="${this.localize('components.more-less.more')}"></d2l-selection-action>
 			</d2l-overflow-group>
 		`;
 	}
