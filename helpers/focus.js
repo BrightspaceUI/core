78a37fa704444a674559893c6112028509d38b9d
GZolla <43836485+GZolla@users.noreply.github.com>
2025-07-21 10:20:57 -0700

    fix: helper fails when no visible ancestor

--- helpers/focus.js
+++ helpers/focus.js
@@ -42,9 +42,9 @@ export function getFirstFocusableDescendant(node, includeHidden, predicate = () 
 }
 
 export function getFirstFocusableRelative(node, { includeHidden, predicate = () => true, includeTabbablesOnly, nodeVisibilityUnknown = true } = {}) {
+	if (!includeHidden && nodeVisibilityUnknown) node = getFirstVisibleAncestor(node);
 	if (!node) return null;
 
-	if (!includeHidden && nodeVisibilityUnknown) node = getFirstVisibleAncestor(node);
 
 	if (isFocusable(node, true) && predicate(node)) return node;
 
