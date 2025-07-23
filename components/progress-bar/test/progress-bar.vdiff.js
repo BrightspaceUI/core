617afa3e02a64fed139922e0918c6616d3d9cf3f
GZolla <43836485+GZolla@users.noreply.github.com>
2025-07-23 10:04:08 -0700

    Appease Linter

--- components/progress-bar/test/progress-bar.vdiff.js
+++ components/progress-bar/test/progress-bar.vdiff.js
@@ -1,31 +1,28 @@
 import '../progress-bar.js';
 import { expect, fixture, html } from '@brightspace-ui/testing';
 
-const collapsedContentFixture = html`<d2l-progress-bar>A message.</d2l-progress-bar>`;
-const expandedContentFixture = html`<d2l-progress-bar expanded>A message.</d2l-progress-bar>`;
-
 describe('d2l-progress-bar', () => {
 
 	[true, false].forEach(small => {
-		it(small ? 'small' : 'basic', async () => {
+		it(small ? 'small' : 'basic', async() => {
 			const ele = await fixture(html`<div>
 				<d2l-progress-bar label="No Progress" value="0" max-value="10" ?small=${small}></d2l-progress-bar>
 				<d2l-progress-bar label="Progress" value="8" max-value="10" ?small=${small}></d2l-progress-bar>
 				<d2l-progress-bar label="Complete" value="10" max-value="10" ?small=${small}></d2l-progress-bar>
-			</div>`)
+			</div>`);
 
 			await expect(ele).to.be.golden();
-		})
-	})
+		});
+	});
 
-	it('hidden states', async () => {
+	it('hidden states', async() => {
 		const ele = await fixture(html`<div>
 			<d2l-progress-bar label="Progress" label-hidden></d2l-progress-bar>
 			<br>
 			<d2l-progress-bar label="Progress" value-hidden></d2l-progress-bar>
 			<br>
 			<d2l-progress-bar label="Progress" label-hidden value-hidden></d2l-progress-bar>
-		</div>`)
+		</div>`);
 
 		await expect(ele).to.be.golden();
 	});
