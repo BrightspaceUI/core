6f2b565ebeb632b8535e4ceebe8680c0cca365bc
GZolla <43836485+GZolla@users.noreply.github.com>
2025-07-24 16:10:23 -0700

    feat: Progress component (#6000)
    * feat: Progress bar component
    
    * Appease Linter
    
    * Updating vdiff goldens (#6001)
    
    * From review
    
    Co-authored-by: Dave Lockhart <dlockhart@users.noreply.github.com>
    
    * From review
    
    * Fix tests
    
    * Update README.md
    
    * From demo
    
    * Rename dir structure
    
    * Fix imports
    
    * Updating vdiff goldens (#6007)
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Dave Lockhart <dlockhart@users.noreply.github.com>

---
+++ components/progress/test/progress.test.js
@@ -0,0 +1,63 @@
+import '../progress.js';
+import { expect, fixture, html, runConstructor } from '@brightspace-ui/testing';
+
+describe('d2l-progress', () => {
+	describe('constructor', () => {
+
+		it('should construct', () => {
+			runConstructor('d2l-progress');
+		});
+	});
+
+	describe('values', () => {
+		let element, bar;
+
+		beforeEach(async() => {
+			element = await fixture(html`<d2l-progress></d2l-progress>`);
+			bar = element.shadowRoot.querySelector('progress');
+		});
+
+		it('sets default values', () => {
+			expect(element.max).to.equal(100);
+			expect(element.value).to.equal(0);
+		});
+
+		it('changes value attribute of progress element', async() => {
+			for (const val of [1, 10, 100]) {
+				element.value = val;
+				await element.updateComplete;
+				expect(bar.getAttribute('value')).to.equal(val.toString());
+			}
+		});
+		it('changes max attribute of progress element', async() => {
+			for (const max of [1, 10, 100]) {
+				element.max = max;
+				await element.updateComplete;
+				expect(bar.getAttribute('max')).to.equal(max.toString());
+			}
+		});
+		it('sets aria-valuetext to a percentage regardless of max value', async() => {
+			element.value = 1;
+			const tests = [
+				{ max: 1, expected: '100 %' },
+				{ max: 10, expected: '10 %' },
+				{ max: 100, expected: '1 %' }
+			];
+			for (const { max, expected } of tests) {
+				element.max = max;
+				await element.updateComplete;
+				expect(bar.getAttribute('aria-valuetext')).to.equal(expected);
+			}
+		});
+
+		it('uses the floor value of the percentage', async() => {
+			const valueNode = element.shadowRoot.querySelector('.text span:last-child');
+			element.value = 2;
+			element.max = 3;
+			await element.updateComplete;
+			expect(valueNode.textContent).to.equal('66 %');
+			expect(bar.getAttribute('aria-valuetext')).to.equal('66 %');
+		});
+	});
+
+});
