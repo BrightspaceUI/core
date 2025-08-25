abc27643a1fb61f99c47640a10a97c1ad53c22c9
GZolla <43836485+GZolla@users.noreply.github.com>
2025-08-25 07:50:29 -0700

    fix: remove GAUD-7495-add-button-white-background (#6074)

--- components/list/list-item-mixin.js
+++ components/list/list-item-mixin.js
@@ -121,7 +121,6 @@ export const ListItemMixin = superclass => class extends composeMixins(
 			_listItemNewStyles: { type: Boolean, reflect: true, attribute: '_list-item-new-styles' },
 			_showAddButton: { type: Boolean, attribute: '_show-add-button', reflect: true },
 			_siblingHasColor: { state: true },
-			_whiteBackgroundAddButton: { type: Boolean, attribute: '_white-background-add-button', reflect: true },
 		};
 	}
 
@@ -163,13 +162,6 @@ export const ListItemMixin = superclass => class extends composeMixins(
 				inset-inline-start: 1.5rem; /* left and right margins of 0.3rem + drag handle width of 0.9rem */
 				width: calc(100% - 1.5rem);
 			}
-			:host(:not([_white-background-add-button])[_has-nested-list-add-button]) [slot="before-content"] {
-				border-bottom: 1px solid var(--d2l-color-mica);
-				margin-bottom: -1px;
-			}
-			:host(:not([_white-background-add-button])[_has-nested-list-add-button]:not([selection-disabled]):not([skeleton])[selected]) [slot="before-content"] {
-				border-bottom-color: ${unsafeCSS(useNewStylesFlag ? 'var(--d2l-color-mica)' : '#b6cbe8')}; /* stylelint-disable-line */
-			}
 			:host(:first-of-type) [slot="control-container"]::before {
 				top: 0;
 			}
@@ -191,9 +183,6 @@ export const ListItemMixin = superclass => class extends composeMixins(
 			:host([_focusing-primary-action]) [slot="control-container"]::after,
 			:host([selected]:not([selection-disabled]):not([skeleton])) [slot="control-container"]::before,
 			:host([selected]:not([selection-disabled]):not([skeleton])) [slot="control-container"]::after,
-			:host(:not([_white-background-add-button])[_show-add-button]) [slot="control-container"]::before,
-			.hide-bottom-border[slot="control-container"]::after,
-			:host(:not([_white-background-add-button])[_has-nested-list-add-button]) [slot="control-container"]::after,
 			:host(:first-of-type[_nested]) [slot="control-container"]::before {
 				border-top-color: transparent;
 			}
@@ -358,29 +347,7 @@ export const ListItemMixin = superclass => class extends composeMixins(
 				border-color: var(--d2l-color-mica);
 				margin-bottom: -1px;
 			}
-			/* below hides the border under the d2l-button-add; clean up with GAUD-7495-add-button-white-background */
-			:host([_hovering-control]) [slot="outside-control-container"].hide-bottom-border,
-			:host([_hovering-primary-action]) [slot="outside-control-container"].hide-bottom-border,
-			:host([_hovering-selection]) [slot="outside-control-container"].hide-bottom-border,
-			:host([_focusing-primary-action]) [slot="outside-control-container"].hide-bottom-border,
-			:host(:not([selection-disabled]):not([skeleton])[selected]) [slot="outside-control-container"].hide-bottom-border,
-			:host(:not([selection-disabled]):not([skeleton])[selected][_hovering-selection]) [slot="outside-control-container"].hide-bottom-border,
-			:host(:not([_list-item-new-styles]):not([selection-disabled]):not([skeleton])[selectable][_focusing]) [slot="outside-control-container"].hide-bottom-border,
-			:host([_list-item-new-styles]:not([selection-disabled]):not([button-disabled]):not([skeleton])[_focusing]) [slot="outside-control-container"].hide-bottom-border {
-				background-clip: content-box, border-box;
-				background-image: linear-gradient(white, white), linear-gradient(to right, ${unsafeCSS(useNewStylesFlag ? 'var(--d2l-color-mica)' : '#b6cbe8')} 20%, transparent 20%, transparent 80%, ${unsafeCSS(useNewStylesFlag ? 'var(--d2l-color-mica)' : '#b6cbe8')} 80%); /* stylelint-disable-line */
-				background-origin: border-box;
-				border: double 1px transparent;
-				border-radius: 6px;
-			}
 			/* clean up with GAUD-7495-list-item-new-styles flag */
-			:host(:not([selection-disabled]):not([button-disabled]):not([skeleton])[_focusing-elem]) [slot="outside-control-container"].hide-bottom-border {
-				background-clip: content-box, border-box;
-				background-image: linear-gradient(white, white), linear-gradient(to right, var(--d2l-color-mica) 20%, transparent 20%, transparent 80%, var(--d2l-color-mica) 80%);
-				background-origin: border-box;
-				border: double 1px transparent;
-				border-radius: 6px;
-			}
 			:host(:not([selection-disabled]):not([skeleton])[selected]) [slot="outside-control-container"].hide-bottom-border {
 				background-image: linear-gradient(#f3fbff, #f3fbff), linear-gradient(to right, ${unsafeCSS(useNewStylesFlag ? 'var(--d2l-color-mica)' : '#b6cbe8')} 20%, transparent 20%, transparent 80%, ${unsafeCSS(useNewStylesFlag ? 'var(--d2l-color-mica)' : '#b6cbe8')} 80%); /* stylelint-disable-line */
 			}
@@ -492,7 +459,6 @@ export const ListItemMixin = superclass => class extends composeMixins(
 		this._listItemInteractiveEnabled = listItemInteractiveFlag;
 		this._listItemNewStyles = useNewStylesFlag;
 		this._siblingHasColor = false;
-		this._whiteBackgroundAddButton = getFlag('GAUD-7495-add-button-white-background', true);
 	}
 
 	get color() {
@@ -732,9 +698,6 @@ export const ListItemMixin = superclass => class extends composeMixins(
 			'd2l-list-item-color-inner': true,
 			'd2l-skeletize': this.color
 		};
-		const bottomBorderClasses = {
-			'hide-bottom-border': !this._whiteBackgroundAddButton && (this._showAddButton && (!this._hasNestedList || this._hasNestedListAddButton))
-		};
 
 		const alignNested = ((this.draggable && this.selectable) || (this.expandable && this.selectable && this.color) || (this._listItemInteractiveEnabled && this.expandable && !this.selectable)) ? 'control' : undefined;
 		const contentAreaContent = html`
@@ -782,12 +745,12 @@ export const ListItemMixin = superclass => class extends composeMixins(
 					</d2l-button-add>
 				</div>
 				` : nothing}
-				<div slot="outside-control-container" class="${classMap(bottomBorderClasses)}"></div>
+				<div slot="outside-control-container"></div>
 				<div slot="before-content"></div>
 				${this._renderDropTarget()}
 				${this._renderDragHandle(this._renderOutsideControl)}
 				${this._renderDragTarget(this.dragTargetHandleOnly ? this._renderOutsideControlHandleOnly : this._renderOutsideControlAction)}
-				<div slot="control-container" class="${classMap(bottomBorderClasses)}"></div>
+				<div slot="control-container"></div>
 				${this._hasColorSlot ? html`
 				<div slot="color-indicator" class="d2l-list-item-color-outer">
 					<div class="${classMap(colorClasses)}" style="${styleMap(colorStyles)}"></div>
