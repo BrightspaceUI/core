7499a9f73b06025d87526c6f4fa7357b06d1c45a
Dave Lockhart <dlockhart@users.noreply.github.com>
2025-09-10 13:16:50 -0400

    fix: don't sync column widths if table isn't set

--- components/table/table-wrapper.js
+++ components/table/table-wrapper.js
@@ -628,6 +628,7 @@ export class TableWrapper extends PageableMixin(SelectionMixin(LitElement)) {
 	}
 
 	_syncColumnWidths() {
+		if (!this._table) return;
 		const head = this._table.querySelector('thead');
 		const body = this._table.querySelector('tbody');
 
