c0e6df048b659d2768d30805fd8bc7948e9b0faf
Anthony Wikkerink <awikkerink@desire2learn.com>
2025-10-07 10:00:00 -0400

    Fix updateComplete typo in form-element-mixin.js (#6207)

--- components/form/form-element-mixin.js
+++ components/form/form-element-mixin.js
@@ -224,7 +224,7 @@ export const FormElementMixin = superclass => class extends LocalizeCoreElement(
 		if (oldValidationError !== this.validationError) {
 			this._errors = errors;
 		}
-		await this.updatedComplete;
+		await this.updateComplete;
 	}
 
 	resetValidation() {
