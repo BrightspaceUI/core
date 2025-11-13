733d6f3b66a1bddc32b6ab982fb710349e15a53f
Margaree Peacocke <margaree.p@gmail.com>
2025-11-13 13:36:23 -0500

    Enhance Tests: button-copy

--- components/button/test/button-copy.vdiff.js
+++ components/button/test/button-copy.vdiff.js
@@ -1,5 +1,5 @@
 import '../button-copy.js';
-import { clickElem, expect, fixture, html, oneEvent } from '@brightspace-ui/testing';
+import { clickElem, expect, fixture, html, oneEvent, waitUntil } from '@brightspace-ui/testing';
 import { stub } from 'sinon';
 
 const clickAction = async(elem) => {
@@ -33,6 +33,19 @@ describe('button-copy', () => {
 		const elem = await fixture(template, { viewport: { width: 700, height: 200 } });
 		await clickAction(elem);
 		await expect(document).to.be.golden();
+	});
+
+	it('icon reverts to copy after timeout', async() => {
+		writeTextStub = stub(navigator.clipboard, 'writeText').resolves();
+		const elem = await fixture(template, { viewport: { width: 700, height: 200 } });
+		await clickAction(elem);
+
+		await waitUntil(() => {
+			const buttonIcon = elem.shadowRoot.querySelector('d2l-button-icon');
+			return buttonIcon.icon === 'tier1:copy';
+		}, 'icon did not revert to copy', { timeout: 3000 });
+
+		await expect(elem).to.be.golden();
 	});
 
 });
