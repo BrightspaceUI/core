9af028ed0db24f19b2fd2c67039ced95487fac13
Margaree Peacocke <margaree.p@gmail.com>
2025-11-20 09:48:00 -0500

    remove focus management axe tests

--- components/button/test/button-toggle.axe.js
+++ components/button/test/button-toggle.axe.js
@@ -33,30 +33,4 @@ describe('d2l-button-toggle', () => {
 		await expect(el).to.be.accessible();
 	});
 
-	describe('focus management', () => {
-
-		it('focused not-pressed button', async() => {
-			const el = await fixture(html`
-				<d2l-button-toggle>
-					<d2l-button-icon slot="not-pressed" icon="tier1:pin-hollow" text="Unpinned, click to pin."></d2l-button-icon>
-					<d2l-button-icon slot="pressed" icon="tier1:pin-filled" text="Pinned, click to unpin."></d2l-button-icon>
-				</d2l-button-toggle>
-			`);
-			await focusElem(el);
-			await expect(el).to.be.accessible();
-		});
-
-		it('focused pressed button', async() => {
-			const el = await fixture(html`
-				<d2l-button-toggle pressed>
-					<d2l-button-icon slot="not-pressed" icon="tier1:pin-hollow" text="Unpinned, click to pin."></d2l-button-icon>
-					<d2l-button-icon slot="pressed" icon="tier1:pin-filled" text="Pinned, click to unpin."></d2l-button-icon>
-				</d2l-button-toggle>
-			`);
-			await focusElem(el);
-			await expect(el).to.be.accessible();
-		});
-
-	});
-
 });
