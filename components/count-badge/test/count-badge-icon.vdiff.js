960ae5875ef689ffecceda9623463d77d727eaee
Margaree Peacocke <margaree.p@gmail.com>
2025-12-01 10:27:12 -0500

    tidy tests

--- components/count-badge/test/count-badge-icon.vdiff.js
+++ components/count-badge/test/count-badge-icon.vdiff.js
@@ -4,35 +4,24 @@ import { expect, fixture, focusElem, html, oneEvent } from '@brightspace-ui/test
 describe('count-badge-icon', () => {
 
 	describe('basic rendering', () => {
-		[
-			{ name: 'small-notification', template: html`<d2l-count-badge-icon icon="tier3:gear" size="small" type="notification" number="1" text="1 notification"></d2l-count-badge-icon>` },
-			{ name: 'large-notification', template: html`<d2l-count-badge-icon icon="tier3:gear" size="large" type="notification" number="5" text="5 notifications"></d2l-count-badge-icon>` },
-			{ name: 'small-notification-triple-digit', template: html`<d2l-count-badge-icon icon="tier3:gear" size="small" type="notification" number="100" text="100 notifications"></d2l-count-badge-icon>` },
-			{ name: 'small-notification-four-digit', template: html`<d2l-count-badge-icon icon="tier3:gear" size="small" type="notification" number="5555" text="5555 notifications"></d2l-count-badge-icon>` },
-			{ name: 'small-count', template: html`<d2l-count-badge-icon icon="tier3:gear" size="small" type="count" number="42" text="42 items"></d2l-count-badge-icon>` },
-			{ name: 'large-count', template: html`<d2l-count-badge-icon icon="tier3:gear" size="large" type="count" number="1" text="1 item"></d2l-count-badge-icon>` },
-			{ name: 'small-count-triple-digit', template: html`<d2l-count-badge-icon icon="tier3:gear" size="small" type="count" number="100" text="100 items"></d2l-count-badge-icon>` },
-			{ name: 'small-count-four-digit', template: html`<d2l-count-badge-icon icon="tier3:gear" size="small" type="count" number="5555" text="5555 items"></d2l-count-badge-icon>` }
-		].forEach(({ name, template }) => {
-			it(name, async() => {
-				const elem = await fixture(template);
-				await expect(elem).to.be.golden();
+
+		['small', 'large'].forEach(size => {
+			[1, 42, 100, 5555, 100000].forEach(number => {
+				it(`${size}-notification-${number}`, async() => {
+					const elem = await fixture(html`<d2l-count-badge-icon icon="tier3:gear" size="${size}" type="notification" number="${number}" text="${number} notification${number !== 1 ? 's' : ''}"></d2l-count-badge-icon>`);
+					await expect(elem).to.be.golden();
+				});
+
+				it(`${size}-count-${number}`, async() => {
+					const elem = await fixture(html`<d2l-count-badge-icon icon="tier3:gear" size="${size}" type="count" number="${number}" text="${number} item${number !== 1 ? 's' : ''}"></d2l-count-badge-icon>`);
+					await expect(elem).to.be.golden();
+				});
 			});
 		});
-	});
 
-	describe('number truncation', () => {
-		[
-			{ name: 'small-notification-truncated-2-digits', template: html`<d2l-count-badge-icon icon="tier3:gear" size="small" type="notification" number="100" text="100 notifications"></d2l-count-badge-icon>` },
-			{ name: 'large-count-3-digits', template: html`<d2l-count-badge-icon icon="tier3:gear" size="large" type="count" number="123" text="123 items"></d2l-count-badge-icon>` },
-			{ name: 'large-count-4-digits', template: html`<d2l-count-badge-icon icon="tier3:gear" size="large" type="count" number="1234" text="1234 items"></d2l-count-badge-icon>` },
-			{ name: 'large-number', template: html`<d2l-count-badge-icon icon="tier3:gear" size="large" type="count" number="100000" text="100000 items"></d2l-count-badge-icon>` },
-			{ name: 'custom-max-digits-4', template: html`<d2l-count-badge-icon icon="tier3:gear" max-digits="4" number="50000" text="50000 items"></d2l-count-badge-icon>` }
-		].forEach(({ name, template }) => {
-			it(name, async() => {
-				const elem = await fixture(template);
-				await expect(elem).to.be.golden();
-			});
+		it('custom-max-digits', async() => {
+			const elem = await fixture(html`<d2l-count-badge-icon icon="tier3:gear" max-digits="4" number="50000" text="50000 items"></d2l-count-badge-icon>`);
+			await expect(elem).to.be.golden();
 		});
 	});
 
@@ -54,7 +43,7 @@ describe('count-badge-icon', () => {
 			{ name: 'small-notification-icon-focused', template: html`<d2l-count-badge-icon tab-stop icon="tier3:gear" number="100" type="notification" text="100 notifications"></d2l-count-badge-icon>`, action: elem => focusElem(elem) },
 			{ name: 'large-count-icon-focused', template: html`<d2l-count-badge-icon icon="tier3:bookmark" tab-stop size="large" type="count" number="1" text="1 item"></d2l-count-badge-icon>`, action: elem => focusElem(elem) },
 			{ name: 'not-tabbable-by-default', template: html`<d2l-count-badge-icon icon="tier3:alert" number="5" text="5 items"></d2l-count-badge-icon>`, action: elem => focusElem(elem) },
-			{ name: 'not-tabbable-hide-zero-with-zero', template: html`<d2l-count-badge-icon icon="tier3:email" tab-stop hide-zero number="0" text="0 items"></d2l-count-badge-icon>`, action: elem => focusElem(elem) },
+			{ name: 'hide-zero-with-zero-focused', template: html`<d2l-count-badge-icon icon="tier3:email" tab-stop hide-zero number="0" text="0 items"></d2l-count-badge-icon>`, action: elem => focusElem(elem) },
 			{ name: 'not-tabbable-skeleton', template: html`<d2l-count-badge-icon icon="tier3:gear" tab-stop skeleton number="5" text="5 items"></d2l-count-badge-icon>`, action: elem => focusElem(elem) }
 		].forEach(({ name, template, action }) => {
 			it(name, async() => {
