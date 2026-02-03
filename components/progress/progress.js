f0b0ec7a4c3337f82ab458faa12aca1756ec8531
GZolla <43836485+GZolla@users.noreply.github.com>
2026-02-03 11:35:01 -0800

    feat: progress bar indeterminate state (#6547)
    * feat: progress bar indeterminate state
    
    * fix vdiffs
    
    * Appease Linter
    
    * Updating vdiff goldens (#6548)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/progress/progress.js
+++ components/progress/progress.js
@@ -1,6 +1,6 @@
 import '../../components/colors/colors.js';
 import { bodyCompactStyles, bodySmallStyles } from '../typography/styles.js';
-import { css, html, LitElement, unsafeCSS } from 'lit';
+import { css, html, LitElement, nothing, unsafeCSS } from 'lit';
 import { classMap } from 'lit/directives/class-map.js';
 import { formatPercent } from '@brightspace-ui/intl';
 import { ifDefined } from 'lit/directives/if-defined.js';
@@ -19,7 +19,7 @@ class Progress extends LocalizeCoreElement(LitElement) {
 			 * The current value of the progress bar
 			 * @type {number}
 			 */
-			value: { type: Number },
+			value: { type: Number, reflect: true },
 			/**
 			 * REQUIRED: Label for the progress bar
 			 * @type {string}
@@ -39,7 +39,7 @@ class Progress extends LocalizeCoreElement(LitElement) {
 			 * Hide the bar's value
 			 * @type {boolean}
 			 */
-			valueHidden: { type: Boolean, attribute: 'value-hidden' },
+			valueHidden: { type: Boolean, attribute: 'value-hidden', reflect: true },
 			/**
 			 * The size of the progress bar
 			 * @type {'small'|'medium'|'large'}
@@ -52,19 +52,20 @@ class Progress extends LocalizeCoreElement(LitElement) {
 		return [bodySmallStyles, bodyCompactStyles, css`
 				:host {
 					align-items: center;
-					display: flex;
-					flex-wrap: wrap;
+					display: grid;
+					grid-template: auto auto / 1fr auto;
+					grid-template-areas:
+						"label label"
+						"bar value";
 					min-width: 6rem;
 				}
 				:host([hidden]) {
 					display: none;
 				}
-				:host([value-hidden]) {
-					row-gap: 0.3rem;
-				}
 
 				#label {
-					flex: 1 0 100%;
+					grid-area: label;
+					width: 100%;
 				}
 
 				progress {
@@ -73,38 +74,41 @@ class Progress extends LocalizeCoreElement(LitElement) {
 					-webkit-appearance: none;
 					appearance: none;
 					background-color: var(--d2l-color-gypsum);
+					box-shadow: inset 0 2px var(--d2l-color-mica);
+					width: 100%;
+				}
+				.bar {
 					border: none;
 					border-radius: 0.9rem;
-					box-shadow: inset 0 2px var(--d2l-color-mica);
 					display: block;
-					flex: 1;
+					grid-area: bar;
 					height: 0.6rem;
-					margin-inline-end: 0.4rem;
 				}
 				.value {
+					grid-area: value;
+					margin-inline-start: 0.4rem;
 					text-align: end;
 					width: 2.42rem;
 				}
 
 				:host([size="small"]) {
-					progress {
+					.bar {
 						height: 0.3rem;
-						margin-inline-end: 0.3rem;
 					}
 
 					.value {
+						margin-inline-start: 0.3rem;
 						width: 2.15rem;
 					}
 				}
 
 				:host([size="large"]) {
 					line-height: 1.5rem;
-					progress {
+					.bar {
 						height: 0.9rem;
-						margin-inline-end: 0.5rem;
 					}
-
 					.value {
+						margin-inline-start: 0.5rem;
 						width: 2.82rem;
 					}
 				}
@@ -115,6 +119,33 @@ class Progress extends LocalizeCoreElement(LitElement) {
 				/* this is necessary to avoid white bleed over rounded corners in chrome and safari */
 				progress::-webkit-progress-bar {
 					background-color: transparent;
+				}
+
+
+				:host(:not([value-hidden]):not([value])) .bar {
+					margin-bottom: 0.3rem;
+				}
+
+				:host(:not([value]):not([label-hidden])),
+				:host([value-hidden]:not([label-hidden])) {
+					row-gap: 0.3rem;
+				}
+
+				.indeterminate-bar-container {
+					overflow: hidden;
+				}
+				.indeterminate-bar {
+					--d2l-progress-indeterminate-width: calc(100% / 3);
+					animation: 1450ms 50ms ease-in-out indeterminateState alternate infinite;
+					background-color: var(--d2l-color-celestine);
+					height: 100%;
+					margin-inline-start: calc(var(--d2l-progress-indeterminate-width) * -0.75);
+					width: var(--d2l-progress-indeterminate-width);
+				}
+				@keyframes indeterminateState {
+					100% {
+						margin-inline-start: calc(100% - var(--d2l-progress-indeterminate-width) * 0.25);
+					}
 				}
 			`,
 		/* comma separating the selectors for these pseudo-elements causes them to break */
@@ -127,8 +158,13 @@ class Progress extends LocalizeCoreElement(LitElement) {
 					transition: width 0.4s ease-out;
 				}
 				/* these are necessary to avoid showing border when value is 0 */
+				progress:not([value])::${unsafeCSS(selector)},
 				progress[value="0"]::${unsafeCSS(selector)} {
 					border: none;
+				}
+
+				progress:not([value])::${unsafeCSS(selector)} {
+					transition: none;
 				}
 				@media (prefers-reduced-motion: reduce) {
 					progress::${unsafeCSS(selector)} {
@@ -145,7 +181,6 @@ class Progress extends LocalizeCoreElement(LitElement) {
 		this.labelHidden = false;
 		this.max = 100;
 		this.size = 'medium';
-		this.value = 0;
 		this.valueHidden = false;
 	}
 
@@ -155,26 +190,33 @@ class Progress extends LocalizeCoreElement(LitElement) {
 
 	render() {
 		const classes = {
-			'complete': this.isComplete
+			'complete': this.isComplete,
+			'bar': true
 		};
 		const textClasses = {
 			'd2l-body-small': this.size === 'small',
 			'd2l-body-compact': this.size === 'medium'
 		};
 		const valueClasses = { ...textClasses, value: true };
+		const isIndeterminate = this.value === undefined;
 
-		const percentageText = formatPercent(this.isComplete ? 1 : Math.floor(100 * this.value / this.max) / 100);
+		const percentageText = isIndeterminate ? undefined : formatPercent(this.isComplete ? 1 : Math.floor(100 * this.value / this.max) / 100);
 		return html`
 			<div aria-live=${this.announceLabel ? 'polite' : 'off'} ?hidden=${this.labelHidden} id="label" class=${classMap(textClasses)}>${this.label}</div>
 			<progress
 				aria-labelledby="${ifDefined(this.labelHidden ? undefined : 'label')}"
 				aria-label="${ifDefined(this.labelHidden ? this.label : undefined)}"
-				aria-valuetext="${percentageText}"
+				aria-valuetext="${ifDefined(percentageText)}"
 				class="${classMap(classes)}"
-				value="${this.value}"
+				value="${ifDefined(this.value)}"
 				max="${this.max}">
 			</progress>
-			<div ?hidden=${this.valueHidden} class=${classMap(valueClasses)}>${percentageText}</div>
+			${isIndeterminate ? html`
+				<div class="indeterminate-bar-container bar">
+					<div class="indeterminate-bar bar"></div>
+				</div>
+			` : nothing}
+			<div ?hidden=${this.valueHidden || isIndeterminate} class=${classMap(valueClasses)}>${percentageText}</div>
 		`;
 	}
 
