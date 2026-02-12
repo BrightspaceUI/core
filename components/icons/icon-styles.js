3f18e901b46934156070bcc7f56f2357735f2a01
Dave Batiste <dave.batiste@d2l.com>
2026-02-12 16:35:05 -0500

    Update d2l-icon with semantic colors.

--- components/icons/icon-styles.js
+++ components/icons/icon-styles.js
@@ -5,7 +5,7 @@ export const iconStyles = css`
 	:host {
 		-webkit-align-items: center;
 		align-items: center;
-		color: var(--d2l-color-tungsten);
+		color: var(--d2l-color-icon-standard);
 		display: -ms-inline-flexbox;
 		display: -webkit-inline-flex;
 		display: inline-flex;
