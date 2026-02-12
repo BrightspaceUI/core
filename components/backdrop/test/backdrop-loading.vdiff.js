fec04c5cc0b3e3cb41c3a4e89b3a143879e3074a
Duncan Uszkay <Duncan.Uszkay@D2L.com>
2026-02-12 11:42:21 -0500

    Simplify vdiff

--- components/backdrop/test/backdrop-loading.vdiff.js
+++ components/backdrop/test/backdrop-loading.vdiff.js
@@ -2,38 +2,11 @@ import '../backdrop-loading.js';
 import { expect, fixture, html } from '@brightspace-ui/testing';
 
 const template = html`
-	<div style="background-color: orange; height: 600px; padding: 1rem; width: 800px;">
-		<style>
-			th, td {
-				border: 1px solid #dddddd;
-				padding: 30px;
-				text-align: left;
-			}
-			#grade-container {
-				position: relative;
-				width: fit-content;
-			}
-		</style>
-		<div id="grade-container" style="position:relative;width:fit-content;">
-			<table>
-				<thead>
-					<th>Course</th>
-					<th>Grade</th>
-					<th>Hours Spent in Content</th>
-				</thead>
-				<tr>
-					<td>Math</td>
-					<td class="grade">85%</td>
-					<td>100</td>
-				</tr>
-				<tr>
-					<td>Art</td>
-					<td class="grade">98%</td>
-					<td>10</td>
-				</tr>
-			</table>
-			<d2l-backdrop-loading></d2l-backdrop-loading>
+	<div style="position:relative">
+		<div style="background-color: orange; height: 200px; padding: 1rem;">
+			Stuff in here
 		</div>
+		<d2l-backdrop-loading></d2l-backdrop-loading>
 	</div>
 `;
 
