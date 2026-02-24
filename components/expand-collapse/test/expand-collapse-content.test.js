115c6db890c965b39ac3254d412f31f3f4e55bf6
GZolla <43836485+GZolla@users.noreply.github.com>
2026-02-23 16:01:38 -0800

    fix: add tests to expand-collapse-content

--- components/expand-collapse/test/expand-collapse-content.test.js
+++ components/expand-collapse/test/expand-collapse-content.test.js
@@ -1,8 +1,52 @@
-import '../expand-collapse-content.js';
-import { fixture, html, runConstructor } from '@brightspace-ui/testing';
+import { defineCE, expect, fixture, html, nextFrame, oneEvent, runConstructor } from '@brightspace-ui/testing';
+import { LitElement, nothing } from 'lit';
+import { states } from '../expand-collapse-content.js';
 
-const collapsedContentFixture = html`<d2l-expand-collapse-content>A message.</d2l-expand-collapse-content>`;
-const expandedContentFixture = html`<d2l-expand-collapse-content expanded>A message.</d2l-expand-collapse-content>`;
+const tagName = defineCE(
+	class extends LitElement {
+		static get properties() {
+			return {
+				transitions: { type: Boolean }, //test transition logic
+				empty: { type: Boolean },
+				expanded: { type: Boolean }
+			};
+		}
+
+		constructor() {
+			super();
+			this.transitions = false;
+			this.empty = false;
+			this.expanded = false;
+			this.hasExpanded = false;
+			this.hasCollapsed = false;
+		}
+		render() {
+			return html`
+				<d2l-expand-collapse-content
+					._reduceMotion="${!this.transitions}"
+					@d2l-expand-collapse-content-collapse="${this.onCollapse}"
+					@d2l-expand-collapse-content-expand="${this.onExpand}"
+					?expanded="${this.expanded}">${this.empty ? nothing : 'Content'}</d2l-expand-collapse-content>
+			`;
+		}
+		async updated(changedProperties) {
+			super.updated(changedProperties);
+			if (changedProperties.has('transitions') && this.transitions) {
+				const content = this.shadowRoot.querySelector('d2l-expand-collapse-content');
+				await content.updateComplete;
+				content.shadowRoot.querySelector('.d2l-expand-collapse-content-container').style.transition = 'height 100ms ease-in-out, opacity 100ms ease-in-out';
+			}
+		}
+
+		onCollapse() {
+			this.hasCollapsed = true;
+		}
+
+		onExpand() {
+			this.hasExpanded = true;
+		}
+	}
+);
 
 describe('d2l-expand-collapse-content', () => {
 
@@ -16,24 +60,50 @@ describe('d2l-expand-collapse-content', () => {
 
 	describe('events', () => {
 
-		it('should fire d2l-expand-collapse-content-expand event with complete promise', async() => {
-			const content = await fixture(collapsedContentFixture);
-			setTimeout(() => content.expanded = true);
-			const e = await new Promise(resolve => {
-				content.addEventListener('d2l-expand-collapse-content-expand', (e) => resolve(e), { once: true });
-			});
-			await e.detail.expandComplete;
-		});
+		[true, false].forEach(expand => {
+			const event = `d2l-expand-collapse-content-${expand ? 'expand' : 'collapse'}`;
 
-		it('should fire d2l-expand-collapse-content-collapse event with complete promise', async() => {
-			const content = await fixture(expandedContentFixture);
-			setTimeout(() => content.expanded = false);
-			const e = await new Promise(resolve => {
-				content.addEventListener('d2l-expand-collapse-content-collapse', (e) => resolve(e), { once: true });
-			});
-			await e.detail.collapseComplete;
-		});
+			async function changeState(content) {
+				setTimeout(() => content.expanded = expand);
+				return oneEvent(content, event);
+			}
 
+			it(`does not fire ${event} on first update`, async() => {
+				const content = await fixture(`<${tagName}${expand ? ' expanded' : ''}></${tagName}>`);
+				expect(content[`has${expand ? 'Expanded' : 'Collapsed'}`]).to.be.false;
+			});
+
+			it(`should fire ${event} event with complete promise`, async() => {
+				const content = (await fixture(`<${tagName}${expand ? '' : ' expanded'}></${tagName}>`)).shadowRoot.querySelector('d2l-expand-collapse-content');
+
+				const e = await changeState(content);
+
+				await e.detail[`${expand ? 'expand' : 'collapse'}Complete`];
+				expect(content._state).to.equal(expand ? states.EXPANDED : states.COLLAPSED);
+			});
+
+			it(`should fire ${event} event with transition states`, async() => {
+				const content = (await fixture(`<${tagName}${expand ? '' : ' expanded'} transitions></${tagName}>`)).shadowRoot.querySelector('d2l-expand-collapse-content');
+
+				const e = await changeState(content);
+				expect(content._state).to.equal(expand ? states.PREEXPANDING : states.PRECOLLAPSING);
+
+				await content.updateComplete;
+				await nextFrame();
+				await nextFrame();
+
+				expect(content._state).to.equal(expand ? states.EXPANDING : states.COLLAPSING);
+				await e.detail[`${expand ? 'expand' : 'collapse'}Complete`];
+				expect(content._state).to.equal(expand ? states.EXPANDED : states.COLLAPSED);
+			});
+
+			it(`should finish ${event} transition when empty`, async() => {
+				const content = (await fixture(`<${tagName}${expand ? '' : ' expanded'} transitions empty></${tagName}>`)).shadowRoot.querySelector('d2l-expand-collapse-content');
+
+				const e = await changeState(content);
+				await e.detail[`${expand ? 'expand' : 'collapse'}Complete`];
+				expect(content._state).to.equal(expand ? states.EXPANDED : states.COLLAPSED);
+			});
+		});
 	});
-
 });
