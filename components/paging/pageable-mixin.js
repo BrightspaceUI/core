bdcc133a8674a126238603f857b614ee119142c4
GZolla <43836485+GZolla@users.noreply.github.com>
2026-02-25 14:13:26 -0800

    Revert pageable-mixin change

--- components/paging/pageable-mixin.js
+++ components/paging/pageable-mixin.js
@@ -21,8 +21,8 @@ export const PageableMixin = superclass => class extends CollectionMixin(supercl
 		});
 	}
 
-	connectedCallback() {
-		super.connectedCallback();
+	firstUpdated() {
+		super.firstUpdated();
 		this._updateItemShowingCount();
 	}
 
