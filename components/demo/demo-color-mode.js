59057bc51a0dc5adc636079f4e8ad2f415bada06
Dave Batiste <dave.batiste@d2l.com>
2026-02-27 10:34:44 -0500

    Update to theme to color-mode and update light & dark palettes.

---
+++ components/demo/demo-color-mode.js
@@ -0,0 +1,7 @@
+const urlParams = new URLSearchParams(window.location.search);
+const colorMode = urlParams.get('color-mode') || localStorage.getItem('color-mode');
+if (colorMode) {
+	document.documentElement.dataset.colorMode = colorMode;
+} else {
+	delete document.documentElement.dataset.colorMode;
+}
