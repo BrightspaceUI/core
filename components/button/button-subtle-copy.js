9f90628d78267d80ac15ffe3dad08b14bac47f44
Edwin A. Collazos <42192151+EdwinACL831@users.noreply.github.com>
2026-03-16 17:07:42 -0400

    feat: GAUD-9551 Implement the d2l-button-subtle-copy (#6671)
    * GAUD-9551: Implement the d2l-button-subtle-copy
    
    Creates a new component named d2l-button-subtle-copy
    Creates a new mixin to encapsulate the copy logic: CopyMixin.js
    Updates the d2l-button-copy to inherit from the CopyMixin.js
    Updates the button-copy.html file to demonstrate the new
    d2l-button-subtle-copy
    
    * Improves separation of concerns
    
    Adds the new reactive variable _recentCopySuccessful
    Hides the implementation of the timer from subclasses by renaming the
    _iconCheckTimeoutId to #recentCopyTimerId
    Rename the mixin file to button-copy-mixin.js and so the name of the
    exported mixin function to ButtonCopyMixin
    Stops the click event propagated when the toast was clicked
    
    * Adds test file for the subtle copy button
    
    * Adds test file for button-copy-mixin. Some test were moved from button-copy.test.js to button-copy-mixin.test.js
    
    * Adds vdiff test cases. Adds disabled html in the demo file
    
    * Updates the order of the mixins to match original order
    
    * Adds meaningful comment and updates test
    
    * Updating vdiff goldens
    
    * Remove redundant vdiff test covered already in button-subtle
    
    * Adds FocusMixin to ButtonSubtleCopy to make if focusabe
    
    * Removes constructor from ButtonCopy since disabled is initialized in parent mixin
    
    * Updates the button readme file to include new component
    
    ---------
    
    Co-authored-by: Edwin Collazos <Edwin.Collazos@D2L.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ components/button/button-subtle-copy.js
@@ -0,0 +1,49 @@
+import './button-subtle.js';
+import { html, LitElement } from 'lit';
+import { ButtonCopyMixin } from './button-copy-mixin.js';
+import { FocusMixin } from '../../mixins/focus/focus-mixin.js';
+
+/**
+ * A button component that copies to the clipboard, using the "subtle" button style.
+ */
+class ButtonSubtleCopy extends FocusMixin(ButtonCopyMixin(LitElement)) {
+	static get properties() {
+		return {
+			/**
+			 * ACCESSIBILITY: A description to be added to the button for accessibility when text on button does not provide enough context
+			 * @type {string}
+			 */
+			description: { type: String },
+			/**
+			 * ACCESSIBILITY: REQUIRED: Text for the button
+			 * @type {string}
+			 */
+			text: { type: String, reflect: true },
+			/**
+			 * Whether to render the slimmer version of the button
+			 * @type {boolean}
+			 */
+			slim: { type: Boolean, reflect: true },
+		};
+	}
+
+	static get focusElementSelector() {
+		return 'd2l-button-subtle';
+	}
+
+	render() {
+		return html`
+			<d2l-button-subtle
+				?slim="${this.slim}"
+				?disabled="${this.disabled}"
+				icon="${this._recentCopySuccessful ? 'tier1:check' : 'tier1:copy'}"
+				text="${this.text}"
+				description="${this.description}"
+				@click="${this._handleClick}">
+			</d2l-button-subtle>
+			${this._renderToast()}
+		`;
+	}
+}
+
+customElements.define('d2l-button-subtle-copy', ButtonSubtleCopy);
