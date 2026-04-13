652249b8715d390da3223205000d5f1cbbca0f6b
Duncan Uszkay <Duncan.Uszkay@D2L.com>
2026-04-13 15:02:02 -0400

    Fix transition bug

--- components/backdrop/backdrop-loading.js
+++ components/backdrop/backdrop-loading.js
@@ -60,27 +60,36 @@ class LoadingBackdrop extends LocalizeCoreElement(LitElement) {
 			.backdrop {
 				background-color: var(--d2l-color-regolith);
 				height: 100%;
-				opacity: 0;
 				position: absolute;
 				top: 0;
-				transition: opacity ${FADE_DURATION_MS}ms ease-in;
 				width: 100%;
+			}
+			:host([_state="showing"]) .backdrop {
+				opacity: 0;
 			}
 			:host([_state="shown"]) .backdrop {
 				opacity: 0.7;
+				transition: opacity ${FADE_DURATION_MS}ms ease-in;
+			}
+			:host([_state="hiding"]) .backdrop {
+				opacity: 0;
+				transition: opacity ${FADE_DURATION_MS}ms ease-out;
 			}
 
 			d2l-loading-spinner {
-				opacity: 0;
 				position: absolute;
-				transition: opacity ${FADE_DURATION_MS}ms ease-in ${SPINNER_DELAY_MS}ms;
+				opacity: 0;
 			}
 			:host([_state="shown"]) d2l-loading-spinner {
 				opacity: 1;
+				transition: opacity ${FADE_DURATION_MS}ms ease-in ${SPINNER_DELAY_MS}ms;
 			}
 
 			:host([_state="hiding"]) .d2l-backdrop,
 			:host([_state="hiding"]) d2l-loading-spinner {
+				opacity: 0;
+				transition: opacity ${FADE_DURATION_MS}ms ease-out;
+			}
 				transition: opacity ${FADE_DURATION_MS}ms ease-out;
 			}
 
