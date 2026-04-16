c6b0f2bbdb88a4f361604bd73cb961589539d3c0
Dave Batiste <dave.batiste@d2l.com>
2026-04-16 11:41:03 -0400

    Update basic input components to use semantic variables.

--- components/inputs/input-styles.js
+++ components/inputs/input-styles.js
@@ -1,23 +1,25 @@
-import '../colors/colors.js';
 import { css, unsafeCSS } from 'lit';
 import { getFocusPseudoClass } from '../../helpers/focus.js';
-import { svgToCSS } from '../../helpers/svg-to-css.js';
+import { registerSemanticVariableForSvgImageUrl } from '../colors/colors.js';
 
 const focusClass = unsafeCSS(getFocusPseudoClass());
 
-export const invalidIcon = svgToCSS(`<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
-	<path fill="#cd2026" d="M17.79 15.11l-7-14a2 2 0 0 0-3.58 0l-7 14a1.975 1.975 0 0 0 .09 1.94A2 2 0 0 0 2 18h14a1.994 1.994 0 0 0 1.7-.95 1.967 1.967 0 0 0 .09-1.94zM9 16a1.5 1.5 0 1 1 1.5-1.5A1.5 1.5 0 0 1 9 16zm.98-4.806a1 1 0 0 1-1.96 0l-.99-5A1 1 0 0 1 8.01 5h1.983a1 1 0 0 1 .98 1.194z"/>
-	<path fill="#FFF" d="M9 16a1.5 1.5 0 1 1 1.5-1.5A1.5 1.5 0 0 1 9 16zm.98-4.806a1 1 0 0 1-1.96 0l-.99-5A1 1 0 0 1 8.01 5h1.983a1 1 0 0 1 .98 1.194z"/>
-  </svg>`);
+registerSemanticVariableForSvgImageUrl(
+	'--d2l-input-invalid-image',
+	`<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+		<path fill="var(--d2l-theme-status-color-error)" d="M17.79 15.11l-7-14a2 2 0 0 0-3.58 0l-7 14a1.975 1.975 0 0 0 .09 1.94A2 2 0 0 0 2 18h14a1.994 1.994 0 0 0 1.7-.95 1.967 1.967 0 0 0 .09-1.94zM9 16a1.5 1.5 0 1 1 1.5-1.5A1.5 1.5 0 0 1 9 16zm.98-4.806a1 1 0 0 1-1.96 0l-.99-5A1 1 0 0 1 8.01 5h1.983a1 1 0 0 1 .98 1.194z"/>
+		<path fill="var(--d2l-theme-background-color-base)" d="M9 16a1.5 1.5 0 1 1 1.5-1.5A1.5 1.5 0 0 1 9 16zm.98-4.806a1 1 0 0 1-1.96 0l-.99-5A1 1 0 0 1 8.01 5h1.983a1 1 0 0 1 .98 1.194z"/>
+	</svg>`
+);
 
 export const inputStyles = css`
 	.d2l-input {
-		background-color: var(--d2l-input-background-color, #ffffff);
+		background-color: var(--d2l-input-background-color, var(--d2l-theme-background-color-base));
 		border-radius: var(--d2l-input-border-radius, 0.3rem);
 		border-style: solid;
-		box-shadow: inset 0 2px 0 0 rgba(177, 185, 190, 0.2); /* corundum */
+		box-shadow: var(--d2l-theme-shadow-inset);
 		box-sizing: border-box;
-		color: var(--d2l-color-ferrite);
+		color: var(--d2l-theme-text-color-static-standard);
 		display: inline-block;
 		font-family: inherit;
 		font-size: 0.8rem;
@@ -37,34 +39,34 @@ export const inputStyles = css`
 	.d2l-input:hover:disabled,
 	.d2l-input:${focusClass}:disabled,
 	[aria-invalid="true"].d2l-input:disabled {
-		border-color: var(--d2l-input-border-color, var(--d2l-color-galena));
+		border-color: var(--d2l-input-border-color, var(--d2l-theme-border-color-emphasized));
 		border-width: 1px;
 		padding: var(--d2l-input-padding, 0.4rem 0.75rem);
 	}
 	.d2l-input::placeholder {
-		color: var(--d2l-color-galena);
+		color: var(--d2l-theme-text-color-static-faint);
 		font-size: 0.8rem;
 		font-weight: 400;
 		opacity: 1; /* Firefox has non-1 default */
 	}
 	.d2l-input::-ms-input-placeholder {
-		color: var(--d2l-color-galena);
+		color: var(--d2l-theme-text-color-static-faint);
 		font-size: 0.8rem;
 		font-weight: 400;
 	}
 	.d2l-input:hover,
 	.d2l-input:${focusClass},
 	.d2l-input-focus {
-		border-color: var(--d2l-color-celestine);
+		border-color: var(--d2l-theme-border-color-focus);
 		border-width: 2px;
 		outline: none;
 		padding: var(--d2l-input-padding-focus, calc(0.4rem - 1px) calc(0.75rem - 1px));
 	}
 	[aria-invalid="true"].d2l-input {
-		border-color: var(--d2l-color-cinnabar);
+		border-color: var(--d2l-theme-status-color-error);
 	}
 	.d2l-input:disabled {
-		opacity: 0.5;
+		opacity: var(--d2l-theme-opacity-disabled-control);
 	}
 	.d2l-input::-webkit-search-cancel-button,
 	.d2l-input::-webkit-search-decoration {
@@ -90,7 +92,7 @@ export const inputStyles = css`
 		padding-block: calc(0.5rem - 1px);
 	}
 	textarea.d2l-input[aria-invalid="true"] {
-		background-image: ${invalidIcon};
+		background-image: var(--d2l-input-invalid-image);
 		background-position: top 12px var(--d2l-inline-end, right) 18px;
 		background-repeat: no-repeat;
 		background-size: 0.8rem 0.8rem;
@@ -109,7 +111,7 @@ export const inputStyles = css`
 	@media (prefers-contrast: more) {
 		[aria-invalid="true"].d2l-input {
 			background-color: Field;
-			border-color: var(--d2l-color-cinnabar);
+			border-color: var(--d2l-theme-status-color-error);
 			box-shadow: none;
 			color: FieldText;
 			forced-color-adjust: none;
