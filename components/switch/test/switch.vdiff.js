8d3958bd45d5b7bdbcade379ad404699c9d8a9b7
GZolla <43836485+GZolla@users.noreply.github.com>
2026-04-20 13:24:15 -0700

    Test color mode tests and action

--- components/switch/test/switch.vdiff.js
+++ components/switch/test/switch.vdiff.js
@@ -8,7 +8,7 @@ describe('d2l-switch', () => {
 
 	describe('basic', () => {
 		[
-			{ name: 'off', template: offFixture },
+			{ name: 'off', template: offFixture, allColorModes: true },
 			{ name: 'off-focus', template: offFixture, action: async(elem) => await focusElem(elem) },
 			{ name: 'off-disabled', template: html`<d2l-switch text="Test Text" disabled></d2l-switch>` },
 			{ name: 'off-hover', template: offFixture, action: async(elem) => await hoverElem(elem.shadowRoot.querySelector('.d2l-switch-inner')) },
@@ -24,12 +24,12 @@ describe('d2l-switch', () => {
 			{ name: 'toggle on', template: offFixture, action: async(elem) => { elem.on = true; await elem.updateComplete; } },
 			{ name: 'toggle off', template: onFixture, action: async(elem) => { elem.on = false; await elem.updateComplete; } },
 			{ name: 'background-color', template: html`<d2l-switch on style="--d2l-switch-container-background-color: red;"></d2l-switch>` }
-		].forEach(({ name, template, action }) => {
+		].forEach(({ name, template, action, allColorModes }) => {
 			it(name, async() => {
 				const elem = await fixture(template);
 				if (action) await action(elem);
 				await elem.updateComplete;
-				await expect(elem).to.be.golden();
+				await expect(elem).to.be.golden({ allColorModes });
 			});
 		});
 	});
