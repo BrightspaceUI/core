acf040d4b19f461252f63b3ad3ac2d28a18b8226
Dave Batiste <dave.batiste@d2l.com>
2026-04-20 14:50:18 -0400

    fix: Update backdrop components to use semantic variables. (#6830)
    * Update backdrop components to use semantic variables.
    
    * Updating vdiff goldens (#6831)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/backdrop/test/backdrop.vdiff.js
+++ components/backdrop/test/backdrop.vdiff.js
@@ -10,14 +10,21 @@ const template = html`
 `;
 
 describe('backdrop', () => {
+
 	it('not shown', async() => {
 		const elem = await fixture(template);
 		await expect(elem).to.be.golden();
 	});
 
-	it('shown', async() => {
-		const elem = await fixture(template);
-		elem.querySelector('d2l-backdrop').shown = true;
-		await expect(elem).to.be.golden();
+	[
+		{ name: 'shown' },
+		{ name: 'shown-dark', colorMode: 'dark' }
+	].forEach(({ name, colorMode }) => {
+		it(name, async() => {
+			const elem = await fixture(template, { colorMode });
+			elem.querySelector('d2l-backdrop').shown = true;
+			await expect(elem).to.be.golden();
+		});
 	});
+
 });
