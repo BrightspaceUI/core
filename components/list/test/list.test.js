b54696eb0e65a1d2e55f2e478f9082126e89a6c3
Dave Lockhart <dlockhart@users.noreply.github.com>
2026-04-27 09:39:30 -0400

    try different approach

--- components/list/test/list.test.js
+++ components/list/test/list.test.js
@@ -329,11 +329,8 @@ describe('d2l-list', () => {
 
 		it('getSelectedListItems returns array excluding indeterminate items', async() => {
 			clickItemInput(elem.querySelector('[key="L2-1"]'));
-			await oneEvent(elem, 'd2l-list-selection-change');
 			clickItemInput(elem.querySelector('[key="L2-2"]'));
-			await oneEvent(elem, 'd2l-list-selection-change');
-			clickItemInput(elem.querySelector('[key="L2-3"]'));
-			await oneEvent(elem, 'd2l-list-selection-changes');
+			await clickItemInput(elem.querySelector('[key="L2-3"]'));
 			expect(elem.getSelectedListItems(true).length).to.equal(2);
 		});
 
