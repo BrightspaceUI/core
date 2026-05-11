54dde0d2a2bf658fdeb26275f36a0cf89745127b
Dave Lockhart <dlockhart@users.noreply.github.com>
2026-05-11 12:17:14 -0400

    fix: remove no-highlight-border option (#6962)

--- components/page/test/page-header-button-fixtures.js
+++ components/page/test/page-header-button-fixtures.js
@@ -5,7 +5,6 @@ export const pageHeaderButtonFixtures = {
 	disabled: html`<d2l-page-header-button icon="tier3:classes" text="Classes" disabled></d2l-page-header-button>`,
 	iconText: html`<d2l-page-header-button icon="tier3:classes" text="Classes"></d2l-page-header-button>`,
 	flipIcon: html`<d2l-page-header-button icon="tier3:classes" text="Classes" icon-position="end"></d2l-page-header-button>`,
-	noHighlightBorder: html`<d2l-page-header-button icon="tier3:classes" text="Classes" no-highlight-border></d2l-page-header-button>`,
 	textHidden: html`<d2l-page-header-button icon="tier3:classes" text="Classes" text-hidden></d2l-page-header-button>`,
 	textHiddenDisabled: html`<d2l-page-header-button disabled icon="tier3:classes" text="Classes" text-hidden></d2l-page-header-button>`,
 };
