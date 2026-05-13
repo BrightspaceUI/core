dc1634aa3df524e9a6385ecfc4bf9348efc01bd2
Danny Gleckler <daniel.gleckler@d2l.com>
2026-05-13 00:34:46 -0400

    Fix lint

--- components/popover/test/popover-mixin.vdiff.js
+++ components/popover/test/popover-mixin.vdiff.js
@@ -3,9 +3,11 @@ import { clickElem, expect, fixture, html, oneEvent } from '@brightspace-ui/test
 
 describe('popover-mixin', () => {
 
+	const open = e => e.target.nextSibling.open(e.target);
+
 	[
-		{ name: 'default', template: html`<span><button @click="${e => e.target.nextSibling.open(e.target)}">Open</button><d2l-test-popover class="vdiff-include" style="max-width: 400px;">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover></span>` },
-		{ name: 'maxHeight', template: html`<span><button @click="${e => e.target.nextSibling.open(e.target)}">Open</button><d2l-test-popover class="vdiff-include" max-height="75">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover></span>` }
+		{ name: 'default', template: html`<span><button @click="${open}">Open</button><d2l-test-popover class="vdiff-include" style="max-width: 400px;">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover></span>` },
+		{ name: 'maxHeight', template: html`<span><button @click="${open}">Open</button><d2l-test-popover class="vdiff-include" max-height="75">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover></span>` }
 	].forEach(({ name, template }) => {
 		it(name, async() => {
 			const el = await fixture(template, { viewport: { width: 700, height: 400 } });
