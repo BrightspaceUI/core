b41802d9ba00b070fb7fd59c901e35ac87f14d94
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-05-14 13:54:45 -0400

    Reverts the content of the tabs to its original value

--- components/tabs/test/tabs.vdiff.js
+++ components/tabs/test/tabs.vdiff.js
@@ -404,7 +404,7 @@ describe('d2l-tabs', () => {
 		});
 
 		it('does not expand the tab width beyond 20rem when reached', async() => {
-			const elem = await fixture(getMaxWidthFixture('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'), { viewport: { width: 1300 } });
+			const elem = await fixture(getMaxWidthFixture('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'), { viewport: { width: 1300 } });
 			await expect(elem).to.be.golden();
 		});
 	});
