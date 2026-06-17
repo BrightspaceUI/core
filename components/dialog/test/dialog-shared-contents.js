7e034fde96bfee381edf6a1a112d3d1b8fb5b7db
Dave Batiste <dave.batiste@d2l.com>
2026-06-17 09:53:15 -0400

    fix: Update the tabs new structure flag default to true. (#7106)
    * Update the tabs new structure flag default to true.
    
    * Make fullscreen dialog visual-diffs agnostic to the state of the new tabs structure flag.

--- components/dialog/test/dialog-shared-contents.js
+++ components/dialog/test/dialog-shared-contents.js
@@ -4,8 +4,9 @@ import '../../dropdown/dropdown-content.js';
 import '../../filter/filter.js';
 import '../../filter/filter-dimension-set.js';
 import '../../filter/filter-dimension-set-value.js';
-import '../../tabs/tabs.js';
+import '../../tabs/tab.js';
 import '../../tabs/tab-panel.js';
+import { getUseNewTabsStructureFlag } from '../../tabs/tabs.js';
 import { html } from '@brightspace-ui/testing';
 
 const base = html`
@@ -78,7 +79,54 @@ export const filter = html`
 	</d2l-filter>
 `;
 
-export const tabs = html`
+// Clean up when removing GAUD-8299-core-tabs-use-new-structure flag.
+export const tabs = getUseNewTabsStructureFlag() ? html`
+	<d2l-tabs>
+		<d2l-tab id="s18" text="S18" slot="tabs"></d2l-tab>
+		<d2l-tab id="w18" text="W18" slot="tabs"></d2l-tab>
+		<d2l-tab id="f17" text="F17" slot="tabs"></d2l-tab>
+		<d2l-tab id="s17" text="S17" slot="tabs"></d2l-tab>
+		<d2l-tab id="w17" text="W17" slot="tabs"></d2l-tab>
+		<d2l-tab id="f16" text="F16" slot="tabs"></d2l-tab>
+		<d2l-tab id="s16" text="S16" slot="tabs"></d2l-tab>
+		<d2l-tab id="w16" text="W16" slot="tabs"></d2l-tab>
+		<d2l-tab id="f15" text="F15" slot="tabs"></d2l-tab>
+		<d2l-tab id="s15" text="S15" slot="tabs"></d2l-tab>
+		<d2l-tab id="w15" text="W15" slot="tabs"></d2l-tab>
+		<d2l-tab id="f14" text="F14" slot="tabs"></d2l-tab>
+		<d2l-tab id="s14" text="S14" slot="tabs"></d2l-tab>
+		<d2l-tab id="w14" text="W14" slot="tabs"></d2l-tab>
+		<d2l-tab id="f13" text="F13" slot="tabs"></d2l-tab>
+		<d2l-tab id="s13" text="S13" slot="tabs"></d2l-tab>
+		<d2l-tab id="w13" text="W13" slot="tabs"></d2l-tab>
+		<d2l-tab id="f12" text="F12" slot="tabs"></d2l-tab>
+		<d2l-tab id="s12" text="S12" slot="tabs"></d2l-tab>
+		<d2l-tab id="w12" text="W12" slot="tabs"></d2l-tab>
+		<d2l-tab id="f11" text="F11" slot="tabs"></d2l-tab>
+		<d2l-tab id="s11" text="S11" slot="tabs"></d2l-tab>
+		<d2l-tab-panel labelled-by="s18" slot="panels" id="s18-panel">Tab content for S18</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="w18" slot="panels" id="w18-panel">Tab content for W18</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="f17" slot="panels" id="f17-panel">Tab content for F17</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="s17" slot="panels" id="s17-panel">Tab content for S17</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="w17" slot="panels" id="w17-panel">Tab content for W17</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="f16" slot="panels" id="f16-panel">Tab content for F16</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="s16" slot="panels" id="s16-panel">Tab content for S16</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="w16" slot="panels" id="w16-panel">Tab content for W16</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="f15" slot="panels" id="f15-panel">Tab content for F15</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="s15" slot="panels" id="s15-panel">Tab content for S15</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="w15" slot="panels" id="w15-panel">Tab content for W15</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="f14" slot="panels" id="f14-panel">Tab content for F14</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="s14" slot="panels" id="s14-panel">Tab content for S14</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="w14" slot="panels" id="w14-panel">Tab content for W14</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="f13" slot="panels" id="f13-panel">Tab content for F13</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="s13" slot="panels" id="s13-panel">Tab content for S13</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="w13" slot="panels" id="w13-panel">Tab content for W13</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="f12" slot="panels" id="f12-panel">Tab content for F12</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="s12" slot="panels" id="s12-panel">Tab content for S12</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="w12" slot="panels" id="w12-panel">Tab content for W12</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="f11" slot="panels" id="f11-panel">Tab content for F11</d2l-tab-panel>
+		<d2l-tab-panel labelled-by="s11" slot="panels" id="s11-panel">Tab content for S11</d2l-tab-panel>
+	</d2l-tabs>` : html`
 	<d2l-tabs>
 		<d2l-tab-panel text="S18">Tab content for S18</d2l-tab-panel>
 		<d2l-tab-panel text="W18">Tab content for W18</d2l-tab-panel>
