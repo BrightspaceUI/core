7da8308fc8fc3ccc83df360895e5be653c023614
Nick Hallman <Nick.Hallman@D2L.com>
2026-06-19 11:04:03 -0400

    make buttons larger in side-to-side mode

--- components/button/button-move.js
+++ components/button/button-move.js
@@ -152,7 +152,7 @@ class ButtonMove extends ThemeMixin(FocusMixin(LitElement)) {
 				.down-layer {
 					height: 1.2rem;
 					position: relative;
-					width: 1.3rem;
+					width: 1.25rem;
 				}
 
 				/* Firefox includes a hidden border which messes up button dimensions */
@@ -175,21 +175,23 @@ class ButtonMove extends ThemeMixin(FocusMixin(LitElement)) {
 					align-items: center;
 					flex-direction: row;
 					height: 1.2rem;
-					justify-content: center;
-					width: calc(1.3rem * 2);
+					width: calc(1.25rem * 2);
 				}
 
 				:host([side-to-side]) .layer {
 					flex-direction: row;
 					height: 1.2rem;
 					inset-inline-start: 0;
+					gap: 2px;
 					top: 0;
-					width: calc(1.3rem * 2);
+					width: calc(1.25rem * 2);
 				}
 
 				:host([side-to-side]) d2l-icon {
 					/* Arrows need to be rotated in opposite direction for RTL */
+					height: 1.2rem;
 					transform: rotate(calc(-90deg * var(--d2l-length-factor)));
+					width: 1.25rem;
 				}
 			`
 		];
