e0a04d5e4ebcae81cc0b199a22ad6db4e1f5729d
GZolla <43836485+GZolla@users.noreply.github.com>
2026-06-23 12:33:43 -0700

    fix: add support for :not in _isValidCssSelector (#7125)

--- helpers/internal/css.js
+++ helpers/internal/css.js
@@ -1,5 +1,15 @@
 export function _isValidCssSelector(selector) {
 	const partIsValid = (part) => {
+		const notRegex = /:not\((.+?)\)/;
+		const notMatches = part.match(notRegex);
+		if (notMatches) {
+			return [
+				part.substring(0, notMatches.index),
+				notMatches[1],
+				part.substring(notMatches.index + notMatches[1].length + 6)
+			].every((part, i) => (i !== 1 && part === '') || partIsValid(part));
+		}
+
 		const re = /([a-zA-Z0-9-_ >.#]+)(\[[a-zA-Z0-9-_="]+\])?([a-zA-Z0-9-_ >.#]+)?/g;
 		if (part === ':host') return true;
 		const match = part.match(re);
