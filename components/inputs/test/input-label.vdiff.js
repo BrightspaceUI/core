2ed10a076ea89c1a9215a5869c9da0ca4530f4a6
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-06-25 11:02:58 -0400

    Removes input label sass related code

--- components/inputs/test/input-label.vdiff.js
+++ components/inputs/test/input-label.vdiff.js
@@ -1,6 +1,5 @@
 import { css, LitElement } from 'lit';
 import { defineCE, expect, fixture, html } from '@brightspace-ui/testing';
-import { loadSass, unloadSass } from '../../../test/load-sass.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { inputLabelStyles } from '../input-label-styles.js';
@@ -56,54 +55,11 @@ const wrapTag = defineCE(class extends LitElement {
 
 describe('d2l-input-label', () => {
 
-	before(loadSass);
-	after(unloadSass);
-
 	[
 		{ name: 'ref', template: `<${refTag}></${refTag}>` },
 		{ name: 'ref-required', template: `<${refTag} required></${refTag}>`, allColorModes: true },
 		{ name: 'wrap', template: `<${wrapTag}></${wrapTag}>` },
-		{ name: 'wrap-required', template: `<${wrapTag} required></${wrapTag}>` },
-		{
-			name: 'sass-ref',
-			template: html`
-				<div>
-					<label for="input-sass-ref" class="d2l-test-input-label">Name</label>
-					<input type="text" id="input-sass-ref" class="d2l-test-input-text">
-				</div>
-			`
-		},
-		{
-			name: 'sass-ref-required',
-			template: html`
-				<div>
-					<label for="input-sass-ref-required" class="d2l-test-input-label d2l-test-input-label-required">Name</label>
-					<input type="text" id="input-sass-ref-required" class="d2l-test-input-text" aria-required="true">
-				</div>
-			`
-		},
-		{
-			name: 'sass-wrap',
-			template: html`
-				<div>
-					<label>
-						<span class="d2l-test-input-label">Name</span>
-						<input type="text" class="d2l-test-input-text">
-					</label>
-				</div>
-			`
-		},
-		{
-			name: 'sass-wrap-required',
-			template: html`
-				<div>
-					<label>
-						<span class="d2l-test-input-label d2l-test-input-label-required">Name</span>
-						<input type="text" class="d2l-test-input-text" aria-required="true">
-					</label>
-				</div>
-			`
-		}
+		{ name: 'wrap-required', template: `<${wrapTag} required></${wrapTag}>` }
 	].forEach(({ name, allColorModes, template }) => {
 		it(name, async() => {
 			const elem = await fixture(template, { viewport });
