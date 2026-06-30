bd3cb6f5aa705c4c1891555486e019d441b3fa71
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-06-30 15:28:04 -0400

    Updates vdiffs to use the generated styles to see the diff between sass and the generated ones goldens

--- components/inputs/test/input-select.vdiff.js
+++ components/inputs/test/input-select.vdiff.js
@@ -1,6 +1,8 @@
 import '../demo/input-select-test.js';
 import { expect, fixture, focusElem, html } from '@brightspace-ui/testing';
-import { loadSass, unloadSass } from '../../../test/load-sass.js';
+import { _generateSelectStyles } from '../input-select-styles.js';
+
+const generatedSelectStyles = _generateSelectStyles('.d2l-test-input-select');
 
 const defaultFixture = html`<d2l-test-input-select></d2l-test-input-select>`;
 const overflowFixture = html`<d2l-test-input-select overflow></d2l-test-input-select>`;
@@ -12,8 +14,16 @@ const sassInvalidFixture = html`<select class="d2l-test-input-select" aria-inval
 
 describe('d2l-input-select', () => {
 
-	before(loadSass);
-	after(unloadSass);
+	before(() => {
+		/* append generated styles into document head */
+		const style = document.createElement('style');
+		style.id = 'generated-css';
+		style.textContent = generatedSelectStyles.cssText;
+		document.head.appendChild(style);
+	});
+	after(() => {
+		document.getElementById('generated-css').remove();
+	});
 
 	[
 		{ name: 'default', template: defaultFixture, allColorModes: true },
