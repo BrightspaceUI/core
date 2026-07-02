407a012c5420ca5b673bb80e7be155b004013ed8
Edwin A. Collazos <42192151+EdwinACL831@users.noreply.github.com>
2026-07-02 10:25:13 -0400

    feat: GAUD-8850 Remove Select.scss (#7143)
    * GAUD-8850: Remove Select.scss
    
    - Removes SASS logic from the select styles
    - Remove unused test class using the sass styles
    
    * Removes sass vdiff tests
    
    * Updating vdiff goldens (#7146)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Adds en exports generator function for input selector
    
    * Re-adds the select.scss in order to not break BSI when merged
    
    * Renames from unsafeSelector to finalSelector
    
    * Fixes lint error
    
    * Always includes the media query for prefer-contrast
    
    * Revert "Removes sass vdiff tests"
    
    This reverts commit f1b96e9f4c409514bee86becf5adf84e92480d04.
    
    * Updating vdiff goldens (#7148)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Updates vdiffs to use the generated styles to see the diff between sass and the generated ones goldens
    
    * Revert "Updates vdiffs to use the generated styles to see the diff between sass and the generated ones goldens"
    
    This reverts commit bd3cb6f5aa705c4c1891555486e019d441b3fa71.
    
    ---------
    
    Co-authored-by: Edwin Collazos <Edwin.Collazos@D2L.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/inputs/input-select-styles.js
+++ components/inputs/input-select-styles.js
@@ -1,5 +1,6 @@
 import './input-styles.js';
 import { css, unsafeCSS } from 'lit';
+import { _isValidCssSelector } from '../../helpers/internal/css.js';
 import { getFocusPseudoClass } from '../../helpers/focus.js';
 import { registerSemanticVariableForSvgImageUrl } from '../colors/colors.js';
 
@@ -12,94 +13,104 @@ registerSemanticVariableForSvgImageUrl(
 	</svg>`
 );
 
-export const selectStyles = css`
-	.d2l-input-select {
-		-webkit-appearance: none;
-		-moz-appearance: none;
-		appearance: none;
-		background-color: var(--d2l-theme-background-color-base);
-		background-image: var(--d2l-input-select-chevron-image);
-		background-origin: border-box;
-		background-position: center var(--d2l-inline-end, right) 17px;
-		background-repeat: no-repeat;
-		background-size: 11px 7px;
-		border: none;
-		border-radius: 0.3rem;
-		box-shadow: inset var(--d2l-theme-shadow-inset-offset-x) var(--d2l-theme-shadow-inset-offset-y) var(--d2l-theme-shadow-inset-blur-radius) 1px var(--d2l-theme-shadow-inset-color);
-		color: var(--d2l-theme-text-color-static-standard);
-		display: inline-block;
-		font-family: inherit;
-		font-size: 0.8rem;
-		font-weight: 400;
-		height: auto;
-		letter-spacing: 0.02rem;
-		line-height: 1.2rem;
-		margin: 0;
-		max-height: calc(2rem + 2px);
-		outline: 1px solid var(--d2l-theme-border-color-emphasized);
-		outline-offset: -1px;
-		padding-block: calc(0.4rem + 1px);
-		padding-inline: calc(0.75rem + 1px) calc(2px + 0.8rem + 1px + 11px + 16px + 1px);
-		vertical-align: middle;
-	}
+/**
+ * A private helper method that should not be used by general consumers
+ */
+export function _generateSelectStyles(selector) {
+	if (!_isValidCssSelector(selector)) return '';
+	const finalSelector = unsafeCSS(selector);
 
-	.d2l-input-select:not([disabled]):hover,
-	.d2l-input-select:not([disabled]):${focusClass} {
-		box-shadow: inset var(--d2l-theme-shadow-inset-offset-x) var(--d2l-theme-shadow-inset-offset-y) var(--d2l-theme-shadow-inset-blur-radius) 2px var(--d2l-theme-shadow-inset-color);
-		outline: 2px solid var(--d2l-theme-border-color-focus);
-		outline-offset: -2px;
-	}
-	.d2l-input-select[aria-invalid="true"] {
-		background-image: var(--d2l-input-select-chevron-image), var(--d2l-input-invalid-image);
-		background-position: center var(--d2l-inline-end, right) 17px, center var(--d2l-inline-end, right) calc(1px + 11px + 17px);
-		background-repeat: no-repeat, no-repeat;
-		background-size: 11px 7px, 0.8rem 0.8rem;
-	}
-	.d2l-input-select[aria-invalid="true"],
-	.d2l-input-select[aria-invalid="true"]:${focusClass},
-	.d2l-input-select[aria-invalid="true"]:hover {
-		outline-color: var(--d2l-theme-status-color-error);
-	}
-	.d2l-input-select:disabled {
-		opacity: var(--d2l-theme-opacity-disabled-control);
-	}
-
-	@media (prefers-contrast: more) {
-		.d2l-input-select {
-			appearance: auto;
-			background-color: Field;
-			background-image: none;
-			border: none;
-			border-inline-end: 0.75rem solid transparent;
-			box-shadow: none;
-			color: FieldText;
-			forced-color-adjust: none;
-			height: 2rem;
-			outline: 1px solid ButtonBorder;
-			padding-inline: 0.6rem 16px;
-		}
-
-		.d2l-input-select:not([disabled]):${focusClass},
-		.d2l-input-select:not([disabled]):hover {
-			box-shadow: none;
-			outline: 2px solid Highlight;
-		}
-
-		.d2l-input-select:disabled {
-			outline: 1px solid GrayText;
-		}
-
-		.d2l-input-select[aria-invalid="true"] {
-			background-image: var(--d2l-input-invalid-image);
-			background-position: center var(--d2l-inline-end, right) calc(1px + 11px + 17px);
+	return css`
+		${finalSelector} {
+			-webkit-appearance: none;
+			-moz-appearance: none;
+			appearance: none;
+			background-color: var(--d2l-theme-background-color-base);
+			background-image: var(--d2l-input-select-chevron-image);
+			background-origin: border-box;
+			background-position: center var(--d2l-inline-end, right) 17px;
 			background-repeat: no-repeat;
-			background-size: 0.8rem 0.8rem;
+			background-size: 11px 7px;
+			border: none;
+			border-radius: 0.3rem;
+			box-shadow: inset var(--d2l-theme-shadow-inset-offset-x) var(--d2l-theme-shadow-inset-offset-y) var(--d2l-theme-shadow-inset-blur-radius) 1px var(--d2l-theme-shadow-inset-color);
+			color: var(--d2l-theme-text-color-static-standard);
+			display: inline-block;
+			font-family: inherit;
+			font-size: 0.8rem;
+			font-weight: 400;
+			height: auto;
+			letter-spacing: 0.02rem;
+			line-height: 1.2rem;
+			margin: 0;
+			max-height: calc(2rem + 2px);
+			outline: 1px solid var(--d2l-theme-border-color-emphasized);
+			outline-offset: -1px;
+			padding-block: calc(0.4rem + 1px);
+			padding-inline: calc(0.75rem + 1px) calc(2px + 0.8rem + 1px + 11px + 16px + 1px);
+			vertical-align: middle;
 		}
 
-		.d2l-input-select[aria-invalid="true"],
-		.d2l-input-select[aria-invalid="true"]:${focusClass},
-		.d2l-input-select[aria-invalid="true"]:hover {
+		${finalSelector}:not([disabled]):hover,
+		${finalSelector}:not([disabled]):${focusClass} {
+			box-shadow: inset var(--d2l-theme-shadow-inset-offset-x) var(--d2l-theme-shadow-inset-offset-y) var(--d2l-theme-shadow-inset-blur-radius) 2px var(--d2l-theme-shadow-inset-color);
+			outline: 2px solid var(--d2l-theme-border-color-focus);
+			outline-offset: -2px;
+		}
+		${finalSelector}[aria-invalid="true"] {
+			background-image: var(--d2l-input-select-chevron-image), var(--d2l-input-invalid-image);
+			background-position: center var(--d2l-inline-end, right) 17px, center var(--d2l-inline-end, right) calc(1px + 11px + 17px);
+			background-repeat: no-repeat, no-repeat;
+			background-size: 11px 7px, 0.8rem 0.8rem;
+		}
+		${finalSelector}[aria-invalid="true"],
+		${finalSelector}[aria-invalid="true"]:${focusClass},
+		${finalSelector}[aria-invalid="true"]:hover {
 			outline-color: var(--d2l-theme-status-color-error);
 		}
-	}
-`;
+		${finalSelector}:disabled {
+			opacity: var(--d2l-theme-opacity-disabled-control);
+		}
+
+		@media (prefers-contrast: more) {
+			${finalSelector} {
+				appearance: auto;
+				background-color: Field;
+				background-image: none;
+				border: none;
+				border-inline-end: 0.75rem solid transparent;
+				box-shadow: none;
+				color: FieldText;
+				forced-color-adjust: none;
+				height: 2rem;
+				outline: 1px solid ButtonBorder;
+				padding-inline: 0.6rem 16px;
+			}
+
+			${finalSelector}:not([disabled]):${focusClass},
+			${finalSelector}:not([disabled]):hover {
+				box-shadow: none;
+				outline: 2px solid Highlight;
+			}
+
+			${finalSelector}:disabled {
+				outline: 1px solid GrayText;
+			}
+
+			${finalSelector}[aria-invalid="true"] {
+				background-image: var(--d2l-input-invalid-image);
+				background-position: center var(--d2l-inline-end, right) calc(1px + 11px + 17px);
+				background-repeat: no-repeat;
+				background-size: 0.8rem 0.8rem;
+			}
+
+			${finalSelector}[aria-invalid="true"],
+			${finalSelector}[aria-invalid="true"]:${focusClass},
+			${finalSelector}[aria-invalid="true"]:hover {
+				outline-color: var(--d2l-theme-status-color-error);
+			}
+		}
+	`;
+};
+
+export const selectStyles = _generateSelectStyles('.d2l-input-select');
