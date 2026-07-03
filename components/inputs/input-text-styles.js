ac4a874c173ec6f7928a49ea80efb52b93998305
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-07-03 15:50:19 -0400

    Updates input-text-styles.js to import and use base styles from input-styles.js

--- components/inputs/input-text-styles.js
+++ components/inputs/input-text-styles.js
@@ -1,10 +1,17 @@
 
+import {
+	_generateInputAriaInvalidBaseStyles,
+	_generateInputBaseStyles,
+	_generateInputDisabledBaseStyles,
+	_generateInputPlaceholderBaseStyles,
+} from './input-styles.js';
 import { css, unsafeCSS } from 'lit';
 import { _isValidCssSelector } from '../../helpers/internal/css.js';
 import { getFocusVisibleStyles } from '../../helpers/focus.js';
 
 function _generateInputTextDisabledStyles(selector) {
 	const focusSelectorDelegate = (focusPseudoClass) => `
+		${selector},
 		${selector}:hover:disabled,
 		[aria-invalid="true"]${selector}:disabled,
 		${selector}:${focusPseudoClass}:disabled`;
@@ -41,46 +48,16 @@ export function _generateInputTextStyles(selector) {
 	const finalSelector = unsafeCSS(selector);
 
 	return css`
-		${finalSelector} {
-			background-color: var(--d2l-input-background-color, var(--d2l-theme-background-color-base));
-			border: 1px solid var(--d2l-input-border-color, var(--d2l-theme-border-color-emphasized));
-			border-radius: var(--d2l-input-border-radius, 0.3rem);
-			box-shadow: var(--d2l-theme-shadow-inset);
-			box-sizing: border-box;
-			color: var(--d2l-theme-text-color-static-standard);
-			display: inline-block;
-			font-family: inherit;
-			font-size: 0.8rem;
-			font-weight: 400;
-			height: var(--d2l-input-height, auto);
-			letter-spacing: 0.02rem;
-			line-height: 1.2rem;
-			margin: 0;
-			min-width: calc(2rem + 1em);
-			padding: var(--d2l-input-padding, 0.4rem 0.75rem);
-			position: var(--d2l-input-position, relative); /* overridden by sticky headers in grades */
-			text-align: var(--d2l-input-text-align, start);
-			vertical-align: middle;
-			width: 100%;
-		}
+		${ _generateInputBaseStyles(finalSelector) }
 
-		${finalSelector}::placeholder {
-			color: var(--d2l-theme-text-color-static-faint);
-			font-size: 0.8rem;
-			font-weight: 400;
-			opacity: 1; /* Firefox has non-1 default */
-		}
+		${ _generateInputPlaceholderBaseStyles(finalSelector) }
 
 		${ _generateInputTextFocusStyles(finalSelector) }
 
-		[aria-invalid="true"]${finalSelector}:not(:disabled) {
-			border-color: var(--d2l-theme-status-color-error);
-		}
+		${ _generateInputAriaInvalidBaseStyles(finalSelector) }
 
 		${ _generateInputTextDisabledStyles(finalSelector) }
 
-		${finalSelector}:disabled {
-			opacity: var(--d2l-theme-opacity-disabled-control);
-		}
+		${_generateInputDisabledBaseStyles(finalSelector)}
 	`;
 }
