248ed70b2640cc3f67759b8d964466ffcbb6a21d
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-07-06 09:52:20 -0400

    Removes function implementation and usage for disabled styles since they are already handled by the Input base styles by the :not(:disabled)

--- components/inputs/input-text-styles.js
+++ components/inputs/input-text-styles.js
@@ -10,22 +10,6 @@ import { css, unsafeCSS } from 'lit';
 import { _isValidCssSelector } from '../../helpers/internal/css.js';
 import { getFocusVisibleStyles } from '../../helpers/focus.js';
 
-function _generateInputTextDisabledStyles(selector) {
-	const focusSelectorDelegate = (focusPseudoClass) => `
-		${selector},
-		${selector}:hover:disabled,
-		[aria-invalid="true"]${selector}:disabled,
-		${selector}:${focusPseudoClass}:disabled`;
-
-	return getFocusVisibleStyles(focusSelectorDelegate, (selector) => css`
-		${selector} {
-			border-color: var(--d2l-input-border-color, var(--d2l-theme-border-color-emphasized));
-			border-width: 1px;
-			padding: var(--d2l-input-padding, 0.4rem 0.75rem);
-		}`
-	);
-}
-
 function _generateInputTextFocusStyles(selector) {
 	const input = _getInputBaseStyleDelegates(selector);
 	return getFocusVisibleStyles(input.selector, input.style);
@@ -46,8 +30,6 @@ export function _generateInputTextStyles(selector) {
 		${ _generateInputTextFocusStyles(finalSelector) }
 
 		${ _generateInputAriaInvalidBaseStyles(finalSelector) }
-
-		${ _generateInputTextDisabledStyles(finalSelector) }
 
 		${_generateInputDisabledBaseStyles(finalSelector)}
 	`;
