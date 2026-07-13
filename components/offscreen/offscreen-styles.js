ece19bb2a719a6182a8e49fd7f7fff7501356a3e
GZolla <43836485+GZolla@users.noreply.github.com>
2026-07-13 11:31:04 -0700

    fix: GAUD-8854: Make offscreen styles node safe (#7199)
    * GAUD-8854: Make offscreen styles node safe
    
    * Appease Linter

---
+++ components/offscreen/offscreen-styles.js
@@ -0,0 +1,24 @@
+import { css } from 'lit';
+import { getFlag } from '../../helpers/flags.js';
+
+const OffSCREEN_SIZELESS = getFlag('d2l-offscreen-sizeless', true);
+const HAS_DOCUMENT = globalThis.document !== undefined;
+
+/**
+ * A private helper declarations that should not be used by general consumers
+ */
+export const _offscreenStyleDeclarations = css`
+	direction: var(--d2l-document-direction, ${ !HAS_DOCUMENT ? css`unset` : (document.dir === 'rtl' ? css`rtl` : css`ltr`)}); /* stylelint-disable-line @stylistic/string-quotes */
+	height: ${HAS_DOCUMENT && OffSCREEN_SIZELESS ? 0 : 1}px;
+	inset-inline-start: -10000px;
+	overflow: hidden;
+	position: absolute !important;
+	white-space: nowrap;
+	width: ${HAS_DOCUMENT && OffSCREEN_SIZELESS ? 0 : 1}px;
+`;
+
+export const offscreenStyles = css`
+	.d2l-offscreen {
+		${_offscreenStyleDeclarations}
+	}
+`;
--- components/offscreen/offscreen.js
+++ components/offscreen/offscreen.js
@@ -1,27 +1,7 @@
+import { _offscreenStyleDeclarations, offscreenStyles } from './offscreen-styles.js';
 import { css, html, LitElement } from 'lit';
-import { getFlag } from '../../helpers/flags.js';
 
-const OffSCREEN_SIZELESS = getFlag('d2l-offscreen-sizeless', true);
-
-/**
- * A private helper declarations that should not be used by general consumers
- */
-export const _offscreenStyleDeclarations = css`
-	direction: var(--d2l-document-direction, ${document.dir === 'rtl' ? css`rtl` : css`ltr`}); /* stylelint-disable-line @stylistic/string-quotes */
-	height: ${OffSCREEN_SIZELESS ? 0 : 1}px;
-	inset-inline-start: -10000px;
-	overflow: hidden;
-	position: absolute !important;
-	white-space: nowrap;
-	width: ${OffSCREEN_SIZELESS ? 0 : 1}px;
-	${document.dir === 'rtl' ? css`right` : css`left`}: -10000px;
-`;
-
-export const offscreenStyles = css`
-	.d2l-offscreen {
-		${_offscreenStyleDeclarations}
-	}
-`;
+export { _offscreenStyleDeclarations, offscreenStyles };
 
 /**
  * A component for positioning content offscreen to only be visible to screen readers.
