e470f2e3b4f28406453ba2cfe10a448a97ebb034
GZolla <43836485+GZolla@users.noreply.github.com>
2026-07-21 12:15:46 -0700

    fix: Remove d2l-offscreen-sizeless flag

--- components/offscreen/offscreen-styles.js
+++ components/offscreen/offscreen-styles.js
@@ -1,7 +1,6 @@
 import { css } from 'lit';
 import { getFlag } from '../../helpers/flags.js';
 
-const OffSCREEN_SIZELESS = getFlag('d2l-offscreen-sizeless', true);
 const HAS_DOCUMENT = globalThis.document !== undefined;
 
 /**
@@ -9,12 +8,12 @@ const HAS_DOCUMENT = globalThis.document !== undefined;
  */
 export const _offscreenStyleDeclarations = css`
 	direction: var(--d2l-document-direction, ${ !HAS_DOCUMENT ? css`unset` : (document.dir === 'rtl' ? css`rtl` : css`ltr`)}); /* stylelint-disable-line @stylistic/string-quotes */
-	height: ${HAS_DOCUMENT && OffSCREEN_SIZELESS ? 0 : 1}px; /* The HAS_DOCUMENT check can be removed once the d2l-offscreen-sizeless flag is removed and set to 0px */
+	height: 0px;
 	inset-inline-start: -10000px;
 	overflow: hidden;
 	position: absolute !important;
 	white-space: nowrap;
-	width: ${HAS_DOCUMENT && OffSCREEN_SIZELESS ? 0 : 1}px; /* The HAS_DOCUMENT check can be removed once the d2l-offscreen-sizeless flag is removed and set to 0px */
+	width: 0px;
 `;
 
 export const offscreenStyles = css`
