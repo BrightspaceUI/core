145e7951d89c4efdcee86dfb7cef94c9d5975db3
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2026-07-22 13:34:12 -0400

    feat: GAUD-10310 - Add collapse button/handle to page divider (#7246)
    * GAUD-10310 - Add collapse button/handle to page divider
    
    * Updating vdiff goldens (#7248)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/page/test/page-divider-internal.axe.js
+++ components/page/test/page-divider-internal.axe.js
@@ -1,6 +1,6 @@
 import '../page-divider-internal.js';
-import { clickElem, expect, fixture, hoverElem } from '@brightspace-ui/testing';
-import { getDivider, pageDividerFixtures } from './page-divider-internal-fixtures.js';
+import { clickElem, expect, fixture, hoverElem, nextFrame } from '@brightspace-ui/testing';
+import { getDivider, getSlider, pageDividerFixtures } from './page-divider-internal-fixtures.js';
 
 describe('page-divider-internal', () => {
 
@@ -10,9 +10,33 @@ describe('page-divider-internal', () => {
 		await expect(elem).to.be.accessible();
 	});
 
+	it('hover handle', async() => {
+		const elem = await fixture(pageDividerFixtures.supportingImmersiveFooter);
+		const divider = getDivider(elem, 'supporting');
+		await hoverElem(getSlider(divider));
+		await expect(elem).to.be.accessible();
+	});
+
 	it('focus', async() => {
 		const elem = await fixture(pageDividerFixtures.sideNavBothHeadersFooter);
 		await clickElem(getDivider(elem, 'side-nav'));
+		await nextFrame();
+		await expect(elem).to.be.accessible();
+	});
+
+	it('collapsed-side-nav', async() => {
+		const elem = await fixture(pageDividerFixtures.sideNavBothHeadersFooter);
+		const divider = getDivider(elem, 'side-nav');
+		await clickElem(getSlider(divider));
+		await nextFrame();
+		await expect(elem).to.be.accessible();
+	});
+
+	it('collapsed-supporting', async() => {
+		const elem = await fixture(pageDividerFixtures.supportingImmersiveFooter);
+		const divider = getDivider(elem, 'supporting');
+		await clickElem(getSlider(divider));
+		await nextFrame();
 		await expect(elem).to.be.accessible();
 	});
 
