2a2344982c1ebd922defc93c446da9922a417e8e
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-07-22 12:02:19 -0400

    Creates the _generateButtonBaseStyles to wrapp button base css styles

--- components/button/button-styles.js
+++ components/button/button-styles.js
@@ -1,6 +1,36 @@
 import '../colors/colors.js';
-import { css } from 'lit';
+import { css, unsafeCSS } from 'lit';
+import { _isValidCssSelector } from '../../helpers/internal/css.js';
 import { getFocusRingStyles } from '../../helpers/focus.js';
+
+function _generateButtonBaseStyles(selector) {
+	if (!_isValidCssSelector(selector)) return unsafeCSS('');
+	selector = unsafeCSS(selector.trim());
+
+	return css`
+		${selector} {
+			border-end-end-radius: var(--d2l-button-end-end-radius, 0.3rem);
+			border-end-start-radius: var(--d2l-button-end-start-radius, 0.3rem);
+			border-start-end-radius: var(--d2l-button-start-end-radius, 0.3rem);
+			border-start-start-radius: var(--d2l-button-start-start-radius, 0.3rem);
+			border-style: none;
+			box-sizing: border-box;
+			cursor: pointer;
+			display: inline-block;
+			margin: 0;
+			min-height: calc(2rem + 2px);
+			outline: none;
+			text-align: center;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			-ms-user-select: none;
+			user-select: none;
+			vertical-align: middle;
+			white-space: nowrap;
+			width: auto;
+		}
+	`;
+}
 
 export const buttonStyles = css`
 	button {
