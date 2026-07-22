6f95cd52ae7c47cd22aa52150cfc1a3d17961a86
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-07-22 12:06:08 -0400

    Replaces hardcoded styles by a call to the _generateButtonBaseStyles function

--- components/button/button-styles.js
+++ components/button/button-styles.js
@@ -33,27 +33,7 @@ function _generateButtonBaseStyles(selector) {
 }
 
 export const buttonStyles = css`
-	button {
-		border-end-end-radius: var(--d2l-button-end-end-radius, 0.3rem);
-		border-end-start-radius: var(--d2l-button-end-start-radius, 0.3rem);
-		border-start-end-radius: var(--d2l-button-start-end-radius, 0.3rem);
-		border-start-start-radius: var(--d2l-button-start-start-radius, 0.3rem);
-		border-style: none;
-		box-sizing: border-box;
-		cursor: pointer;
-		display: inline-block;
-		margin: 0;
-		min-height: calc(2rem + 2px);
-		outline: none;
-		text-align: center;
-		-webkit-user-select: none;
-		-moz-user-select: none;
-		-ms-user-select: none;
-		user-select: none;
-		vertical-align: middle;
-		white-space: nowrap;
-		width: auto;
-	}
+	${_generateButtonBaseStyles('button')}
 	${getFocusRingStyles('button')}
 	@media (prefers-contrast: more) {
 		button {
