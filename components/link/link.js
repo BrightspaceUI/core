07a7460639fb462ce94c3dfe49ef1425d753a256
Danny Gleckler <daniel.gleckler@d2l.com>
2026-07-30 13:51:08 -0400

    Apply suggestion from @bearfriend

--- components/link/link.js
+++ components/link/link.js
@@ -180,8 +180,11 @@ class Link extends LocalizeCoreElement(FocusMixin(LitElement)) {
 
 	#linkId = getUniqueId();
 
-	#handleClick() {
-		if (this.disabled) return false;
+	#handleClick(e) {
+		if (this.disabled) {
+			e.stopPropagation();
+			e.preventDefault();
+		}
 	}
 
 }
