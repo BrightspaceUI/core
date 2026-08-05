6a06fa3fed860c1103f2c160199fa56d7aec9da5
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2026-08-05 11:04:59 -0400

    Make tests a bit more resilient

--- components/page/test/page.vdiff.js
+++ components/page/test/page.vdiff.js
@@ -373,13 +373,13 @@ describe('page', () => {
 			[
 				{ name: 'side-nav-larger', position: 'start', fixture: pageFixtures.sideNavHeaderStorageKey, default: sideNavDefault, stored: 500, expected: 500 },
 				{ name: 'side-nav-smaller', position: 'start', fixture: pageFixtures.sideNavHeaderStorageKey, default: sideNavDefault, stored: 325, expected: 325 },
-				{ name: 'side-nav-max', position: 'start', fixture: pageFixtures.sideNavHeaderStorageKey, default: sideNavDefault, stored: 800, expected: maxPanelSize },
-				{ name: 'side-nav-min', position: 'start', fixture: pageFixtures.sideNavHeaderStorageKey, default: sideNavDefault, stored: 200, expected: minPanelSize },
+				{ name: 'side-nav-max', position: 'start', fixture: pageFixtures.sideNavHeaderStorageKey, default: sideNavDefault, stored: maxPanelSize + 200, expected: maxPanelSize },
+				{ name: 'side-nav-min', position: 'start', fixture: pageFixtures.sideNavHeaderStorageKey, default: sideNavDefault, stored: minPanelSize - 120, expected: minPanelSize },
 
 				{ name: 'supporting-larger', position: 'end', fixture: pageFixtures.supportingFooterStorageKey, default: supportingDefault, stored: 500, expected: 500 },
 				{ name: 'supporting-smaller', position: 'end', fixture: pageFixtures.supportingFooterStorageKey, default: supportingDefault, stored: 350, expected: 350 },
-				{ name: 'supporting-max', position: 'end', fixture: pageFixtures.supportingFooterStorageKey, default: supportingDefault, stored: 800, expected: maxPanelSize },
-				{ name: 'supporting-min', position: 'end', fixture: pageFixtures.supportingFooterStorageKey, default: supportingDefault, stored: 200, expected: minPanelSize },
+				{ name: 'supporting-max', position: 'end', fixture: pageFixtures.supportingFooterStorageKey, default: supportingDefault, stored: maxPanelSize + 200, expected: maxPanelSize },
+				{ name: 'supporting-min', position: 'end', fixture: pageFixtures.supportingFooterStorageKey, default: supportingDefault, stored: minPanelSize - 120, expected: minPanelSize },
 			].forEach(test => {
 				it(test.name, async() => {
 					setStoredPanelState({
