8098a0dc825caf8be73650c67f965a32c67df994
Stacey Van Herk <13419300+svanherk@users.noreply.github.com>
2026-08-07 12:06:47 -0400

    Bump down to 97px

--- components/page/page-divider-internal.js
+++ components/page/page-divider-internal.js
@@ -144,7 +144,7 @@ class PageDivider extends FocusMixin(PropertyRequiredMixin(LitElement)) {
 			cursor: pointer;
 			display: none;
 			height: 24px;
-			inset-block-start: max(50%, 90px); /* Do not hide behind slider on short screens */
+			inset-block-start: max(50%, 97px); /* Do not hide behind slider on short screens */
 			justify-content: center;
 			position: absolute;
 			transform: translateY(-50%);
