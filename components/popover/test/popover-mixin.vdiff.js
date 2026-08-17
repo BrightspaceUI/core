6a2ed2bcad88a9044cf5b5379edb4f27b7daf868
Dave Lockhart <dlockhart@users.noreply.github.com>
2026-08-17 17:32:23 -0400

    add vdiff for async popover

--- components/popover/test/popover-mixin.vdiff.js
+++ components/popover/test/popover-mixin.vdiff.js
@@ -1,20 +1,66 @@
 import './popover.js';
-import { clickElem, expect, fixture, html, oneEvent } from '@brightspace-ui/testing';
+import { clickElem, defineCE, expect, fixture, html, oneEvent, sendKeysElem } from '@brightspace-ui/testing';
+import { LitElement } from 'lit';
+
+const asyncPopoverTag = defineCE(class extends LitElement {
+	static properties = {
+		_loaded: { state: true }
+	};
+	constructor() {
+		super();
+		this._loaded = false;
+	}
+	render() {
+		const content = !this._loaded ? 'Loading...' : html`<button>Loading Complete, focus here</button>`;
+		return html`
+			<span>
+				<button @click="${this.#handleOpen}">Open</button>
+				<d2l-test-popover @d2l-popover-before-open="${this.#handlePopoverBeforeOpen}" class="vdiff-include">${content}</d2l-test-popover>
+			</span>
+		`;
+	}
+	#handleOpen(e) {
+		this.shadowRoot.querySelector('d2l-test-popover').open(e.target);
+	}
+	#handlePopoverBeforeOpen(e) {
+		if (!this._loaded) {
+			e.preventDefault();
+			setTimeout(() => {
+				this._loaded = true;
+				e.detail.complete();
+			}, 200);
+		}
+	}
+});
 
 describe('popover-mixin', () => {
 
 	const open = e => e.target.nextSibling.open(e.target);
+	const viewport = { width: 700, height: 400 };
 
 	[
 		{ name: 'default', template: html`<span><button @click="${open}">Open</button><d2l-test-popover class="vdiff-include" style="max-width: 400px;">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover></span>` },
 		{ name: 'maxHeight', template: html`<span><button @click="${open}">Open</button><d2l-test-popover class="vdiff-include" max-height="75">Sink me piracy Gold Road quarterdeck wherry long boat line pillage walk the plank Plate Fleet. Haul wind black spot strike colors deadlights lee Barbary Coast yo-ho-ho ballast gally Shiver me timbers. Sea Legs quarterdeck yard scourge of the seven seas coffer plunder lanyard holystone code of conduct belay.</d2l-test-popover></span>` }
 	].forEach(({ name, template }) => {
 		it(name, async() => {
-			const el = await fixture(template, { viewport: { width: 700, height: 400 } });
+			const el = await fixture(template, { viewport });
 			clickElem(el.querySelector('button'));
 			await oneEvent(el, 'd2l-popover-open');
 			await expect(el).to.be.golden();
 		});
+	});
+
+	it('async-loading', async() => {
+		const el = await fixture(`<${asyncPopoverTag}></${asyncPopoverTag}>`, { viewport });
+		await sendKeysElem(el.shadowRoot.querySelector('button'), 'press', 'Enter');
+		await expect(el.shadowRoot.querySelector('span')).to.be.golden();
+	});
+
+	it('async-loaded', async() => {
+		const el = await fixture(`<${asyncPopoverTag}></${asyncPopoverTag}>`, { viewport });
+		sendKeysElem(el.shadowRoot.querySelector('button'), 'press', 'Enter');
+		await oneEvent(el, 'd2l-popover-open');
+		await expect(el.shadowRoot.querySelector('span')).to.be.golden();
 	});
 
 });
