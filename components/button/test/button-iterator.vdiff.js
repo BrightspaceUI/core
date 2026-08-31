c789757f00073e5ce9b76601b33bd2d665ddf182
Dave Lockhart <dlockhart@users.noreply.github.com>
2026-08-31 10:10:36 -0400

    add dark mode vdiffs

--- components/button/test/button-iterator.vdiff.js
+++ components/button/test/button-iterator.vdiff.js
@@ -3,27 +3,27 @@ import { expect, fixture } from '@brightspace-ui/testing';
 
 describe('button-iterator', () => {
 	[
-		{ name: 'default', template: buttonIteratorFixtures.default },
-		{ name: 'default-next-focus', template: buttonIteratorFixtures.default, action: focusNext },
-		{ name: 'default-next-hover', template: buttonIteratorFixtures.default, action: hoverNext },
+		{ name: 'default', template: buttonIteratorFixtures.default, allColorModes: true },
+		{ name: 'default-next-focus', template: buttonIteratorFixtures.default, action: focusNext, allColorModes: true },
+		{ name: 'default-next-hover', template: buttonIteratorFixtures.default, action: hoverNext, allColorModes: true },
 		{ name: 'default-prev-focus', template: buttonIteratorFixtures.default, action: focusPrevious },
 		{ name: 'default-prev-hover', template: buttonIteratorFixtures.default, action: hoverPrevious },
 		{ name: 'custom-next-focus', template: buttonIteratorFixtures.custom, action: focusNext },
 		{ name: 'custom-next-hover', template: buttonIteratorFixtures.custom, action: hoverNext },
 		{ name: 'custom-prev-focus', template: buttonIteratorFixtures.custom, action: focusPrevious },
 		{ name: 'custom-prev-hover', template: buttonIteratorFixtures.custom, action: hoverPrevious },
-		{ name: 'description', template: buttonIteratorFixtures.description },
+		{ name: 'description', template: buttonIteratorFixtures.description, allColorModes: true },
 		{ name: 'description-small', template: buttonIteratorFixtures.description, width: 500 },
-		{ name: 'disabled', template: buttonIteratorFixtures.disabled },
+		{ name: 'disabled', template: buttonIteratorFixtures.disabled, allColorModes: true },
 		{ name: 'disabled-next-hover', template: buttonIteratorFixtures.disabled, action: hoverNext },
 		{ name: 'disabled-prev-hover', template: buttonIteratorFixtures.disabled, action: hoverPrevious },
-		{ name: 'next-only', template: buttonIteratorFixtures.nextOnly }
-	].forEach(({ name, template, action, width }) => {
+		{ name: 'next-only', template: buttonIteratorFixtures.nextOnly, allColorModes: true }
+	].forEach(({ name, template, action, width, allColorModes }) => {
 		it(name, async() => {
 			const options = (width !== undefined) ? { viewport: { width } } : undefined;
 			const elem = await fixture(template, options);
 			if (action) await action(elem);
-			await expect(elem).to.be.golden();
+			await expect(elem).to.be.golden({ allColorModes });
 		});
 	});
 });
