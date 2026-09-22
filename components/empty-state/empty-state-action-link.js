08738cbec89ed92073e2b6453cb327be7c69abc1
Edwin Collazos <Edwin.Collazos@D2L.com>
2026-09-22 14:37:45 -0400

    Adds support to target attribute with localized string for screen readers

--- components/empty-state/empty-state-action-link.js
+++ components/empty-state/empty-state-action-link.js
@@ -1,13 +1,17 @@
-import { html, LitElement, nothing } from 'lit';
+import '../offscreen/offscreen.js';
+import '../icons/icon.js';
+import { css, html, LitElement, nothing } from 'lit';
 import { bodyCompactStyles } from '../typography/styles.js';
 import { FocusMixin } from '../../mixins/focus-mixin.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { linkStyles } from '../link/link.js';
+import { LocalizeCoreElement } from '../../helpers/localize-core-element.js';
 import { PropertyRequiredMixin } from '../../mixins/property-required/property-required-mixin.js';
 
 /**
  * `d2l-empty-state-action-link` is an empty state action component that can be placed inside of the default slot of `empty-state-simple` or `empty-state-illustrated` to add a link action to the component.
  */
-class EmptyStateActionLink extends FocusMixin(PropertyRequiredMixin(LitElement)) {
+class EmptyStateActionLink extends LocalizeCoreElement(FocusMixin(PropertyRequiredMixin(LitElement))) {
 
 	static properties = {
 		/**
@@ -20,6 +24,11 @@ class EmptyStateActionLink extends FocusMixin(PropertyRequiredMixin(LitElement))
 		 * @type {string}
 		 */
 		href: { type: String, required: true },
+		/**
+		 * The target attribute specifies where to open the link.
+		 * @type {string}
+		 */
+		target: { type: String },
 	};
 
 	static styles = [bodyCompactStyles, linkStyles];
@@ -29,9 +38,12 @@ class EmptyStateActionLink extends FocusMixin(PropertyRequiredMixin(LitElement))
 	}
 
 	render() {
+		const newWindowElements = (this.target === '_blank')
+			? html`<span id="new-window"><span style="font-size: 0;">&nbsp;</span><d2l-icon icon="tier1:new-window"></d2l-icon></span><d2l-offscreen>${this.localize('components.link.open-in-new-window')}</d2l-offscreen>`
+			: nothing;
 		const actionLink = this.text && this.href
 			? html`
-				<a class="d2l-body-compact d2l-link" href=${this.href}>${this.text}</a>`
+				<a class="d2l-body-compact d2l-link" href=${this.href} target=${ifDefined(this.target)}>${this.text}${newWindowElements}</a>`
 			: nothing;
 
 		return html`${actionLink}`;
